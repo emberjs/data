8438b7540a973e35075a5b47b307d5541a46a29a
tchak <paul@chavard.net>
2011-12-31 00:49:41 +0100

    update me naming for consistency

--- packages/ember-data/lib/system/store.js
+++ packages/ember-data/lib/system/store.js
@@ -141,7 +141,7 @@ DS.Store = SC.Object.extend({
   // . CREATE NEW MODEL .
   // ....................
 
-  create: function(type, hash) {
+  createRecord: function(type, hash) {
     hash = hash || {};
 
     var id = hash[getPath(type, 'proto.primaryKey')] || null;
@@ -166,8 +166,8 @@ DS.Store = SC.Object.extend({
   // . DELETE MODEL .
   // ................
 
-  deleteModel: function(model) {
-    model.deleteModel();
+  deleteRecord: function(model) {
+    model.deleteRecord();
   },
 
   // ...............
