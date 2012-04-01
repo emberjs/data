67b5b0d0c0365acba306bb7a982211b93dd79133
tchak <paul@chavard.net>
2012-04-01 22:51:21 +0200

    update quint-runner with latest work from ember.js and add travis.yml

--- tests/qunit/run-qunit.js
+++ tests/qunit/run-qunit.js
@@ -1,7 +1,6 @@
 // PhantomJS QUnit Test Runner
 
 var args = phantom.args;
-
 if (args.length < 1 || args.length > 2) {
   console.log("Usage: " + phantom.scriptName + " <URL> <timeout>");
   phantom.exit(1);
@@ -9,47 +8,41 @@ if (args.length < 1 || args.length > 2) {
 
 var page = require('webpage').create();
 
+var depRe = /^DEPRECATION:/;
 page.onConsoleMessage = function(msg) {
-  if (!/^DEPRECATION:/.test(msg)) console.log(msg);
+  if (!depRe.test(msg)) console.log(msg);
 };
 
 page.open(args[0], function(status) {
   if (status !== 'success') {
     console.error("Unable to access network");
     phantom.exit(1);
+  } else {
+    page.evaluate(addLogging);
+
+    var timeout = parseInt(args[1] || 30000, 10);
+    var start = Date.now();
+    var interval = setInterval(function() {
+      if (Date.now() > start + timeout) {
+        console.error("Tests timed out");
+        phantom.exit(1);
+      } else {
+        var qunitDone = page.evaluate(function() {
+          return window.qunitDone;
+        });
+
+        if (qunitDone) {
+          clearInterval(interval);
+          if (qunitDone.failed > 0) {
+            phantom.exit(1);
+          } else {
+            phantom.exit();
+          }
+        }
+      }
+    }, 500);
   }
-
-  page.evaluate(addLogging);
-
-  var timeout = parseInt(args[1] || 30000);
-  var start = Date.now();
-
-  var interval = setInterval(function() {
-    if (Date.now() > start + timeout) {
-      console.error("Tests timed out");
-      phantom.exit(1);
-    }
-
-    if (isDone()) {
-      clearInterval(interval);
-      if (didFail()) phantom.exit(1);
-      phantom.exit();
-    }
-  }, 500);
 });
-
-function isDone() {
-  return page.evaluate(function() {
-    return !!window.qunitDone;
-  });
-}
-
-function didFail() {
-  var output = page.evaluate(function() {
-    return window.qunitDone;
-  });
-  return output && output.failed > 0;
-}
 
 function addLogging() {
   var testErrors = [];
@@ -74,7 +67,11 @@ function addLogging() {
   QUnit.log(function(context) {
     if (context.result) return;
 
-    var msg = "\n    Assertion Failed: " + (context.message || "");
+    var msg = "\n    Assertion Failed:";
+    if (context.message) {
+      msg += " " + context.message;
+    }
+
     if (context.expected) {
       msg += "\n      Expected: " + context.expected + ", Actual: " + context.actual;
     }
