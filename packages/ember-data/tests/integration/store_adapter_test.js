5354b4666502ef7aaa1281c5fbe13006430dc17e
tomhuda <tomhuda@tilde.io>
2012-07-25 16:06:24 -0700

    Split find and findAll tests
    This commit also adds an invokeAsync helper
    for invoking a callback after a specified
    interval, while not screwing up QUnit tests.

--- packages/ember-data/tests/integration/store_adapter_test.js
+++ packages/ember-data/tests/integration/store_adapter_test.js
@@ -63,33 +63,6 @@ test("when an association is loaded, the adapter's find method should not be cal
   store.load(Person, { id: 1, comments: [ 1 ] });
 });
 
-test("when all records for a type are requested, the adapter's findAll method is called", function() {
-  expect(2);
-
-  var count = 0;
-
-  adapter.findAll = function(store, type) {
-    count++;
-
-    if (count === 1) {
-      stop();
-
-      setTimeout(function() {
-        start();
-
-        store.load(type, { id: 1, name: "Braaaahm Dale" });
-        equal(get(array, 'length'), 1, "The array is now 1 length");
-
-        store.findAll(Person);
-      }, 100);
-    } else {
-      ok(false, "Should not get here");
-    }
-  };
-
-  var array = store.findAll(Person);
-  equal(get(array, 'length'), 0, "The array is 0 length do far");
-});
 
 test("if an adapter implements the generateIdForRecord method, it gets invoked when new records are created", function() {
   expect(7);
