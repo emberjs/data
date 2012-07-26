002a57a7c864cfd8b2aeb63a5d5e0569c528dc7f
tomhuda <tomhuda@tilde.io>
2012-07-26 15:36:00 -0700

    Demonolithize test harness index.html

---
+++ tests/test_helpers.js
@@ -0,0 +1,104 @@
+(function() {
+  window.Ember = window.Ember || {};
+
+  Ember.config = {};
+  Ember.testing = true;
+
+  window.ENV = { TESTING: true };
+
+  window.async = function(callback, timeout) {
+    stop();
+
+    timeout = setTimeout(function() {
+      start();
+      ok(false, "Timeout was reached");
+    }, timeout || 100);
+
+    return function() {
+      clearTimeout(timeout);
+
+      start();
+      callback.apply(this, arguments);
+    };
+  };
+
+  window.invokeAsync = function(callback, timeout) {
+    timeout = timeout || 1;
+
+    setTimeout(async(callback, timeout+100), timeout);
+  };
+
+  var syncForTest = function(fn) {
+    var callSuper;
+
+    if (typeof fn !== "function") { callSuper = true; }
+
+    return function() {
+      var override = false, ret;
+
+      if (Ember.run && !Ember.run.currentRunLoop) {
+        Ember.run.begin();
+        override = true;
+      }
+
+      try {
+        if (callSuper) {
+          ret = this._super.apply(this, arguments);
+        } else {
+          ret = fn.apply(this, arguments);
+        }
+      } finally {
+        if (override) {
+          Ember.run.end();
+        }
+      }
+
+      return ret;
+    };
+  };
+
+  Ember.config.overrideAccessors = function() {
+    Ember.set = syncForTest(Ember.set);
+    Ember.get = syncForTest(Ember.get);
+  };
+
+  Ember.config.overrideClassMixin = function(ClassMixin) {
+    ClassMixin.reopen({
+      create: syncForTest()
+    });
+  };
+
+  Ember.config.overridePrototypeMixin = function(PrototypeMixin) {
+    PrototypeMixin.reopen({
+      destroy: syncForTest()
+    });
+  };
+
+  minispade.register('ember-data/~test-setup', function() {
+    Ember.View.reopen({
+      _insertElementLater: syncForTest()
+    });
+
+    DS.Store.reopen({
+      createRecord: syncForTest(),
+      deleteRecord: syncForTest(),
+      load: syncForTest(),
+      loadMany: syncForTest(),
+      filter: syncForTest(),
+      find: syncForTest(),
+      didCreateRecord: syncForTest(),
+      didCreateRecords: syncForTest(),
+      didUpdateRecord: syncForTest(),
+      didUpdateRecords: syncForTest(),
+    });
+
+    DS.Model.reopen({
+      deleteRecord: syncForTest(),
+      dataDidChange: Ember.observer(syncForTest(), 'data')
+    });
+
+    DS.Transaction.reopen({
+      commit: syncForTest(),
+    });
+  });
+})();
