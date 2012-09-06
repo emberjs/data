90cbd9bd6c8cbc789c501112903cae33416873e1
tchak <paul@chavard.net>
2012-09-06 23:41:41 +0200

    DS.RESTSerializer

---
+++ packages/ember-data/lib/serializers/rest_serializer.js
@@ -0,0 +1,9 @@
+DS.RESTSerializer = DS.Serializer.create({
+  keyForBelongsTo: function(type, name) {
+    return this.keyForAttributeName(type, name) + "_id";
+  },
+
+  keyForAttributeName: function(type, name) {
+    return Ember.String.decamelize(name);
+  }
+});
