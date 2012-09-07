16a9471761886243250e1d5ae52e6811e776ba1b
tchak <paul@chavard.net>
2012-09-07 10:16:43 +0200

    implement addBelongsTo on DS.RESTSerializer

--- packages/ember-data/lib/serializers/rest_serializer.js
+++ packages/ember-data/lib/serializers/rest_serializer.js
@@ -1,3 +1,5 @@
+var get = Ember.get;
+
 DS.RESTSerializer = DS.Serializer.create({
   keyForBelongsTo: function(type, name) {
     return this.keyForAttributeName(type, name) + "_id";
@@ -5,5 +7,11 @@ DS.RESTSerializer = DS.Serializer.create({
 
   keyForAttributeName: function(type, name) {
     return Ember.String.decamelize(name);
+  },
+
+  addBelongsTo: function(hash, record, key, relationship) {
+    var hashKey = this._keyForBelongsTo(record.constructor, key);
+
+    hash[hashKey] = get(record, key+'.id');
   }
 });
