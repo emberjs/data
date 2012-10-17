09220b7b5d2a431b3df781d0a582af7237b8a641
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-10-17 11:07:25 -0500

    update license.js [ci skip]

--- generators/license.js
+++ generators/license.js
@@ -1,6 +1,7 @@
 // ==========================================================================
-// Project:  Ember Data
-// Copyright: ©2011 Living Social Inc. and contributors.
+// Project:   Ember Data
+// Copyright: ©2011-2012 Tilde Inc. and contributors.
+//            Portions ©2011 Living Social Inc. and contributors.
 // License:   Licensed under MIT license (see license.js)
 // ==========================================================================
 
