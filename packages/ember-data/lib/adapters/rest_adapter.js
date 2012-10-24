7e276a0e3a5b84656d7c2e30549ed0ddc218c9df
tomhuda <tomhuda@tilde.io>
2012-10-24 12:56:10 -0700

    Revert "Revert "Fix for relationship acknowledgement issues""
    This reverts commit c1b153b73062fa5a53688799e122fbbede8367ff.

--- packages/ember-data/lib/adapters/rest_adapter.js
+++ packages/ember-data/lib/adapters/rest_adapter.js
@@ -31,6 +31,16 @@ DS.RESTAdapter = DS.Adapter.extend({
     });
   },
 
+  didSaveRecord: function(store, record, hash) {
+    record.eachAssociation(function(name, meta) {
+      if (meta.kind === 'belongsTo') {
+        store.didUpdateRelationship(record, name);
+      }
+    });
+
+    store.didSaveRecord(record, hash);
+  },
+
   didCreateRecord: function(store, type, record, json) {
     var root = this.rootForType(type);
 
