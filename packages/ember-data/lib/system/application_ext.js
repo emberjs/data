708acf8cbfb31abe0accf0e8461069fa5785fc63
tomhuda <tomhuda@tilde.io>
2012-12-27 14:09:00 -0800

    Make sure defaultStore always exists

--- packages/ember-data/lib/system/application_ext.js
+++ packages/ember-data/lib/system/application_ext.js
@@ -70,6 +70,10 @@ Ember.onLoad('Ember.Application', function(Application) {
 
       initialize: function(container, application) {
         container.register('store', 'main', application.Store);
+
+        // Eagerly generate the store so defaultStore is populated.
+        // TODO: Do this in a finisher hook
+        container.lookup('store:main');
       }
     });
 
