017b6a43de1c9b5d60ad4ec6298c86585b10994c
Olivier Balais <obalais@gmail.com>
2013-01-17 11:39:30 +0100

    Added failing tests for deleteRecord on embedded object.
    This points out the fact that a deleteRecord on an embedded relationship doesnt dirty the tree and though, doesnt throw a POST request for the parent object.
    Issue #606 points out that deleterecord does not update manyarray.

--- packages/ember-data/tests/integration/embedded/embedded_saving_test.js
+++ packages/ember-data/tests/integration/embedded/embedded_saving_test.js
@@ -1,5 +1,6 @@
 var store, Adapter, adapter;
 var Post, Comment, User;
+var post;
 var attr = DS.attr;
 
 module("Embedded Saving", {
@@ -27,6 +28,27 @@ module("Embedded Saving", {
     store = DS.Store.create({
       adapter: adapter
     });
+
+    adapter.load(store, Post, {
+      id: 1,
+      title: "A New MVC Framework in Under 100 Lines of Code",
+
+      comments: [{
+        title: "Why not use a more lightweight solution?",
+        user: {
+          name: "mongodb_user"
+        },
+        votes: [ { voter: "tomdale" }, { voter: "wycats" } ]
+      }, {
+          title: "This does not seem to reflect the Unix philosophy haha",
+          user: {
+            name: "microuser"
+          },
+          votes: [ { voter: "ebryn" } ]
+        }]
+    });
+
+    post = store.find(Post, 1);
   }
 });
 
@@ -50,4 +72,20 @@ asyncTest("Adding a new embedded record to an unsaved record: Both records use t
   post.get('comments').createRecord({ title: 'This embedded record is also unsaved' });
 
   transaction.commit();
+});
+
+asyncTest("Removing an embedded record from a parent record: A POST request is sent to update parent record.", function() {
+  adapter.ajax = function(url, type, hash) {
+    equal(url, '/posts');
+    equal(type, 'POST');
+    equal(hash.data.post.comments.length, 1);
+
+    setTimeout(function() {
+      hash.success.call(hash.context);
+      start();
+    });
+  };
+
+  post.get('comments.firstObject').deleteRecord();
+  store.commit();
 });
