bc720dc02e0b6383c48abcbe8637127a672a2f05
tomhuda <tomhuda@tilde.io>
2013-03-11 10:45:12 -0700

    Fix mistaken synchronous code in tests

--- packages/ember-data/tests/integration/find_all_test.js
+++ packages/ember-data/tests/integration/find_all_test.js
@@ -33,16 +33,19 @@ test("When all records for a type are requested, the store should call the adapt
     invokeAsync(function() {
       store.loadMany(type, [{ id: 1, name: "Braaaahm Dale" }]);
 
-      equal(get(allRecords, 'length'), 1, "the record array's length is 1 after a record is loaded into it");
-      equal(allRecords.objectAt(0).get('name'), "Braaaahm Dale", "the first item in the record array is Braaaahm Dale");
-
       // Only one record array per type should ever be created (identity map)
       strictEqual(allRecords, store.all(Person), "the same record array is returned every time all records of a type are requested");
     });
   };
 
   allRecords = store.find(Person);
+
   equal(get(allRecords, 'length'), 0, "the record array's length is zero before any records are loaded");
+
+  Ember.addObserver(allRecords, 'length', function() {
+    equal(get(allRecords, 'length'), 1, "the record array's length is 1 after a record is loaded into it");
+    equal(allRecords.objectAt(0).get('name'), "Braaaahm Dale", "the first item in the record array is Braaaahm Dale");
+  });
 });
 
 test("When all records for a type are requested, records that are already loaded should be returned immediately.", function() {
