a9b118f1adaa6ffc6940aa5079340c222124ebcd
Adrien Coquio <adrien.coquio@gmail.com>
2013-04-19 01:02:12 +0200

    Renamed relationships/one_to_many_change to relationships/relationship_change
    It contains now a lot more than OneToManyChange

--- packages/ember-data/lib/system/relationships.js
+++ packages/ember-data/lib/system/relationships.js
@@ -6,4 +6,4 @@
 require("ember-data/system/relationships/belongs_to");
 require("ember-data/system/relationships/has_many");
 require("ember-data/system/relationships/ext");
-require("ember-data/system/relationships/one_to_many_change");
+require("ember-data/system/relationships/relationship_change");
