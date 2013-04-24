59b82a5fffd325e5325f392c3d8da8abb4daa668
Stefan Penner <stefan.penner@gmail.com>
2013-04-24 01:45:38 -0400

    use local defered mixin reference

--- packages/ember-data/lib/system/mixins/load_promise.js
+++ packages/ember-data/lib/system/mixins/load_promise.js
@@ -3,7 +3,7 @@ var Evented = Ember.Evented,              // ember-runtime/mixins/evented
     run = Ember.run,                      // ember-metal/run-loop
     get = Ember.get;                      // ember-metal/accessors
 
-var LoadPromise = Ember.Mixin.create(Evented, Ember.DeferredMixin, {
+var LoadPromise = Ember.Mixin.create(Evented, Deferred, {
   init: function() {
     this._super.apply(this, arguments);
     this.one('didLoad', function() {
