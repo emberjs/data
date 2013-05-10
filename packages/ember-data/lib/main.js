8267b68cf3647df64cd62a91d86923c0d223d18a
Tom Dale <tom@tomdale.net>
2013-05-10 15:04:53 -0700

    Rearrange change records and record array manager

--- packages/ember-data/lib/main.js
+++ packages/ember-data/lib/main.js
@@ -24,13 +24,14 @@
 */
 
 require("ember-data/core");
+require("ember-data/initializers");
 require("ember-data/ext");
 require("ember-data/system/store");
-require("ember-data/system/record_arrays");
 require("ember-data/system/model");
+require("ember-data/system/changes");
 require("ember-data/system/relationships");
-require("ember-data/system/application_ext");
-require("ember-data/system/serializer");
+require("ember-data/system/record_arrays");
+require("ember-data/system/record_array_manager");
 require("ember-data/system/adapter");
-require("ember-data/system/attribute_change");
+require("ember-data/system/serializer");
 require("ember-data/adapters");
---
+++ packages/ember-data/lib/system/changes.js
@@ -0,0 +1,7 @@
+/**
+  @module data
+  @submodule data-changes
+*/
+
+require("ember-data/system/changes/attribute_change");
+require("ember-data/system/changes/relationship_change");
