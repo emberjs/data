9c01792e942679766e5cbf112b28e8afefb22b28
tchak <paul@chavard.net>
2013-09-01 15:10:13 +0200

    buildURL on RESTAdapter support host and namespace

--- packages/ember-data/lib/adapters/rest_adapter.js
+++ packages/ember-data/lib/adapters/rest_adapter.js
@@ -839,8 +839,18 @@ DS.RESTAdapter = DS.Adapter.extend({
     @returns String
   */
   buildURL: function(type, id) {
-    var url = "/" + Ember.String.pluralize(type.typeKey);
-    if (id) { url += "/" + id; }
+    var host = get(this, 'host'),
+        namespace = get(this, 'namespace'),
+        url = [];
+
+    if (host) { url.push(host); }
+    if (namespace) { url.push(namespace); }
+
+    url.push(Ember.String.pluralize(type.typeKey));
+    if (id) { url.push(id); }
+
+    url = url.join('/');
+    if (!host) { url = '/' + url; }
 
     return url;
   },
