24f2a61ca086d241b98a152ee5e5226db2ee23bd
Sylvain MINA <sylvain.mina@gmail.com>
2013-09-03 16:53:20 +0200

    replace mapProperty with mapBy
    just a detail, but as I understood, the mapProperty has been quietly deprecated in favor of mapBy

--- packages/ember-data/lib/serializers/json_serializer.js
+++ packages/ember-data/lib/serializers/json_serializer.js
@@ -126,7 +126,7 @@ DS.JSONSerializer = Ember.Object.extend({
     var relationshipType = DS.RelationshipChange.determineRelationshipType(record.constructor, relationship);
     
     if (relationshipType === 'manyToNone' || relationshipType === 'manyToMany') {
-      json[key] = get(record, key).mapProperty('id');
+      json[key] = get(record, key).mapBy('id');
       // TODO support for polymorphic manyToNone and manyToMany relationships
     }
   },
