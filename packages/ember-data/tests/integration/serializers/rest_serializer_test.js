11bd97ad97d6456c5021b43c06008d3deaf0f447
Bradley Priest <bradleypriest@gmail.com>
2013-09-09 09:35:24 +0800

    Add test for custom typeFromRoot

---
+++ packages/ember-data/tests/integration/serializers/rest_serializer_test.js
@@ -0,0 +1,68 @@
+var get = Ember.get, set = Ember.set;
+var HomePlanet, league, SuperVillain, superVillain, EvilMinion, YellowMinion, DoomsdayDevice, env;
+
+module("integration/serializer/rest - RESTSerializer", {
+  setup: function() {
+    HomePlanet = DS.Model.extend({
+      name:          DS.attr('string'),
+      superVillains: DS.hasMany('superVillain')
+    });
+    SuperVillain = DS.Model.extend({
+      firstName:     DS.attr('string'),
+      lastName:      DS.attr('string'),
+      homePlanet:    DS.belongsTo("homePlanet"),
+      evilMinions:   DS.hasMany("evilMinion")
+    });
+    EvilMinion = DS.Model.extend({
+      superVillain: DS.belongsTo('superVillain'),
+      name:         DS.attr('string')
+    });
+    YellowMinion = EvilMinion.extend();
+    DoomsdayDevice = DS.Model.extend({
+      name:         DS.attr('string'),
+      evilMinion:   DS.belongsTo('evilMinion', {polymorphic: true})
+    });
+    env = setupStore({
+      superVillain:   SuperVillain,
+      homePlanet:     HomePlanet,
+      evilMinion:     EvilMinion,
+      yellowMinion:   YellowMinion,
+      doomsdayDevice: DoomsdayDevice
+    });
+    env.store.modelFor('superVillain');
+    env.store.modelFor('homePlanet');
+    env.store.modelFor('evilMinion');
+    env.store.modelFor('yellowMinion');
+    env.store.modelFor('doomsdayDevice');
+  },
+
+  teardown: function() {
+    env.store.destroy();
+  }
+});
+
+test("extractArray with customModelFromType", function() {
+  env.container.register('serializer:homePlanet', DS.RESTSerializer.extend({
+    modelTypeFromRoot: function(root) {
+      var camelized = Ember.String.camelize(root);
+      return Ember.String.singularize(camelized);
+    }
+  }));
+
+  var json_hash = {
+    home_planets: [{id: "1", name: "Umber", superVillains: [1]}],
+    super_villains: [{id: "1", firstName: "Tom", lastName: "Dale", homePlanet: "1"}]
+  };
+
+  var array = env.container.lookup("serializer:homePlanet").extractArray(env.store, HomePlanet, json_hash);
+
+  deepEqual(array, [{
+    "id": "1",
+    "name": "Umber",
+    "superVillains": [1]
+  }]);
+
+  env.store.find("superVillain", 1).then(async(function(minion){
+    equal(minion.get('firstName'), "Tom");
+  }));
+});
