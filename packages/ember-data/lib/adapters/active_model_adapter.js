f4fa886bc9570ec7038481653db419a0344e8e17
Bradley Priest <bradleypriest@gmail.com>
2013-09-10 11:41:02 +0800

    Document the AMS adapter

--- packages/ember-data/lib/adapters/active_model_adapter.js
+++ packages/ember-data/lib/adapters/active_model_adapter.js
@@ -1,10 +1,49 @@
 require('ember-data/adapters/rest_adapter');
+
 require('ember-data/serializers/active_model_serializer');
 
 /**
-  DS.ActiveModelAdapter
+  @module ember-data
+*/
 
-  TODO: Description
+/**
+  The ActiveModelAdapter is a subclass of the RESTAdapter designed to integrate
+  with a JSON API that uses an underscored naming convention instead of camelcasing.
+  It has been designed to work out of the box with the
+  [active_model_serializers](http://github.com/rails-api/active_model_serializers)
+  Ruby gem.
+
+  ## JSON Structure
+
+  The ActiveModelAdapter expects the JSON returned from your server to follow
+  the REST adapter conventions substituting underscored keys for camelcased ones.
+
+  ### Conventional Names
+
+  Attribute names in your JSON payload should be the underscored versions of
+  the attributes in your Ember.js models.
+
+  For example, if you have a `Person` model:
+
+  ```js
+  App.FamousPerson = DS.Model.extend({
+    firstName: DS.attr('string'),
+    lastName: DS.attr('string'),
+    occupation: DS.attr('string')
+  });
+  ```
+
+  The JSON returned should look like this:
+
+  ```js
+  {
+    "famous_person": {
+      "first_name": "Barack",
+      "last_name": "Obama",
+      "occupation": "President"
+    }
+  }
+  ```
 
   @class ActiveModelAdapter
   @constructor
@@ -14,6 +53,19 @@ require('ember-data/serializers/active_model_serializer');
 
 DS.ActiveModelAdapter = DS.RESTAdapter.extend({
   defaultSerializer: '_ams',
+  /**
+    The ActiveModelAdapter overrides the `pathForType` method
+    to build underscored URLs.
+
+    ```js
+      this.pathForType("famousPerson");
+      //=> "famous_people"
+    ```
+
+    @method pathForType
+    @param {String} type
+    @returns String
+  */
   pathForType: function(type) {
     var decamelized = Ember.String.decamelize(type);
     return Ember.String.pluralize(decamelized);
