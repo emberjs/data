70fd6137f50299b02264274c531c6d25416cbf8e
Stefan Penner <stefan.penner@gmail.com>
2013-09-26 14:43:00 -0400

    prefer Ember.Logger.log over console.log

--- tests/ember_configuration.js
+++ tests/ember_configuration.js
@@ -136,7 +136,7 @@
       // otherwise, let a future turn of the event loop
       // handle the error.
       if (reason && reason instanceof Error) {
-        console.log(reason.stack);
+        Ember.Logger.log(reason.stack);
         throw reason;
       }
     });
