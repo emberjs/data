4eda8576e6d44aa354b22a6d86cbfa28b3633e78
Robert Jackson <robert.w.jackson@me.com>
2014-01-16 02:12:28 -0500

    Deprecate former underscored names.
    This adds a `ContainerProxy` that is used to create proxied factories.
    When looked up a deprecation is thrown indicating which container key
    was used for lookup and which should have been used.
    
    Update `ember-dev` to latest version with recent changes adding an
    `expectDeprecation` helper (added
    [here](https://github.com/emberjs/ember-dev/pull/121)).

--- packages/activemodel-adapter/lib/initializers.js
+++ packages/activemodel-adapter/lib/initializers.js
@@ -1,8 +1,16 @@
+require("ember-data/system/container_proxy");
+
 Ember.onLoad('Ember.Application', function(Application) {
   Application.initializer({
     name: "activeModelAdapter",
 
     initialize: function(container, application) {
+      var proxy = new DS.ContainerProxy(container);
+      proxy.registerDeprecations([
+        {deprecated: 'serializer:_ams',  valid: 'serializer:active-model'},
+        {deprecated: 'adapter:_ams',     valid: 'adapter:active-model'}
+      ]);
+
       application.register('serializer:active-model', DS.ActiveModelSerializer);
       application.register('adapter:active-model', DS.ActiveModelAdapter);
     }
