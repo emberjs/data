b9e709b6abb3ce6304ad10f65c2629ace32fcec3
Robert Jackson <robert.w.jackson@me.com>
2014-01-29 00:20:20 -0500

    Enable multi-channel testing.
    Adds the following tasks:
    
    * `test:local` (alias to previous `test` task)
    * `test:release` (tests against current release builds of Ember)
    * `test:beta` (tests against current beta builds of Ember)
    * `test:canary` (tests against current canary builds of Ember)
    * `test:all` (tests against all channels of Ember)
    
    Changes the default Travis test script to `test:all`.

--- tasks/options/qunit.js
+++ tasks/options/qunit.js
@@ -1,7 +1,22 @@
 module.exports = {
-  all: {
+  local: {
     options: {
       urls: [ 'http://localhost:9997/tests/index.html' ]
+    }
+  },
+  release: {
+    options: {
+      urls: [ 'http://localhost:9997/tests/index.html?emberchannel=release' ]
+    }
+  },
+  beta: {
+    options: {
+      urls: [ 'http://localhost:9997/tests/index.html?emberchannel=beta' ]
+    }
+  },
+  canary: {
+    options: {
+      urls: [ 'http://localhost:9997/tests/index.html?emberchannel=canary' ]
     }
   }
 };
