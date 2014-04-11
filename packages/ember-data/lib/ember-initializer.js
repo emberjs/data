0da4d5a77e210277076594081e274f1cb951241a
Matthew Beale <matt.beale@madhatted.com>
2014-04-11 10:27:15 -0400

    Bring back deprecated initializers
    Libraries such as ic-ajax are dependent on the old initializers in
    ember-data (such as "store"). This brings them back temporarially, but
    they will eventually be removed and are non-functional.

--- packages/ember-data/lib/ember-initializer.js
+++ packages/ember-data/lib/ember-initializer.js
@@ -1,5 +1,7 @@
 import setupContainer from './setup-container';
 
+var K = Ember.K;
+
 /**
   @module ember-data
 
@@ -7,8 +9,41 @@ import setupContainer from './setup-container';
 */
 
 Ember.onLoad('Ember.Application', function(Application) {
+
   Application.initializer({
     name:       "ember-data",
     initialize: setupContainer
+  });
+
+  // Deprecated initializers to satisfy old code that depended on them
+
+  Application.initializer({
+    name:       "store",
+    after:      "ember-data",
+    initialize: K
+  });
+
+  Application.initializer({
+    name:       "activeModelAdapter",
+    before:     "store",
+    initialize: K
+  });
+
+  Application.initializer({
+    name:       "transforms",
+    before:     "store",
+    initialize: K
+  });
+
+  Application.initializer({
+    name:       "data-adapter",
+    before:     "store",
+    initialize: K
+  });
+
+  Application.initializer({
+    name:       "injectStore",
+    before:     "store",
+    initialize: K
   });
 });
