95e1de9feca9bf2f427787965e776129530f6214
Matthew Beale <matt.beale@madhatted.com>
2014-05-12 14:30:57 -0400

    Put the initialization docs back with the initializers

--- packages/ember-data/lib/ember-initializer.js
+++ packages/ember-data/lib/ember-initializer.js
@@ -4,8 +4,38 @@ var K = Ember.K;
 
 /**
   @module ember-data
+*/
+
+/**
 
   This code initializes Ember-Data onto an Ember application.
+
+  If an Ember.js developer defines a subclass of DS.Store on their application,
+  as `App.ApplicationStore` (or via a module system that resolves to `store:application`)
+  this code will automatically instantiate it and make it available on the
+  router.
+
+  Additionally, after an application's controllers have been injected, they will
+  each have the store made available to them.
+
+  For example, imagine an Ember.js application with the following classes:
+
+  App.ApplicationStore = DS.Store.extend({
+    adapter: 'custom'
+  });
+
+  App.PostsController = Ember.ArrayController.extend({
+    // ...
+  });
+
+  When the application is initialized, `App.ApplicationStore` will automatically be
+  instantiated, and the instance of `App.PostsController` will have its `store`
+  property set to that instance.
+
+  Note that this code will only be run if the `ember-application` package is
+  loaded. If Ember Data is being used in an environment other than a
+  typical application (e.g., node.js where only `ember-runtime` is available),
+  this code will be ignored.
 */
 
 Ember.onLoad('Ember.Application', function(Application) {
