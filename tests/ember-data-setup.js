3c164079c2e3937f84bad1a1a60703d05b6a3688
Doug Yun <yun.douglas@gmail.com>
2014-08-11 22:27:04 -0400

    Adds JSHint globals.
    * Adds semicolons.

--- tests/ember-data-setup.js
+++ tests/ember-data-setup.js
@@ -1,3 +1,5 @@
+/* globals syncForTest */
+
 ;(function(){
 
   Ember.RSVP.configure('onerror', function(reason) {
@@ -5,7 +7,7 @@
     // otherwise, let a future turn of the event loop
     // handle the error.
     if (reason && reason instanceof Error) {
-      Ember.Logger.log(reason, reason.stack)
+      Ember.Logger.log(reason, reason.stack);
       throw reason;
     }
   });
