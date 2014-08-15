f1c0b9feaadd483d190bc0a1a33d0615a11557d7
Ricardo Mendes <rokusu@gmail.com>
2014-08-15 03:05:59 +0100

    Moves PromiseArray to separate file

--- packages/ember-data/lib/system/store.js
+++ packages/ember-data/lib/system/store.js
@@ -5,6 +5,7 @@
   @module ember-data
 */
 
+import PromiseArray from "ember-data/system/store/promise_array";
 import {
   InvalidError,
   Adapter
@@ -1741,37 +1742,6 @@ function uniqById(data, records) {
   });
 }
 
-// Delegation to the adapter and promise management
-/**
-  A `PromiseArray` is an object that acts like both an `Ember.Array`
-  and a promise. When the promise is resolved the resulting value
-  will be set to the `PromiseArray`'s `content` property. This makes
-  it easy to create data bindings with the `PromiseArray` that will be
-  updated when the promise resolves.
-
-  For more information see the [Ember.PromiseProxyMixin
-  documentation](/api/classes/Ember.PromiseProxyMixin.html).
-
-  Example
-
-  ```javascript
-  var promiseArray = DS.PromiseArray.create({
-    promise: $.getJSON('/some/remote/data.json')
-  });
-
-  promiseArray.get('length'); // 0
-
-  promiseArray.then(function() {
-    promiseArray.get('length'); // 100
-  });
-  ```
-
-  @class PromiseArray
-  @namespace DS
-  @extends Ember.ArrayProxy
-  @uses Ember.PromiseProxyMixin
-*/
-PromiseArray = Ember.ArrayProxy.extend(Ember.PromiseProxyMixin);
 /**
   A `PromiseObject` is an object that acts like both an `Ember.Object`
   and a promise. When the promise is resolved, then the resulting value
