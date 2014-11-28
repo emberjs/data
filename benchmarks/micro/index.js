23f3d8a8b98ebcad036c29b09901413a260b8b74
Stefan Penner <stefan.penner@gmail.com>
2014-11-27 23:49:37 -0500

    restore creation benchmarks

--- benchmarks/micro/index.js
+++ benchmarks/micro/index.js
@@ -1,3 +1,13 @@
+require('./runner')(
+  {
+    distribution: [0, 1, 5],
+    name: 'create',
+    suites: [
+      require('./create/create-object'),
+      require('./create/create-record')
+    ]
+  }
+);
 
 require('./runner')(
   {
