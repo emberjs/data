d52d3326c6b78cecc601d4c525cae1364434130c
Stefan Penner <stefan.penner@gmail.com>
2014-11-27 23:47:56 -0500

    cleanup

--- benchmarks/micro/create/create-object.js
+++ benchmarks/micro/create/create-object.js
@@ -1,31 +1,26 @@
 var name = 'Ember.Object.create';
 
 function fn() {
- return this.data.klass.create();
+ return Klass.create();
 }
 
 module.exports.fn    = fn;
 module.exports.name  = name;
 module.exports.setup = function() {
+  var Klass;
   if (this.distribution === 0) {
-    this.data = {
-      klass: Ember.Object.extend({})
-    };
+    Klass = Ember.Object.extend({});
   } else if (this.distribution === 1) {
-    this.data = {
-      klass: Ember.Object.extend({
-        firstName: Ember.computed(function() { })
-      })
-    };
+    Klass = Ember.Object.extend({
+      firstName: Ember.computed(function() { })
+    });
   } else if (this.distribution === 5) {
-    this.data = {
-      klass: Ember.Object.extend({
-        firstName:  Ember.computed(function() { }),
-        lastName:   Ember.computed(function() { }),
-        middleName: Ember.computed(function() { }),
-        age:        Ember.computed(function() { }),
-        preference: Ember.computed(function() { })
-      })
-    };
+    Klass = Ember.Object.extend({
+      firstName:  Ember.computed(function() { }),
+      lastName:   Ember.computed(function() { }),
+      middleName: Ember.computed(function() { }),
+      age:        Ember.computed(function() { }),
+      preference: Ember.computed(function() { })
+    });
   }
 };
