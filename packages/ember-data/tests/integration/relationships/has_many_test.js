emberjs/data

commit 75dbd8ffb0101f82c0a84e44132fdda1661f2d2a

Schedule relationship normalizaiton and split paths for canonical/client relationship updates
This PR makes several changes to relationship handling:
1. It separates the path through which server side and client side updates happen, which lays the ground work for tracking relationship changes.
2. When pushing records into the store, it splits the push process into three phases using a custom runloop:
a) Push primary key data and attribtues
b) Turn foreign keys into records
c) Update relationships

The runloop changes have the effect of making pushing relationships at least 50% faster, more if you have more relationships. It also fixes bugs like #2210

packages/ember-data/lib/system/record_arrays/many_array.js
@@ -1,5 +1,3 @@
-import RecordArray from "ember-data/system/record_arrays/record_array";
-
 /**
   @module ember-data
 */
@@ -42,11 +40,39 @@ var get = Ember.get, set = Ember.set;
   @namespace DS
   @extends DS.RecordArray
 */
-export default RecordArray.extend({
+export default Ember.Object.extend(Ember.MutableArray, Ember.Evented, {
   init: function() {
-    this._super.apply(this, arguments);
+    this.currentState = Ember.A([]);
+    this.diff = [];
   },
 
+  record: null,
+
+  canonicalState: null,
+  currentState: null,
+
+  diff: null,
+
+  length: 0,
+
+  objectAt: function(index) {
+    if (this.currentState[index]) {
+      return this.currentState[index];
+    } else {
+      return this.canonicalState[index];
+    }
+  },
+
+  flushCanonical: function() {
+    //TODO make this smarter, currently its plenty stupid
+    this.arrayContentWillChange(0, this.length, this.length);
+    this.set('length', this.canonicalState.length);
+    this.currentState = this.canonicalState.slice(0);
+    this.arrayContentDidChange(0, this.length, this.length);
+    //TODO Figure out to notify only on additions and maybe only if unloaded
+    this.relationship.notifyHasManyChanged();
+    this.record.updateRecordArrays();
+  },
   /**
     `true` if the relationship is polymorphic, `false` otherwise.
 
@@ -70,7 +96,48 @@ export default RecordArray.extend({
    */
   relationship: null,
 
+  internalReplace: function(idx, amt, objects) {
+    if (!objects) {
+      objects = [];
+    }
+    this.arrayContentWillChange(idx, 0, objects.length);
+    this.currentState.splice.apply(this.currentState, [idx, amt].concat(objects));
+    this.set('length', this.currentState.length);
+    this.arrayContentDidChange(idx, 0, objects.length);
+    if (objects){
+      //TODO(Igor) probably needed only for unloaded records
+      this.relationship.notifyHasManyChanged();
+    }
+    this.record.updateRecordArrays();
+  },
 
+  //TODO(Igor) optimize
+  internalRemoveRecords: function(records) {
+    var index;
+    for(var i=0; i < records.length; i++) {
+      index = this.currentState.indexOf(records[i]);
+      this.internalReplace(index, 1);
+    }
+  },
+
+  //TODO(Igor) optimize
+  internalAddRecords: function(records, idx) {
+    if (idx === undefined) {
+      idx = this.currentState.length;
+    }
+    this.internalReplace(idx, 0, records);
+  },
+
+  replace: function(idx, amt, objects) {
+    var records;
+    if (amt > 0){
+      records = this.currentState.slice(idx, idx+amt);
+      this.get('relationship').removeRecords(records);
+    }
+    if (objects){
+      this.get('relationship').addRecords(objects, idx);
+    }
+  },
   /**
     Used for async `hasMany` arrays
     to keep track of when they will resolve.
@@ -101,16 +168,6 @@ export default RecordArray.extend({
     }
   },
 
-  replaceContent: function(idx, amt, objects){
-    var records;
-    if (amt > 0){
-      records = get(this, 'content').slice(idx, idx+amt);
-      this.get('relationship').removeRecords(records);
-    }
-    if (objects){
-      this.get('relationship').addRecords(objects, idx);
-    }
-  },
   /**
     @method reload
     @public

.gitignore
@@ -17,3 +17,4 @@ docs/node_modules/
 
 node_modules/
 bower_components/
+.metadata_never_index

packages/ember-data/lib/main.js
@@ -64,7 +64,7 @@ import "ember-data/ember-initializer";
 import setupContainer from "ember-data/setup-container";
 
 import ContainerProxy from "ember-data/system/container_proxy";
-import {Relationship} from "ember-data/system/relationships/relationship";
+import Relationship from "ember-data/system/relationships/state/relationship";
 
 DS.Store         = Store;
 DS.PromiseArray  = PromiseArray;

packages/ember-data/lib/system/model/model.js
@@ -1,9 +1,9 @@
 import RootState from "ember-data/system/model/states";
 import Errors from "ember-data/system/model/errors";
 import { PromiseObject } from "ember-data/system/promise_proxies";
-import { createRelationshipFor } from "ember-data/system/relationships/relationship";
 import merge from "ember-data/system/merge";
 import JSONSerializer from "ember-data/serializers/json_serializer";
+import createRelationshipFor from "ember-data/system/relationships/state/create";
 
 /**
   @module ember-data

packages/ember-data/lib/system/record_array_manager.js
@@ -5,8 +5,7 @@
 import {
   RecordArray,
   FilteredRecordArray,
-  AdapterPopulatedRecordArray,
-  ManyArray
+  AdapterPopulatedRecordArray
 } from "ember-data/system/record_arrays";
 import {
   MapWithDefault,
@@ -156,31 +155,6 @@ export default Ember.Object.extend({
         this.updateRecordArray(array, filter, type, record);
       }
     }
-  },
-
-  /**
-    Create a `DS.ManyArray` for a type and list of record references, and index
-    the `ManyArray` under each reference. This allows us to efficiently remove
-    records from `ManyArray`s when they are deleted.
-
-    @method createManyArray
-    @param {Class} type
-    @param {Array} references
-    @return {DS.ManyArray}
-  */
-  createManyArray: function(type, records) {
-    var manyArray = ManyArray.create({
-      type: type,
-      content: records,
-      store: this.store
-    });
-
-    forEach(records, function(record) {
-      var arrays = this.recordArraysForRecord(record);
-      arrays.add(manyArray);
-    }, this);
-
-    return manyArray;
   },
 
   /**

packages/ember-data/lib/system/relationships/belongs_to.js
@@ -101,11 +101,7 @@ function belongsTo(type, options) {
   @namespace DS
 */
 Model.reopen({
-  notifyBelongsToAdded: function(key, relationship) {
-    this.notifyPropertyChange(key);
-  },
-
-  notifyBelongsToRemoved: function(key) {
+  notifyBelongsToChanged: function(key) {
     this.notifyPropertyChange(key);
   }
 });

packages/ember-data/lib/system/relationships/has_many.js
@@ -122,21 +122,14 @@ function hasMany(type, options) {
 }
 
 Model.reopen({
-  notifyHasManyAdded: function(key, record, idx) {
-    var relationship = this._relationships[key];
-    var manyArray = relationship.manyArray;
-    manyArray.addRecord(record, idx);
+  notifyHasManyAdded: function(key) {
     //We need to notifyPropertyChange in the adding case because we need to make sure
     //we fetch the newly added record in case it is unloaded
     //TODO(Igor): Consider whether we could do this only if the record state is unloaded
+
+    //Goes away once hasMany is double promisified
     this.notifyPropertyChange(key);
   },
-
-  notifyHasManyRemoved: function(key, record) {
-    var relationship = this._relationships[key];
-    var manyArray = relationship.manyArray;
-    manyArray.removeRecord(record);
-  }
 });
 
 

packages/ember-data/lib/system/relationships/relationship.js
@@ -1,394 +1,0 @@
-import {
-  PromiseManyArray,
-  PromiseObject
-} from "ember-data/system/promise_proxies";
-import {
-  OrderedSet
-} from "ember-data/system/map";
-
-var forEach = Ember.EnumerableUtils.forEach;
-
-var Relationship = function(store, record, inverseKey, relationshipMeta) {
-  this.members = new OrderedSet();
-  this.store = store;
-  this.key = relationshipMeta.key;
-  this.inverseKey = inverseKey;
-  this.record = record;
-  this.isAsync = relationshipMeta.options.async;
-  this.relationshipMeta = relationshipMeta;
-  //This probably breaks for polymorphic relationship in complex scenarios, due to
-  //multiple possible typeKeys
-  this.inverseKeyForImplicit = this.store.modelFor(this.record.constructor).typeKey + this.key;
-  //Cached promise when fetching the relationship from a link
-  this.linkPromise = null;
-};
-
-Relationship.prototype = {
-  constructor: Relationship,
-
-  destroy: Ember.K,
-
-  clear: function() {
-    var members = this.members.list;
-    var member;
-
-    while (members.length > 0){
-      member = members[0];
-      this.removeRecord(member);
-    }
-  },
-
-  disconnect: function(){
-    this.members.forEach(function(member) {
-      this.removeRecordFromInverse(member);
-    }, this);
-  },
-
-  reconnect: function(){
-    this.members.forEach(function(member) {
-      this.addRecordToInverse(member);
-    }, this);
-  },
-
-  removeRecords: function(records){
-    var self = this;
-    forEach(records, function(record){
-      self.removeRecord(record);
-    });
-  },
-
-  addRecords: function(records, idx){
-    var self = this;
-    forEach(records, function(record){
-      self.addRecord(record, idx);
-      if (idx !== undefined) {
-        idx++;
-      }
-    });
-  },
-
-  addRecord: function(record, idx) {
-    if (!this.members.has(record)) {
-      this.members.add(record);
-      this.notifyRecordRelationshipAdded(record, idx);
-      if (this.inverseKey) {
-        record._relationships[this.inverseKey].addRecord(this.record);
-      } else {
-        if (!record._implicitRelationships[this.inverseKeyForImplicit]) {
-          record._implicitRelationships[this.inverseKeyForImplicit] = new Relationship(this.store, record, this.key,  {options:{}});
-        }
-        record._implicitRelationships[this.inverseKeyForImplicit].addRecord(this.record);
-      }
-      this.record.updateRecordArrays();
-    }
-  },
-
-  removeRecord: function(record) {
-    if (this.members.has(record)) {
-      this.removeRecordFromOwn(record);
-      if (this.inverseKey) {
-        this.removeRecordFromInverse(record);
-      } else {
-        if (record._implicitRelationships[this.inverseKeyForImplicit]) {
-          record._implicitRelationships[this.inverseKeyForImplicit].removeRecord(this.record);
-        }
-      }
-    }
-  },
-
-  addRecordToInverse: function(record) {
-    if (this.inverseKey) {
-      record._relationships[this.inverseKey].addRecord(this.record);
-    }
-  },
-
-  removeRecordFromInverse: function(record) {
-    var inverseRelationship = record._relationships[this.inverseKey];
-    //Need to check for existence, as the record might unloading at the moment
-    if (inverseRelationship) {
-      inverseRelationship.removeRecordFromOwn(this.record);
-    }
-  },
-
-  removeRecordFromOwn: function(record) {
-    this.members.delete(record);
-    this.notifyRecordRelationshipRemoved(record);
-    this.record.updateRecordArrays();
-  },
-
-  updateLink: function(link) {
-    Ember.warn("You have pushed a record of type '" + this.record.constructor.typeKey + "' with '" + this.key + "' as a link, but the association is not an aysnc relationship.", this.isAsync);
-    Ember.assert("You have pushed a record of type '" + this.record.constructor.typeKey + "' with '" + this.key + "' as a link, but the value of that link is not a string.", typeof link === 'string' || link === null);
-    if (link !== this.link) {
-      this.link = link;
-      this.linkPromise = null;
-      this.record.notifyPropertyChange(this.key);
-    }
-  },
-
-  findLink: function() {
-    if (this.linkPromise) {
-      return this.linkPromise;
-    } else {
-      var promise = this.fetchLink();
-      this.linkPromise = promise;
-      return promise.then(function(result) {
-        return result;
-      });
-    }
-  },
-
-  updateRecordsFromAdapter: function(records) {
-    //TODO Once we have adapter support, we need to handle updated and canonical changes
-    this.computeChanges(records);
-  },
-
-  notifyRecordRelationshipAdded: Ember.K,
-  notifyRecordRelationshipRemoved: Ember.K
-};
-
-var ManyRelationship = function(store, record, inverseKey, relationshipMeta) {
-  this._super$constructor(store, record, inverseKey, relationshipMeta);
-  this.belongsToType = relationshipMeta.type;
-  this.manyArray = store.recordArrayManager.createManyArray(this.belongsToType, Ember.A());
-  this.manyArray.relationship = this;
-  this.isPolymorphic = relationshipMeta.options.polymorphic;
-  this.manyArray.isPolymorphic = this.isPolymorphic;
-};
-
-ManyRelationship.prototype = Ember.create(Relationship.prototype);
-ManyRelationship.prototype.constructor = ManyRelationship;
-ManyRelationship.prototype._super$constructor = Relationship;
-
-ManyRelationship.prototype.destroy = function() {
-  this.manyArray.destroy();
-};
-
-ManyRelationship.prototype.notifyRecordRelationshipAdded = function(record, idx) {
-  Ember.assert("You cannot add '" + record.constructor.typeKey + "' records to this relationship (only '" + this.belongsToType.typeKey + "' allowed)", !this.belongsToType || record instanceof this.belongsToType);
-  this.record.notifyHasManyAdded(this.key, record, idx);
-};
-
-ManyRelationship.prototype.notifyRecordRelationshipRemoved = function(record) {
-  this.record.notifyHasManyRemoved(this.key, record);
-};
-
-ManyRelationship.prototype.reload = function() {
-  var self = this;
-  if (this.link) {
-    return this.fetchLink();
-  } else {
-    return this.store.scheduleFetchMany(this.manyArray.toArray()).then(function() {
-      //Goes away after the manyArray refactor
-      self.manyArray.set('isLoaded', true);
-      return self.manyArray;
-    });
-  }
-};
-
-ManyRelationship.prototype.computeChanges = function(records) {
-  var members = this.members;
-  var recordsToRemove = [];
-  var length;
-  var record;
-  var i;
-
-  records = setForArray(records);
-
-  members.forEach(function(member) {
-    if (member.get('isNew') || records.has(member)) return;
-
-    recordsToRemove.push(member);
-  });
-  this.removeRecords(recordsToRemove);
-
-  var hasManyArray = this.manyArray;
-
-  // Using records.toArray() since currently using
-  // removeRecord can modify length, messing stuff up
-  // forEach since it directly looks at "length" each
-  // iteration
-  records = records.toArray();
-  length = records.length;
-  for (i = 0; i < length; i++){
-    record = records[i];
-    //Need to preserve the order of incoming records
-    if (hasManyArray.objectAt(i) === record ) {
-      continue;
-    }
-    this.removeRecord(record);
-    this.addRecord(record, i);
-  }
-};
-
-ManyRelationship.prototype.fetchLink = function() {
-  var self = this;
-  return this.store.findHasMany(this.record, this.link, this.relationshipMeta).then(function(records){
-    self.updateRecordsFromAdapter(records);
-    return self.manyArray;
-  });
-};
-
-ManyRelationship.prototype.findRecords = function() {
-  var manyArray = this.manyArray;
-  return this.store.findMany(manyArray.toArray()).then(function(){
-    //Goes away after the manyArray refactor
-    manyArray.set('isLoaded', true);
-    return manyArray;
-  });
-};
-
-ManyRelationship.prototype.getRecords = function() {
-  if (this.isAsync) {
-    var self = this;
-    var promise;
-    if (this.link) {
-      promise = this.findLink().then(function() {
-        return self.findRecords();
-      });
-    } else {
-      promise = this.findRecords();
-    }
-    return PromiseManyArray.create({
-      content: this.manyArray,
-      promise: promise
-    });
-  } else {
-      Ember.assert("You looked up the '" + this.key + "' relationship on a '" + this.record.constructor.typeKey + "' with id " + this.record.get('id') +  " but some of the associated records were not loaded. Either make sure they are all loaded together with the parent record, or specify that the relationship is async (`DS.hasMany({ async: true })`)", this.manyArray.isEvery('isEmpty', false));
-
-    if (!this.manyArray.get('isDestroyed')) {
-      this.manyArray.set('isLoaded', true);
-    }
-    return this.manyArray;
- }
-};
-
-var BelongsToRelationship = function(store, record, inverseKey, relationshipMeta) {
-  this._super$constructor(store, record, inverseKey, relationshipMeta);
-  this.record = record;
-  this.key = relationshipMeta.key;
-  this.inverseRecord = null;
-};
-
-BelongsToRelationship.prototype = Ember.create(Relationship.prototype);
-BelongsToRelationship.prototype.constructor = BelongsToRelationship;
-BelongsToRelationship.prototype._super$constructor = Relationship;
-
-BelongsToRelationship.prototype.setRecord = function(newRecord) {
-  if (newRecord) {
-    this.addRecord(newRecord);
-  } else if (this.inverseRecord) {
-    this.removeRecord(this.inverseRecord);
-  }
-};
-
-BelongsToRelationship.prototype._super$addRecord = Relationship.prototype.addRecord;
-BelongsToRelationship.prototype.addRecord = function(newRecord) {
-  if (this.members.has(newRecord)){ return;}
-  var type = this.relationshipMeta.type;
-  Ember.assert("You can only add a '" + type.typeKey + "' record to this relationship", newRecord instanceof type);
-
-  if (this.inverseRecord) {
-    this.removeRecord(this.inverseRecord);
-  }
-
-  this.inverseRecord = newRecord;
-  this._super$addRecord(newRecord);
-};
-
-BelongsToRelationship.prototype.setRecordPromise = function(newPromise) {
-  var content = newPromise.get && newPromise.get('content');
-  Ember.assert("You passed in a promise that did not originate from an EmberData relationship. You can only pass promises that come from a belongsTo or hasMany relationship to the get call.", content !== undefined);
-  this.setRecord(content);
-};
-
-BelongsToRelationship.prototype.notifyRecordRelationshipAdded = function(newRecord) {
-  this.record.notifyBelongsToAdded(this.key, this);
-};
-
-BelongsToRelationship.prototype.notifyRecordRelationshipRemoved = function(record) {
-  this.record.notifyBelongsToRemoved(this.key, this);
-};
-
-BelongsToRelationship.prototype._super$removeRecordFromOwn = Relationship.prototype.removeRecordFromOwn;
-BelongsToRelationship.prototype.removeRecordFromOwn = function(record) {
-  if (!this.members.has(record)) { return; }
-  this.inverseRecord = null;
-  this._super$removeRecordFromOwn(record);
-};
-
-BelongsToRelationship.prototype.findRecord = function() {
-  if (this.inverseRecord) {
-    return this.store._findByRecord(this.inverseRecord);
-  } else {
-    return Ember.RSVP.Promise.resolve(null);
-  }
-};
-
-BelongsToRelationship.prototype.fetchLink = function() {
-  var self = this;
-  return this.store.findBelongsTo(this.record, this.link, this.relationshipMeta).then(function(record){
-    if (record) {
-      self.addRecord(record);
-    }
-    return record;
-  });
-};
-
-BelongsToRelationship.prototype.getRecord = function() {
-  if (this.isAsync) {
-    var promise;
-    if (this.link){
-      var self = this;
-      promise = this.findLink().then(function() {
-        return self.findRecord();
-      });
-    } else {
-      promise = this.findRecord();
-    }
-
-    return PromiseObject.create({
-      promise: promise,
-      content: this.inverseRecord
-    });
-  } else {
-    Ember.assert("You looked up the '" + this.key + "' relationship on a '" + this.record.constructor.typeKey + "' with id " + this.record.get('id') +  " but some of the associated records were not loaded. Either make sure they are all loaded together with the parent record, or specify that the relationship is async (`DS.belongsTo({ async: true })`)", this.inverseRecord === null || !this.inverseRecord.get('isEmpty'));
-    return this.inverseRecord;
-  }
-};
-
-function setForArray(array) {
-  var set = new OrderedSet();
-
-  if (array) {
-    for (var i=0, l=array.length; i<l; i++) {
-      set.add(array[i]);
-    }
-  }
-
-  return set;
-}
-
-var createRelationshipFor = function(record, relationshipMeta, store){
-  var inverseKey;
-  var inverse = record.constructor.inverseFor(relationshipMeta.key);
-
-  if (inverse) {
-    inverseKey = inverse.name;
-  }
-
-  if (relationshipMeta.kind === 'hasMany'){
-    return new ManyRelationship(store, record, inverseKey, relationshipMeta);
-  }
-  else {
-    return new BelongsToRelationship(store, record, inverseKey, relationshipMeta);
-  }
-};
-
-
-export {
-  Relationship,
-  ManyRelationship,
-  BelongsToRelationship,
-  createRelationshipFor
-};

commit 43ced435a5a47e3de7fb2f1bb557cd615354cf5b

Fix array change notification in many arrays
Fixes a bug introduced in 75dbd8ffb0101f82c0a84e44132fdda1661f2d2a where removals from
a many array were not being notified correctly.

packages/ember-data/lib/system/record_arrays/many_array.js
@@ -107,10 +107,10 @@ export default Ember.Object.extend(Ember.MutableArray, Ember.Evented, {
     if (!objects) {
       objects = [];
     }
-    this.arrayContentWillChange(idx, 0, objects.length);
+    this.arrayContentWillChange(idx, amt, objects.length);
     this.currentState.splice.apply(this.currentState, [idx, amt].concat(objects));
     this.set('length', this.currentState.length);
-    this.arrayContentDidChange(idx, 0, objects.length);
+    this.arrayContentDidChange(idx, amt, objects.length);
     if (objects){
       //TODO(Igor) probably needed only for unloaded records
       this.relationship.notifyHasManyChanged();

packages/ember-data/tests/integration/relationships/has_many_test.js
@@ -55,7 +55,8 @@ module("integration/relationships/has_many - Has-Many Relationships", {
     Book.toString = stringify('Book');
 
     Chapter = DS.Model.extend({
-      title: attr()
+      title: attr(),
+      pages: hasMany('page')
     });
     Chapter.toString = stringify('Chapter');
 
@@ -1068,6 +1069,64 @@ test("Rollbacking a deleted record restores implicit relationship correctly when
   });
   run(function(){
     equal(page.get('chapter'), chapter, "Page has a chapter after rollback");
+  });
+});
+
+test("ManyArray notifies the array observers and flushes bindings when removing", function () {
+  expect(2);
+  var chapter, page, page2;
+  var observe = false;
+  var arr = Ember.A([1,2]);
+  run(function(){
+    page = env.store.push('page', { id: 1, number: 1 });
+    page2 = env.store.push('page', { id: 2, number: 2 });
+    chapter = env.store.push('chapter', { id: 1, title: 'Sailing the Seven Seas', pages: [1, 2] });
+    chapter.get('pages').addEnumerableObserver(this, {
+      willChange: function(pages, removing, addCount){
+        if (observe) {
+          equal(removing[0], page2, 'page2 is passed to willChange');
+        }
+      },
+      didChange:function(pages, removeCount, adding){
+        if (observe) {
+          equal(removeCount, 1, 'removeCount is correct');
+        }
+      }
+    });
+  });
+  run(function(){
+    observe = true;
+    page2.set('chapter', null);
+    observe = false;
+  });
+});
+
+test("ManyArray notifies the array observers and flushes bindings when adding", function () {
+  expect(2);
+  var chapter, page, page2;
+  var observe = false;
+  var arr = Ember.A([1,2]);
+  run(function(){
+    page = env.store.push('page', { id: 1, number: 1 });
+    page2 = env.store.push('page', { id: 2, number: 2 });
+    chapter = env.store.push('chapter', { id: 1, title: 'Sailing the Seven Seas', pages: [1] });
+    chapter.get('pages').addEnumerableObserver(this, {
+      willChange: function(pages, removing, addCount){
+        if (observe) {
+          equal(addCount, 1, 'addCount is correct');
+        }
+      },
+      didChange:function(pages, removeCount, adding){
+        if (observe) {
+          equal(adding[0], page2, 'page2 is passed to didChange');
+        }
+      }
+    });
+  });
+  run(function(){
+    observe = true;
+    page2.set('chapter', chapter);
+    observe = false;
   });
 });
 