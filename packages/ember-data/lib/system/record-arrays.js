13d64418abd9d8ad45293e7d36b16d38c512e416
Jacob Jewell <jacobjewell@eflexsystems.com>
2015-03-14 17:33:24 -0400

    commit record-arrays.js separately so it doesn't clobber the rename

--- packages/ember-data/lib/system/record-arrays.js
+++ packages/ember-data/lib/system/record-arrays.js
@@ -2,9 +2,9 @@
   @module ember-data
 */
 
-import RecordArray from "ember-data/system/record_arrays/record_array";
-import FilteredRecordArray from "ember-data/system/record_arrays/filtered_record_array";
-import AdapterPopulatedRecordArray from "ember-data/system/record_arrays/adapter_populated_record_array";
+import RecordArray from "ember-data/system/record-arrays/record-array";
+import FilteredRecordArray from "ember-data/system/record-arrays/filtered-record-array";
+import AdapterPopulatedRecordArray from "ember-data/system/record-arrays/adapter-populated-record-array";
 
 export {
   RecordArray,
