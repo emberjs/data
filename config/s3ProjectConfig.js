edcf0996b4eb8b3edced3ad9db0332a33db26c6a
bmac <bmac325@GMail.com>
2015-03-26 16:20:48 -0400

    Update the s3ProjectConfig to upload tagged builds

--- config/s3ProjectConfig.js
+++ config/s3ProjectConfig.js
@@ -1,32 +1,46 @@
-module.exports = function(revision, tag, date){
+function fileMap(revision,tag,date) {
   return {
-    'ember-data.js': fileObject('ember-data.js', 'text/javascript', revision, tag, date),
-    'ember-data.js.map': fileObject('ember-data.js.map', 'application/json', revision, tag, date),
-    'ember-data.min.js': fileObject('ember-data.min.js', 'text/javascript', revision, tag, date),
-    'ember-data.prod.js': fileObject('ember-data.prod.js', 'text/javascript', revision, tag, date)
-  }
+    'ember-data.js': fileObject('ember-data',             '.js',   'text/javascript',  revision, tag, date),
+    'ember-data.js.map': fileObject('ember-data.js',      '.map',  'application/json', revision, tag, date),
+    'ember-data.min.js': fileObject('ember-data.min',     '.js',   'text/javascript',  revision, tag, date),
+    'ember-data.prod.js': fileObject('ember-data.prod',   '.js',   'text/javascript',  revision, tag, date),
+    '../docs/data.json':  fileObject('ember-data-docs',   '.json', 'application/json', revision, tag, date)
+  };
+};
+
+function fileObject(baseName, extension, contentType, currentRevision, tag, date){
+  var fullName = '/' + baseName + extension;
+  var obj =  {
+    contentType: contentType,
+      destinations: {
+        canary: [
+          'latest' + fullName,
+          'canary' + fullName,
+          'canary/daily/' + date + fullName,
+          'canary/shas/' + currentRevision + fullName
+        ],
+        release: [
+          'stable' + fullName,
+          'release' + fullName,
+          'release/daily/' + date + fullName,
+          'release/shas/' + currentRevision + fullName
+        ],
+        beta: [
+          'beta' + fullName,
+          'beta/daily/' + date + fullName,
+          'beta/shas/' + currentRevision + fullName
+        ],
+        wildcard: []
+      }
+   };
+
+   if (tag) {
+     for (var key in obj.destinations) {
+       obj.destinations[key].push('tags/' + tag + fullName);
+     }
+   }
+
+   return obj;
 }
 
-function fileObject(fileName, contentType, currentRevision, tag, date) {
-  var filePath = '/' + fileName;
-  return {
-    contentType: contentType,
-    destinations: {
-      canary: [
-        'canary' + filePath,
-        'canary/daily/' + date + filePath,
-        'canary/shas/' + currentRevision + filePath
-      ],
-      stable: [
-        'stable' + filePath,
-        'stable/daily/' + date + filePath,
-        'stable/shas/' + currentRevision + filePath
-      ],
-      beta: [
-        'beta' + filePath,
-        'beta/daily/' + date + filePath,
-        'beta/shas/' + currentRevision + filePath
-      ]
-    }
-  }
-}
+module.exports = fileMap;
