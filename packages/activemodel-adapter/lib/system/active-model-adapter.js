fb8568f3801cb1f5fd8d48f52426f3529072e018
pangratz <cmueller.418@gmail.com>
2015-05-02 23:56:17 +0200

    Remove logic for camelCase-ing error keys in ActiveModelAdapter
    Since c947e254ac6df93bd8b1b836e526fbd7b648f8ba the keys of the error
    object are camelCased in the adapter, which is actually a serializer
    concern. The JSONSerializer, from which the ActiveModelSerializer
    extends, does a normalization of the errors object in the
    `extractErrors` hook (see http://git.io/vJ0A6 & http://git.io/vJ0AS) and
    this hook gets called by the store when the adapter rejects (see
    http://git.io/vJ0AI), the camelCase can be removed from inside the
    adapter.
    
    A test for the correct normalization of the errors object is added to
    the ActiveModelSerializer test suite.

--- packages/activemodel-adapter/lib/system/active-model-adapter.js
+++ packages/activemodel-adapter/lib/system/active-model-adapter.js
@@ -6,7 +6,6 @@ import {pluralize} from "ember-inflector";
   @module ember-data
 */
 
-var camelize = Ember.String.camelize;
 var decamelize = Ember.String.decamelize;
 var underscore = Ember.String.underscore;
 
@@ -144,15 +143,6 @@ var ActiveModelAdapter = RESTAdapter.extend({
     if (jqXHR && jqXHR.status === 422) {
       var response = Ember.$.parseJSON(jqXHR.responseText);
       var errors = response.errors ? response.errors : response;
-
-      for (var underscoredError in errors) {
-        var camelizedError = camelize(underscoredError);
-
-        if (camelizedError !== underscoredError) {
-          errors[camelizedError] = errors[underscoredError];
-          delete errors[underscoredError];
-        }
-      }
 
       return new InvalidError(errors);
     } else {
--- packages/activemodel-adapter/tests/integration/active-model-adapter-test.js
+++ packages/activemodel-adapter/tests/integration/active-model-adapter-test.js
@@ -42,17 +42,6 @@ test('ajaxError - accepts any kind of json on 422', function() {
   equal(adapter.ajaxError(jqXHR), error.toString());
 });
 
-test('ajaxError - invalid error has camelized keys', function() {
-  var error = new DS.InvalidError({ firstName: "can't be blank" });
-
-  var jqXHR = {
-    status: 422,
-    responseText: JSON.stringify({ errors: { first_name: "can't be blank" } })
-  };
-
-  equal(adapter.ajaxError(jqXHR), error.toString());
-});
-
 test('ajaxError - returns ajax response if not 422 response', function() {
   var jqXHR = {
     status: 500,
