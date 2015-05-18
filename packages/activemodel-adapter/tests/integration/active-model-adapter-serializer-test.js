17a2a18e5c7cf0b52059ef70a28dccfd66de6c1b
Ben Holmes <mail@benholmes.uk>
2015-05-17 20:22:53 -1000

    Test that active model adapter passes errors to serializer
    Thanks to @pangratz for putting this test together. Currently we only have tests
    for the adapter and serializer correctly behaving in isolation.

---
+++ packages/activemodel-adapter/tests/integration/active-model-adapter-serializer-test.js
@@ -0,0 +1,54 @@
+var env, store, adapter, User;
+var originalAjax;
+
+module("integration/active_model_adapter_serializer - AMS Adapter and Serializer", {
+  setup: function() {
+    originalAjax = Ember.$.ajax;
+
+    User = DS.Model.extend({
+      firstName: DS.attr()
+    });
+
+    env = setupStore({
+      user: User,
+      adapter: DS.ActiveModelAdapter
+    });
+
+    store = env.store;
+    adapter = env.adapter;
+
+    env.registry.register('serializer:application', DS.ActiveModelSerializer);
+  },
+
+  teardown: function() {
+    Ember.$.ajax = originalAjax;
+  }
+});
+
+test('errors are camelCased and are expected under the `errors` property of the payload', function() {
+  var jqXHR = {
+    status: 422,
+    responseText: JSON.stringify({
+      errors: {
+        first_name: ["firstName error"]
+      }
+    })
+  };
+
+  Ember.$.ajax = function(hash) {
+    hash.error(jqXHR);
+  };
+
+  var user;
+  Ember.run(function() {
+    user = store.push('user', { id: 1 });
+  });
+
+  Ember.run(function() {
+    user.save().then(null, function() {
+      var errors = user.get('errors');
+      ok(errors.has('firstName'), "there are errors for the firstName attribute");
+      deepEqual(errors.errorsFor('firstName').getEach('message'), ['firstName error']);
+    });
+  });
+});
