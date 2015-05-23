c8c8af8880380819cbe7e0cca3cd0f4134c5c458
Stanley Stuart <stanley@stan.li>
2015-05-22 21:35:05 -0500

    add babel to the build pipeline

---
+++ config/babel.js
@@ -0,0 +1,15 @@
+module.exports = {
+  loose: true,
+  whitelist: [
+    'es6.templateLiterals',
+    'es6.parameters.rest',
+    'es6.arrowFunctions',
+    'es6.destructuring',
+    'es6.spread',
+    'es6.parameters.default',
+    'es6.properties.computed',
+    'es6.properties.shorthand',
+    'es6.blockScoping',
+    'es6.constants'
+  ]
+};
