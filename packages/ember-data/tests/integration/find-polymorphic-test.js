832b5a3b793228374ae7d6ff016e9405d0ed5ac8
Matt Moon <moon.matthew@gmail.com>
2015-05-25 21:21:23 -0500

    fixed issues with build

--- packages/ember-data/tests/integration/find-polymorphic-test.js
+++ packages/ember-data/tests/integration/find-polymorphic-test.js
@@ -1,3 +1,4 @@
+var Attachment, ImageAttachment, LinkAttachment, store;
 var get = Ember.get;
 var resolve = Ember.RSVP.resolve;
 var run = Ember.run;
@@ -6,20 +7,24 @@ var attr = DS.attr;
 
 module("integration/find_polymorphic - Find Polymorphic", {
   setup: function () {
-    Attacment = DS.Model.extend({
+    Attachment = DS.Model.extend({
       type: attr('string')
     });
 
-    ImageAttacment = Attacment.extend({
-      type: attr('string', {defaultValue: 'image_attachment'})
+    ImageAttachment = Attachment.extend({
+      type: attr('string', {
+        defaultValue: 'image_attachment'
+      })
     });
 
-    LinkAttacment = Attacment.extend({
-      type: attr('string', {defaultValue: 'link_attachment'})
+    LinkAttachment = Attachment.extend({
+      type: attr('string', {
+        defaultValue: 'link_attachment'
+      })
     });
   },
   teardown: function(){
-
+    run(store, 'destroy');
   }
 });
 
@@ -28,16 +33,16 @@ test("When find is called on a base of a polymorphic model, each record will be 
   var deferred = Ember.RSVP.defer();
   store = createStore({ adapter: DS.Adapter.extend({
     find: function(store, type, id, snapshot) {
-      return Ember.RSVP.resolve([{'id': 1, 'type': 'image_attachment'},{'id': 2, 'type': 'link_attachment'}]);
+      return Ember.RSVP.resolve([{ 'id': 1, 'type': 'image_attachment' },{ 'id': 2, 'type': 'link_attachment' }]);
     }
   })
   });
 
   run(function() {
-    store.find(Attacment).then(function(attachments) {
+    store.find(Attachment).then(function(attachments) {
       equal(attachments.get('length'), 2, 'We recieved 2 attachments');
-      equal(store.hasRecordForId(ImageAttacment, 1), true, 'First attachment is an image attachment');
-      equal(store.hasRecordForId(LinkAttacment, 2), true, 'Second attachment is a link attachment');
+      equal(store.hasRecordForId(ImageAttachment, 1), true, 'First attachment is an image attachment');
+      equal(store.hasRecordForId(LinkAttachment, 2), true, 'Second attachment is a link attachment');
       ok(true, 'We loaded the appropriate models');
     });
   });
@@ -55,8 +60,8 @@ test("When find is called on a base of a polymorphic model with id the record wi
   });
 
   run(function() {
-    store.find(Attacment, 1).then(function(attachment) {
-      equal(store.hasRecordForId(ImageAttacment, 1), true, 'The attachment is an image attachment');
+    store.find(Attachment, 1).then(function(attachment) {
+      equal(store.hasRecordForId(ImageAttachment, 1), true, 'The attachment is an image attachment');
       ok(true, 'We loaded the appropriate model');
     });
   });
