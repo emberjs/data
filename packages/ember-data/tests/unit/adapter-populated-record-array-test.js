4741717e3dd314222aeb3bc582372936ccc41bb5
yratanov <organium@gmail.com>
2015-06-03 16:00:39 +0700

    add missing tests for record arrays

--- packages/ember-data/tests/unit/adapter-populated-record-array-test.js
+++ packages/ember-data/tests/unit/adapter-populated-record-array-test.js
@@ -40,3 +40,12 @@ test("when a record is deleted in an adapter populated record array, it should b
 
   equal(recordArray.get('length'), 2, "expected recordArray to contain exactly 2 records");
 });
+
+test('recordArray.replace() throws error', function() {
+  var recordArray = store.recordArrayManager
+    .createAdapterPopulatedRecordArray(Person, null);
+
+  throws(function() {
+    recordArray.replace();
+  }, Error("The result of a server query (on (subclass of DS.Model)) is immutable."), 'throws error')
+});
