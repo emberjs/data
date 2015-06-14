7e7208b765f0693d4e937aba6824396d8c7e49de
pangratz <cmueller.418@gmail.com>
2015-06-14 14:23:32 +0200

    Soft deprecate `store.pushMany`

--- tests/ember-data-setup.js
+++ tests/ember-data-setup.js
@@ -1,5 +1,11 @@
 ;(function() {
 
+  // Since store.pushMany is used pretty heavily inside the tests, fixing all
+  // the deprecation warnings at once would be a big changeset. That's why the
+  // log level for this deprecation is set to LOG, so the deprecations are
+  // logged but they do not result in a failed test.
+  Ember.Debug._addDeprecationLevel("ember-data-pushMany", Ember.Debug._deprecationLevels.LOG);
+
   Ember.RSVP.configure('onerror', function(reason) {
     // only print error messages if they're exceptions;
     // otherwise, let a future turn of the event loop
