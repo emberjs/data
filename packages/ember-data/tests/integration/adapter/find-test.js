83f3cf472bc445acfe441af8d8a7a3a06db473a7
Brian Runnells <brunnells@heroku.com>
2015-06-21 20:23:01 -0600

    [Bugfix release] Fix minor typo in deprecation warning
    you -> your
    
    (cherry picked from commit 41b0f9a5fe79afbdbcb96b12695a153b8b5ef825)

--- packages/ember-data/tests/integration/adapter/find-test.js
+++ packages/ember-data/tests/integration/adapter/find-test.js
@@ -62,7 +62,7 @@ test("store.findAll should trigger a deprecation warning about store.shouldReloa
   run(function() {
     expectDeprecation(function() {
       store.findAll('person');
-    }, 'The default behavior of shouldReloadAll will change in Ember Data 2.0 to always return false when there is at least one "person" record in the store. If you would like to preserve the current behavior please override shouldReloadAll in you adapter:application and return true.');
+    }, 'The default behavior of shouldReloadAll will change in Ember Data 2.0 to always return false when there is at least one "person" record in the store. If you would like to preserve the current behavior please override shouldReloadAll in your adapter:application and return true.');
   });
 });
 
