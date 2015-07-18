7625b8ee3d199e552cc30291e57a45c7ad830bc8
Kyle Coberly <kyle.coberly@gmail.com>
2015-07-18 11:56:44 -0600

    [DOC release] Adding documentation for SnapshotRecordArray
    Fixes #3464
    (cherry picked from commit f39992857c316afcab88eb97227331257a48f520)
    
    Conflicts:
    	packages/ember-data/lib/system/snapshot-record-array.js

--- packages/ember-data/lib/system/snapshot-record-array.js
+++ packages/ember-data/lib/system/snapshot-record-array.js
@@ -11,14 +11,51 @@
   @param {Object} meta
 */
 function SnapshotRecordArray(recordArray, meta, adapterOptions) {
+  /**
+    An array of snapshots
+    @private
+    @property _snapshots
+    @type {Array}
+  */
   this._snapshots = null;
+  /**
+    An array of records
+    @private
+    @property _recordArray
+    @type {Array}
+  */
   this._recordArray = recordArray;
+  /**
+    Number of records in the array
+    @property length
+    @type {Number}
+  */
   this.length = recordArray.get('length');
+  /**
+    The type of the underlying records for the snapshots in the array, as a DS.Model
+    @property type
+    @type {DS.Model}
+  */
   this.type = recordArray.get('type');
+  /**
+    Meta object
+    @property meta
+    @type {Object}
+  */
   this.meta = meta;
+  /**
+    A hash of adapter options
+    @property adapterOptions
+    @type {Object}
+  */
   this.adapterOptions = adapterOptions;
 }
 
+/**
+  Get snapshots of the underlying record array
+  @method snapshots
+  @return {Array} Array of snapshots
+*/
 SnapshotRecordArray.prototype.snapshots = function() {
   if (this._snapshots) {
     return this._snapshots;
