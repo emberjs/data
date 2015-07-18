11dbbc1eff7ee7fbc59050236c9c6981826fdcb2
Stanley Stuart <stanley@stan.li>
2015-07-18 11:28:23 -0500

    remove babel warnings around parameters

--- config/babel.js
+++ config/babel.js
@@ -2,11 +2,10 @@ module.exports = {
   loose: true,
   whitelist: [
     'es6.templateLiterals',
-    'es6.parameters.rest',
+    'es6.parameters',
     'es6.arrowFunctions',
     'es6.destructuring',
     'es6.spread',
-    'es6.parameters.default',
     'es6.properties.computed',
     'es6.properties.shorthand',
     'es6.blockScoping',
