9344a93e8e27dcb4d2cccbeec028c69beba59c34
Stefan Penner <stefan.penner@gmail.com>
2015-08-13 13:52:38 -0700

    Don’t crash \w Ember 2.0

--- packages/ember/lib/main.js
+++ packages/ember/lib/main.js
@@ -1,2 +1,20 @@
 // Shim Ember module
+
+// #support for Ember 2.0
+Ember.ArrayPolyfills = Ember.ArrayPolyfills || Array.prototype;
+
+function macroFor(name) {
+  return function(obj, callback, thisArg) {
+    return obj[name] ? obj[name](callback, thisArg) : Array.prototype[name].call(obj, callback, thisArg);
+  };
+}
+
+Ember.EnumerableUtils = Ember.EnumerableUtils || {
+  map:     macroFor('map'),
+  indexOf: macroFor('indexOf'),
+  forEach: macroFor('forEach'),
+  filter:  macroFor('filter')
+};
+// /support for Ember 2.0
+
 export default Ember;
