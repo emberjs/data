068af57d8d656a031ddb619ee97c38c50cacf10e
Robert Jackson <robert.w.jackson@me.com>
2015-08-17 12:07:37 -0400

    [BUGFIX beta] Use public API for container in instance initializer.
    As of 2.1.0 the ApplicationInstance instance that we receive in an
    instance initializer has a `lookup` method on it directly. This is new
    public API that replaces `appInstance.container.lookup` as of 2.1.0.

--- packages/ember-data/lib/instance-initializers/initialize-store-service.js
+++ packages/ember-data/lib/instance-initializers/initialize-store-service.js
@@ -6,7 +6,7 @@
  @param {Ember.ApplicationInstance} applicationOrRegistry
  */
 export default function initializeStoreService(application) {
-  var container = application.container;
+  var container = application.lookup ? application : application.container;
   // Eagerly generate the store so defaultStore is populated.
   container.lookup('service:store');
 }
