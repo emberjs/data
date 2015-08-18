4bf88b4a16a4662fc7265b8fb1cb250b61081316
greyhwndz <greyhwndz@gmail.com>
2015-08-19 00:22:04 +0800

    [DOC beta] Change to decorator-style CPs in RESTAdapter API doc samples
    [ci skip]

--- packages/ember-data/lib/adapters/rest-adapter.js
+++ packages/ember-data/lib/adapters/rest-adapter.js
@@ -150,12 +150,12 @@ import BuildURLMixin from "ember-data/adapters/build-url-mixin";
   import DS from 'ember-data';
 
   export default DS.RESTAdapter.extend({
-    headers: function() {
+    headers: Ember.computed('session.authToken', function() {
       return {
         "API_KEY": this.get("session.authToken"),
         "ANOTHER_HEADER": "Some header value"
       };
-    }.property("session.authToken")
+    })
   });
   ```
 
