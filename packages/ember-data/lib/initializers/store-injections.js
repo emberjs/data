8089fbbb43b45230db6c448ccd08aa37f64944fc
bmac <bmac325@gmail.com>
2015-08-20 12:41:51 -0400

    [BUGFIX beta] Prefer the new injection methods.
    This way Ember Data won't log deprecation warnings when https://github.com/emberjs/ember.js/pull/12157 lands in Ember.
    
    (cherry picked from commit c08f725d980d6b91547bf86de51e737cebc8e959)

--- packages/ember-data/lib/initializers/store-injections.js
+++ packages/ember-data/lib/initializers/store-injections.js
@@ -8,7 +8,7 @@
 export default function initializeStoreInjections(registry) {
   // registry.injection for Ember < 2.1.0
   // application.inject for Ember 2.1.0+
-  var inject = registry.injection || registry.inject;
+  var inject = registry.inject || registry.injection;
   inject.call(registry, 'controller', 'store', 'service:store');
   inject.call(registry, 'route', 'store', 'service:store');
   inject.call(registry, 'data-adapter', 'store', 'service:store');
