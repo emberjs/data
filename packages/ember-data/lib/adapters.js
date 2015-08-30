a090c98e9bf95889ce9b6e484ecec2c491ad2b37
greyhwndz <greyhwndz@gmail.com>
2015-08-30 11:44:02 +0800

    Remove the Fixture Adapter
    (cherry picked from commit 60b7ae7a94405e1dc0bf29b012ed8b02ca32b7f2)

--- packages/ember-data/lib/adapters.js
+++ packages/ember-data/lib/adapters.js
@@ -2,12 +2,10 @@
   @module ember-data
 */
 
-import FixtureAdapter from "ember-data/adapters/fixture-adapter";
 import JSONAPIAdapter from "ember-data/adapters/json-api-adapter";
 import RESTAdapter from "ember-data/adapters/rest-adapter";
 
 export {
-  FixtureAdapter,
   JSONAPIAdapter,
   RESTAdapter
 };
