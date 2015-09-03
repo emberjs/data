emberjs/data

commit b7f7b7a9dce7f126c045c65277da8dc53cdd1e8b

Use the correct modelClass and serializer to extract a polymorphic type when normalizing async relationships.
Closes #3547

packages/ember-data/lib/serializers/rest-serializer.js
@@ -149,7 +149,7 @@ var RESTSerializer = JSONSerializer.extend({
     let serializer = store.serializerFor(modelName);
 
     arrayHash.forEach((hash) => {
-      let { data, included } = serializer.normalize(modelClass, hash, prop);
+      let { data, included } = this._normalizePolymorphicRecord(store, hash, prop, modelClass, serializer);
       documentHash.data.push(data);
       if (included) {
         documentHash.included.push(...included);
@@ -159,7 +159,20 @@ var RESTSerializer = JSONSerializer.extend({
     return documentHash;
   },
 
-  /**
+  _normalizePolymorphicRecord(store, hash, prop, primaryModelClass, primarySerializer) {
+    let serializer, modelClass;
+    // Support polymorphic records in async relationships
+    if (hash.type && store._hasModelFor(this.modelNameFromPayloadKey(hash.type))) {
+      serializer = store.serializerFor(hash.type);
+      modelClass = store.modelFor(hash.type);
+    } else {
+      serializer = primarySerializer;
+      modelClass = primaryModelClass;
+    }
+    return serializer.normalize(modelClass, hash, prop);
+  },
+
+  /*
     @method _normalizeResponse
     @param {DS.Store} store
     @param {DS.Model} primaryModelClass
@@ -241,7 +254,7 @@ var RESTSerializer = JSONSerializer.extend({
         ```
        */
       if (isPrimary && Ember.typeOf(value) !== 'array') {
-        let {data, included} = this.normalize(primaryModelClass, value, prop);
+        let { data, included } = this._normalizePolymorphicRecord(store, value, prop, primaryModelClass, this);
         documentHash.data = data;
         if (included) {
           documentHash.included.push(...included);

packages/ember-data/tests/integration/serializers/rest-serializer-test.js
@@ -17,7 +17,9 @@ module("integration/serializer/rest - RESTSerializer", {
       superVillain: DS.belongsTo('super-villain', { async: false }),
       name:         DS.attr('string')
     });
-    YellowMinion = EvilMinion.extend();
+    YellowMinion = EvilMinion.extend({
+      eyes: DS.attr('number')
+    });
     DoomsdayDevice = DS.Model.extend({
       name:         DS.attr('string'),
       evilMinion:   DS.belongsTo('evil-minion', { polymorphic: true, async: true })
@@ -458,6 +460,81 @@ test('serializeIntoHash uses payloadKeyFromModelName to normalize the payload ro
     'home-planet': {
       name: "Umber"
     }
+  });
+});
+
+test('normalizeResponse with async polymorphic belongsTo', function() {
+  env.registry.register('serializer:application', DS.RESTSerializer.extend({
+    isNewSerializerAPI: true
+  }));
+  var store = env.store;
+  env.adapter.findRecord = () => {
+    return {
+      doomsdayDevices: [{
+        id: 1,
+        name: "DeathRay",
+        links: {
+          evilMinion: '/doomsday-device/1/evil-minion'
+        }
+      }]
+    };
+  };
+
+  env.adapter.findBelongsTo = () => {
+    return {
+      evilMinion: {
+        id: 1,
+        type: 'yellowMinion',
+        name: 'Alex',
+        eyes: 3
+      }
+    };
+  };
+  run(function() {
+    store.findRecord('doomsday-device', 1).then((deathRay) => {
+      return deathRay.get('evilMinion');
+    }).then((evilMinion) => {
+      equal(evilMinion.get('eyes'), 3);
+    });
+  });
+});
+
+test('normalizeResponse with async polymorphic hasMany', function() {
+  SuperVillain.reopen({ evilMinions: DS.hasMany('evil-minion', { async: true, polymorphic: true }) });
+  env.registry.register('serializer:application', DS.RESTSerializer.extend({
+    isNewSerializerAPI: true
+  }));
+  var store = env.store;
+  env.adapter.findRecord = () => {
+    return {
+      superVillains: [{
+        id: "1",
+        firstName: "Yehuda",
+        lastName: "Katz",
+        links: {
+          evilMinions: '/super-villain/1/evil-minions'
+        }
+      }]
+    };
+  };
+
+  env.adapter.findHasMany = () => {
+    return {
+      evilMinion: [{
+        id: 1,
+        type: 'yellowMinion',
+        name: 'Alex',
+        eyes: 3
+      }]
+    };
+  };
+  run(function() {
+    store.findRecord('super-villain', 1).then((superVillain) => {
+      return superVillain.get('evilMinions');
+    }).then((evilMinions) => {
+      ok(evilMinions.get('firstObject') instanceof YellowMinion);
+      equal(evilMinions.get('firstObject.eyes'), 3);
+    });
   });
 });
 

commit a117f018edea21e869bb6038aae671ad21f056df

[BUGFIX release-1-13] Disable polymorphic deserialization when a model expects a type attribute
b7f7b7a introduced a bug where if there is a type key in a payload that is part
of an array, it would be used for polymorphic deserialization even when the
model expects an attribute that is named "type".

Interestingly, for such payloads, `arrayHash` passed into `normalizeArray()` in
rest-serializer.js contains Ember.Object instances as opposed to plain objects.
This causes the code to throw, since `hash.type` would be a computed property
in that case. This is probably because of extending from JSONAPISerializer.

packages/ember-data/lib/serializers/rest-serializer.js
@@ -228,9 +228,10 @@ var RESTSerializer = JSONSerializer.extend({
 
     Ember.assert(`${this.toString()} has opted into the new serializer API and expects the ${serializer.toString()} it collaborates with to also support the new serializer API by setting its \`isNewSerializerAPI\` property to true.`, get(serializer, 'isNewSerializerAPI'));
 
+    const primaryHasTypeAttribute = get(modelClass, 'attributes').get('type');
     /*jshint loopfunc:true*/
     forEach.call(arrayHash, (hash) => {
-      let { data, included } = this._normalizePolymorphicRecord(store, hash, prop, modelClass, serializer);
+      let { data, included } = this._normalizePolymorphicRecord(store, hash, prop, modelClass, serializer, primaryHasTypeAttribute);
       documentHash.data.push(data);
       if (included) {
         documentHash.included.push(...included);
@@ -240,10 +241,10 @@ var RESTSerializer = JSONSerializer.extend({
     return documentHash;
   },
 
-  _normalizePolymorphicRecord(store, hash, prop, primaryModelClass, primarySerializer) {
+  _normalizePolymorphicRecord(store, hash, prop, primaryModelClass, primarySerializer, primaryHasTypeAttribute) {
     let serializer, modelClass;
     // Support polymorphic records in async relationships
-    if (hash.type && store._hasModelFor(this.modelNameFromPayloadKey(hash.type))) {
+    if (!primaryHasTypeAttribute && hash.type && store._hasModelFor(this.modelNameFromPayloadKey(hash.type))) {
       serializer = store.serializerFor(hash.type);
       modelClass = store.modelFor(hash.type);
     } else {

packages/ember-data/tests/integration/serializers/rest-serializer-test.js
@@ -1,5 +1,5 @@
 var get = Ember.get;
-var HomePlanet, league, SuperVillain, EvilMinion, YellowMinion, DoomsdayDevice, Comment, env;
+var HomePlanet, league, SuperVillain, EvilMinion, YellowMinion, DoomsdayDevice, Comment, Basket, env;
 var run = Ember.run;
 
 module("integration/serializer/rest - RESTSerializer", {
@@ -30,13 +30,18 @@ module("integration/serializer/rest - RESTSerializer", {
       root: DS.attr('boolean'),
       children: DS.hasMany('comment', { inverse: null, async: false })
     });
+    Basket = DS.Model.extend({
+      type: DS.attr('string'),
+      size: DS.attr('number')
+    });
     env = setupStore({
       superVillain:   SuperVillain,
       homePlanet:     HomePlanet,
       evilMinion:     EvilMinion,
       yellowMinion:   YellowMinion,
       doomsdayDevice: DoomsdayDevice,
-      comment:        Comment
+      comment:        Comment,
+      basket:         Basket
     });
     env.store.modelFor('super-villain');
     env.store.modelFor('home-planet');
@@ -44,6 +49,7 @@ module("integration/serializer/rest - RESTSerializer", {
     env.store.modelFor('yellow-minion');
     env.store.modelFor('doomsday-device');
     env.store.modelFor('comment');
+    env.store.modelFor('basket');
   },
 
   teardown: function() {
@@ -512,4 +518,29 @@ test("normalizeResponse can load secondary records of the same type without affe
       "relationships": {}
     }]
   });
+});
+
+test("don't polymorphically deserialize base on the type key in payload when a type attribute exist", function() {
+  env.registry.register('serializer:application', DS.RESTSerializer.extend({
+    isNewSerializerAPI: true
+  }));
+
+  run(function() {
+    env.restSerializer.normalizeArrayResponse(env.store, Basket, {
+      basket: [
+        env.store.createRecord('Basket', { type: 'bamboo', size: 10, id: '1' }),
+        env.store.createRecord('Basket', { type: 'yellowMinion', size: 10, id: '65536' })
+      ]
+    });
+  });
+
+  const normalRecord = env.store.peekRecord('basket', '1');
+  ok(normalRecord, "payload with type that doesn't exist");
+  strictEqual(normalRecord.get('type'), 'bamboo');
+  strictEqual(normalRecord.get('size'), 10);
+
+  const clashingRecord = env.store.peekRecord('basket', '65536');
+  ok(clashingRecord, 'payload with type that matches another model name');
+  strictEqual(clashingRecord.get('type'), 'yellowMinion');
+  strictEqual(clashingRecord.get('size'), 10);
 });