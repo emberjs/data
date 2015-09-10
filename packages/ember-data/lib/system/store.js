0d3eeaeea528f8d7f5bb024bbcb16d0761a7be17
Mina Smart <mina.smart@thescore.com>
2015-09-10 10:49:19 -0400

    [DOC release] Remove normalizePayload, associated docs, and mention of functionality.
    - Fixes [#3750](https://github.com/emberjs/data/issues/3750)
    
    (cherry picked from commit dcd77f2cc6554a181481a3ca4d4f12d5320df2db)

--- packages/ember-data/lib/system/store.js
+++ packages/ember-data/lib/system/store.js
@@ -1690,9 +1690,6 @@ Store = Service.extend({
 
     Alternatively, `pushPayload` will accept a model type which
     will determine which serializer will process the payload.
-    However, the serializer itself (processing this data via
-    `normalizePayload`) will not know which model it is
-    deserializing.
 
     ```app/serializers/application.js
     import DS from 'ember-data';
