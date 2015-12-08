f58ffe442dbbd26a9cc71b53d153c5bd56d617c6
pangratz <cmueller.418@gmail.com>
2015-12-08 20:22:45 +0100

    Add possibility to run tests for feature flags via `ember test`
    Optional features and the corresponding tests can now be enabled within
    the tests by specifying the `test-optional-features` environment:
    
        ember test --environment=test-optional-features

--- config/environment.js
+++ config/environment.js
@@ -1,13 +1,23 @@
 'use strict';
 
+var fs = require('fs');
+var featuresJson = fs.readFileSync('config/features.json', { encoding: 'utf8' });
+var featureFlags = JSON.parse(featuresJson);
+
 module.exports = function(environment, appConfig) {
   var ENV = { };
 
-  if (environment === 'testing') {
-    ENV.EmberENV = {
-      RAISE_ON_DEPRECATION: true,
-      ENABLE_DS_FILTER: true
-    };
+  ENV.EmberENV = {
+    FEATURES: featureFlags,
+    ENABLE_DS_FILTER: true,
+
+    // don't raise on deprecation yet, since there are too many thrown errors;
+    // this should be addressed in another PR
+    // RAISE_ON_DEPRECATION: true
+  };
+
+  if (environment === 'test-optional-features') {
+    ENV.EmberENV.ENABLE_OPTIONAL_FEATURES = true;
   }
 
   return ENV;
