446d65f7ebbb6b34a3dc50c24a5d992889d581a1
Robert Jackson <robert.w.jackson@me.com>
2015-12-14 15:39:37 -0500

    [BUGFIX beta] Use `bower` version of ember-data if present.
    In order to keep backwards compat, we must honor the version of
    ember-data that is present in `bower.json` (even though it is not
    needed). This is due to the fact that many people update the
    `package.json` and `bower.json` versions of Ember Data completely
    independently.
    
    If `bower.json` includes `ember-data` a warning will be issued, and that
    version will be used.

--- index.js
+++ index.js
@@ -4,10 +4,50 @@
 module.exports = {
   name: 'ember-data',
 
+  _warn: function(message) {
+    var chalk = require('chalk');
+    var warning = chalk.yellow('WARNING: ' + message);
+
+    if (this.ui && this.ui.writeWarnLine) {
+      this.ui.writeWarnLine(message);
+    } else if (this.ui) {
+      this.ui.writeLine(warning);
+    } else {
+      console.log(warning);
+    }
+  },
+
+  init: function() {
+    var bowerDeps = this.project.bowerDependencies();
+
+    if (bowerDeps['ember-data']) {
+      this._warn('Please remove `ember-data` from `bower.json`. As of Ember Data 2.3.0, only the NPM package is needed.');
+      this._forceBowerUsage = true;
+    } else {
+      this._forceBowerUsage = false;
+    }
+  },
+
   treeForAddon: function(dir) {
+    if (this._forceBowerUsage) {
+      // Fakes an empty broccoli tree
+      return { inputTree: dir, rebuild: function() { return []; } };
+    }
+
     var version      = require('./lib/version');
     var merge        = require('broccoli-merge-trees');
 
     return this._super.treeForAddon.call(this, merge([version(), dir]));
+  },
+
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+
+    if (this._forceBowerUsage) {
+      this.app.import({
+        development: app.bowerDirectory + '/ember-data/ember-data.js',
+        production: app.bowerDirectory + '/ember-data/ember-data.prod.js'
+      });
+    }
   }
 };
