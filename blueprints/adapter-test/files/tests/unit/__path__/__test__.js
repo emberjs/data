f4932bdae1c40c5cdf41595f98afa8194e6c2d01
Robert Jackson <robert.w.jackson@me.com>
2015-12-14 21:47:22 -0500

    Update blueprints to match ember-cli 2.2.0-beta.1.
    (cherry picked from commit 304f0176b2b9fd434d50ebc2c440c57d602d75aa)

--- blueprints/adapter-test/files/tests/unit/__path__/__test__.js
+++ blueprints/adapter-test/files/tests/unit/__path__/__test__.js
@@ -1,12 +1,12 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('transform:<%= dasherizedModuleName %>', '<%= friendlyTestDescription %>', {
+moduleFor('adapter:<%= dasherizedModuleName %>', '<%= friendlyTestDescription %>', {
   // Specify the other units that are required for this test.
   // needs: ['serializer:foo']
 });
 
 // Replace this with your real tests.
 test('it exists', function(assert) {
-  let transform = this.subject();
-  assert.ok(transform);
+  let adapter = this.subject();
+  assert.ok(adapter);
 });
