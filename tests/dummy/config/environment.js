400b98473ebeeea2c7101b4e99e1a6c5c1a5a74c
Robert Jackson <robert.w.jackson@me.com>
2015-12-14 21:15:27 -0500

    Ensure `config/features.json` works with different cwd.
    (cherry picked from commit 3446090ff474dc1601025c0b7659d45c6ba7c5bf)
    
    Conflicts:
    	tests/dummy/config/environment.js

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -1,5 +1,11 @@
 /* jshint node: true */
 
+var fs = require('fs');
+var path = require('path');
+var featuresJsonPath = path.join(__dirname, '../../../config/features.json');
+var featuresJson = fs.readFileSync(featuresJsonPath, { encoding: 'utf8' });
+var featureFlags = JSON.parse(featuresJson);
+
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
