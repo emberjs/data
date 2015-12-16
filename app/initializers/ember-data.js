040398e58b351147d1424f128fb458349017db62
bmac <bmac325@gmail.com>
2015-12-16 15:26:06 -0500

    Only run the Ember.onload initializers in globals mode

--- app/initializers/ember-data.js
+++ app/initializers/ember-data.js
@@ -1,5 +1,37 @@
 import setupContainer from 'ember-data/-private/setup-container';
 
+/*
+
+  This code initializes Ember-Data onto an Ember application.
+
+  If an Ember.js developer defines a subclass of DS.Store on their application,
+  as `App.StoreService` (or via a module system that resolves to `service:store`)
+  this code will automatically instantiate it and make it available on the
+  router.
+
+  Additionally, after an application's controllers have been injected, they will
+  each have the store made available to them.
+
+  For example, imagine an Ember.js application with the following classes:
+
+  App.StoreService = DS.Store.extend({
+    adapter: 'custom'
+  });
+
+  App.PostsController = Ember.ArrayController.extend({
+    // ...
+  });
+
+  When the application is initialized, `App.ApplicationStore` will automatically be
+  instantiated, and the instance of `App.PostsController` will have its `store`
+  property set to that instance.
+
+  Note that this code will only be run if the `ember-application` package is
+  loaded. If Ember Data is being used in an environment other than a
+  typical application (e.g., node.js where only `ember-runtime` is available),
+  this code will be ignored.
+*/
+
 export default {
   name: 'ember-data',
   initialize: setupContainer
