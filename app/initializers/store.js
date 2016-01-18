51bc6f7367250b13b04cd11f226e32779233d522
Christoffer Persson <christoffer@valdoo.se>
2016-01-18 19:01:45 +0100

    [BUFGIX release] Fix regression with missing initializers
    (cherry picked from commit 08e4f12426812816cdb58ce3886c100555892d4e)

---
+++ app/initializers/store.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+/*
+  This initializer is here to keep backwards compatibility with code depending
+  on the `store` initializer (before Ember Data was an addon).
+
+  Should be removed for Ember Data 3.x
+*/
+
+export default {
+  name: 'store',
+  after: 'ember-data',
+  initialize: Ember.K
+};
