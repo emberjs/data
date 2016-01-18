08e4f12426812816cdb58ce3886c100555892d4e
Christoffer Persson <christoffer@valdoo.se>
2016-01-18 19:01:45 +0100

    [BUFGIX release] Fix regression with missing initializers

---
+++ app/initializers/transforms.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+/*
+  This initializer is here to keep backwards compatibility with code depending
+  on the `transforms` initializer (before Ember Data was an addon).
+
+  Should be removed for Ember Data 3.x
+*/
+
+export default {
+  name: 'transforms',
+  before: 'store',
+  initialize: Ember.K
+};
