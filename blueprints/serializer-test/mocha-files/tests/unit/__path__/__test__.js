d70afb01d0f1a0b1919253d5eb5ce3d7ab8c39bb
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-02-17 15:24:36 +0100

    [BUGFIX beta] Add test blueprints from "ember-cli-mocha"
    The filesPath() method will automatically figure out if "qunit" or "mocha" files should be used by looking at the project packages.
    
    (cherry picked from commit 7dc3218a72d7005406a46a54296f5bcd77d1cf51)

---
+++ blueprints/serializer-test/mocha-files/tests/unit/__path__/__test__.js
@@ -0,0 +1,22 @@
+/* jshint expr:true */
+import { expect } from 'chai';
+import { describeModel, it } from 'ember-mocha';
+
+describeModel(
+  '<%= dasherizedModuleName %>',
+  '<%= friendlyTestDescription %>',
+  {
+    // Specify the other units that are required for this test.
+    needs: ['serializer:<%= dasherizedModuleName %>']
+  },
+  function() {
+    // Replace this with your real tests.
+    it('serializes records', function() {
+      let record = this.subject();
+
+      let serializedRecord = record.serialize();
+
+      expect(serializedRecord).to.be.ok;
+    });
+  }
+);
