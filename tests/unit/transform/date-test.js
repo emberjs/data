8d08d60e68c8c3feb4391a67ad69030aa5e88be5
zigahertz <zach.zibrat@locusenergy.com>
2016-03-28 11:48:07 -0700

    adds deprecation warning and returns native Date.parse method

--- tests/unit/transform/date-test.js
+++ tests/unit/transform/date-test.js
@@ -1,14 +1,16 @@
-import Ember from 'ember';
-
 import {module, test} from 'qunit';
 
 import DS from 'ember-data';
+import Ember from 'ember';
+
+import testInDebug from 'dummy/tests/helpers/test-in-debug';
 
 module("unit/transform - DS.DateTransform");
 
 var dateString = "2015-01-01T00:00:00.000Z";
-var dateInMillis = Ember.Date.parse(dateString);
+var dateInMillis = Date.parse(dateString);
 var date = new Date(dateInMillis);
+var run = Ember.run;
 
 test("#serialize", function(assert) {
   var transform = new DS.DateTransform();
@@ -34,4 +36,12 @@ test("#deserialize", function(assert) {
   // from none
   assert.equal(transform.deserialize(null), null);
   assert.equal(transform.deserialize(undefined), null);
+});
+
+testInDebug('Ember.Date.parse has been deprecated', function(assert) {
+  run(function() {
+    assert.expectDeprecation(function() {
+      Ember.Date.parse(dateString);
+    }, /Ember.Date.parse is deprecated/);
+  });
 });
