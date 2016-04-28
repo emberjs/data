1f26efb508209b8242f14d5eab4a4e4c721de342
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-04-28 18:53:32 +0200

    ESLint: Enable equivalent rules from ".jscsrc"

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,5 +25,35 @@ module.exports = {
     'no-irregular-whitespace': 'error',
     'no-undef': 'error',
     'no-eq-null': 'error',
+
+    // from JSCS
+    'array-bracket-spacing': ['error', 'never'],
+    'comma-style': ['error', 'last'],
+    'brace-style': ['error', '1tbs', {
+      'allowSingleLine': true,
+    }],
+    'no-spaced-func': 'error',
+    'no-empty': 'error',
+    'curly': ['error', 'all'],
+    'eol-last': 'error',
+    'no-trailing-spaces': 'error',
+    'comma-dangle': ['error', 'never'],
+    'space-before-blocks': ['error', 'always'],
+    'indent': ['error', 2, {
+      'SwitchCase': 1,
+    }],
+    'keyword-spacing': ['error', {
+      'overrides': {
+        'else': {
+          'before': true,
+        },
+        'while': {
+          'before': true,
+        },
+        'catch': {
+          'before': true,
+        },
+      },
+    }],
   },
 };
