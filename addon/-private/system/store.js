601a418e33cba91f2e937233703be09b48989378
bmac <bmac325@gmail.com>
2016-07-25 11:36:06 -0400

    [BUGFIX beta] Update API docs to import DS from 'ember-data'
    (cherry picked from commit 5d3c1b1de309bc2462d03d204d25b22e2102e00c)

--- addon/-private/system/store.js
+++ addon/-private/system/store.js
@@ -1127,9 +1127,9 @@ Store = Service.extend({
 
     ```javascript
     // app/adapters/user.js
-    import Adapter from "ember-data/adapter";
+    import DS from "ember-data";
 
-    export default Adapter.extend({
+    export default DS.Adapter.extend({
       queryRecord(modelName, query) {
         return Ember.$.getJSON("/api/current_user");
       }
