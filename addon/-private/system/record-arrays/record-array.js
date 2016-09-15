a892a1d25ce7e37dc5516d406279d5095502f34b
Chris <christopher.kong.lam@gmail.com>
2016-09-15 16:12:49 -0700

    Update error message with suggestion to use toArray()

--- addon/-private/system/record-arrays/record-array.js
+++ addon/-private/system/record-arrays/record-array.js
@@ -85,7 +85,7 @@ export default Ember.ArrayProxy.extend(Ember.Evented, {
 
   replace() {
     var type = get(this, 'type').toString();
-    throw new Error("The result of a server query (for all " + type + " types) is immutable.");
+    throw new Error("The result of a server query (for all " + type + " types) is immutable. To modify contents, use toArray()");
   },
 
   /**
