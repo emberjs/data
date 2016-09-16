c69a338ec84bc2ddf172c90f3be687e87e75498e
Chris Thoburn <runspired@users.noreply.github.com>
2016-09-15 19:24:35 -0700

    bump to latest ember-cli, run ember init, cleanup post init (#4536)
    (cherry picked from commit 523e87fc4427cdcdad90df9e98089ee5f94e63c0)

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,13 +1,12 @@
 import Ember from 'ember';
 import Application from '../../app';
 import config from '../../config/environment';
-var assign = Ember.assign || Ember.merge;
 
 export default function startApp(attrs) {
   let application;
 
-  let attributes = assign({}, config.APP);
-  attributes = assign(attributes, attrs); // use defaults, but you can override;
+  let attributes = Ember.merge({}, config.APP);
+  attributes = Ember.merge(attributes, attrs); // use defaults, but you can override;
 
   Ember.run(() => {
     application = Application.create(attributes);
