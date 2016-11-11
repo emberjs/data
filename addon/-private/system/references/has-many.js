ae7e8ab308f91efbcc598c7d06ab0409b9e2c23b
bmac <bmac325@gmail.com>
2016-11-11 14:56:26 -0500

    Add API docs for the HasManyReference

--- addon/-private/system/references/has-many.js
+++ addon/-private/system/references/has-many.js
@@ -13,6 +13,13 @@ const {
   get
 } = Ember;
 
+/**
+   A HasManyReference is a low level API that allows users and addon
+   author to perform meta-operations on a has-many relationship.
+
+   @class HasManyReference
+   @namespace DS
+*/
 var HasManyReference = function(store, parentInternalModel, hasManyRelationship) {
   this._super$constructor(store, parentInternalModel);
   this.hasManyRelationship = hasManyRelationship;
@@ -26,6 +33,45 @@ HasManyReference.prototype = Object.create(Reference.prototype);
 HasManyReference.prototype.constructor = HasManyReference;
 HasManyReference.prototype._super$constructor = Reference;
 
+/**
+   This returns a string that represents how the reference will be
+   looked up when it is loaded. If the relationship has a link it will
+   use the "link" otherwise it defaults to "id".
+
+   Example
+
+   ```app/models/post.js
+   export default DS.Model.extend({
+     comments: DS.hasMany({ async: true })
+   });
+   ```
+
+   ```javascript
+   var post = store.push({
+     data: {
+       type: 'post',
+       id: 1,
+       relationships: {
+         comments: {
+           data: [{ type: 'comment', id: 1 }]
+         }
+       }
+     }
+   });
+
+   var commentsRef = post.hasMany('comments');
+
+   // get the identifier of the reference
+   if (commentsRef.remoteType() === "ids") {
+     var ids = commentsRef.ids();
+   } else if (commentsRef.remoteType() === "link") {
+     var link = commentsRef.link();
+   }
+   ```
+
+   @method remoteType
+   @return {String} The name of the remote type. This should either be "link" or "ids"
+*/
 HasManyReference.prototype.remoteType = function() {
   if (this.hasManyRelationship.link) {
     return "link";
@@ -34,10 +80,77 @@ HasManyReference.prototype.remoteType = function() {
   return "ids";
 };
 
+/**
+   The link Ember Data will use to fetch or reload this has-many
+   relationship.
+
+   Example
+
+   ```app/models/post.js
+   export default DS.Model.extend({
+     comments: DS.hasMany({ async: true })
+   });
+   ```
+
+   ```javascript
+   var post = store.push({
+     data: {
+       type: 'post',
+       id: 1,
+       relationships: {
+         comments: {
+           links: {
+             related: '/posts/1/comments'
+           }
+         }
+       }
+     }
+   });
+
+   var commentsRef = post.hasMany('comments');
+
+   commentsRef.link(); // '/posts/1/comments'
+   ```
+
+   @method link
+   @return {String} The link Ember Data will use to fetch or reload this has-many relationship.
+*/
 HasManyReference.prototype.link = function() {
   return this.hasManyRelationship.link;
 };
 
+/**
+   `ids()` returns an array of the record ids in this relationship.
+
+   Example
+
+   ```app/models/post.js
+   export default DS.Model.extend({
+     comments: DS.hasMany({ async: true })
+   });
+   ```
+
+   ```javascript
+   var post = store.push({
+     data: {
+       type: 'post',
+       id: 1,
+       relationships: {
+         comments: {
+           data: [{ type: 'comment', id: 1 }]
+         }
+       }
+     }
+   });
+
+   var commentsRef = post.hasMany('comments');
+
+   commentsRef.ids(); // ['1']
+   ```
+
+   @method remoteType
+   @return {Array} The ids in this has-many relationship
+*/
 HasManyReference.prototype.ids = function() {
   let members = this.hasManyRelationship.members.toArray();
 
@@ -46,10 +159,92 @@ HasManyReference.prototype.ids = function() {
   });
 };
 
+/**
+   The link Ember Data will use to fetch or reload this has-many
+   relationship.
+
+   Example
+
+   ```app/models/post.js
+   export default DS.Model.extend({
+     comments: DS.hasMany({ async: true })
+   });
+   ```
+
+   ```javascript
+   var post = store.push({
+     data: {
+       type: 'post',
+       id: 1,
+       relationships: {
+         comments: {
+           links: {
+             related: {
+               href: '/posts/1/comments',
+               meta: {
+                 count: 10
+               }
+             }
+           }
+         }
+       }
+     }
+   });
+
+   var commentsRef = post.hasMany('comments');
+
+   commentsRef.meta(); // { count: 10 }
+   ```
+
+   @method meta
+   @return {Object} The meta information for the has-many relationship.
+*/
 HasManyReference.prototype.meta = function() {
   return this.hasManyRelationship.meta;
 };
 
+/**
+   `push` can be used to update the data in the relationship and Ember
+   Data will treat the new data as the canonical value of this
+   relationship on the backend.
+
+   Example
+
+   ```app/models/post.js
+   export default DS.Model.extend({
+     comments: DS.hasMany({ async: true })
+   });
+   ```
+
+   ```
+   var post = store.push({
+     data: {
+       type: 'post',
+       id: 1,
+       relationships: {
+         comments: {
+           data: [{ type: 'comment', id: 1 }]
+         }
+       }
+     }
+   });
+
+   var commentsRef = post.hasMany('comments');
+
+   commentsRef.ids(); // ['1']
+
+   commentsRef.push([
+     [{ type: 'comment', id: 2 }],
+     [{ type: 'comment', id: 3 }],
+   ])
+
+   commentsRef.ids(); // ['2', '3']
+   ```
+
+   @method push
+   @param {Array|Promise} objectOrPromise a promise that resolves to a JSONAPI document object describing the new value of this relationship.
+   @return {DS.ManyArray}
+*/
 HasManyReference.prototype.push = function(objectOrPromise) {
   return resolve(objectOrPromise).then((payload) => {
     var array = payload;
@@ -121,6 +316,44 @@ HasManyReference.prototype._isLoaded = function() {
   });
 };
 
+/**
+   `value()` sycronously returns the current value of the has-many
+    relationship. Unlike `record.get('relationshipName')`, calling
+    `value()` on a reference does not trigger a fetch if the async
+    relationship is not yet loaded. If the relationship is not loaded
+    it will always return `null`.
+
+   Example
+
+   ```app/models/post.js
+   export default DS.Model.extend({
+     comments: DS.hasMany({ async: true })
+   });
+   ```
+
+   ```javascript
+   var post = store.push({
+     data: {
+       type: 'post',
+       id: 1,
+       relationships: {
+         comments: {
+           data: [{ type: 'comment', id: 1 }]
+         }
+       }
+     }
+   });
+
+   var commentsRef = post.hasMany('comments');
+
+   post.get('comments').then(function(comments) {
+     commentsRef.value() === comments
+   })
+   ```
+
+   @method value
+   @return {DS.ManyArray}
+*/
 HasManyReference.prototype.value = function() {
   if (this._isLoaded()) {
     return this.hasManyRelationship.getManyArray();
@@ -129,6 +362,43 @@ HasManyReference.prototype.value = function() {
   return null;
 };
 
+/**
+   Loads the relationship if it is not already loaded.  If the
+   relationship is already loaded this method does not trigger a new
+   load.
+
+   Example
+
+   ```app/models/post.js
+   export default DS.Model.extend({
+     comments: DS.hasMany({ async: true })
+   });
+   ```
+
+   ```javascript
+   var post = store.push({
+     data: {
+       type: 'post',
+       id: 1,
+       relationships: {
+         comments: {
+           data: [{ type: 'comment', id: 1 }]
+         }
+       }
+     }
+   });
+
+   var commentsRef = post.hasMany('comments');
+
+   commentsRef.load().then(function(comments) {
+     //...
+   });
+   ```
+
+   @method load
+   @return {Promise} a promise that resolves with the ManyArray in
+   this has-many relationship.
+*/
 HasManyReference.prototype.load = function() {
   if (!this._isLoaded()) {
     return this.hasManyRelationship.getRecords();
@@ -137,6 +407,40 @@ HasManyReference.prototype.load = function() {
   return resolve(this.hasManyRelationship.getManyArray());
 };
 
+/**
+   Reloads this has-many relationship.
+
+   Example
+
+   ```app/models/post.js
+   export default DS.Model.extend({
+     comments: DS.hasMany({ async: true })
+   });
+   ```
+
+   ```javascript
+   var post = store.push({
+     data: {
+       type: 'post',
+       id: 1,
+       relationships: {
+         comments: {
+           data: [{ type: 'comment', id: 1 }]
+         }
+       }
+     }
+   });
+
+   var commentsRef = post.hasMany('comments');
+
+   commentsRef.reload().then(function(comments) {
+     //...
+   });
+   ```
+
+   @method reload
+   @return {Promise} a promise that resolves with the ManyArray in this has-many relationship.
+*/
 HasManyReference.prototype.reload = function() {
   return this.hasManyRelationship.reload();
 };
