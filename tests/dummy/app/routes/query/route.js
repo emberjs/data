3a31d0a788cde5c7e31574019d67970330e20e09
Chris Thoburn <cthoburn@linkedin.com>
2016-12-21 01:46:07 -0800

    more instrumentation things

--- tests/dummy/app/routes/query/route.js
+++ tests/dummy/app/routes/query/route.js
@@ -24,6 +24,10 @@ export default Route.extend({
     let modelName = params.modelName;
     delete params.modelName;
 
+    // Once heimdall 0.4.x is released we can uncomment this
+    // to have nice timeline and trace events integration
+    // heimdall.enableTimelineFeatures();
+
     let token = heimdall.start('ember-data');
     return this.get('store').query(modelName, params)
       .then((records) => {
