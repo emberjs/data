9302f6d8a6abc6b4edf9e8b19b046d086630ffad
Mateusz Nowak <kontakt@estshy.pl>
2017-01-19 15:28:59 +0100

    Update store.js

--- addon/-private/system/store.js
+++ addon/-private/system/store.js
@@ -1783,7 +1783,7 @@ Store = Service.extend({
     @return {boolean}
   */
   recordIsLoaded(modelName, id) {
-    deprecate(`Use of recordIsLoaded is deprecated, use hasRecordForId instead.`, true, {
+    deprecate(`Use of recordIsLoaded is deprecated, use hasRecordForId instead.`, false, {
       id: 'ds.store.recordIsLoaded',
       until: '3.0'
     });
@@ -2617,7 +2617,7 @@ Store = Service.extend({
   },
 
   lookupAdapter(name) {
-    deprecate(`Use of lookupAdapter is deprecated, use adapterFor instead.`, true, {
+    deprecate(`Use of lookupAdapter is deprecated, use adapterFor instead.`, false, {
       id: 'ds.store.lookupAdapter',
       until: '3.0'
     });
@@ -2625,7 +2625,7 @@ Store = Service.extend({
   },
 
   lookupSerializer(name) {
-    deprecate(`Use of lookupSerializer is deprecated, use serializerFor instead.`, true, {
+    deprecate(`Use of lookupSerializer is deprecated, use serializerFor instead.`, false, {
       id: 'ds.store.lookupSerializer',
       until: '3.0'
     });
