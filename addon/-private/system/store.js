ffe093bb9b54dabf1f915c1c7ecb121831513160
Mateusz Nowak <kontakt@estshy.pl>
2017-01-23 15:08:05 +0100

    Update deprecate arguments (#4760)
    * Update deprecate arguments
    
    According to docs (https://github.com/emberjs/ember.js/blob/v2.10.0/packages/ember-debug/lib/deprecate.js#L97) ember deprecate requires 3 arguments.
    
    * Update store.js
    
    (cherry picked from commit 0c26d2df8c1baf7cb5099c3fe5ae90d1feade681)

--- addon/-private/system/store.js
+++ addon/-private/system/store.js
@@ -1781,7 +1781,7 @@ Store = Service.extend({
     @return {boolean}
   */
   recordIsLoaded(modelName, id) {
-    deprecate(`Use of recordIsLoaded is deprecated, use hasRecordForId instead.`, {
+    deprecate(`Use of recordIsLoaded is deprecated, use hasRecordForId instead.`, false, {
       id: 'ds.store.recordIsLoaded',
       until: '3.0'
     });
@@ -2593,7 +2593,7 @@ Store = Service.extend({
   },
 
   lookupAdapter(name) {
-    deprecate(`Use of lookupAdapter is deprecated, use adapterFor instead.`, {
+    deprecate(`Use of lookupAdapter is deprecated, use adapterFor instead.`, false, {
       id: 'ds.store.lookupAdapter',
       until: '3.0'
     });
@@ -2601,7 +2601,7 @@ Store = Service.extend({
   },
 
   lookupSerializer(name) {
-    deprecate(`Use of lookupSerializer is deprecated, use serializerFor instead.`, {
+    deprecate(`Use of lookupSerializer is deprecated, use serializerFor instead.`, false, {
       id: 'ds.store.lookupSerializer',
       until: '3.0'
     });
