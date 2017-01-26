6e1ee33112e35cd37b97fa8f37054e4324d4f38a
Tom Lagier <lagierta@gmail.com>
2017-01-26 12:02:18 -0800

    [BUGFIX beta] Ensure ISO-8601 regex correctly matches timestamps  (#4771)
    * [BUGFIX beta] Ensure ISO-8601 regex correctly matches timestamps with a timezone offset
    
    * Wrap Date prototype stubbing in try/finally to ensure it always reverts after tests complete
    
    (cherry picked from commit 3506441354dc849369a3168f03b47793c94c8422)

--- addon/-private/ext/date.js
+++ addon/-private/ext/date.js
@@ -31,7 +31,7 @@ export const parseDate = function (date) {
   // before falling back to any implementation-specific date parsing, so that’s what we do, even if native
   // implementations could be faster
   //              1 YYYY                2 MM       3 DD           4 HH    5 mm       6 ss        7 msec        8 Z 9 ±    10 tzHH    11 tzmm
-  if ((struct = /^(\d{4}|[+\-]\d{6})(?:-(\d{2})(?:-(\d{2}))?)?(?:T(\d{2}):(\d{2})(?::(\d{2})(?:\.(\d{3}))?)?(?:(Z)|([+\-])(\d{2})(?:(\d{2}))?)?)?$/.exec(date))) {
+  if ((struct = /^(\d{4}|[+\-]\d{6})(?:-(\d{2})(?:-(\d{2}))?)?(?:T(\d{2}):(\d{2})(?::(\d{2})(?:\.(\d{3}))?)?(?:(Z)|([+\-])(\d{2}):?(?:(\d{2}))?)?)?$/.exec(date))) {
     // avoid NaN timestamps caused by “undefined” values being passed to Date.UTC
     for (var i = 0, k; (k = numericKeys[i]); ++i) {
       struct[k] = +struct[k] || 0;
