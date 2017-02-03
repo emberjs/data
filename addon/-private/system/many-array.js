d94a2994d2f12acacdfc910e87720975dbbe134c
Chris Thoburn <cthoburn@linkedin.com>
2017-02-03 11:42:02 -0800

    don't remove unpersisted deletes

--- addon/-private/system/many-array.js
+++ addon/-private/system/many-array.js
@@ -145,7 +145,7 @@ export default Ember.Object.extend(Ember.MutableArray, Ember.Evented, {
     // TODO make this smarter, currently its plenty stupid
     // TODO this filtering was re-introduced as a bugfix, but seems unneeded in 2.13
     // with the changes to internalModel cleanup in that version.
-    let toSet = this.canonicalState.filter((internalModel) => !internalModel.isDeleted());
+    let toSet = this.canonicalState.filter((internalModel) => internalModel.currentState.stateName !== 'root.deleted.saved');
 
     //a hack for not removing new records
     //TODO remove once we have proper diffing
