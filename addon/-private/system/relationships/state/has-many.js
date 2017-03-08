ac354276d1e342784c3d54ba74dd1d60eeadc180
Stefan Penner <stefan.penner@gmail.com>
2017-03-07 21:16:29 -0800

    Cleanup (#4841)
    * cleanup system/relationship/has-many
    
    * cleanup system/relationships/state/relationship
    
    * cleanup system/relationship/state/has-many

--- addon/-private/system/relationships/state/has-many.js
+++ addon/-private/system/relationships/state/has-many.js
@@ -1,10 +1,10 @@
-import { assert } from "ember-data/-private/debug";
-import { PromiseManyArray, promiseManyArray } from "ember-data/-private/system/promise-proxies";
-import Relationship from "ember-data/-private/system/relationships/state/relationship";
-import OrderedSet from "ember-data/-private/system/ordered-set";
-import ManyArray from "ember-data/-private/system/many-array";
+import { assert } from 'ember-data/-private/debug';
+import { PromiseManyArray, promiseManyArray } from 'ember-data/-private/system/promise-proxies';
+import Relationship from 'ember-data/-private/system/relationships/state/relationship';
+import OrderedSet from 'ember-data/-private/system/ordered-set';
+import ManyArray from 'ember-data/-private/system/many-array';
 
-import { assertPolymorphicType } from "ember-data/-private/debug";
+import { assertPolymorphicType } from 'ember-data/-private/debug';
 
 export default class ManyRelationship extends Relationship {
   constructor(store, record, inverseKey, relationshipMeta) {
@@ -74,7 +74,7 @@ export default class ManyRelationship extends Relationship {
   }
 
   removeCanonicalRecordFromOwn(record, idx) {
-    var i = idx;
+    let i = idx;
     if (!this.canonicalMembers.has(record)) {
       return;
     }
@@ -141,7 +141,7 @@ export default class ManyRelationship extends Relationship {
     let recordsToRemove = [];
     let recordSet = setForArray(records);
 
-    members.forEach(function(member) {
+    members.forEach(member => {
       if (recordSet.has(member)) { return; }
 
       recordsToRemove.push(member);
@@ -157,7 +157,7 @@ export default class ManyRelationship extends Relationship {
   }
 
   fetchLink() {
-    return this.store.findHasMany(this.record, this.link, this.relationshipMeta).then((records) => {
+    return this.store.findHasMany(this.record, this.link, this.relationshipMeta).then(records => {
       if (records.hasOwnProperty('meta')) {
         this.updateMeta(records.meta);
       }
@@ -212,7 +212,7 @@ export default class ManyRelationship extends Relationship {
       });
       return this._loadingPromise;
     } else {
-      assert("You looked up the '" + this.key + "' relationship on a '" + this.record.type.modelName + "' with id " + this.record.id +  " but some of the associated records were not loaded. Either make sure they are all loaded together with the parent record, or specify that the relationship is async (`DS.hasMany({ async: true })`)", manyArray.isEvery('isEmpty', false));
+      assert(`You looked up the '${this.key}' relationship on a '${this.record.type.modelName}' with id ${this.record.id} but some of the associated records were not loaded. Either make sure they are all loaded together with the parent record, or specify that the relationship is async ('DS.hasMany({ async: true })')`, manyArray.isEvery('isEmpty', false));
 
       //TODO(Igor) WTF DO I DO HERE?
       // TODO @runspired equal WTFs to Igor
