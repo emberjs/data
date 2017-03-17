d8884c0e3c66358e415f288931e16a2552a7be91
Robert Jackson <rjackson@linkedin.com>
2017-03-17 07:49:06 -0400

    Update test suite to test 2.4 and 2.8 LTS's.

--- config/ember-try.js
+++ config/ember-try.js
@@ -16,6 +16,37 @@ module.exports = {
         resolutions: {
           'ember': '1.13.13'
         }
+      },
+      npm: {
+        'ember-source': null
+      }
+    },
+    {
+      name: 'ember-2-4',
+      bower: {
+        dependencies: {
+          'ember': '~2.4.0'
+        },
+        resolutions: {
+          'ember': '~2.4.0'
+        }
+      },
+      npm: {
+        'ember-source': null
+      }
+    },
+    {
+      name: 'ember-2-8',
+      bower: {
+        dependencies: {
+          'ember': '~2.8.0'
+        },
+        resolutions: {
+          'ember': '~2.8.0'
+        }
+      },
+      npm: {
+        'ember-source': null
       }
     },
     {
@@ -27,6 +58,9 @@ module.exports = {
         resolutions: {
           'ember': 'release'
         }
+      },
+      npm: {
+        'ember-source': null
       }
     },
     {
@@ -38,6 +72,9 @@ module.exports = {
         resolutions: {
           'ember': 'beta'
         }
+      },
+      npm: {
+        'ember-source': null
       }
     },
     {
@@ -49,6 +86,9 @@ module.exports = {
         resolutions: {
           'ember': 'canary'
         }
+      },
+      npm: {
+        'ember-source': null
       }
     },
     {
@@ -60,6 +100,9 @@ module.exports = {
         resolutions: {
           'ember': 'alpha'
         }
+      },
+      npm: {
+        'ember-source': null
       }
     }
   ]
