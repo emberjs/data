7ed1c8a09d0e0737d0b425010bdba9a6715f7c9e
Stefan Penner <stefan.penner@gmail.com>
2017-04-26 10:56:36 -0700

    remove needless change events when creating a liveRecordArray

--- addon/-private/system/store.js
+++ addon/-private/system/store.js
@@ -1652,9 +1652,7 @@ Store = Service.extend({
   */
   _didUpdateAll(modelName) {
     heimdall.increment(_didUpdateAll);
-    let liveRecordArray = this.recordArrayManager.liveRecordArrayFor(modelName);
-
-    set(liveRecordArray, 'isUpdating', false);
+    this.recordArrayManager._didUpdateAll(modelName);
   },
 
   didUpdateAll(modelName) {
@@ -1691,11 +1689,7 @@ Store = Service.extend({
     assert(`You need to pass a model name to the store's peekAll method`, isPresent(modelName));
     assert(`Passing classes to store methods has been removed. Please pass a dasherized string instead of ${modelName}`, typeof modelName === 'string');
     let normalizedModelName = normalizeModelName(modelName);
-    let liveRecordArray = this.recordArrayManager.liveRecordArrayFor(normalizedModelName);
-
-    this.recordArrayManager.syncLiveRecordArray(liveRecordArray, normalizedModelName);
-
-    return liveRecordArray;
+    return this.recordArrayManager.liveRecordArrayFor(normalizedModelName);
   },
 
   /**
