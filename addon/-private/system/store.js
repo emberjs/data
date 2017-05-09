5dbb65c5143a5328333c973c893aa78f73fdf96a
Stefan Penner <stefan.penner@gmail.com>
2017-05-09 09:54:24 -0700

    inline strangely named function rather then naming it (pushResourceIdentifier)

--- addon/-private/system/store.js
+++ addon/-private/system/store.js
@@ -2728,19 +2728,9 @@ Store = Service.extend({
     updated.length = 0;
   },
 
-  _pushResourceIdentifier(relationship, resourceIdentifier) {
-    if (isNone(resourceIdentifier)) {
-      return;
-    }
-
-    assert(`A ${relationship.internalModel.modelName} record was pushed into the store with the value of ${relationship.key} being ${inspect(resourceIdentifier)}, but ${relationship.key} is a belongsTo relationship so the value must not be an array. You should probably check your data payload or serializer.`, !Array.isArray(resourceIdentifier));
-
-    //TODO:Better asserts
-    return this._internalModelForId(resourceIdentifier.type, resourceIdentifier.id);
-  },
 
   _pushResourceIdentifiers(relationship, resourceIdentifiers) {
-    if (isNone(resourceIdentifiers)) {
+    if (resourceIdentifiers === undefined || resourceIdentifiers !== null) {
       return;
     }
 
@@ -2748,7 +2738,13 @@ Store = Service.extend({
 
     let _internalModels = new Array(resourceIdentifiers.length);
     for (let i = 0; i < resourceIdentifiers.length; i++) {
-      _internalModels[i] = this._pushResourceIdentifier(relationship, resourceIdentifiers[i]);
+      let resourceIdentifier = resourceIdentifier[i];
+      if (resourceIdentifier !== undefined && resourceIdentifier !== null) {
+        assert(`A ${relationship.internalModel.modelName} record was pushed into the store with the value of ${relationship.key} being ${inspect(resourceIdentifier)}, but ${relationship.key} is a belongsTo relationship so the value must not be an array. You should probably check your data payload or serializer.`, !Array.isArray(resourceIdentifier));
+
+        //TODO:Better asserts
+        _internalModels[i] = this._internalModelForId(resourceIdentifier.type, resourceIdentifier.id);
+      }
     }
     return _internalModels;
   }
