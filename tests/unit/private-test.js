96a920863946cc6a0878ebe9d836a66e372a8984
Wesley Workman <wworkman@batterii.com>
2017-05-30 08:33:20 -0400

    [BUGFIX BETA] Added `system/store/container-instance-cache` to the -private export file

---
+++ tests/unit/private-test.js
@@ -0,0 +1,16 @@
+import { module, test } from 'qunit';
+import { ContainerInstanceCache, InternalModel, RootState } from 'ember-data/-private';
+
+module('-private');
+
+test('`ContainerInstanceCache` is accessible via private import', function(assert) {
+  assert.ok(!!ContainerInstanceCache);
+});
+
+test('`InternalModel` is accessible via private import', function(assert) {
+  assert.ok(!!InternalModel);
+});
+
+test('`RootState` is accessible via private import', function(assert) {
+  assert.ok(!!RootState);
+});
