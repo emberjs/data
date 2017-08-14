8ab2a59d54ebcf25493792db0d1b185ae76e4f0b
Robert Jackson <rjackson@linkedin.com>
2017-08-14 10:19:07 -0400

    Avoid MODEL_FACTORY_INJECTION deprecations in tests on Ember >= 2.14.

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -2,10 +2,13 @@ import Ember from 'ember';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+import {
+  setup as setupModelFactoryInjections
+} from 'dummy/tests/helpers/model-factory-injection';
 
 let App;
 
-Ember.MODEL_FACTORY_INJECTIONS = true;
+setupModelFactoryInjections();
 
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
