3c079d6ec78f546888606081af7b5a32f0f0349a
Clemens Müller <cmueller.418@gmail.com>
2017-11-04 21:19:18 +0100

    [BUGFIX beta] setProperties to clear errors works (#5248)
    (cherry picked from commit e01b89ade56b8798787e461f479b154c7ab9ae61)

--- tests/integration/records/error-test.js
+++ tests/integration/records/error-test.js
@@ -1,8 +1,9 @@
 import { run } from '@ember/runloop';
-import { module } from 'qunit';
+import { module, test } from 'qunit';
 import DS from 'ember-data';
 import setupStore from 'dummy/tests/helpers/store';
 import testInDebug from 'dummy/tests/helpers/test-in-debug';
+import RSVP from 'rsvp';
 
 var env, store, Person;
 var attr = DS.attr;
@@ -165,4 +166,42 @@ testInDebug('adding errors root.loaded.created.invalid works add + (remove, add)
   assert.deepEqual(person.get('errors').toArray(), [
     { attribute: 'firstName', message: 'is invalid' }
   ]);
+});
+
+test('using setProperties to clear errors', function(assert) {
+  env.adapter.reopen({
+    createRecord() {
+      return RSVP.reject(new DS.InvalidError([
+        {
+          detail: 'Must be unique',
+          source: { pointer: '/data/attributes/first-name' }
+        },
+        {
+          detail: 'Must not be blank',
+          source: { pointer: '/data/attributes/last-name'}
+        }
+      ]));
+    }
+  });
+
+  return run(() => {
+    let person = store.createRecord('person');
+
+    return person.save().then(null, function() {
+      let errors = person.get('errors');
+
+      assert.equal(errors.get('length'), 2);
+      assert.ok(errors.has('firstName'));
+      assert.ok(errors.has('lastName'));
+
+      person.setProperties({
+        firstName: "updated",
+        lastName: "updated"
+      });
+
+      assert.equal(errors.get('length'), 0);
+      assert.notOk(errors.has('firstName'));
+      assert.notOk(errors.has('lastName'));
+    });
+  });
 });
