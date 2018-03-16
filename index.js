c6d5a26462cb8106ba0a84ee360211488ed69bd9
sly7-7 <sylvain.mina@gmail.com>
2018-03-16 19:38:46 +0100

    fix external ordered set dependency
    (cherry picked from commit 022be46a67ce7184db36657946f90fc592126d4b)

--- index.js
+++ index.js
@@ -172,7 +172,8 @@ module.exports = {
           'ember-inflector',
           'ember-data/version',
           'ember-data/-debug',
-          'ember-data/adapters/errors'
+          'ember-data/adapters/errors',
+          '@ember/ordered-set'
         ]
         // cache: true|false Defaults to true
       }
