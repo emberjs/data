5cf7d25d2b736fd9783ff933cd1ea48abba31d5f
Chris Thoburn <cthoburn@linkedin.com>
2018-03-16 19:07:24 -0700

    Adds a test for #5167

--- tests/integration/record-arrays/peeked-records-test.js
+++ tests/integration/record-arrays/peeked-records-test.js
@@ -1,10 +1,9 @@
 import { run } from '@ember/runloop';
-import { watchProperties } from '../../helpers/watch-property';
 import { createStore } from 'dummy/tests/helpers/store';
-
 import { module, test } from 'qunit';
-
 import DS from 'ember-data';
+import { get } from '@ember/object';
+import { watchProperties } from '../../helpers/watch-property';
 
 let store;
 
@@ -199,5 +198,67 @@ test('immediately peeking after unloading newly created records works as expecte
     watcher,
     { length: 2, '[]': 2 },
     'RecordArray state when a new record is unloaded'
+  );
+});
+
+test('unloadAll followed by peekAll in the same run-loop works as expected', function(assert) {
+  let peekedRecordArray = run(() => store.peekAll('person'));
+  let watcher = watchProperties(peekedRecordArray, ['length', '[]']);
+
+  run(() => {
+    store.push({
+      data: [
+        {
+          type: 'person',
+          id: '1',
+          attributes: {
+            name: 'John'
+          }
+        },
+        {
+          type: 'person',
+          id: '2',
+          attributes: {
+            name: 'Joe'
+          }
+        }
+      ]
+    });
+  });
+
+  run(() => {
+    store.peekAll('person');
+
+    assert.watchedPropertyCounts(
+      watcher,
+      { length: 1, '[]': 1 },
+      'RecordArray state after a single push with multiple records to add'
+    );
+
+    store.unloadAll('person');
+
+    assert.watchedPropertyCounts(
+      watcher,
+      { length: 1, '[]': 1 },
+      'RecordArray state after unloadAll has not changed yet'
+    );
+
+    assert.equal(get(peekedRecordArray, 'length'), 2, 'Array length is unchanged before the next peek');
+
+    store.peekAll('person');
+
+    assert.equal(get(peekedRecordArray, 'length'), 0, 'We no longer have any array content');
+
+    assert.watchedPropertyCounts(
+      watcher,
+      { length: 2, '[]': 2 },
+      'RecordArray state after a follow up peekAll reflects unload changes'
+    );
+  });
+
+  assert.watchedPropertyCounts(
+    watcher,
+    { length: 2, '[]': 2 },
+    'RecordArray state has not changed any further'
   );
 });
