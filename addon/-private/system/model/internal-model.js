7c193c5862082abca8278586dfcc7e8b60e8bf9b
Brendan McLoughlin <bmac@users.noreply.github.com>
2018-03-30 11:19:16 -0400

    remove `id` observer from DS.Model (#5407)

--- addon/-private/system/model/internal-model.js
+++ addon/-private/system/model/internal-model.js
@@ -344,7 +344,6 @@ export default class InternalModel {
       let createOptions = {
         store: this.store,
         _internalModel: this,
-        id: this.id,
         currentState: this.currentState,
         isError: this.isError,
         adapterError: this.error
@@ -1019,9 +1018,11 @@ export default class InternalModel {
 
   setId(id) {
     assert('A record\'s id cannot be changed once it is in the loaded state', this.id === null || this.id === id || this.isNew());
+    let didChange = id !== this.id;
     this.id = id;
-    if (this._record.get('id') !== id) {
-      this._record.set('id', id);
+
+    if (didChange && this.hasRecord) {
+      this._record.notifyPropertyChange('id');
     }
   }
 
--- addon/-private/system/model/model.js
+++ addon/-private/system/model/model.js
@@ -4,8 +4,7 @@ import EmberError from '@ember/error';
 import Evented from '@ember/object/evented';
 import EmberObject, {
   computed,
-  get,
-  observer
+  get
 } from '@ember/object';
 import Map from '../map';
 import { DEBUG } from '@glimmer/env';
@@ -340,7 +339,6 @@ const Model = EmberObject.extend(Evented, {
     @property id
     @type {String}
   */
-  id: null,
 
   /**
     @property currentState
@@ -744,7 +742,10 @@ const Model = EmberObject.extend(Evented, {
   },
 
   toStringExtension() {
-    return get(this, 'id');
+    // the _internalModel guard exists, because some dev-only deprecation code
+    // (addListener via validatePropertyInjections) invokes toString before the
+    // object is real.
+    return this._internalModel && this._internalModel.id;
   },
 
   /**
@@ -980,10 +981,6 @@ const Model = EmberObject.extend(Evented, {
     return this._internalModel.referenceFor('hasMany', name);
   },
 
-  setId: observer('id', function () {
-    this._internalModel.setId(this.get('id'));
-  }),
-
   /**
    Provides info about the model for debugging purposes
    by grouping the properties into more semantic groups.
@@ -1133,8 +1130,23 @@ const Model = EmberObject.extend(Evented, {
  @type {Object}
  */
 Object.defineProperty(Model.prototype, 'data', {
+  configurable: false,
   get() {
     return this._internalModel._data;
+  }
+});
+
+Object.defineProperty(Model.prototype, 'id', {
+  configurable: false,
+  set(id) {
+    this._internalModel.setId(id);
+  },
+
+  get() {
+    // the _internalModel guard exists, because some dev-only deprecation code
+    // (addListener via validatePropertyInjections) invokes toString before the
+    // object is real.
+    return this._internalModel && this._internalModel.id;
   }
 });
 
