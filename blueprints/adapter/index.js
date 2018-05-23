201f35a391677864e4e343e4b78edd8f7b2e5005
Kevin Ansfield <kevin@lookingsideways.co.uk>
2018-05-23 21:26:24 +0100

    [Feature] added module-unification adapter and adapter-test blueprints

--- blueprints/adapter/index.js
+++ blueprints/adapter/index.js
@@ -1,11 +1,30 @@
-var extendFromApplicationEntity = require('../../lib/utilities/extend-from-application-entity');
+const extendFromApplicationEntity = require('../../lib/utilities/extend-from-application-entity');
+const isModuleUnificationProject = require('../../lib/utilities/module-unification')
+  .isModuleUnificationProject;
+const path = require('path');
 
 module.exports = {
   description: 'Generates an ember-data adapter.',
 
   availableOptions: [{ name: 'base-class', type: String }],
 
-  locals: function(options) {
+  fileMapTokens(options) {
+    if (isModuleUnificationProject(this.project)) {
+      return {
+        __root__() {
+          return 'src';
+        },
+        __path__(options) {
+          return path.join('data', 'models', options.dasherizedModuleName);
+        },
+        __name__() {
+          return 'adapter';
+        },
+      };
+    }
+  },
+
+  locals(options) {
     return extendFromApplicationEntity('adapter', 'DS.JSONAPIAdapter', options);
   },
 };
