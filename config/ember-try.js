5d5b99bb9e0cc879f5dee27b8531a7210a02f3c4
Chris Thoburn <cthoburn@linkedin.com>
2018-06-20 17:01:49 -0700

    drop explicit testing for 2.12

--- config/ember-try.js
+++ config/ember-try.js
@@ -17,14 +17,6 @@ module.exports = function() {
           npm: {},
         },
         {
-          name: 'ember-lts-2.12',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.12.0',
-            },
-          },
-        },
-        {
           name: 'ember-lts-2.16',
           npm: {
             devDependencies: {
