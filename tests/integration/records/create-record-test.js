aa4dfa44b4f2b395ff8f6cf5aef84b0a36f924cc
Chris Thoburn <cthoburn@linkedin.com>
2018-06-28 15:16:12 -0700

    pairing with Gabby for createRecord + unloadRecord tests

---
+++ tests/integration/records/create-record-test.js
@@ -0,0 +1,72 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+import Store from 'ember-data/store';
+import Model from 'ember-data/model';
+import { attr, belongsTo, hasMany } from '@ember-decorators/data';
+
+class Person extends Model {
+  @hasMany('pet', { inverse: 'owner', async: false })
+  pets;
+  @attr name;
+}
+
+class Pet extends Model {
+  @belongsTo('person', { inverse: 'pets', async: false })
+  owner;
+  @attr name;
+}
+
+module('Store.createRecord() coverage', function(hooks) {
+  let store;
+  setupTest(hooks);
+
+  hooks.beforeEach(function() {
+    let { owner } = this;
+    owner.register('service:store', Store);
+    owner.register('model:person', Person);
+    owner.register('model:pet', Pet);
+    store = owner.lookup('service:store');
+  });
+
+  test('unloading a newly created a record with a relationship', function (assert) {
+    let chris = store.push({
+      data: {
+        id: '1',
+        type: 'person',
+        attributes: {
+          name: 'Chris'
+        },
+        relationships: {
+          pets: {
+            data: []
+          }
+        }
+      }
+    });
+
+    let pet = store.createRecord('pet', {
+      name: 'Shen',
+      owner: chris
+    });
+
+    // check that we are properly configured
+    assert.ok(pet.get('owner') === chris, 'Precondition: Our owner is Chris');
+
+    let pets = chris.get('pets').toArray().map((pet => pet.get('name')));
+    assert.deepEqual(pets, ['Shen'], 'Precondition: Chris has Shen as a pet');
+
+    pet.unloadRecord();
+
+    debugger;
+
+    assert.ok(pet.get('owner') === null, 'Shen no longer has an owner');
+
+    // check that the relationship has been dissolved
+    pets = chris.get('pets').toArray().map((pet => pet.get('name')));
+    assert.deepEqual(pets, [], 'Chris no longer has any pets');
+  });
+
+  test('unloading a record related to a newly created record', function (assert) {
+
+  });
+});
