e5a9006b0a87c0d28d82b3494ca64d54f50442a5
Robert Jackson <rjackson@linkedin.com>
2018-06-29 12:44:19 -0400

    Fix test helper setup to avoid error on ember-source@3.3.0+.

--- tests/helpers/resolver.js
+++ tests/helpers/resolver.js
@@ -1,11 +1,9 @@
 import Resolver from '../../resolver';
 import config from '../../config/environment';
 
-const resolver = Resolver.create();
-
-resolver.namespace = {
-  modulePrefix: config.modulePrefix,
-  podModulePrefix: config.podModulePrefix
-};
-
-export default resolver;
+export default Resolver.create({
+  namespace: {
+    modulePrefix: config.modulePrefix,
+    podModulePrefix: config.podModulePrefix,
+  },
+});
