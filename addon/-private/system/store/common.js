9decc7e99c3440c314ece2f60c5f4fc59693f66e
Chris Thoburn <cthoburn@linkedin.com>
2018-07-26 15:13:59 -0700

    remove test waiter

--- addon/-private/system/store/common.js
+++ addon/-private/system/store/common.js
@@ -1,6 +1,6 @@
 import { get } from '@ember/object';
-import { DEBUG } from '@glimmer/env';
-import Ember from 'ember';
+// import { DEBUG } from '@glimmer/env';
+// import Ember from 'ember';
 import { resolve } from 'rsvp';
 
 const { __bind, __guard, __objectIsAlive } = heimdall.registerMonitor(
@@ -34,24 +34,26 @@ export function _objectIsAlive(object) {
   return !(get(object, 'isDestroyed') || get(object, 'isDestroying'));
 }
 
-let ASYNC_REQUEST_COUNT = 0;
+// let ASYNC_REQUEST_COUNT = 0;
 export function incrementRequestCount() {
-  ASYNC_REQUEST_COUNT++;
+  // ASYNC_REQUEST_COUNT++;
 }
 
+/*
 if (DEBUG) {
   Ember.Test.registerWaiter(() => {
     return ASYNC_REQUEST_COUNT === 0;
   });
 }
+*/
 
 export function guardDestroyedStore(promise, store, label) {
   let wrapperPromise = resolve(promise, label).then(v => promise);
 
   return _guard(wrapperPromise, () => {
-    if (DEBUG) {
-      ASYNC_REQUEST_COUNT--;
-    }
+    // if (DEBUG) {
+    // ASYNC_REQUEST_COUNT--;
+    // }
     return _objectIsAlive(store);
   });
 }
