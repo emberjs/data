fe115e9780bbf177da7a3fa4e1a60c12f56571bf
Robert Jackson <rjackson@linkedin.com>
2018-08-08 10:21:48 -0400

    Fix ES compatibility issues.
    These cases were all previously working due to transpilation from `let`
    to `var`. You *cannot* reference a block scoped variable (`let` /
    `const`) during the variables own initializer.

--- tests/integration/store-test.js
+++ tests/integration/store-test.js
@@ -176,9 +176,9 @@ test("destroying the store correctly cleans everything up", function(assert) {
     };
   };
 
-  let adapterPopulatedPeople =run(() => {
-    return adapterPopulatedPeople = store.query('person', {
-      someCrazy: 'query'
+  let adapterPopulatedPeople = run(() => {
+    return store.query('person', {
+      someCrazy: 'query',
     });
   });
 
