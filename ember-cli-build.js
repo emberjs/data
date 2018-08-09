650c7c2752d67a33778d09a6981429fa7fdfe3b9
Stefan Penner <stefan.penner@gmail.com>
2018-08-09 12:32:04 -0700

    [BUGFIX beta] Fix parallel builds (#5535)

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,7 +3,11 @@
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  let app = new EmberAddon(defaults, {});
+  let app = new EmberAddon(defaults, {
+    'ember-cli-babel': {
+      throwUnlessParallelizable: true,
+    },
+  });
 
   /*
     This build file specifies the options for the dummy test app of this
