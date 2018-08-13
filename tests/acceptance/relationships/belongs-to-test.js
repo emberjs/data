b9c9155eeb3669e26cb92313471af89ee859240d
Chris Thoburn <cthoburn@linkedin.com>
2018-08-13 13:29:00 -0700

    cleanup belongsTo tests

--- tests/acceptance/relationships/belongs-to-test.js
+++ tests/acceptance/relationships/belongs-to-test.js
@@ -2,7 +2,7 @@ import { module } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import Model from 'ember-data/model';
-import { render } from '@ember/test-helpers';
+import { render, settled } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { attr, hasMany, belongsTo } from '@ember-decorators/data';
 import JSONAPISerializer from 'ember-data/serializers/json-api';
@@ -10,6 +10,7 @@ import Store from 'ember-data/store';
 import { resolve, reject } from 'rsvp';
 import { ServerError } from 'ember-data/adapters/errors';
 import { skipRecordData as test } from '../../helpers/test-in-debug';
+import Ember from 'ember';
 
 class Person extends Model {
   @attr name;
@@ -59,7 +60,7 @@ class TestAdapter extends JSONAPIAdapter {
   }
 
   deleteRecord() {
-    resolve();
+    return resolve({ data: null });
   }
 }
 
@@ -166,7 +167,9 @@ module('async belongs-to rendering tests', function(hooks) {
     let { owner } = this;
     owner.register('model:person', Person);
     owner.register('adapter:application', TestAdapter);
-    owner.register('serializer:application', JSONAPISerializer);
+    owner.register('serializer:application', JSONAPISerializer.extend({
+      normalizeResponse(_, __, jsonApi) { return jsonApi }
+    }));
     owner.register('service:store', Store);
     store = owner.lookup('service:store');
     adapter = store.adapterFor('application');
@@ -191,36 +194,44 @@ module('async belongs-to rendering tests', function(hooks) {
       assert.equal(this.element.textContent.trim(), 'Kevin has two children and one parent');
     });
 
-    // Failing test for https://github.com/emberjs/data/issues/5523
-    test('We can delete async belongs-to', async function(assert) {
+    test('We can delete an async belongs-to', async function(assert) {
       let people = makePeopleWithRelationshipData();
       let sedona = store.push({
         data: people.dict['5:has-parent-no-children'],
       });
 
-      adapter.setupPayloads(assert, [{ data: people.dict['3:has-2-children-and-parent'] }]);
+      adapter.setupPayloads(assert, [
+        { data: people.dict['3:has-2-children-and-parent'] }
+      ]);
 
       // render
       this.set('sedona', sedona);
+
       await render(hbs`
       <p>{{sedona.parent.name}}</p>
       `);
 
-      await sedona
-        .belongsTo('parent')
-        .value()
-        .destroyRecord();
-      assert.equal(this.element.textContent.trim(), '');
+      let parent = await sedona.get('parent');
+      await parent.destroyRecord();
+
+      let newParent = await sedona.get('parent');
+
+      await settled();
+
+      assert.ok(newParent === null, 'We no longer have a parent');
+      assert.equal(this.element.textContent.trim(), '', "We no longer render our parent's name because we no longer have a parent");
+
     });
 
-    // failing test for https://github.com/emberjs/data/issues/5517
     test('Re-rendering an async belongsTo does not cause a new fetch', async function(assert) {
       let people = makePeopleWithRelationshipData();
       let sedona = store.push({
         data: people.dict['5:has-parent-no-children'],
       });
 
-      adapter.setupPayloads(assert, [{ data: people.dict['3:has-2-children-and-parent'] }]);
+      adapter.setupPayloads(assert, [
+        { data: people.dict['3:has-2-children-and-parent'] }
+      ]);
 
       // render
       this.set('sedona', sedona);
@@ -231,11 +242,95 @@ module('async belongs-to rendering tests', function(hooks) {
 
       assert.equal(this.element.textContent.trim(), 'Kevin has two children and one parent');
 
-      this.set('sedona.parent', null);
+      this.set('sedona', null);
       assert.equal(this.element.textContent.trim(), '');
 
-      this.set('sedona.parent', { data: people.dict['3:has-2-children-and-parent'] });
+      this.set('sedona', sedona);
       assert.equal(this.element.textContent.trim(), 'Kevin has two children and one parent');
+    });
+
+
+    test('Rendering an async belongs-to whose fetch fails does not trigger a new request', async function(assert) {
+      assert.expect(15);
+      let people = makePeopleWithRelationshipData();
+      let sedona = store.push({
+        data: people.dict['5:has-parent-no-children'],
+      });
+
+      adapter.setupPayloads(assert, [
+        new ServerError([], 'hard error while finding <person>5:has-parent-no-children'),
+      ]);
+
+      // render
+      this.set('sedona', sedona);
+
+      let originalOnError = Ember.onerror;
+      Ember.onerror = function(e) {
+        assert.ok(true, 'Children promise did reject');
+        assert.equal(
+          e.message,
+          'hard error while finding <person>5:has-parent-no-children',
+          'Rejection has the correct message'
+        );
+      };
+
+      // needed for LTS 2.12 and 2.16
+      Ember.Test.adapter.exception = e => {
+        assert.ok(true, 'Children promise did reject');
+        assert.equal(
+          e.message,
+          'hard error while finding <person>5:has-parent-no-children',
+          'Rejection has the correct message'
+        );
+      };
+
+      await render(hbs`
+      <p>{{sedona.parent.name}}</p>
+      `);
+
+      assert.equal(this.element.textContent.trim(), '', 'we have no parent');
+
+      let relationshipState = sedona.belongsTo('parent').belongsToRelationship;
+
+      assert.equal(relationshipState.isAsync, true, 'The relationship is async');
+      assert.equal(relationshipState.relationshipIsEmpty, false, 'The relationship is not empty');
+      assert.equal(relationshipState.relationshipIsStale, true, 'The relationship is still stale');
+      assert.equal(
+        relationshipState.allInverseRecordsAreLoaded,
+        false,
+        'The relationship is missing some or all related resources'
+      );
+      assert.equal(
+        relationshipState.hasAnyRelationshipData,
+        true,
+        'The relationship knows which record it needs'
+      );
+      assert.equal(
+        relationshipState.fetchPromise === null,
+        true,
+        'The relationship has no fetchPromise'
+      );
+      assert.equal(
+        relationshipState.hasFailedLoadAttempt === true,
+        true,
+        'The relationship has attempted a load'
+      );
+      assert.equal(
+        relationshipState.shouldForceReload === false,
+        true,
+        'The relationship will not force a reload'
+      );
+      assert.equal(
+        relationshipState._promiseProxy !== null,
+        true,
+        'The relationship has a loadingPromise'
+      );
+      assert.equal(!!relationshipState.link, false, 'The relationship does not have a link');
+      assert.equal(relationshipState.shouldMakeRequest(), false, 'The relationship does not need to make a request');
+      let result = await sedona.get('parent');
+      assert.ok(result === null, 're-access is safe');
+
+      Ember.onerror = originalOnError;
     });
   });
 });
