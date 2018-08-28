f09d1054c78f995116c0f35dcf9e0536d0c89f16
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-08-27 18:01:13 -0700

    [BUGFIX] backport use internalModel promise if already loading (#5598)

--- addon/-private/system/relationships/state/belongs-to.js
+++ addon/-private/system/relationships/state/belongs-to.js
@@ -282,7 +282,11 @@ export default class BelongsToRelationship extends Relationship {
 }
 
 function proxyRecord(internalModel) {
-  return resolve(internalModel).then(resolvedInternalModel => {
+  let promise = internalModel;
+  if (internalModel && internalModel.isLoading()) {
+    promise = internalModel._promiseProxy;
+  }
+  return resolve(promise).then(resolvedInternalModel => {
     return resolvedInternalModel ? resolvedInternalModel.getRecord() : null;
   });
 }
