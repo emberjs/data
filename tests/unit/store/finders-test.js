9540b30ca207d79888fd5bee3f30c01071432034
Stanley Stuart <sstuart@heroku.com>
2018-08-28 17:47:53 -0700

    failing tests for missing info on relationship finders

--- tests/unit/store/finders-test.js
+++ tests/unit/store/finders-test.js
@@ -163,6 +163,472 @@ test('findHasMany does not load a serializer until the adapter promise resolves'
   });
 });
 
+test('findHasMany/explicit inverse - adds parent relationship information to the payload if it is not included/added by the serializer', function(assert) {
+  assert.expect(5);
+
+  let deferedFind = defer();
+
+  this.env.registry.register(
+    'adapter:person',
+    DS.Adapter.extend({
+      findHasMany: () => deferedFind.promise,
+    })
+  );
+
+  this.Person.reopen({
+    dogs: DS.hasMany('dog', { async: true }),
+  });
+
+  this.Dog.reopen({
+    person: DS.belongsTo('person', { async: true }),
+  });
+
+  let serializerLoaded = false;
+  let serializerFor = this.store.serializerFor;
+  this.store.serializerFor = modelName => {
+    if (modelName === 'dog') {
+      serializerLoaded = true;
+    }
+    return serializerFor.call(this.store, modelName);
+  };
+
+  let storePromise = run(() => {
+    this.env.store.push({
+      data: {
+        type: 'person',
+        id: '1',
+        attributes: {
+          name: 'John Churchill',
+        },
+        relationships: {
+          dogs: {
+            links: {
+              related: 'http://exmaple.com/person/1/dogs',
+            },
+          },
+        },
+      },
+    });
+
+    return this.store.peekRecord('person', 1).get('dogs');
+  });
+  assert.equal(false, serializerLoaded, 'serializer is not eagerly loaded');
+
+  return run(() => {
+    deferedFind.resolve({
+      data: [
+        { id: 1, type: 'dog', attributes: { name: 'Scooby' } },
+        { id: 2, type: 'dog', attributes: { name: 'Scrappy' } },
+      ],
+    });
+    return storePromise.then(dogs => {
+      assert.equal(dogs.get('length'), 2);
+      assert.equal(
+        dogs.objectAt(0).get('person.id'),
+        '1',
+        'dog.person inverse relationship is set up correctly when adapter does not include parent relationships in data.relationships'
+      );
+      assert.equal(
+        dogs.objectAt(1).get('person.id'),
+        '1',
+        'dog.person inverse relationship is set up correctly when adapter does not include parent relationships in data.relationships'
+      );
+      assert.equal(true, serializerLoaded, 'serializer is loaded');
+    });
+  });
+});
+
+test('findHasMany/implicit inverse - adds parent relationship information to the payload if it is not included/added by the serializer', function(assert) {
+  assert.expect(7);
+  const store = this.env.store;
+
+  let deferedFind = defer();
+
+  this.env.registry.register(
+    'adapter:person',
+    DS.Adapter.extend({
+      findHasMany: () => deferedFind.promise,
+    })
+  );
+
+  this.Person.reopen({
+    dogs: DS.hasMany('dog', { async: true }),
+  });
+
+  let serializerLoaded = false;
+  let serializerFor = this.store.serializerFor;
+  store.serializerFor = modelName => {
+    if (modelName === 'dog') {
+      serializerLoaded = true;
+    }
+    return serializerFor.call(this.store, modelName);
+  };
+
+  let storePromise = run(() => {
+    store.push({
+      data: {
+        type: 'person',
+        id: '1',
+        attributes: {
+          name: 'John Churchill',
+        },
+        relationships: {
+          dogs: {
+            links: {
+              related: 'http://exmaple.com/person/1/dogs',
+            },
+          },
+        },
+      },
+    });
+
+    return store.peekRecord('person', 1).get('dogs');
+  });
+  assert.equal(false, serializerLoaded, 'serializer is not eagerly loaded');
+
+  return run(() => {
+    deferedFind.resolve({
+      data: [
+        { id: 1, type: 'dog', attributes: { name: 'Scooby' } },
+        { id: 2, type: 'dog', attributes: { name: 'Scrappy' } },
+      ],
+    });
+
+    return storePromise.then(dogs => {
+      assert.equal(dogs.get('length'), 2);
+      let person = store.peekRecord('person', '1');
+
+      // TODO: need better way of asserting inverse relationship was set up correctly.
+      function implicitPerson(dog, person) {
+        let implicitRelationshipName = person.hasMany('dogs').hasManyRelationship
+          .inverseKeyForImplicit;
+        return dog._internalModel._implicitRelationships[implicitRelationshipName].members.list[0];
+      }
+
+      assert.equal(
+        implicitPerson(dogs.objectAt(0), person).id,
+        '1',
+        'dog.person inverse relationship ID is set up correctly when adapter does not include parent relationships in data.relationships'
+      );
+      assert.equal(
+        implicitPerson(dogs.objectAt(0), person).modelName,
+        'person',
+        'dog.person inverse relationship type is set up correctly when adapter does not include parent relationships in data.relationships'
+      );
+      assert.equal(
+        implicitPerson(dogs.objectAt(1), person).id,
+        '1',
+        'dog.person inverse relationship ID is set up correctly when adapter does not include parent relationships in data.relationships'
+      );
+      assert.equal(
+        implicitPerson(dogs.objectAt(1), person).modelName,
+        'person',
+        'dog.person inverse relationship type is set up correctly when adapter does not include parent relationships in data.relationships'
+      );
+      assert.equal(true, serializerLoaded, 'serializer is loaded');
+    });
+  });
+});
+
+test('findHasMany/explicit null inverse - adds parent relationship information to the payload if it is not included/added by the serializer', function(assert) {
+  assert.expect(7);
+  const store = this.env.store;
+
+  let deferedFind = defer();
+
+  this.env.registry.register(
+    'adapter:person',
+    DS.Adapter.extend({
+      findHasMany: () => deferedFind.promise,
+    })
+  );
+
+  this.Person.reopen({
+    dogs: DS.hasMany('dog', { async: true, inverse: null }),
+  });
+
+  let serializerLoaded = false;
+  let serializerFor = this.store.serializerFor;
+  store.serializerFor = modelName => {
+    if (modelName === 'dog') {
+      serializerLoaded = true;
+    }
+    return serializerFor.call(this.store, modelName);
+  };
+
+  let storePromise = run(() => {
+    store.push({
+      data: {
+        type: 'person',
+        id: '1',
+        attributes: {
+          name: 'John Churchill',
+        },
+        relationships: {
+          dogs: {
+            links: {
+              related: 'http://exmaple.com/person/1/dogs',
+            },
+          },
+        },
+      },
+    });
+
+    return store.peekRecord('person', 1).get('dogs');
+  });
+  assert.equal(false, serializerLoaded, 'serializer is not eagerly loaded');
+
+  return run(() => {
+    deferedFind.resolve({
+      data: [
+        { id: 1, type: 'dog', attributes: { name: 'Scooby' } },
+        { id: 2, type: 'dog', attributes: { name: 'Scrappy' } },
+      ],
+    });
+
+    return storePromise.then(dogs => {
+      assert.equal(dogs.get('length'), 2);
+      let person = store.peekRecord('person', '1');
+
+      // TODO: need better way of asserting inverse relationship was set up correctly.
+      function implicitPerson(dog, person) {
+        let implicitRelationshipName = person.hasMany('dogs').hasManyRelationship
+          .inverseKeyForImplicit;
+        return dog._internalModel._implicitRelationships[implicitRelationshipName].members.list[0];
+      }
+
+      assert.equal(
+        implicitPerson(dogs.objectAt(0), person).id,
+        '1',
+        'dog.person inverse relationship ID is set up correctly when adapter does not include parent relationships in data.relationships'
+      );
+      assert.equal(
+        implicitPerson(dogs.objectAt(0), person).modelName,
+        'person',
+        'dog.person inverse relationship type is set up correctly when adapter does not include parent relationships in data.relationships'
+      );
+      assert.equal(
+        implicitPerson(dogs.objectAt(1), person).id,
+        '1',
+        'dog.person inverse relationship ID is set up correctly when adapter does not include parent relationships in data.relationships'
+      );
+      assert.equal(
+        implicitPerson(dogs.objectAt(1), person).modelName,
+        'person',
+        'dog.person inverse relationship type is set up correctly when adapter does not include parent relationships in data.relationships'
+      );
+      assert.equal(true, serializerLoaded, 'serializer is loaded');
+    });
+  });
+});
+
+test('findBelongsTo/explicit inverse - ensures inverse relationship is set up when payload does not return parent relationship info', function(assert) {
+  assert.expect(4);
+
+  let deferedFind = defer();
+
+  this.env.registry.register(
+    'adapter:person',
+    DS.Adapter.extend({
+      findBelongsTo: () => deferedFind.promise,
+    })
+  );
+
+  this.Person.reopen({
+    favoriteDog: DS.belongsTo('dog', { async: true }),
+  });
+
+  this.Dog.reopen({
+    person: DS.belongsTo('person', { async: true }),
+  });
+
+  let serializerLoaded = false;
+  let serializerFor = this.store.serializerFor;
+  this.store.serializerFor = modelName => {
+    if (modelName === 'dog') {
+      serializerLoaded = true;
+    }
+    return serializerFor.call(this.store, modelName);
+  };
+
+  let storePromise = run(() => {
+    this.env.store.push({
+      data: {
+        type: 'person',
+        id: '1',
+        attributes: {
+          name: 'John Churchill',
+        },
+        relationships: {
+          favoriteDog: {
+            links: {
+              related: 'http://exmaple.com/person/1/favorite-dog',
+            },
+          },
+        },
+      },
+    });
+
+    return this.store.peekRecord('person', 1).get('favoriteDog');
+  });
+  assert.equal(false, serializerLoaded, 'serializer is not eagerly loaded');
+
+  return run(() => {
+    deferedFind.resolve({ data: { id: 1, type: 'dog', attributes: { name: 'Scooby' } } });
+    return storePromise.then(favoriteDog => {
+      assert.equal(favoriteDog.get('id'), '1', 'favoriteDog id is set correctly');
+      assert.equal(
+        favoriteDog.get('person.id'),
+        '1',
+        'favoriteDog.person inverse relationship is set up correctly when adapter does not include parent relationships in data.relationships'
+      );
+      assert.equal(true, serializerLoaded, 'serializer is loaded');
+    });
+  });
+});
+
+test('findBelongsTo/implicit inverse - ensures inverse relationship is set up when payload does not return parent relationship info', function(assert) {
+  assert.expect(4);
+
+  let deferedFind = defer();
+  let store = this.env.store;
+
+  this.env.registry.register(
+    'adapter:person',
+    DS.Adapter.extend({
+      findBelongsTo: () => deferedFind.promise,
+    })
+  );
+
+  this.Person.reopen({
+    favoriteDog: DS.belongsTo('dog', { async: true }),
+  });
+
+  let serializerLoaded = false;
+  let serializerFor = this.store.serializerFor;
+  store.serializerFor = modelName => {
+    if (modelName === 'dog') {
+      serializerLoaded = true;
+    }
+    return serializerFor.call(this.store, modelName);
+  };
+
+  let storePromise = run(() => {
+    store.push({
+      data: {
+        type: 'person',
+        id: '1',
+        attributes: {
+          name: 'John Churchill',
+        },
+        relationships: {
+          favoriteDog: {
+            links: {
+              related: 'http://exmaple.com/person/1/favorite-dog',
+            },
+          },
+        },
+      },
+    });
+
+    return store.peekRecord('person', 1).get('favoriteDog');
+  });
+  assert.equal(false, serializerLoaded, 'serializer is not eagerly loaded');
+
+  return run(() => {
+    deferedFind.resolve({ data: { id: 1, type: 'dog', attributes: { name: 'Scooby' } } });
+    return storePromise.then(favoriteDog => {
+      let person = store.peekRecord('person', '1');
+
+      assert.equal(favoriteDog.get('id'), '1', 'favoriteDog id is set correctly');
+      // TODO: need better way of asserting inverse relationship was set up correctly.
+      function implicitPerson(dog, person) {
+        let implicitRelationshipName = person.belongsTo('favoriteDog').belongsToRelationship
+          .inverseKeyForImplicit;
+        return dog._internalModel._implicitRelationships[implicitRelationshipName].members.list[0];
+      }
+
+      assert.equal(
+        implicitPerson(favoriteDog, person).id,
+        '1',
+        'favoriteDog.person inverse relationship is set up correctly when adapter does not include parent relationships in data.relationships'
+      );
+      assert.equal(true, serializerLoaded, 'serializer is loaded');
+    });
+  });
+});
+
+test('findBelongsTo/explicit null inverse - ensures inverse relationship is set up when payload does not return parent relationship info', function(assert) {
+  assert.expect(4);
+
+  let deferedFind = defer();
+  let store = this.env.store;
+
+  this.env.registry.register(
+    'adapter:person',
+    DS.Adapter.extend({
+      findBelongsTo: () => deferedFind.promise,
+    })
+  );
+
+  this.Person.reopen({
+    favoriteDog: DS.belongsTo('dog', { async: true, inverse: null }),
+  });
+
+  let serializerLoaded = false;
+  let serializerFor = this.store.serializerFor;
+  store.serializerFor = modelName => {
+    if (modelName === 'dog') {
+      serializerLoaded = true;
+    }
+    return serializerFor.call(this.store, modelName);
+  };
+
+  let storePromise = run(() => {
+    store.push({
+      data: {
+        type: 'person',
+        id: '1',
+        attributes: {
+          name: 'John Churchill',
+        },
+        relationships: {
+          favoriteDog: {
+            links: {
+              related: 'http://exmaple.com/person/1/favorite-dog',
+            },
+          },
+        },
+      },
+    });
+
+    return store.peekRecord('person', 1).get('favoriteDog');
+  });
+  assert.equal(false, serializerLoaded, 'serializer is not eagerly loaded');
+
+  return run(() => {
+    deferedFind.resolve({ data: { id: 1, type: 'dog', attributes: { name: 'Scooby' } } });
+    return storePromise.then(favoriteDog => {
+      let person = store.peekRecord('person', '1');
+
+      assert.equal(favoriteDog.get('id'), '1', 'favoriteDog id is set correctly');
+      // TODO: need better way of asserting inverse relationship was set up correctly.
+      function implicitPerson(dog, person) {
+        let implicitRelationshipName = person.belongsTo('favoriteDog').belongsToRelationship
+          .inverseKeyForImplicit;
+        return dog._internalModel._implicitRelationships[implicitRelationshipName].members.list[0];
+      }
+
+      assert.equal(
+        implicitPerson(favoriteDog, person).id,
+        '1',
+        'favoriteDog.person inverse relationship is set up correctly when adapter does not include parent relationships in data.relationships'
+      );
+      assert.equal(true, serializerLoaded, 'serializer is loaded');
+    });
+  });
+});
+
 test('findBelongsTo does not load a serializer until the adapter promise resolves', function(assert) {
   assert.expect(2);
 
