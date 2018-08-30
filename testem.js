92324b3fb65d3814f2d908f7bb8fd764a1b38517
Chris Thoburn <cthoburn@linkedin.com>
2018-08-30 12:15:08 -0700

    make tests great again

--- testem.js
+++ testem.js
@@ -1,25 +1,23 @@
-/* eslint-env node */
 module.exports = {
-  "framework": "qunit",
-  "test_page": "tests/index.html?hidepassed",
-  "disable_watching": true,
-  "reporter": "dot",
-  "launch_in_ci": [
-    "Chrome"
-  ],
-  "launch_in_dev": [
-    "Chrome"
-  ],
+  framework: 'qunit',
+  test_page: 'tests/index.html?hidepassed',
+  disable_watching: true,
+  reporter: 'dot',
+  launch_in_ci: ['Chrome'],
+  launch_in_dev: ['Chrome'],
   browser_args: {
     Chrome: {
       mode: 'ci',
       args: [
-        '--disable-gpu',
         '--headless',
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disable-software-rasterizer',
+        '--mute-audio',
         '--remote-debugging-port=0',
         '--window-size=1440,900',
-        '--no-sandbox'
-      ]
-    }
-  }
+        '--no-sandbox',
+      ],
+    },
+  },
 };
