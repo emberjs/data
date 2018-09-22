c085b100c8d3166a85a6afd166c3ee117b50b7e2
Cyrille David <dcyriller@gmail.com>
2018-09-22 21:06:40 +0200

    [Feature] Add MU model{,-test} blueprint

--- blueprints/model/index.js
+++ blueprints/model/index.js
@@ -1,33 +1,52 @@
-var inflection = require('inflection');
-var stringUtils = require('ember-cli-string-utils');
-var EOL = require('os').EOL;
+const inflection = require('inflection');
+const stringUtils = require('ember-cli-string-utils');
+const EOL = require('os').EOL;
+const isModuleUnificationProject = require('../../lib/utilities/module-unification')
+  .isModuleUnificationProject;
+const path = require('path');
 
 module.exports = {
   description: 'Generates an ember-data model.',
 
   anonymousOptions: ['name', 'attr:type'],
 
-  locals: function(options) {
-    var attrs = [];
-    var needs = [];
-    var entityOptions = options.entity.options;
+  fileMapTokens(options) {
+    if (isModuleUnificationProject(this.project)) {
+      return {
+        __root__() {
+          return 'src';
+        },
+        __path__(options) {
+          return path.join('data', 'models', options.dasherizedModuleName);
+        },
+        __name__() {
+          return 'model';
+        },
+      };
+    }
+  },
 
-    for (var name in entityOptions) {
-      var type = entityOptions[name] || '';
-      var foreignModel = name;
+  locals(options) {
+    let attrs = [];
+    let needs = [];
+    let entityOptions = options.entity.options;
+
+    for (let name in entityOptions) {
+      let type = entityOptions[name] || '';
+      let foreignModel = name;
       if (type.indexOf(':') > -1) {
         foreignModel = type.split(':')[1];
         type = type.split(':')[0];
       }
-      var dasherizedName = stringUtils.dasherize(name);
-      var camelizedName = stringUtils.camelize(name);
-      var dasherizedType = stringUtils.dasherize(type);
-      var dasherizedForeignModel = stringUtils.dasherize(foreignModel);
-      var dasherizedForeignModelSingular = inflection.singularize(dasherizedForeignModel);
+      let dasherizedName = stringUtils.dasherize(name);
+      let camelizedName = stringUtils.camelize(name);
+      let dasherizedType = stringUtils.dasherize(type);
+      let dasherizedForeignModel = stringUtils.dasherize(foreignModel);
+      let dasherizedForeignModelSingular = inflection.singularize(dasherizedForeignModel);
 
-      var attr;
+      let attr;
       if (/has-many/.test(dasherizedType)) {
-        var camelizedNamePlural = inflection.pluralize(camelizedName);
+        let camelizedNamePlural = inflection.pluralize(camelizedName);
         attr = dsAttr(dasherizedForeignModelSingular, dasherizedType);
         attrs.push(camelizedNamePlural + ': ' + attr);
       } else if (/belongs-to/.test(dasherizedType)) {
@@ -43,7 +62,7 @@ module.exports = {
       }
     }
 
-    var needsDeduplicated = needs.filter(function(need, i) {
+    let needsDeduplicated = needs.filter(function(need, i) {
       return needs.indexOf(need) === i;
     });
 
