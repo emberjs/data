0826f81569feb28b4604f53200615febeea704ea
Chris Thoburn <cthoburn@linkedin.com>
2018-10-04 15:50:19 -0700

    remove legacy build

--- tests/helpers/test-in-debug.js
+++ tests/helpers/test-in-debug.js
@@ -1,8 +1,5 @@
 import { DEBUG } from '@glimmer/env';
 import { test, skip } from 'qunit';
-import config from 'dummy/config/environment';
-
-const IS_RECORD_DATA = config.emberData.enableRecordDataRFCBuild;
 
 export function testInDebug() {
   if (DEBUG) {
@@ -15,17 +12,9 @@ export function testInDebug() {
 export default testInDebug;
 
 export function testRecordData() {
-  if (IS_RECORD_DATA) {
-    test(...arguments);
-  } else {
-    skip(...arguments);
-  }
+  test(...arguments);
 }
 
 export function skipRecordData() {
-  if (IS_RECORD_DATA) {
-    skip(...arguments);
-  } else {
-    test(...arguments);
-  }
+  skip(...arguments);
 }
