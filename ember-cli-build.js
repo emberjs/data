ff5da16d2202b514c283ca47c5355fcb8a1a5225
NullVoxPopuli <LPSego3+dev@gmail.com>
2018-10-26 14:09:58 -0400

    rebase, update e-c-ts - tests still seem to barf

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,7 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
     'ember-cli-babel': {
-      throwUnlessParallelizable: false,
+      throwUnlessParallelizable: true,
     },
   });
 
