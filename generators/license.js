aed597ce367a6867b40c8f554ef8f2627f1d9c69
Cyrille David <dcyriller@users.noreply.github.com>
2019-03-23 23:43:58 +0100

    Remove some old / unused files (#5926)
    * Remove old / unused files
    
    * Revert changelog generation and lint features

--- generators/license.js
+++
@@ -1,7 +0,0 @@
-/*!
- * @overview  Ember Data
- * @copyright Copyright 2011-2017 Tilde Inc. and contributors.
- *            Portions Copyright 2011 LivingSocial Inc.
- * @license   Licensed under MIT license (see license.js)
- * @version   VERSION_STRING_PLACEHOLDER
- */
