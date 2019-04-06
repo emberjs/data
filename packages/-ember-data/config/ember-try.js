e19643d5a31f8fdbb98d41a31c5e10037f79ac0d
Cyrille David <dcyriller@gmail.com>
2019-04-06 22:35:18 +0200

    Drop ember-source@2.18 compatibility

--- packages/-ember-data/config/ember-try.js
+++ packages/-ember-data/config/ember-try.js
@@ -16,33 +16,13 @@ module.exports = function() {
           npm: {},
         },
         {
-          name: 'ember-lts-2.18',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
-          },
-          npm: {
-            devDependencies: {
-              '@ember/jquery': '^0.5.1',
-              'ember-source': '~2.18.0',
-            },
-          },
-        },
-        {
-          name: 'ember-lts-2.18',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.18.0',
-            },
-          },
-        },
-        {
           name: 'ember-lts-3.4',
           env: {
             EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
           },
           npm: {
             devDependencies: {
-              '@ember/jquery': '^0.5.1',
+              '@ember/jquery': '^0.6.0',
               'ember-source': '~3.4.0',
             },
           },
