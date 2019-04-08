7431d8fee31300cdcdb04b2686bde23b72b0918d
Chris Thoburn <cthoburn@linkedin.com>
2019-04-08 11:14:32 -0700

    prepare for release

--- config/ember-try.js
+++ config/ember-try.js
@@ -16,26 +16,6 @@ module.exports = function() {
           npm: {},
         },
         {
-          name: 'ember-lts-2.18',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
-          },
-          npm: {
-            devDependencies: {
-              '@ember/jquery': '^0.5.1',
-              'ember-source': '~2.18.0',
-            },
-          },
-        },
-        {
-          name: 'ember-lts-2.18',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.18.0',
-            },
-          },
-        },
-        {
           name: 'ember-lts-3.4',
           env: {
             EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
@@ -48,10 +28,14 @@ module.exports = function() {
           },
         },
         {
-          name: 'ember-lts-3.4',
+          name: 'ember-lts-3.8',
+          env: {
+            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true }),
+          },
           npm: {
             devDependencies: {
-              'ember-source': '~3.4.0',
+              '@ember/jquery': '^0.5.1',
+              'ember-source': '~3.8.0',
             },
           },
         },
