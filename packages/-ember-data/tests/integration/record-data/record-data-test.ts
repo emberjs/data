aa9a9f7f703c7dbfa658b8c2aee1678eedc19beb
Igor Terzic <iterzic@linkedin.com>
2019-04-11 17:30:20 -0700

    RecordData relationship and storeWrapper tests

--- packages/-ember-data/tests/integration/record-data/record-data-test.ts
+++ packages/-ember-data/tests/integration/record-data/record-data-test.ts
@@ -2,7 +2,7 @@ import { get } from '@ember/object';
 import { setupTest } from 'ember-qunit';
 import Model from 'ember-data/model';
 import Store from 'ember-data/store';
-import { attr, belongsTo } from '@ember-decorators/data';
+import { attr, belongsTo, hasMany } from '@ember-decorators/data';
 import { module, test } from 'qunit';
 import { settled } from '@ember/test-helpers';
 
@@ -12,7 +12,21 @@ class Person extends Model {
   name;
 }
 
-// TODO Make the class implement the RD interface
+class House extends Model {
+  // TODO fix the typing for naked attrs
+  @attr('string', {})
+  name;
+
+  @belongsTo('person', { async: false })
+  landlord;
+
+  @hasMany('person', { async: false })
+  tenants;
+}
+
+
+// TODO: this should work
+// class TestRecordData implements RecordData
 class TestRecordData {
   pushData(data, calculateChange?: boolean) { }
   clientDidCreate() { }
@@ -65,6 +79,8 @@ let CustomStore = Store.extend({
   }
 });
 
+let houseHash, davidHash, runspiredHash, igorHash;
+
 module('integration/record-data - Custom RecordData Implementations', function (hooks) {
   setupTest(hooks);
 
@@ -73,7 +89,40 @@ module('integration/record-data - Custom RecordData Implementations', function (
   hooks.beforeEach(function () {
     let { owner } = this;
 
+    houseHash = {
+      type: 'house',
+      id: '1',
+      attributes: {
+        name: 'Moomin'
+      }
+    };
+
+    davidHash = {
+      type: 'person',
+      id: '1',
+      attributes: {
+        name: 'David'
+      }
+    };
+
+    runspiredHash = {
+      type: 'person',
+      id: '2',
+      attributes: {
+        name: 'Runspired'
+      }
+    };
+
+    igorHash = {
+      type: 'person',
+      id: '3',
+      attributes: {
+        name: 'Igor'
+      }
+    };
+
     owner.register('model:person', Person);
+    owner.register('model:house', House);
     owner.register('service:store', CustomStore);
   });
 
@@ -258,8 +307,6 @@ module('integration/record-data - Custom RecordData Implementations', function (
 
   test("Record Data attribute settting", async function (assert) {
     assert.expect(11);
-    let called = 0;
-    let createCalled = 0;
     const personHash = {
       type: 'person',
       id: '1',
@@ -323,4 +370,300 @@ module('integration/record-data - Custom RecordData Implementations', function (
     assert.equal(calledGet, 3, 'called getAttr after notifyPropertyChange');
     assert.deepEqual(person.changedAttributes(), { name: ['old', 'new'] }, 'changed attributes passes through RD value');
   });
+
+  test("Record Data controls belongsTo notifications", async function (assert) {
+    assert.expect(6);
+    let called = 0;
+    let createCalled = 0;
+
+    let { owner } = this;
+    let belongsToReturnValue = { data: { id: '1', type: 'person' } };
+
+    class RelationshipRecordData extends TestRecordData {
+      constructor(storeWrapper) {
+        super();
+        this._storeWrapper = storeWrapper;
+      }
+
+      getBelongsTo(key: string) {
+        assert.equal(key, 'landlord', 'Passed correct key to getBelongsTo');
+        return belongsToReturnValue;
+      }
+
+      setDirtyBelongsTo(key: string, recordData: this | null) {
+        assert.equal(key, 'landlord', 'Passed correct key to setBelongsTo');
+        assert.equal(recordData.id, '2', 'Passed correct RD to setBelongsTo');
+      }
+    }
+
+    let TestStore = Store.extend({
+      createRecordDataFor(modelName, id, clientId, storeWrapper) {
+        if (modelName === 'house') {
+          return new RelationshipRecordData(storeWrapper);
+        } else {
+          return this._super(modelName, id, clientId, storeWrapper);
+        }
+      }
+    });
+
+    owner.register('service:store', TestStore);
+
+    store = owner.lookup('service:store');
+
+    store.push({
+      data: [davidHash, runspiredHash]
+    });
+
+    store.push({
+      data: [houseHash]
+    });
+
+    let house = store.peekRecord('house', '1');
+    let runspired = store.peekRecord('person', '2');
+    assert.equal(house.get('landlord.name'), 'David', 'belongsTo get correctly looked up');
+
+    house.set('landlord', runspired);
+    assert.equal(house.get('landlord.name'), 'David', 'belongsTo does not change if RD did not notify');
+  });
+
+  test("Record Data custom belongsTo", async function (assert) {
+    assert.expect(4);
+    let { owner } = this;
+
+    let belongsToReturnValue = { data: { id: '1', type: 'person' } };
+
+    class RelationshipRecordData extends TestRecordData {
+      constructor(storeWrapper) {
+        super();
+        this._storeWrapper = storeWrapper;
+      }
+
+      getBelongsTo(key: string) {
+        assert.equal(key, 'landlord', 'Passed correct key to getBelongsTo');
+        return belongsToReturnValue;
+      }
+
+      setDirtyBelongsTo(key: string, recordData: this | null) {
+        belongsToReturnValue = { data: { id: '3', type: 'person' } };
+        this._storeWrapper.notifyBelongsToChange('house', '1', null, 'landlord');
+      }
+    }
+
+    let TestStore = Store.extend({
+      createRecordDataFor(modelName, id, clientId, storeWrapper) {
+        if (modelName === 'house') {
+          return new RelationshipRecordData(storeWrapper);
+        } else {
+          return this._super(modelName, id, clientId, storeWrapper);
+        }
+      }
+    });
+
+    owner.register('service:store', TestStore);
+
+    store = owner.lookup('service:store');
+
+    store.push({
+      data: [davidHash, runspiredHash, igorHash]
+    });
+
+    store.push({
+      data: [houseHash]
+    });
+
+
+    let house = store.peekRecord('house', '1');
+    assert.equal(house.get('landlord.name'), 'David', 'belongsTo get correctly looked up');
+
+    let runspired = store.peekRecord('person', '2');
+    house.set('landlord', runspired);
+
+    // This is intentionally !== runspired to test the custom RD implementation
+    assert.equal(house.get('landlord.name'), 'Igor', 'RecordData sets the custom belongsTo value');
+  });
+
+  test("Record Data controls hasMany notifications", async function (assert) {
+    assert.expect(10);
+    let called = 0;
+    let createCalled = 0;
+
+    let { owner } = this;
+
+    let calledAddToHasMany = 0;
+    let calledRemoveFromHasMany = 0;
+    let hasManyReturnValue = { data: [{ id: '1', type: 'person' }] };
+
+    class RelationshipRecordData extends TestRecordData {
+      constructor(storeWrapper) {
+        super();
+        this._storeWrapper = storeWrapper;
+      }
+
+      getHasMany(key: string) {
+        return hasManyReturnValue;
+      }
+
+      // TODO: investigate addToHasMany being called during unloading 
+      addToHasMany(key: string, recordDatas: this[], idx?: number) {
+        // dealing with getting called during unload
+        if (calledAddToHasMany === 1) {
+          return;
+        }
+        assert.equal(key, 'tenants', 'Passed correct key to addToHasMany');
+        assert.equal(recordDatas[0].id, '2', 'Passed correct RD to addToHasMany');
+        calledAddToHasMany++;
+      }
+
+      removeFromHasMany(key: string, recordDatas: this[]) {
+        // dealing with getting called during unload
+        if (calledRemoveFromHasMany === 1) {
+          return;
+        }
+        assert.equal(key, 'tenants', 'Passed correct key to removeFromHasMany');
+        assert.equal(recordDatas[0].id, '1', 'Passed correct RD to removeFromHasMany');
+        calledRemoveFromHasMany++;
+      }
+      
+      setDirtyHasMany(key: string, recordDatas: this[]) {
+        assert.equal(key, 'tenants', 'Passed correct key to addToHasMany');
+        assert.equal(recordDatas[0].id, '3', 'Passed correct RD to addToHasMany');
+      }
+    }
+
+    let TestStore = Store.extend({
+      createRecordDataFor(modelName, id, clientId, storeWrapper) {
+        if (modelName === 'house') {
+          return new RelationshipRecordData(storeWrapper);
+        } else {
+          return this._super(modelName, id, clientId, storeWrapper);
+        }
+      }
+    });
+
+    owner.register('service:store', TestStore);
+
+    store = owner.lookup('service:store');
+
+    store.push({
+      data: [davidHash, runspiredHash, igorHash]
+    });
+
+    store.push({
+      data: [houseHash]
+    });
+
+    let house = store.peekRecord('house', '1');
+    let people = house.get('tenants');
+    let david = store.peekRecord('person', '1');
+    let runspired = store.peekRecord('person', '2');
+    let igor = store.peekRecord('person', '3');
+
+    assert.deepEqual(people.toArray(), [david], 'getHasMany correctly looked up');
+
+    people.pushObject(runspired);
+    assert.deepEqual(people.toArray(), [david], 'has many doesnt change if RD did not notify');
+
+    people.removeObject(david);
+    assert.deepEqual(people.toArray(), [david], 'hasMany removal doesnt apply the change unless notified');
+
+    house.set('tenants', [igor]);
+    assert.deepEqual(people.toArray(), [david], 'setDirtyHasMany doesnt apply unless notified');
+  });
+
+  test("Record Data supports custom hasMany handling", async function (assert) {
+    assert.expect(10);
+    let { owner } = this;
+
+    let calledAddToHasMany = 0;
+    let calledRemoveFromHasMany = 0;
+    let calledSetDirtyHasMany = 0;
+    let hasManyReturnValue = { data: [{ id: '1', type: 'person' }] };
+
+    class RelationshipRecordData extends TestRecordData {
+      constructor(storeWrapper) {
+        super();
+        this._storeWrapper = storeWrapper;
+      }
+
+      getHasMany(key: string) {
+        return hasManyReturnValue;
+      }
+
+      // TODO: investigate addToHasMany being called during unloading 
+      addToHasMany(key: string, recordDatas: this[], idx?: number) {
+        // dealing with getting called during unload
+        if (calledAddToHasMany === 1) {
+          return;
+        }
+        assert.equal(key, 'tenants', 'Passed correct key to addToHasMany');
+        assert.equal(recordDatas[0].id, '2', 'Passed correct RD to addToHasMany');
+        calledAddToHasMany++;
+
+        hasManyReturnValue = { data: [{ id: '3', type: 'person'} , { id: '2', type: 'person' }] };
+        this._storeWrapper.notifyHasManyChange('house', '1', null, 'tenants');
+      }
+
+      removeFromHasMany(key: string, recordDatas: this[]) {
+        // dealing with getting called during unload
+        if (calledRemoveFromHasMany === 1) {
+          return;
+        }
+        assert.equal(key, 'tenants', 'Passed correct key to removeFromHasMany');
+        assert.equal(recordDatas[0].id, '2', 'Passed correct RD to removeFromHasMany');
+        calledRemoveFromHasMany++;
+        hasManyReturnValue = { data: [{ id: '1', type: 'person'}] };
+        this._storeWrapper.notifyHasManyChange('house', '1', null, 'tenants');
+      }
+      
+      setDirtyHasMany(key: string, recordDatas: this[]) {
+        assert.equal(key, 'tenants', 'Passed correct key to addToHasMany');
+        assert.equal(recordDatas[0].id, '3', 'Passed correct RD to addToHasMany');
+        hasManyReturnValue = { data: [{ id: '1', type: 'person'} , { id: '2', type: 'person' }] };
+        this._storeWrapper.notifyHasManyChange('house', '1', null, 'tenants');
+      }
+    }
+
+    let TestStore = Store.extend({
+      createRecordDataFor(modelName, id, clientId, storeWrapper) {
+        if (modelName === 'house') {
+          return new RelationshipRecordData(storeWrapper);
+        } else {
+          return this._super(modelName, id, clientId, storeWrapper);
+        }
+      }
+    });
+
+    owner.register('service:store', TestStore);
+
+    store = owner.lookup('service:store');
+
+    store.push({
+      data: [davidHash, runspiredHash, igorHash]
+    });
+
+    store.push({
+      data: [houseHash]
+    });
+
+    let house = store.peekRecord('house', '1');
+    let people = house.get('tenants');
+    let david = store.peekRecord('person', '1');
+    let runspired = store.peekRecord('person', '2');
+    let igor = store.peekRecord('person', '3');
+
+    assert.deepEqual(people.toArray(), [david], 'getHasMany correctly looked up');
+    people.pushObject(runspired);
+
+    // This is intentionally !== [david, runspired] to test the custom RD implementation
+    assert.deepEqual(people.toArray(), [igor, runspired], 'hasMany changes after notifying');
+
+    people.removeObject(runspired);
+    // This is intentionally !== [igor] to test the custom RD implementation
+    assert.deepEqual(people.toArray(), [david], 'hasMany removal applies the change when notified');
+
+    house.set('tenants', [igor]);
+    // This is intentionally !== [igor] to test the custom RD implementation
+    assert.deepEqual(people.toArray(), [david, runspired], 'setDirtyHasMany applies changes');
+  });
+
 });
