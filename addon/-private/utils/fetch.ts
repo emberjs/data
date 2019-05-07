4891ed57a86d207c378a8c49c94d7b978bdceb81
Chris Thoburn <cthoburn@linkedin.com>
2019-05-07 00:38:54 -0700

    [BUGFIX fetch] fix typings for require

--- addon/-private/utils/fetch.ts
+++ addon/-private/utils/fetch.ts
@@ -1,12 +1,12 @@
 import require, { has } from 'require';
 
- type MaybeFetch = {
+type MaybeFetch = {
   (input: RequestInfo, init?: RequestInit | undefined): Promise<Response>;
 } | null;
 
- let _fetch: MaybeFetch = null;
+let _fetch: MaybeFetch = null;
 
- if (has('fetch')) {
+if (has('fetch')) {
   // use `fetch` module by default, this is commonly provided by ember-fetch
   _fetch = require('fetch').default;
 } else if (typeof fetch === 'function') {
@@ -14,4 +14,4 @@ import require, { has } from 'require';
   _fetch = fetch;
 }
 
- export default _fetch;
+export default _fetch;
---
+++ types/require/index.d.ts
@@ -0,0 +1,2 @@
+export default function require(moduleName: string): { default: any } {}
+export function has(moduleName: string): boolean {}
