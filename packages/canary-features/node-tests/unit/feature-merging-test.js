55559dc9f8a69504761bf4e95f4e58a26ac3f2c4
Eli Flanagan <eflanagan@innovu.com>
2019-07-01 09:18:14 -0400

    scaffold mocha unit testing for feature flags

---
+++ packages/canary-features/node-tests/unit/feature-merging-test.js
@@ -0,0 +1,5 @@
+/* eslint-env mocha */
+describe('Unit: canary-features', function() {
+  it('features.js is the single source of truth for flags');
+  it('provides correct default values for feature flags');
+});
