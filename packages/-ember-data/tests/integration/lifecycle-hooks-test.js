3119c09b3794670486f6f80eac8fee89d9aad626
pliljegr <pliljegr@linkedin.com>
2019-07-04 12:54:31 -0700

    CHORE: Modernize integration/ tests and remove run loop calls

--- packages/-ember-data/tests/integration/lifecycle-hooks-test.js
+++ packages/-ember-data/tests/integration/lifecycle-hooks-test.js
@@ -1,28 +1,25 @@
 import { resolve } from 'rsvp';
-import { run } from '@ember/runloop';
+import { setupTest } from 'ember-qunit';
 import { deprecatedTest } from 'dummy/tests/helpers/deprecated-test';
-import setupStore from 'dummy/tests/helpers/store';
+import Model from '@ember-data/model';
+import { attr } from '@ember-data/model';
 
 import { module } from 'qunit';
 
-import DS from 'ember-data';
-
-let Person, env;
-const { attr } = DS;
-
 module('integration/lifecycle_hooks - Lifecycle Hooks', function(hooks) {
+  setupTest(hooks);
+  let store;
+  let adapter;
   hooks.beforeEach(function() {
-    Person = DS.Model.extend({
-      name: attr('string'),
-    });
+    let { owner } = this;
+    class Person extends Model {
+      @attr()
+      name;
+    }
 
-    env = setupStore({
-      person: Person,
-    });
-  });
-
-  hooks.afterEach(function() {
-    run(env.container, 'destroy');
+    owner.register('model:person', Person);
+    store = owner.lookup('service:store');
+    adapter = store.adapterFor('application');
   });
 
   deprecatedTest(
@@ -31,15 +28,15 @@ module('integration/lifecycle_hooks - Lifecycle Hooks', function(hooks) {
       id: 'ember-data:evented-api-usage',
       until: '4.0',
     },
-    function(assert) {
+    async function(assert) {
       let done = assert.async();
       assert.expect(3);
 
-      env.adapter.createRecord = function(store, type, snapshot) {
+      adapter.createRecord = function(store, type, snapshot) {
         return resolve({ data: { id: 99, type: 'person', attributes: { name: 'Yehuda Katz' } } });
       };
 
-      let person = env.store.createRecord('person', { name: 'Yehuda Katz' });
+      let person = store.createRecord('person', { name: 'Yehuda Katz' });
 
       person.on('didCreate', function() {
         assert.equal(this, person, 'this is bound to the record');
@@ -48,7 +45,7 @@ module('integration/lifecycle_hooks - Lifecycle Hooks', function(hooks) {
         done();
       });
 
-      run(person, 'save');
+      await person.save();
     }
   );
 
@@ -58,14 +55,14 @@ module('integration/lifecycle_hooks - Lifecycle Hooks', function(hooks) {
       id: 'ember-data:evented-api-usage',
       until: '4.0',
     },
-    function(assert) {
+    async function(assert) {
       assert.expect(3);
 
-      env.adapter.createRecord = function(store, type, snapshot) {
+      adapter.createRecord = function(store, type, snapshot) {
         return resolve();
       };
 
-      let person = env.store.createRecord('person', { id: 99, name: 'Yehuda Katz' });
+      let person = store.createRecord('person', { id: 99, name: 'Yehuda Katz' });
 
       person.on('didCreate', function() {
         assert.equal(this, person, 'this is bound to the record');
@@ -73,7 +70,7 @@ module('integration/lifecycle_hooks - Lifecycle Hooks', function(hooks) {
         assert.equal(this.get('name'), 'Yehuda Katz', 'the attribute has been assigned');
       });
 
-      run(person, 'save');
+      await person.save();
     }
   );
 });
