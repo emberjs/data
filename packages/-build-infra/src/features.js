756ed108a03939c0b55829b755b0def1c45dffa0
Eli Flanagan <eflanagan@innovu.com>
2019-07-05 16:45:03 -0400

    parse the addon/index.js for one source of flags

--- packages/-build-infra/src/features.js
+++ packages/-build-infra/src/features.js
@@ -1,11 +1,22 @@
 'use strict';
 
+const { parse } = require('@babel/parser');
+const fs = require('fs');
+
+// See ember-source for other implementation to parse typescript file
+function extractFeaturesHash() {
+  let fileName = require.resolve('@ember-data/canary-features/addon/index.js');
+  let fileContents = fs.readFileSync(fileName, { encoding: 'utf8' }).toString();
+  let parsed = parse(fileContents, { sourceType: 'module' })
+  return parsed.program.body;
+}
 function getFeatures() {
   const features = {
     SAMPLE_FEATURE_FLAG: null,
     RECORD_DATA_ERRORS: null,
     RECORD_DATA_STATE: null,
   };
+  console.log(extractFeaturesHash());
 
   const FEATURE_OVERRIDES = process.env.EMBER_DATA_FEATURE_OVERRIDE;
   if (FEATURE_OVERRIDES === 'ENABLE_ALL_OPTIONAL') {
@@ -21,7 +32,7 @@ function getFeatures() {
     // enable only the specific features listed in the environment
     // variable (comma separated)
     const forcedFeatures = FEATURE_OVERRIDES.split(',');
-    for (var i = 0; i < forcedFeatures.length; i++) {
+    for (let i = 0; i < forcedFeatures.length; i++) {
       let featureName = forcedFeatures[i];
 
       features[featureName] = true;
