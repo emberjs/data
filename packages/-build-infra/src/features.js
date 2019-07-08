ec943fe8e1f4aabd538746ed008b3c39f2b2d417
Eli Flanagan <eflanagan@innovu.com>
2019-07-08 12:34:02 -0400

    try using esm to load default features

--- packages/-build-infra/src/features.js
+++ packages/-build-infra/src/features.js
@@ -1,58 +1,10 @@
 'use strict';
 
-function castValue({ type, name, value }) {
-  if (type === 'NullLiteral') {
-    return null;
-  }
-  if (type === 'BooleanLiteral') {
-    return value;
-  }
-  if (type === 'NumericLiteral') {
-    return value;
-  }
-  if (type === 'StringLiteral') {
-    return value;
-  }
-  if (type === 'Identifier' && name === 'undefined') {
-    return undefined;
-  }
-}
-const { parse } = require('@babel/parser');
-const fs = require('fs');
+const requireEsm = require('esm')(module);
 // See ember-source for other implementation to parse typescript file
 function extractFeaturesHash() {
-  let fileName = require.resolve('@ember-data/canary-features/addon/index.js');
-  let fileContents = fs.readFileSync(fileName, { encoding: 'utf8' }).toString();
-  let parsed = parse(fileContents, {
-    sourceType: 'module',
-    estree: true,
-  });
-  // TODO manual extracting, but should considering using a babel plugin
-  // Passing the plugins configuration option did not seem to work
-  let exportedHashDeclaration = parsed.program.body.find(node => {
-    return (
-      node.type === 'ExportNamedDeclaration' &&
-      node.declaration.type === 'VariableDeclaration' &&
-      node.declaration.declarations.length === 1 &&
-      node.declaration.declarations[0].id &&
-      node.declaration.declarations[0].id.type === 'Identifier' &&
-      node.declaration.declarations[0].id.name === 'DEFAULT_FEATURES'
-    );
-  });
-
-  // grab the fields we need
-  const {
-    declaration: {
-      declarations: [node],
-    },
-  } = exportedHashDeclaration;
-  // populate an object with the values
-  // static values declared in the file
-  const defaults = {};
-  node.init.properties.forEach(prop => {
-    defaults[prop.key.name] = castValue(prop.value);
-  });
-  return defaults;
+  let features = requireEsm('@ember-data/canary-features/addon/index.js');
+  return features.DEFAULT_FEATURES;
 }
 function getFeatures() {
   const features = extractFeaturesHash();
