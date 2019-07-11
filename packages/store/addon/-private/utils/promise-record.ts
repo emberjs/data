318627618abebb1d7ae9b19358eda1edf5df4aae
Chris Thoburn <runspired@users.noreply.github.com>
2019-07-11 16:42:55 -0700

    chore: refactor promiseRecord (#6161)
    * chore: refactor promiseRecord
    
    * cleanup

---
+++ packages/store/addon/-private/utils/promise-record.ts
@@ -0,0 +1,24 @@
+import InternalModel from '../system/model/internal-model';
+import { promiseObject } from '../system/promise-proxies';
+import { Record } from '../ts-interfaces/record';
+
+// shim type until we can properly type
+// these proxies
+export type PromiseProxy<T> = Promise<T>;
+
+/**
+ * Get the materialized model from the internalModel/promise
+ * that returns an internal model and return it in a promiseObject.
+ *
+ * Useful for returning from find methods
+ *
+ * @internal
+ */
+export default function promiseRecord(
+  internalModelPromise: Promise<InternalModel>,
+  label: string
+): PromiseProxy<Record> {
+  let toReturn = internalModelPromise.then(internalModel => internalModel.getRecord());
+
+  return promiseObject(toReturn, label);
+}
