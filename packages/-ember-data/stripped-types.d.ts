a80f2d07b5530e615058d0a20f8b8ff078108bd5
Chris Thoburn <runspired@users.noreply.github.com>
2019-07-18 14:05:03 -0700

    [BUGFIX release] fix type overrides (#6256)
    * [BUGFIX release] safer type stripping strategy
    
    * remove uneeded command parts

--- packages/-ember-data/stripped-types.d.ts
+++
@@ -1,2 +0,0 @@
-// intentionally empty types to help us preserve semver as we migrate to TS
-declare module 'ember-data';
--- packages/serializer/stripped-types.d.ts
+++
@@ -1,3 +0,0 @@
-// intentionally empty types to help us preserve semver as we migrate to TS
-declare module 'ember-data';
-declare module '@ember-data/serializer';
