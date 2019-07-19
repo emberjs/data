bef5375516228b378987d2cc2404c01c2c03e5c8
Chris Thoburn <cthoburn@linkedin.com>
2019-07-19 00:43:28 -0700

    fix test

--- packages/-ember-data/tests/integration/relationships/belongs-to-test.js
+++ packages/-ember-data/tests/integration/relationships/belongs-to-test.js
@@ -12,6 +12,7 @@ import testInDebug from 'dummy/tests/helpers/test-in-debug';
 import DS from 'ember-data';
 import { RecordData, recordDataFor, relationshipsFor, relationshipStateFor } from '@ember-data/store/-private';
 import { identifierCacheFor } from '@ember-data/store/-private';
+import { IDENTIFIERS } from '@ember-data/canary-features';
 
 const { attr: DSattr, hasMany: DShasMany, belongsTo: DSbelongsTo } = DS;
 const { hash } = RSVP;
@@ -1892,8 +1893,8 @@ module('integration/relationship/belongs_to Belongs-To Relationships', function(
 
   test("belongsTo relationship doesn't trigger when model data doesn't support implicit relationship", function(assert) {
     class TestRecordData extends RecordData {
-      constructor(identifier, storeWrapper) {
-        super(identifier, storeWrapper);
+      constructor(...args) {
+        super(...args);
         delete this.__implicitRelationships;
         delete this.__relationships;
       }
@@ -1931,12 +1932,16 @@ module('integration/relationship/belongs_to Belongs-To Relationships', function(
     const createRecordDataFor = env.store.createRecordDataFor;
     env.store.createRecordDataFor = function(modelName, id, lid, storeWrapper) {
       if (modelName === 'book1' || modelName === 'section') {
-        let identifier = identifierCacheFor(this).getOrCreateRecordIdentifier({
-          type: modelName,
-          id,
-          lid,
-        });
-        return new TestRecordData(identifier, storeWrapper);
+        if (IDENTIFIERS) {
+          let identifier = identifierCacheFor(this).getOrCreateRecordIdentifier({
+            type: modelName,
+            id,
+            lid,
+          });
+          return new TestRecordData(identifier, storeWrapper);
+        } else {
+          return new TestRecordData(modelName, id, lid, storeWrapper);
+        }
       }
       return createRecordDataFor.call(this, modelName, id, lid, storeWrapper);
     };
