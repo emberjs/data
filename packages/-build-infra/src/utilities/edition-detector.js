919eca3b11de4e69ef70297f053e5dcc99a7063d
Gaurav Munjal <gaurav9576@yahoo.com>
2019-07-23 20:31:33 -0400

    [BUGFIX RELEASE] Fix generating from blueprints (#6274)
    * [BUGFIX RELEASE] Fix generating from blueprints
    
    * Revert "skip node tests"
    
    This reverts commit db5cac2257578c635d9dbdc118f8c946dc684a83.
    
    * fix failing node tests
    
    * fix prettier eslint
    
    * blueprintPath -> root
    
    * fix eslint prettier again

--- packages/-build-infra/src/utilities/edition-detector.js
+++ packages/-build-infra/src/utilities/edition-detector.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = function(blueprint) {
   blueprint.filesPath = function() {
     let rootPath = process.env.EMBER_VERSION === 'OCTANE' ? 'native-files' : 'files';
-    return path.join(this.path, rootPath);
+    return path.join(blueprint.root, rootPath);
   };
 
   return blueprint;
