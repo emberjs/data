emberjs/data

commit 2bd02463315871ab5fb4060d813f944d24d5aa58

[BUGFIX publish] branch for latest should be release

bin/publish.js
@@ -141,7 +141,12 @@ function assertGitIsClean() {
     }
   }
 
-  let expectedChannelBranch = options.distTag === 'canary' ? 'master' : options.distTag;
+  let expectedChannelBranch =
+    options.distTag === 'canary'
+      ? 'master'
+      : options.distTag === 'latest'
+      ? 'release'
+      : options.distTag;
 
   if (options.channel === 'lts') {
     expectedChannelBranch = `lts-${semver.major(options.currentVersion)}-${semver.minor(options.currentVersion)}`;

commit af46f59b7787c8e9aa3f1369f7de3ab4388efae6

[BUGFIX BETA] Fix linting issue in bin/publish.js
https://github.com/emberjs/data/commit/2bd02463315871ab5fb4060d813f944d24d5aa58
introduced a change to bin/publish.js that failed linting, this fixes
it.

bin/publish.js
@@ -142,11 +142,7 @@ function assertGitIsClean() {
   }
 
   let expectedChannelBranch =
-    options.distTag === 'canary'
-      ? 'master'
-      : options.distTag === 'latest'
-      ? 'release'
-      : options.distTag;
+    options.distTag === 'canary' ? 'master' : options.distTag === 'latest' ? 'release' : options.distTag;
 
   if (options.channel === 'lts') {
     expectedChannelBranch = `lts-${semver.major(options.currentVersion)}-${semver.minor(options.currentVersion)}`;