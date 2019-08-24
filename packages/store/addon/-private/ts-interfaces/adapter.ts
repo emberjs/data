f60bcbc39a13af38c4c77ad416476b96ba92f0e7
snewcomer <snewcomer24@gmail.com>
2019-08-24 09:12:49 -0700

    [FEAT] - add a public Typescript interface for the minimal required adapter interface

---
+++ packages/store/addon/-private/ts-interfaces/adapter.ts
@@ -0,0 +1,18 @@
+export interface Adapter {
+  findRecord(store: any, type: string, id: string | number, snapshot: any): Promise<any>;
+  findAll(store: any, type: string): Promise<any[]>;
+  query(store: any, type: string, query: object): Promise<any>;
+  queryRecord(store: any, type: string, query: object): Promise<any>;
+  generateIdForRecord(store: any, type: string, inputProperties: object): string | number;
+  serialize(snapshot: any, options: object): object;
+  createRecord(store: any, type: string, snapshot: any): Promise<any>;
+  updateRecord(store: any, type: string, snapshot: any): Promise<any>;
+  deleteRecord(store: any, type: string, snapshot: any): Promise<any>;
+  coalesceFindRequests: boolean;
+  findMany(store: any, type: string, ids: string | number[], snapshots: any[]): Promise<any[]>;
+  groupRecordsForFindMany(store: any, snapshots: any[]): object[][];
+  shouldReloadRecord(store: any, snapshot: any): boolean;
+  shouldReloadAll(store: any, snapshot: any[]): boolean;
+  shouldBackgroundReloadRecord(store: any, snapshot: any): boolean;
+  shouldBackgroundReloadAll(store: any, snapshot: any[]): boolean;
+}
