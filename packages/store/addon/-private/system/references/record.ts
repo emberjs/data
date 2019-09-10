b84070dc0043fb9bbaa2e2b6e3303e8d2a048f54
Bert De Block <bert.deblock@bagaar.be>
2019-09-10 21:17:32 +0200

    [DOC RecordReference] Minor spelling improvements

--- packages/store/addon/-private/system/references/record.ts
+++ packages/store/addon/-private/system/references/record.ts
@@ -8,8 +8,8 @@ import { SingleResourceDocument } from '../../ts-interfaces/ember-data-json-api'
 */
 
 /**
-   An RecordReference is a low-level API that allows users and
-   addon author to perform meta-operations on a record.
+   A `RecordReference` is a low-level API that allows users and
+   addon authors to perform meta-operations on a record.
 
    @class RecordReference
    @extends Reference
@@ -42,9 +42,9 @@ export default class RecordReference extends Reference {
   }
 
   /**
-     How the reference will be looked up when it is loaded: Currently
-     this always return `identity` to signifying that a record will be
-     loaded by the `type` and `id`.
+     How the reference will be looked up when it is loaded. Currently
+     this always returns `identity` to signify that a record will be
+     loaded by its `type` and `id`.
 
      Example
 
