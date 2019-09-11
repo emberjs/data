53e1a2acfeb7c7f41cb10cf1dc97531d8859ea39
Chris Thoburn <cthoburn@linkedin.com>
2019-09-11 13:05:18 -0700

    [BUGFIX BETA RELEASE] make fetch function JIT

--- packages/adapter/addon/-private/utils/fetch.ts
+++ packages/adapter/addon/-private/utils/fetch.ts
@@ -2,19 +2,23 @@ import require, { has } from 'require';
 
 type FetchFunction = (input: RequestInfo, init?: RequestInit | undefined) => Promise<Response>;
 
-let _fetch: (() => FetchFunction) | null = null;
+let _fetch: FetchFunction | null = null;
 
-if (has('fetch')) {
-  // use `fetch` module by default, this is commonly provided by ember-fetch
-  let foundFetch = require('fetch').default;
-  _fetch = () => foundFetch;
-} else if (typeof fetch === 'function') {
-  // fallback to using global fetch
-  _fetch = () => fetch;
-} else {
-  throw new Error(
-    'cannot find the `fetch` module or the `fetch` global. Did you mean to install the `ember-fetch` addon?'
-  );
+export default function getFetchFunction(): FetchFunction {
+  if (_fetch !== null) {
+    return _fetch;
+  }
+
+  if (has('fetch')) {
+    // use `fetch` module by default, this is commonly provided by ember-fetch
+    _fetch = require('fetch').default;
+  } else if (typeof fetch === 'function') {
+    // fallback to using global fetch
+    _fetch = fetch;
+  } else {
+    throw new Error(
+      'cannot find the `fetch` module or the `fetch` global. Did you mean to install the `ember-fetch` addon?'
+    );
+  }
+  return _fetch;
 }
-
-export default _fetch;
