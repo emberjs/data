a50ebe4addfc17e623404bb36a5573c85a5e75c5
Gaurav Munjal <Gaurav0@aol.com>
2019-09-29 21:31:40 -0400

    Move fastboot test to its own package/app

---
+++ packages/-fastboot-test-app/testem.js
@@ -0,0 +1,20 @@
+module.exports = {
+  test_page: 'tests/index.html?hidepassed',
+  disable_watching: true,
+  reporter: 'dot',
+  launch_in_ci: ['Chrome'],
+  launch_in_dev: ['Chrome'],
+  browser_args: {
+    Chrome: {
+      ci: [
+        '--headless',
+        '--disable-dev-shm-usage',
+        '--disable-software-rasterizer',
+        '--mute-audio',
+        '--remote-debugging-port=0',
+        '--window-size=1440,900',
+        '--no-sandbox',
+      ],
+    },
+  },
+};
