eadb32a5041946d181fff2cbc2484127b9ab3ef3
Gaurav Munjal <Gaurav0@aol.com>
2019-10-02 13:58:14 -0400

    Fix prettier

--- packages/-fastboot-test-app/types/global.d.ts
+++ packages/-fastboot-test-app/types/global.d.ts
@@ -1,5 +1,5 @@
 // Types for compiled templates
-declare module 'fastboot-test-app/templates/*' { 
+declare module 'fastboot-test-app/templates/*' {
   import { TemplateFactory } from 'htmlbars-inline-precompile';
   const tmpl: TemplateFactory;
   export default tmpl;
