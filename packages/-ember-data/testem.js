9b89797c952fde87e5239451cff491e90b2844d3
Eric Kelly <heroiceric@gmail.com>
2019-10-14 08:15:37 -0700

    wip

--- packages/-ember-data/testem.js
+++ packages/-ember-data/testem.js
@@ -7,14 +7,15 @@ module.exports = {
   browser_args: {
     Chrome: {
       ci: [
+        // --no-sandbox is needed when running Chrome inside a container
+        process.env.CI ? '--no-sandbox' : null,
         '--headless',
         '--disable-dev-shm-usage',
         '--disable-software-rasterizer',
         '--mute-audio',
         '--remote-debugging-port=0',
         '--window-size=1440,900',
-        '--no-sandbox',
-      ],
+      ].filter(Boolean),
     },
   },
 };
