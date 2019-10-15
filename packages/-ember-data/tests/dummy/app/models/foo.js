355aea911c0ccc8d483bfa858f8040da24dc84e7
Chris Thoburn <cthoburn@linkedin.com>
2019-10-15 14:01:42 -0500

    [CHORE] fix babel warning emitted due to anonymous class

--- packages/-ember-data/tests/dummy/app/models/foo.js
+++ packages/-ember-data/tests/dummy/app/models/foo.js
@@ -1,6 +1,6 @@
 import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 
-export default class extends Model {
+export default class Foo extends Model {
   @attr name;
   @belongsTo('foo', { async: false }) parent;
   @hasMany('foo', { async: false }) children;
