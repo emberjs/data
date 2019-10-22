c88c0818b5a2252b6c34023a092edc3e0b333dab
David Tang <dtang@auditboard.com>
2019-10-22 08:59:14 -0700

    Adds relationship-performance-test-app

---
+++ packages/-relationship-performance-test-app/app/routes/application.js
@@ -0,0 +1,22 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  model() {
+    performance.mark('start-find-all');
+    return this.store.findAll('car', { reload: true }).then(cars => {
+      performance.mark('start-outer-materialization');
+      const flattened = cars.map(car => {
+        // enforce materialization of our relationships
+        return {
+          name: car.id,
+          size: car.size.name,
+          type: car.type.name,
+          colors: car.colors.map(color => color.name),
+        };
+      });
+      performance.mark('stop-outer-materialization');
+      performance.mark('end-find-all');
+      return flattened;
+    });
+  },
+});
