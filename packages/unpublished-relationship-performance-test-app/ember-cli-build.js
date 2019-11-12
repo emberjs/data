f8c409d4e280d6415fda7b2ff1f2aa7db0408f0f
Chris Thoburn <cthoburn@linkedin.com>
2019-11-11 22:13:18 -0800

    turn off fingerprinting in the perf app

--- packages/unpublished-relationship-performance-test-app/ember-cli-build.js
+++ packages/unpublished-relationship-performance-test-app/ember-cli-build.js
@@ -6,7 +6,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    fingerprint: {
+      enabled: false,
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
