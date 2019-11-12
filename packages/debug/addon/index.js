bff806cdd7e6dca00bf6c85012d720f4244e7f78
Chris Thoburn <cthoburn@linkedin.com>
2019-11-12 12:23:51 -0800

    [CHORE] documentation for @ember-data/debug package

--- packages/debug/addon/index.js
+++ packages/debug/addon/index.js
@@ -1,5 +1,28 @@
 /**
+  # Overview
+
+  This package provides the `DataAdapter` which the [Ember Inspector](https://github.com/emberjs/ember-inspector)
+  uses to subscribe and retrieve information for the `data` tab in the inspector.
+
+  This package adds roughly .6 KB when minified and compressed to your application in production; however,
+  you can opt out of shipping this addon in production via options in `ember-cli-build.js`
+
+  ```js
+  let app = new EmberApp(defaults, {
+    emberData: {
+      includeDataAdapterInProduction: false
+    }
+  });
+  ```
+
+  When using `ember-data` as a dependency of your app, the default is to ship the inspector support to production.
+
+  When not using `ember-data` as a dependency but instead using EmberData via declaring specific `@ember-data/<package>`
+  dependencies the default is to not ship to production.
+
   @module @ember-data/debug
+  @main @ember-data/debug
+  @public
 */
 import { addObserver, removeObserver } from '@ember/object/observers';
 import { inject as service } from '@ember/service';
@@ -14,7 +37,7 @@ import { typesMapFor } from './setup';
   Implements `@ember/debug/data-adapter` with for EmberData
   integration with the ember-inspector.
 
-  @class InspectorDebugAdapter
+  @class InspectorDataAdapter
   @extends DataAdapter
   @private
 */
