078c63ae3408372628b07ea1e610ca2394694f4f
Chris Thoburn <cthoburn@linkedin.com>
2019-11-14 13:03:38 -0800

    even simpler

--- packages/private-build-infra/addon/index.ts
+++ packages/private-build-infra/addon/index.ts
@@ -5,20 +5,15 @@
 import { has } from 'require';
 import AVAILABLE_PACKAGES from './available-packages';
 
-type PackageSettings = {
-  [key in keyof typeof AVAILABLE_PACKAGES]: boolean;
-};
+function flagState(flag: keyof typeof AVAILABLE_PACKAGES): boolean {
+  const packageName = AVAILABLE_PACKAGES[flag];
+  return has(packageName) || false;
+}
 
-const PACKAGES = {} as PackageSettings;
-Object.keys(AVAILABLE_PACKAGES).forEach(name => {
-  const NAME = AVAILABLE_PACKAGES[name];
-  PACKAGES[NAME] = has(name) || false;
-});
-
-export const HAS_EMBER_DATA_PACKAGE = PACKAGES.HAS_EMBER_DATA_PACKAGE;
-export const HAS_STORE_PACKAGE = PACKAGES.HAS_STORE_PACKAGE;
-export const HAS_MODEL_PACKAGE = PACKAGES.HAS_MODEL_PACKAGE;
-export const HAS_ADAPTER_PACKAGE = PACKAGES.HAS_ADAPTER_PACKAGE;
-export const HAS_SERIALIZER_PACKAGE = PACKAGES.HAS_SERIALIZER_PACKAGE;
-export const HAS_DEBUG_PACKAGE = PACKAGES.HAS_DEBUG_PACKAGE;
-export const HAS_RECORD_DATA_PACKAGE = PACKAGES.HAS_RECORD_DATA_PACKAGE;
+export const HAS_EMBER_DATA_PACKAGE = flagState('HAS_EMBER_DATA_PACKAGE');
+export const HAS_STORE_PACKAGE = flagState('HAS_STORE_PACKAGE');
+export const HAS_MODEL_PACKAGE = flagState('HAS_MODEL_PACKAGE');
+export const HAS_ADAPTER_PACKAGE = flagState('HAS_ADAPTER_PACKAGE');
+export const HAS_SERIALIZER_PACKAGE = flagState('HAS_SERIALIZER_PACKAGE');
+export const HAS_DEBUG_PACKAGE = flagState('HAS_DEBUG_PACKAGE');
+export const HAS_RECORD_DATA_PACKAGE = flagState('HAS_RECORD_DATA_PACKAGE');
