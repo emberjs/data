9f2fb21bf60522c30ff8265ab84042e61be464d2
Chris Thoburn <cthoburn@linkedin.com>
2019-11-14 16:14:43 -0800

    cleanup uniqueAdd

--- packages/private-build-infra/src/stripped-build-plugins.js
+++ packages/private-build-infra/src/stripped-build-plugins.js
@@ -3,7 +3,6 @@
 const FilterImports = require.resolve('babel-plugin-filter-imports');
 const StripClassCallCheck = require.resolve('babel6-plugin-strip-class-callcheck');
 const TransformBlockScoping = require.resolve('@babel/plugin-transform-block-scoping');
-const uniqueAdd = require('./utilities/unique-add');
 
 function isProduction(environment) {
   return /production/.test(environment);
@@ -12,20 +11,18 @@ function isProduction(environment) {
 module.exports = function(environment, app) {
   const isProd = isProduction(environment);
   let plugins = [];
-  let filteredImports = {};
   const DebugMacros = require('./debug-macros')(app, isProd);
   let postTransformPlugins = [];
 
   if (isProd) {
     postTransformPlugins.push([StripClassCallCheck]);
-    uniqueAdd(filteredImports, '@ember-data/store/-debug', ['assertPolymorphicType']);
+    let filteredImports = {
+      '@ember-data/store/-debug': ['assertPolymorphicType'],
+    };
+    plugins.push([FilterImports, { imports: filteredImports }]);
   }
 
-  plugins.push(
-    [FilterImports, { imports: filteredImports }],
-    [TransformBlockScoping, { throwIfClosureRequired: true }],
-    ...DebugMacros
-  );
+  plugins.push([TransformBlockScoping, { throwIfClosureRequired: true }], ...DebugMacros);
 
   return { plugins, postTransformPlugins };
 };
--- packages/private-build-infra/src/utilities/unique-add.js
+++
@@ -1,9 +0,0 @@
-module.exports = function uniqueAdd(obj, key, values) {
-  const a = (obj[key] = obj[key] || []);
-
-  for (let i = 0; i < values.length; i++) {
-    if (a.indexOf(values[i]) === -1) {
-      a.push(values[i]);
-    }
-  }
-};
