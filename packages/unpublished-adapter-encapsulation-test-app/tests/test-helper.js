15817f6a0bd98ee217f6a260b49e3418bbf44645
Gaurav Munjal <Gaurav0@aol.com>
2019-11-25 01:50:12 -0500

    belongsTo tests for links

--- packages/unpublished-adapter-encapsulation-test-app/tests/test-helper.js
+++ packages/unpublished-adapter-encapsulation-test-app/tests/test-helper.js
@@ -2,6 +2,9 @@ import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import configureAsserts from '@ember-data/unpublished-test-infra/test-support/qunit-asserts';
+
+configureAsserts();
 
 setApplication(Application.create(config.APP));
 
