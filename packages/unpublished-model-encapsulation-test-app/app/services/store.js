633b739b26e0c97381f530ea3d153ebd621722e4
Gaurav Munjal <Gaurav0@aol.com>
2019-11-25 19:28:03 -0500

    fix encapsulation test

---
+++ packages/unpublished-model-encapsulation-test-app/app/services/store.js
@@ -0,0 +1,5 @@
+import Store from '@ember-data/store';
+
+export default class DefaultStore extends Store {
+  _modelForMixin() {}
+}
--- packages/unpublished-model-encapsulation-test-app/tests/integration/model-for-test.js
+++ packages/unpublished-model-encapsulation-test-app/tests/integration/model-for-test.js
@@ -1,4 +1,4 @@
-import Store from '@ember-data/store';
+import Store from 'model-encapsulation-test-app/services/store';
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
