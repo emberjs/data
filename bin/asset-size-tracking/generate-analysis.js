c4d4ec7df9210ee84ca0c3b8cf53916a6166c671
Chris Thoburn <cthoburn@linkedin.com>
2019-11-29 21:11:39 -0800

    fix path

--- bin/asset-size-tracking/generate-analysis.js
+++ bin/asset-size-tracking/generate-analysis.js
@@ -9,12 +9,12 @@
  */
 const fs = require('fs');
 const path = require('path');
-let INPUT_FILE = process.argv[2] || false;
+let INPUT_DIST = process.argv[2] || false;
 let OUTPUT_FILE = process.argv[3] || './current-data.json';
 const parseModules = require('./src/parse-modules');
 const getBuiltDist = require('./src/get-built-dist');
 
-const builtAsset = getBuiltDist(INPUT_FILE);
+const builtAsset = getBuiltDist(path.join(INPUT_DIST, 'assets/vendor.js'));
 const library = parseModules(builtAsset);
 const outputPath = path.resolve(__dirname, OUTPUT_FILE);
 
