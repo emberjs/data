517f11a5f4cf0473492c7ec1821dbc6f1b505f63
Gaurav Munjal <Gaurav0@aol.com>
2019-12-05 14:34:55 -0500

    more fixes

--- packages/unpublished-fastboot-test-app/testem.js
+++ packages/unpublished-fastboot-test-app/testem.js
@@ -1,8 +1,17 @@
+const customDotReporter = require('@ember-data/unpublished-test-infra/src/testem/custom-dot-reporter');
+
+const TestIE = process.env.TEST_IE11;
+
+if (TestIE) {
+  // eslint-disable-next-line no-console
+  console.log('\n\nLaunching with IE\n\n');
+}
+
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  reporter: 'dot',
-  launch_in_ci: ['Chrome'],
+  reporter: customDotReporter,
+  launch_in_ci: TestIE ? ['IE'] : ['Chrome'],
   launch_in_dev: ['Chrome'],
   browser_start_timeout: 120,
   browser_args: {
