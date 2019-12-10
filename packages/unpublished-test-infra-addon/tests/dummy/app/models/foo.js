04f240c9753f5f6a09764ee8b3f06bb69e9f2869
Gaurav Munjal <Gaurav0@aol.com>
2019-12-10 17:04:51 -0500

    cull some files we don't need

--- packages/unpublished-test-infra-addon/tests/dummy/app/models/foo.js
+++
@@ -1,7 +0,0 @@
-import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
-
-export default class Foo extends Model {
-  @attr name;
-  @belongsTo('foo', { async: false }) parent;
-  @hasMany('foo', { async: false }) children;
-}
