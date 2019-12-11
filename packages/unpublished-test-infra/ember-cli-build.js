af4ad4696e0dfbe6abb82ca7bdc22e920728c8db
Gaurav Munjal <Gaurav0@aol.com>
2019-12-11 03:36:51 -0500

    [TEST] deprecation infra (#6881)
    Co-authored-by: Chris Thoburn <runspired@gmail.com>

---
+++ packages/unpublished-test-infra/ember-cli-build.js
@@ -0,0 +1,26 @@
+/* eslint node/no-unpublished-require: 'off' */
+
+'use strict';
+
+const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+
+const isProd = true;
+const compatWith = process.env.COMPAT_WITH || '0.0.0';
+
+module.exports = function(defaults) {
+  let app = new EmberAddon(defaults, {
+    emberData: {
+      compatWith,
+    },
+    babel: {
+      // this ensures that the same build-time code stripping that is done
+      // for library packages is also done for our tests and dummy app
+      plugins: [...require('@ember-data/private-build-infra/src/debug-macros')(null, isProd, compatWith)],
+    },
+    'ember-cli-babel': {
+      throwUnlessParallelizable: true,
+    },
+  });
+
+  return app.toTree();
+};
