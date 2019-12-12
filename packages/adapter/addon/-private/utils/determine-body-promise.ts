d2b042075bdc15f9b8cb8d2f5955d8041c15012f
Chris Thoburn <cthoburn@linkedin.com>
2019-12-12 13:29:05 -0800

    more cleanup

--- packages/adapter/addon/-private/utils/determine-body-promise.ts
+++ packages/adapter/addon/-private/utils/determine-body-promise.ts
@@ -1,19 +1,11 @@
 import { warn } from '@ember/debug';
 import { DEBUG } from '@glimmer/env';
-
-import { resolve } from 'rsvp';
-
 import continueOnReject from './continue-on-reject';
-
-interface CustomSyntaxError extends SyntaxError {
-  payload?: string | object;
-}
 
 type Payload = object | string | undefined;
 
-function checkStatus(response: Response, requestData: JQueryAjaxSettings): boolean {
-  const status = response.status;
-  return response.ok && (status === 204 || status === 205 || requestData.method === 'HEAD');
+interface CustomSyntaxError extends SyntaxError {
+  payload: Payload;
 }
 
 /*
@@ -21,57 +13,50 @@ function checkStatus(response: Response, requestData: JQueryAjaxSettings): boole
  * returns `undefined` if the response is successful and has a status code of 204 (No Content),
  * or 205 (Reset Content) or if the request method was 'HEAD', and the plain payload otherwise.
  */
-function _determineBodyPromise(
-  response: Response,
-  requestData: JQueryAjaxSettings,
-  payload: Payload
-): Promise<Payload> {
+function _determineContent(response: Response, requestData: JQueryAjaxSettings, payload: Payload): Payload {
   let ret: Payload = payload;
+  let error;
 
   if (!response.ok) {
-    return resolve(payload);
+    return payload;
   }
 
   try {
     ret = JSON.parse(payload as string);
-  } catch (error) {
-    if (!(error instanceof SyntaxError)) {
-      throw error;
+  } catch (e) {
+    if (!(e instanceof SyntaxError)) {
+      throw e;
     }
-    (error as CustomSyntaxError).payload = payload;
+    (e as CustomSyntaxError).payload = payload;
+    error = e;
+  }
 
-    if (checkStatus(response, requestData)) {
-      ret = undefined;
-      return resolve(ret);
+  const status = response.status;
+  if (response.ok && (status === 204 || status === 205 || requestData.method === 'HEAD')) {
+    return;
+  }
+
+  if (DEBUG) {
+    let message = `The server returned an empty string for ${requestData.method} ${requestData.url}, which cannot be parsed into a valid JSON. Return either null or {}.`;
+    if (payload === '') {
+      warn(message, true, {
+        id: 'ds.adapter.returned-empty-string-as-JSON',
+      });
     }
+  }
 
-    if (DEBUG) {
-      let message = `The server returned an empty string for ${requestData.method} ${requestData.url}, which cannot be parsed into a valid JSON. Return either null or {}.`;
-      if (payload === '') {
-        warn(message, true, {
-          id: 'ds.adapter.returned-empty-string-as-JSON',
-        });
-      }
-    }
-
+  if (error) {
     // eslint-disable-next-line no-console
     console.warn('This response was unable to be parsed as json.', payload);
     throw error;
   }
 
-  if (checkStatus(response, requestData)) {
-    ret = undefined;
-  }
-
-  return resolve(ret);
+  return ret;
 }
 
 // This must remain compatible with ember-fetch
-export function determineBodyPromise(
-  response: Response,
-  requestData: JQueryAjaxSettings
-): Promise<Payload> {
+export function determineBodyPromise(response: Response, requestData: JQueryAjaxSettings): Promise<Payload> {
   // response.text() may resolve or reject
   // it is a native promise, may not have finally
-  return continueOnReject(response.text()).then(payload => _determineBodyPromise(response, requestData, payload));
+  return continueOnReject(response.text()).then(payload => _determineContent(response, requestData, payload));
 }
