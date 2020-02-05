09e718bbbd72007429f370519cb5b887ccd5a463
snewcomer <snewcomer24@gmail.com>
2020-02-04 18:06:04 -0800

    remove accidental addition

--- packages/store/addon/-private/system/store/internal-model-factory.ts
+++ packages/store/addon/-private/system/store/internal-model-factory.ts
@@ -61,7 +61,6 @@ export function internalModelFactoryFor(store: CoreStore): InternalModelFactory 
   if (factory === undefined) {
     factory = new InternalModelFactory(store);
     FactoryCache.set(store, factory);
-    ((store as unknown) as any).__internalModelFactory = factory;
   }
 
   return factory;
