67f974030580084192ccbc0b2e6cc5bc9ade49a6
Chris Thoburn <runspired@users.noreply.github.com>
2020-04-22 13:46:37 -0700

    fix: blueprint tests for classic (#7132)
    * fix: blueprint tests for classic
    
    * fix lint

--- packages/private-build-infra/src/utilities/edition-detector.js
+++ packages/private-build-infra/src/utilities/edition-detector.js
@@ -6,7 +6,11 @@ const { has } = require('@ember/edition-utils');
 
 module.exports = function(blueprint) {
   blueprint.filesPath = function() {
-    let rootPath = has('octane') ? 'native-files' : 'files';
+    let hasOctane = has('octane');
+    if (hasOctane && process.env.EMBER_EDITION === 'classic') {
+      hasOctane = false; //forcible override
+    }
+    let rootPath = hasOctane ? 'native-files' : 'files';
     return path.join(blueprint.root, rootPath);
   };
 
