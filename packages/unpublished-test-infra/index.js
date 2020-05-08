8fc5d67906b2fb4a13cb14b2493d4147033f8084
Chris Thoburn <runspired@gmail.com>
2020-05-08 16:17:55 -0700

    fix lint

--- packages/unpublished-test-infra/index.js
+++ packages/unpublished-test-infra/index.js
@@ -1,4 +1,5 @@
 'use strict';
+// eslint-disable-next-line node/no-unpublished-require
 const merge = require('broccoli-merge-trees');
 const version = require('@ember-data/private-build-infra/src/create-version-module');
 const addonBuildConfigForDataPackage = require('@ember-data/private-build-infra/src/addon-build-config-for-data-package');
