7e65342be6a22d15ac75b3d8083aaccae24620c3
snewcomer <snewcomer24@gmail.com>
2020-05-09 16:09:38 -0700

    [INFRA]: Add Safari to test pipeline

--- packages/-ember-data/testem.js
+++ packages/-ember-data/testem.js
@@ -1,16 +1,34 @@
 const customDotReporter = require('@ember-data/unpublished-test-infra/src/testem/custom-dot-reporter');
 
 const TestIE = process.env.TEST_IE11;
+const TestSafari = process.env.TEST_SAFARI;
 
 if (TestIE) {
   // eslint-disable-next-line no-console
   console.log('\n\nLaunching with IE\n\n');
 }
+if (TestSafari) {
+  // eslint-disable-next-line no-console
+  console.log('\n\nLaunching with Safari\n\n');
+}
+
+function browserPicker() {
+  if (TestSafari) {
+    return ['Safari'];
+  }
+
+  if (TestIE) {
+    return ['IE'];
+  }
+
+  return ['Chrome'];
+}
+
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   reporter: customDotReporter,
-  launch_in_ci: TestIE ? ['IE'] : ['Chrome'],
+  launch_in_ci: browserPicker(),
   launch_in_dev: ['Chrome'],
   browser_start_timeout: 120,
   browser_args: {
