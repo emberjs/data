c08523cdd170088da500ef2930053c82cf2c1a22
Chris Thoburn <runspired@gmail.com>
2020-06-22 11:50:45 -0700

    extend WeakMap

--- packages/store/addon/-private/system/weak-cache.ts
+++ packages/store/addon/-private/system/weak-cache.ts
@@ -2,43 +2,34 @@ import { DEBUG } from '@glimmer/env';
 
 import { addSymbol, symbol } from '../ts-interfaces/utils/symbol';
 
-interface WeakCache<K extends object, V> {
+interface WeakCache<K extends object, V> extends WeakMap<K, V> {
   _symbol?: Symbol | string;
   _fieldName?: string;
   _generator?: (key: K) => V;
   _expectMsg?: (key: K) => string;
 }
 
-class WeakCache<K extends object, V> {
-  private _cache = new WeakMap<K, V>();
-
+class WeakCache<K extends object, V> extends WeakMap<K, V> {
   constructor(_fieldName?: string) {
+    super();
     if (DEBUG) {
       this._fieldName = _fieldName;
       this._symbol = symbol(_fieldName || '');
     }
   }
 
-  get(obj: K): V | undefined {
-    return this._cache.get(obj);
+  has(obj: unknown): obj is K {
+    return super.has(obj as K);
   }
 
-  has(obj: object): obj is K {
-    return this._cache.has(obj as K);
-  }
-
-  delete(obj: K) {
-    return this._cache.delete(obj);
-  }
-
-  set(obj: K, value: V): void {
-    if (DEBUG && this._cache.has(obj)) {
+  set(obj: K, value: V): this {
+    if (DEBUG && super.has(obj)) {
       throw new Error(`${obj} was already assigned a value for ${this._fieldName}`);
     }
     if (DEBUG) {
       addSymbol(obj, this._symbol!, value);
     }
-    this._cache.set(obj, value);
+    return super.set(obj, value);
   }
 
   getWithError(obj: K): V {
@@ -52,11 +43,11 @@ class WeakCache<K extends object, V> {
   }
 
   lookup(obj: K): V {
-    let v = this._cache.get(obj);
+    let v = super.get(obj);
 
     if (v === undefined) {
       v = this._generator!(obj);
-      this._cache.set(obj, v);
+      super.set(obj, v);
 
       if (DEBUG) {
         addSymbol(obj, this._symbol!, v);
