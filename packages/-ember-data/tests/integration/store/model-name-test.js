5bc9c134a2e2d309485e960b9c890d1bd13332c1
Scott Newcomer <snewcomer24@gmail.com>
2020-07-15 22:15:49 -0600

    [CI]: Add Firefox to test pipeline

--- packages/-ember-data/tests/integration/store/model-name-test.js
+++ packages/-ember-data/tests/integration/store/model-name-test.js
@@ -63,6 +63,8 @@ module('@ember-data/model klass.modelName', function(hooks) {
     } catch (e) {
       assert.strictEqual(
         startsWith(e.message, `Cannot assign to read only property 'modelName' of `) ||
+          // Firefox has a different message
+          startsWith(e.message, `"modelName" is read-only`) ||
           // IE11 has a different message
           startsWith(e.message, `Assignment to read-only properties is not allowed in strict mode`) ||
           // Safari aso has a different message
--- packages/unpublished-relationship-performance-test-app/testem.js
+++ packages/unpublished-relationship-performance-test-app/testem.js
@@ -1,18 +1,14 @@
 // eslint-disable-next-line node/no-unpublished-require
 const customDotReporter = require('@ember-data/unpublished-test-infra/src/testem/custom-dot-reporter');
 
-const TestIE = process.env.TEST_IE11;
-
-if (TestIE) {
-  // eslint-disable-next-line no-console
-  console.log('\n\nLaunching with IE\n\n');
-}
+// eslint-disable-next-line no-console
+console.log(`\n\nLaunching with ${process.env.TESTEM_CI_LAUNCHER || 'Chrome'}\n\n`);
 
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   reporter: customDotReporter,
-  launch_in_ci: TestIE ? ['IE'] : ['Chrome'],
+  launch_in_ci: [process.env.TESTEM_CI_LAUNCHER || 'Chrome'],
   launch_in_dev: ['Chrome'],
   browser_start_timeout: 120,
   browser_args: {
@@ -27,5 +23,8 @@ module.exports = {
         '--no-sandbox',
       ],
     },
+  },
+  Firefox: {
+    ci: ['-headless', '-width 1440', '-height 900'],
   },
 };
