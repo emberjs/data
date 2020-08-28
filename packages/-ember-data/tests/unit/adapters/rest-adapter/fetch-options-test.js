1db92097ed44ea6cb208cc76369a443f8d04b7e1
gguri4549 <rudals4549@naver.com>
2020-08-28 10:11:54 +0900

    Update fetch-options-test.js

--- packages/-ember-data/tests/unit/adapters/rest-adapter/fetch-options-test.js
+++ packages/-ember-data/tests/unit/adapters/rest-adapter/fetch-options-test.js
@@ -153,7 +153,7 @@ module('unit/adapters/rest-adapter/fetch-options', function(hooks) {
     const postData = {
       url: 'https://emberjs.com',
       method: 'POST',
-      data: {}
+      data: {},
     };
 
     const postOptions = fetchOptions(postData);
@@ -168,7 +168,7 @@ module('unit/adapters/rest-adapter/fetch-options', function(hooks) {
       url: 'https://emberjs.com',
       method: 'POST',
       data: {},
-      credentials : credentials
+      credentials : credentials,
     };
 
     const postOptions = fetchOptions(postData);
