0ee5c8901e3695cc67001ec9b13ff408506050ba
maxwondercorn <gmartell@pobox.com>
2020-09-07 10:00:40 -0400

    doc: fix review finding - dup export

--- packages/store/addon/-private/system/core-store.ts
+++ packages/store/addon/-private/system/core-store.ts
@@ -852,10 +852,9 @@ abstract class CoreStore extends Service {
 
     ```js
     // app/adapters/post.js
-    import ApplicationAdapter from "./application";
+    import ApplicationAdapter from - "./application";
 
     export default class PostAdapter extends ApplicationAdapter {
-    export default ApplicationAdapter.extend({
       shouldReloadRecord(store, snapshot) {
         return false;
       },
