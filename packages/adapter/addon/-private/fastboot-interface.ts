ba8facdc6d3ff98dd5a46349395aa04133622702
Scott Newcomer <snewcomer24@gmail.com>
2020-12-04 16:19:27 -0800

    Slim down fastboot interface to only used known properties

--- packages/adapter/addon/-private/fastboot-interface.ts
+++ packages/adapter/addon/-private/fastboot-interface.ts
@@ -1,24 +1,9 @@
 export interface Request {
-  method: string;
-  body: unknown;
-  cookies: unknown;
-  headers: unknown;
-  queryParams: unknown;
-  path: string;
   protocol: string;
   host: string;
-}
-
-interface Shoebox {
-  put(key: string, value: unknown): void;
-  retrieve(key: string): undefined | JSON;
 }
 
 export interface FastBoot {
   isFastBoot: boolean;
   request: Request;
-  shoebox: Shoebox;
-  response: unknown; // need types
-  metadata: unknown; // need types
-  deferRendering(promise: Promise<unknown>): unknown;
 }
