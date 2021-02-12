01f5f405b3783a181c26e7d17e9ed021ce89dc3c
Igor Terzic <iterzic@linkedin.com>
2021-02-11 16:48:10 -1000

    Update Changelog for v3.25

--- packages/-ember-data/config/ember-try.js
+++ packages/-ember-data/config/ember-try.js
@@ -63,18 +63,18 @@ module.exports = function() {
           },
         },
         {
-          name: 'ember-lts-3.16',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.16.0',
-            },
-          },
-        },
-        {
           name: 'ember-lts-3.20',
           npm: {
             devDependencies: {
               'ember-source': '~3.20.0',
+            },
+          },
+        },
+        {
+          name: 'ember-lts-3.24',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.24.0',
             },
           },
         },
