3bbf721d3dcf900f8e3ab2648199019f0bed7f19
stpham <stpham@linkedin.com>
2021-03-01 11:39:35 -0800

    chore: eslint styling fix

--- bin/publish.js
+++ bin/publish.js
@@ -24,11 +24,11 @@ Inspiration from https://github.com/glimmerjs/glimmer-vm/commit/01e68d7dddf28ac3
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
+const process = require('process');
 
 const chalk = require('chalk');
 const execa = require('execa');
 const cliArgs = require('command-line-args');
-const process = require('process');
 const semver = require('semver');
 const debug = require('debug')('publish-packages');
 
@@ -121,18 +121,18 @@ function assertGitIsClean() {
     if (options.force) {
       console.log(
         chalk.white('⚠️ ⚠️ ⚠️  Local Git branch has uncommitted changes!\n\t') +
-        chalk.yellow('Passed option: ') +
-        chalk.white('--force') +
-        chalk.grey(' :: ignoring unclean git working tree')
+          chalk.yellow('Passed option: ') +
+          chalk.white('--force') +
+          chalk.grey(' :: ignoring unclean git working tree')
       );
     } else {
       console.log(
         chalk.red('💥 Git working tree is not clean. 💥 \n\t') +
-        chalk.grey('Use ') +
-        chalk.white('--force') +
-        chalk.grey(' to ignore this warning and publish anyway\n') +
-        chalk.yellow('⚠️  Publishing from an unclean working state may result in a broken release ⚠️\n\n') +
-        chalk.grey(`Status:\n${status}`)
+          chalk.grey('Use ') +
+          chalk.white('--force') +
+          chalk.grey(' to ignore this warning and publish anyway\n') +
+          chalk.yellow('⚠️  Publishing from an unclean working state may result in a broken release ⚠️\n\n') +
+          chalk.grey(`Status:\n${status}`)
       );
       process.exit(1);
     }
@@ -142,18 +142,18 @@ function assertGitIsClean() {
     if (options.force) {
       console.log(
         chalk.white('⚠️ ⚠️ ⚠️  Local Git branch is not in sync with origin branch') +
-        chalk.yellow('\n\tPassed option: ') +
-        chalk.white('--force') +
-        chalk.grey(' :: ignoring unsynced git branch')
+          chalk.yellow('\n\tPassed option: ') +
+          chalk.white('--force') +
+          chalk.grey(' :: ignoring unsynced git branch')
       );
     } else {
       console.log(
         chalk.red('💥 Local Git branch is not in sync with origin branch. 💥 \n\t') +
-        chalk.grey('Use ') +
-        chalk.white('--force') +
-        chalk.grey(' to ignore this warning and publish anyway\n') +
-        chalk.yellow('⚠️  Publishing from an unsynced working state may result in a broken release ⚠️') +
-        chalk.grey(`Status:\n${status}`)
+          chalk.grey('Use ') +
+          chalk.white('--force') +
+          chalk.grey(' to ignore this warning and publish anyway\n') +
+          chalk.yellow('⚠️  Publishing from an unsynced working state may result in a broken release ⚠️') +
+          chalk.grey(`Status:\n${status}`)
       );
       process.exit(1);
     }
@@ -175,19 +175,19 @@ function assertGitIsClean() {
         chalk.white(
           `⚠️ ⚠️ ⚠️  Expected to publish npm tag ${options.distTag} from the git branch ${expectedChannelBranch}, but found ${foundBranch}`
         ) +
-        chalk.yellow('\n\tPassed option: ') +
-        chalk.white('--force') +
-        chalk.grey(' :: ignoring unexpected branch')
+          chalk.yellow('\n\tPassed option: ') +
+          chalk.white('--force') +
+          chalk.grey(' :: ignoring unexpected branch')
       );
     } else {
       console.log(
         chalk.red(
           `💥 Expected to publish npm tag ${options.distTag} from the git branch ${expectedChannelBranch}, but found ${foundBranch} 💥 \n\t`
         ) +
-        chalk.grey('Use ') +
-        chalk.white('--force') +
-        chalk.grey(' to ignore this warning and publish anyway\n') +
-        chalk.yellow('⚠️  Publishing from an incorrect branch may result in a broken release ⚠️')
+          chalk.grey('Use ') +
+          chalk.white('--force') +
+          chalk.grey(' to ignore this warning and publish anyway\n') +
+          chalk.yellow('⚠️  Publishing from an incorrect branch may result in a broken release ⚠️')
       );
       process.exit(1);
     }
@@ -281,9 +281,9 @@ function packAllPackages() {
           if (pkgInfo.scripts.prepublish || pkgInfo.scripts.prepare) {
             console.log(
               `⚠️ ` +
-              chalk.grey(
-                `${pkgInfo.name} has both a 'prepublishOnly' and either 'prepare' or 'publish' scripts. Running prepublishOnly manually before instead of after publish and prepare. See https://github.com/npm/npm/issues/15363`
-              )
+                chalk.grey(
+                  `${pkgInfo.name} has both a 'prepublishOnly' and either 'prepare' or 'publish' scripts. Running prepublishOnly manually before instead of after publish and prepare. See https://github.com/npm/npm/issues/15363`
+                )
             );
           }
           execWithLog(`cd ${pkgDir} && npm run prepublishOnly`);
