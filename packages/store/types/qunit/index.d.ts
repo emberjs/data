186df147401be159c48092f051c4b43d7472d354
scott-newcomer <snewcomer24@gmail.com>
2021-03-24 17:33:20 -0500

    yarn resolution 1.7.3

--- packages/store/types/qunit/index.d.ts
+++ packages/store/types/qunit/index.d.ts
@@ -1,29 +1,20 @@
-interface DeprecationConfig {
-  id: string;
-  count?: number;
-  until: string;
-  message?: string;
-  url?: string;
-}
-interface WarningConfig {
-  id: string;
-  count?: number;
-  until?: string;
-  message?: string;
-  url?: string;
-}
+declare global {
+  interface DeprecationConfig {
+    id: string;
+    count?: number;
+    until: string;
+    message?: string;
+    url?: string;
+  }
+  interface WarningConfig {
+    id: string;
+    count?: number;
+    until?: string;
+    message?: string;
+    url?: string;
+  }
 
-interface Assert {
-  expectDeprecation(callback: () => unknown, options: DeprecationConfig | string | RegExp): Promise<void>;
-  expectNoDeprecation(callback: () => unknown): Promise<void>;
-  expectWarning(callback: () => unknown, options: WarningConfig | string | RegExp): Promise<void>;
-  expectNoWarning(callback: () => unknown): Promise<void>;
-  expectAssertion(callback: () => unknown, matcher: string | RegExp): Promise<void>;
-  expectNoAssertion(callback: () => unknown): Promise<void>;
-}
-
-declare namespace QUnit {
-  export interface Assert {
+  interface Assert {
     expectDeprecation(callback: () => unknown, options: DeprecationConfig | string | RegExp): Promise<void>;
     expectNoDeprecation(callback: () => unknown): Promise<void>;
     expectWarning(callback: () => unknown, options: WarningConfig | string | RegExp): Promise<void>;
@@ -31,8 +22,21 @@ declare namespace QUnit {
     expectAssertion(callback: () => unknown, matcher: string | RegExp): Promise<void>;
     expectNoAssertion(callback: () => unknown): Promise<void>;
   }
+
+  declare namespace QUnit {
+    export interface Assert {
+      expectDeprecation(callback: () => unknown, options: DeprecationConfig | string | RegExp): Promise<void>;
+      expectNoDeprecation(callback: () => unknown): Promise<void>;
+      expectWarning(callback: () => unknown, options: WarningConfig | string | RegExp): Promise<void>;
+      expectNoWarning(callback: () => unknown): Promise<void>;
+      expectAssertion(callback: () => unknown, matcher: string | RegExp): Promise<void>;
+      expectNoAssertion(callback: () => unknown): Promise<void>;
+    }
+  }
+
+  declare interface QUnit {
+    assert: Assert;
+  }
 }
 
-declare interface QUnit {
-  assert: Assert;
-}
+export = QUnit;
