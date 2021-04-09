90b02d852d7ed1b2fb57f76a829ad19293890665
Chris Thoburn <runspired@gmail.com>
2021-04-09 10:29:26 -0700

    default to launching with no container

--- packages/unpublished-test-infra/testem.js
+++ packages/unpublished-test-infra/testem.js
@@ -1,5 +1,5 @@
 module.exports = {
-  test_page: 'tests/index.html?hidepassed',
+  test_page: 'tests/index.html?hidepassed&nocontainer',
   disable_watching: true,
   launch_in_ci: ['Chrome'],
   launch_in_dev: ['Chrome'],
