9af88f1dcc2809e61500ac86f2eaf34663533568
Chris Thoburn <runspired@gmail.com>
2021-04-13 14:19:15 -0700

    run tests in the record-data package

--- packages/record-data/ember-cli-build.js
+++ packages/record-data/ember-cli-build.js
@@ -3,9 +3,22 @@
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
+  const isProd = process.env.EMBER_ENV === 'production';
+  const compatWith = process.env.EMBER_DATA_FULL_COMPAT ? '99.0' : null;
   let app = new EmberAddon(defaults, {
+    emberData: {
+      compatWith,
+    },
+    babel: {
+      // this ensures that the same build-time code stripping that is done
+      // for library packages is also done for our tests and dummy app
+      plugins: [...require('@ember-data/private-build-infra/src/debug-macros')(null, isProd, compatWith)],
+    },
     'ember-cli-babel': {
       throwUnlessParallelizable: true,
+    },
+    'ember-cli-terser': {
+      exclude: ['assets/dummy.js', 'assets/tests.js', 'assets/test-support.js'],
     },
   });
 
