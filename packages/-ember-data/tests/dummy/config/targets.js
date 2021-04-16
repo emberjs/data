59cf38f8e2512da47bf373ef3bf19ca793025c5e
Chris Thoburn <runspired@gmail.com>
2021-04-15 23:28:48 -0700

    fix ie11

--- packages/-ember-data/tests/dummy/config/targets.js
+++ packages/-ember-data/tests/dummy/config/targets.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const browsers = ['last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions'];
-const targetIsIE11 = process.env.TESTEM_CI_LAUNCHER === 'IE';
+const targetIsIE11 = process.env.TESTEM_CI_LAUNCHER === 'IE' || !!process.env.TARGET_IE11;
 
 if (targetIsIE11) {
   browsers.push('ie 11');
