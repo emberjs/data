16ab9551adcbd6d2953698b9a4b6511bdd90ec3c
scott-newcomer <snewcomer24@gmail.com>
2021-05-23 22:34:11 -0500

    moar peekRecord and findRecord tests

--- packages/-ember-data/tests/integration/adapter/rest-adapter/find-record-test.js
+++ packages/-ember-data/tests/integration/adapter/rest-adapter/find-record-test.js
@@ -6,6 +6,7 @@ import { setupTest } from 'ember-qunit';
 import RESTAdapter from '@ember-data/adapter/rest';
 import Model, { attr } from '@ember-data/model';
 import RESTSerializer from '@ember-data/serializer/rest';
+import { recordIdentifierFor } from '@ember-data/store';
 
 import { ajaxResponse } from './-ajax-mocks';
 
@@ -51,50 +52,85 @@ module('integration/adapter/rest_adapter - REST Adapter - findRecord', function 
     assert.strictEqual(post.get('name'), 'Rails is omakase');
   });
 
-  test('findRecord - basic payload (with identifier)', async function (assert) {
-    const Post = Model.extend({
-      name: attr('string'),
+  // Identifier tests
+  [
+    // Ok
+    { type: 'post', id: '1', desc: 'type and id' },
+    { type: 'post', id: '1', lid: 'post:1', desc: 'type, id and lid' },
+    {
+      type: 'post',
+      desc: 'type and lid',
+      initFn: (store, findRecordArgs) => {
+        let record = store.push({
+          data: {
+            type: 'post',
+            id: '1',
+          },
+        });
+
+        Object.assign(findRecordArgs, { lid: recordIdentifierFor(record).lid });
+      },
+    },
+    {
+      type: 'post',
+      id: null,
+      desc: 'type, null id, and lid',
+      initFn: (store, findRecordArgs) => {
+        let record = store.push({
+          data: {
+            type: 'post',
+            id: '1',
+          },
+        });
+
+        Object.assign(findRecordArgs, { lid: recordIdentifierFor(record).lid });
+      },
+    },
+    // Error
+    { type: 'post', id: null, errorMsg: 'Assertion Failed: Expected an identifier object with (type and id) or lid' },
+    { lid: 'post:1', desc: 'no local data for lid', errorMsg: 'resource.type needs to be a string' },
+  ].forEach(({ type, id, lid, desc, initFn, errorMsg }) => {
+    test(`findRecord - basic payload (${desc})`, async function (assert) {
+      const Post = Model.extend({
+        name: attr('string'),
+      });
+      const Comment = Model.extend({
+        name: attr('string'),
+      });
+      this.owner.register('model:post', Post);
+      this.owner.register('model:comment', Comment);
+      this.owner.register('adapter:application', RESTAdapter.extend());
+      this.owner.register('serializer:application', RESTSerializer.extend());
+
+      const store = this.owner.lookup('service:store');
+      const adapter = store.adapterFor('application');
+      const ajaxCallback = ajaxResponse(adapter, { posts: [{ id: '1', name: 'Rails is omakase' }] });
+      let allArgs = { type, id, lid };
+      let findRecordArgs = {};
+      Object.keys(allArgs).forEach((key) => {
+        if (key !== 'undefined') {
+          findRecordArgs[key] = allArgs[key];
+        }
+      });
+
+      if (initFn) {
+        initFn(store, findRecordArgs);
+      }
+
+      try {
+        const post = await store.findRecord(findRecordArgs);
+        const { passedUrl, passedVerb, passedHash } = ajaxCallback();
+
+        assert.strictEqual(passedUrl, '/posts/1');
+        assert.strictEqual(passedVerb, 'GET');
+        assert.deepEqual(passedHash.data, {});
+
+        assert.strictEqual(post.get('id'), '1');
+        assert.strictEqual(post.get('name'), 'Rails is omakase');
+      } catch (e) {
+        assert.equal(e.message, errorMsg);
+      }
     });
-    const Comment = Model.extend({
-      name: attr('string'),
-    });
-    this.owner.register('model:post', Post);
-    this.owner.register('model:comment', Comment);
-    this.owner.register('adapter:application', RESTAdapter.extend());
-    this.owner.register('serializer:application', RESTSerializer.extend());
-
-    const store = this.owner.lookup('service:store');
-    const adapter = store.adapterFor('application');
-    const ajaxCallback = ajaxResponse(adapter, { posts: [{ id: '1', name: 'Rails is omakase' }] });
-    const post = await store.findRecord({ type: 'post', id: '1' });
-    const { passedUrl, passedVerb, passedHash } = ajaxCallback();
-
-    assert.strictEqual(passedUrl, '/posts/1');
-    assert.strictEqual(passedVerb, 'GET');
-    assert.deepEqual(passedHash.data, {});
-
-    assert.strictEqual(post.get('id'), '1');
-    assert.strictEqual(post.get('name'), 'Rails is omakase');
-  });
-
-  test('findRecord - fails with insufficient identifier information', async function (assert) {
-    const Post = Model.extend({
-      name: attr('string'),
-    });
-    const Comment = Model.extend({
-      name: attr('string'),
-    });
-    this.owner.register('model:post', Post);
-    this.owner.register('model:comment', Comment);
-    this.owner.register('adapter:application', RESTAdapter.extend());
-    this.owner.register('serializer:application', RESTSerializer.extend());
-
-    const store = this.owner.lookup('service:store');
-    try {
-      await store.findRecord({ id: '1' });
-    } catch (e) {
-      assert.equal(e.message, 'Assertion Failed: Expected an identifier object with (type and id) or lid');
-    }
   });
 
   test('findRecord - passes buildURL a requestType', async function (assert) {
--- packages/-ember-data/tests/unit/store/peek-record-test.js
+++ packages/-ember-data/tests/unit/store/peek-record-test.js
@@ -1,11 +1,11 @@
 import EmberObject from '@ember/object';
-import { run } from '@ember/runloop';
 
 import { module, test } from 'qunit';
 
 import DS from 'ember-data';
 import { setupTest } from 'ember-qunit';
 
+import { recordIdentifierFor } from '@ember-data/store';
 import testInDebug from '@ember-data/unpublished-test-infra/test-support/test-in-debug';
 
 module('unit/store/peekRecord - Store peekRecord', function (hooks) {
@@ -15,72 +15,98 @@ module('unit/store/peekRecord - Store peekRecord', function (hooks) {
     this.owner.register('model:person', DS.Model.extend());
   });
 
-  test('peekRecord should return the record if it is in the store ', function (assert) {
+  test('peekRecord should return the record if it is in the store', function (assert) {
     let store = this.owner.lookup('service:store');
 
-    run(() => {
-      let person = store.push({
-        data: {
-          type: 'person',
-          id: '1',
-        },
-      });
-      assert.equal(
-        person,
-        store.peekRecord('person', 1),
-        'peekRecord only return the corresponding record in the store'
-      );
+    let person = store.push({
+      data: {
+        type: 'person',
+        id: '1',
+      },
     });
+    assert.equal(person, store.peekRecord('person', 1), 'peekRecord only return the corresponding record in the store');
   });
 
   test('peekRecord should return the record with identifier as argument', function (assert) {
     let store = this.owner.lookup('service:store');
 
-    run(() => {
+    let person = store.push({
+      data: {
+        type: 'person',
+        id: '1',
+      },
+    });
+    assert.equal(
+      person,
+      store.peekRecord({ type: 'person', id: 1 }),
+      'peekRecord only return the corresponding record in the store'
+    );
+  });
+
+  test('peekRecord should return null if the record is not in the store ', function (assert) {
+    let store = this.owner.lookup('service:store');
+
+    assert.equal(
+      null,
+      store.peekRecord('person', 1),
+      'peekRecord returns null if the corresponding record is not in the store'
+    );
+  });
+
+  testInDebug('peekRecord should assert if not passed both model name and id', function (assert) {
+    let store = this.owner.lookup('service:store');
+
+    assert.expectAssertion(() => {
+      store.peekRecord('my-id');
+    }, /Expected id to be a string or number, received undefined/);
+  });
+
+  testInDebug('peekRecord should assert if passed a model class instead of model name', function (assert) {
+    let store = this.owner.lookup('service:store');
+
+    assert.expectAssertion(() => {
+      let modelClass = EmberObject.extend();
+      store.peekRecord(modelClass, 'id');
+    }, /Passing classes to store methods has been removed/);
+  });
+
+  // Identifier tests
+  [
+    // Ok
+    { type: 'person', id: '1', desc: 'type and id' },
+    { type: 'person', id: '1', lid: 'person:1', desc: 'type, id and lid' },
+    { type: 'person', lid: 'TODO', desc: 'type and lid' },
+    { type: 'person', id: null, lid: 'TODO', desc: 'type, null id, and lid' },
+    // Error
+    // { type: 'person', id: null, errorMsg: 'Assertion Failed: Expected an identifier object with (type and id) or lid' },
+  ].forEach(({ type, id, lid, desc, errorMsg }) => {
+    test(`peekRecord (${desc})`, function (assert) {
+      let store = this.owner.lookup('service:store');
+
       let person = store.push({
         data: {
           type: 'person',
           id: '1',
         },
       });
+
+      let allArgs = { type, id, lid };
+      let peekRecordArgs = {};
+      Object.keys(allArgs).forEach((key) => {
+        if (key !== 'undefined') {
+          peekRecordArgs[key] = allArgs[key];
+        }
+
+        if (key === 'lid' && lid === 'TODO') {
+          peekRecordArgs[key] = recordIdentifierFor(person).lid;
+        }
+      });
+
       assert.equal(
         person,
-        store.peekRecord({ type: 'person', id: 1 }),
+        store.peekRecord(peekRecordArgs),
         'peekRecord only return the corresponding record in the store'
       );
-    });
-  });
-
-  test('peekRecord should return null if the record is not in the store ', function (assert) {
-    let store = this.owner.lookup('service:store');
-
-    run(() => {
-      assert.equal(
-        null,
-        store.peekRecord('person', 1),
-        'peekRecord returns null if the corresponding record is not in the store'
-      );
-    });
-  });
-
-  testInDebug('peekRecord should assert if not passed both model name and id', function (assert) {
-    let store = this.owner.lookup('service:store');
-
-    run(() => {
-      assert.expectAssertion(() => {
-        store.peekRecord('my-id');
-      }, /Expected id to be a string or number, received undefined/);
-    });
-  });
-
-  testInDebug('peekRecord should assert if passed a model class instead of model name', function (assert) {
-    let store = this.owner.lookup('service:store');
-
-    run(() => {
-      assert.expectAssertion(() => {
-        let modelClass = EmberObject.extend();
-        store.peekRecord(modelClass, 'id');
-      }, /Passing classes to store methods has been removed/);
     });
   });
 });
