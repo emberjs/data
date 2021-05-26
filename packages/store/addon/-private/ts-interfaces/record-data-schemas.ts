351f6e0564009ec1d9807b73f90cf3218e748c2d
Chris Thoburn <runspired@gmail.com>
2021-05-26 16:14:30 -0700

    more schema annotations

--- packages/store/addon/-private/ts-interfaces/record-data-schemas.ts
+++ packages/store/addon/-private/ts-interfaces/record-data-schemas.ts
@@ -15,11 +15,13 @@ export interface RelationshipSchema {
   // TODO @runspired sohuld RFC be update to enforce async and inverse are set? else internals need to know
   // that meta came from DS.Model vs not from DS.Model as defaults should switch.
   options: {
-    async?: boolean;
+    async?: boolean; // controls inverse unloading "client side delete semantics" so we should replace that with a real flag
     polymorphic?: boolean;
     inverse?: string | null; // property key on the related type (if any)
     [key: string]: unknown;
   };
+  inverse?: string | null;
+  inverseIsAsync?: boolean;
   name: string; // property key for this relationship
 }
 
