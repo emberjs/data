5f19dce2018461012d3673bacbcbcf451836ddc8
Igor Terzic <iterzic@linkedin.com>
2021-08-05 09:34:53 -0500

    wrote test

--- packages/-ember-data/tests/integration/store-test.js
+++ packages/-ember-data/tests/integration/store-test.js
@@ -443,6 +443,58 @@ module('integration/store - findRecord', function (hooks) {
     assert.strictEqual(car.get('model'), 'Princess', 'cached record ignored, record reloaded via server');
   });
 
+  test('store#findRecord caches the inflight request igor', async function (assert) {
+    assert.expect(5);
+
+    let calls = 0;
+    let resolveHandler;
+    let result = {
+      data: {
+        type: 'car',
+        id: '1',
+        attributes: {
+          make: 'BMC',
+          model: 'Mini'
+        },
+      },
+    };
+
+    const testAdapter = DS.JSONAPIAdapter.extend({
+      shouldReloadRecord(store, type, id, snapshot) {
+        assert.ok(false, 'shouldReloadRecord should not be called when { reload: true }');
+      },
+      async findRecord() {
+        calls++;
+
+        return new Promise((resolve) => {
+          resolveHandler = resolve;
+        })
+
+      },
+    });
+
+    this.owner.register('adapter:application', testAdapter);
+    this.owner.register('serializer:application', JSONAPISerializer.extend());
+    let firstPromise, secondPromise;
+
+    run(() => {
+      firstPromise = store.findRecord('car', '1');
+    });
+
+    run(() => {
+      secondPromise = store.findRecord('car', '1');
+    });
+
+    assert.strictEqual(calls, 1, 'We made one call to findRecord');
+    assert.strictEqual(firstPromise, secondPromise, 'Promise is cached');
+
+    resolveHandler(result); 
+    let car1 = await firstPromise;
+    let car2 = await secondPromise;
+
+    assert.strictEqual(car1, car2, 'we receive the same car back');
+  });
+
   test('store#findRecord { backgroundReload: false } returns cached record and does not reload in the background', async function (assert) {
     assert.expect(2);
 
