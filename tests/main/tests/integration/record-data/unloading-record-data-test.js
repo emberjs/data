35cff282c73a5e20a2b38ae63d1e98d98175e114
machty <machty@gmail.com>
2021-09-07 08:14:54 -0400

    Address PR feedback, simplify test

--- tests/main/tests/integration/record-data/unloading-record-data-test.js
+++ tests/main/tests/integration/record-data/unloading-record-data-test.js
@@ -6,7 +6,8 @@ import { resolve } from 'rsvp';
 import { setupTest } from 'ember-qunit';
 
 import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
-import { DEPRECATE_V1_RECORD_DATA } from '@ember-data/private-build-infra/deprecations';
+import { RecordData } from '@ember-data/record-data/-private';
+import { recordDataFor } from '@ember-data/store/-private';
 
 class Person extends Model {
   @hasMany('pet', { inverse: null, async: false })
@@ -22,7 +23,80 @@ class Pet extends Model {
   name;
 }
 
-module('RecordData Compatibility', function (hooks) {
+class CustomRecordData extends RecordData {}
+
+module('RecordData - Unloading', function (hooks) {
+  let store, shen, chris;
+  setupTest(hooks);
+
+  hooks.beforeEach(function (assert) {
+    let { owner } = this;
+    owner.register('model:person', Person);
+    owner.register('model:pet', Pet);
+    store = owner.lookup('service:store');
+
+    const originalCreateRecordDataFor = store.createRecordDataFor;
+    store.createRecordDataFor = function provideCustomRecordData(type, id, lid, storeWrapper) {
+      if (type === 'pet') {
+        const identifier = this.identifierCache.getOrCreateRecordIdentifier({ type, id, lid });
+        return new CustomRecordData(identifier, storeWrapper);
+      } else {
+        return originalCreateRecordDataFor.call(this, type, id, lid, storeWrapper);
+      }
+    };
+
+    chris = store.push({
+      data: {
+        type: 'person',
+        id: '1',
+        attributes: { name: 'Chris' },
+        relationships: {
+          pets: {
+            data: [
+              { type: 'pet', id: '1' },
+              { type: 'pet', id: '2' },
+            ],
+          },
+        },
+      },
+      included: [
+        {
+          type: 'pet',
+          id: '1',
+          attributes: { name: 'Shen' },
+          relationships: {
+            owner: { data: { type: 'person', id: '1' } },
+          },
+        },
+        {
+          type: 'pet',
+          id: '2',
+          attributes: { name: 'Prince' },
+          relationships: {
+            owner: { data: { type: 'person', id: '1' } },
+          },
+        },
+      ],
+    });
+    let pets = chris.get('pets');
+    shen = pets.objectAt(0);
+
+    assert.equal(shen.get('name'), 'Shen', 'We found Shen');
+
+    assert.equal(recordDataFor(chris).constructor, RecordData, 'We used the default record-data for person');
+    assert.equal(recordDataFor(shen).constructor, CustomRecordData, 'We used the custom record-data for pets');
+  });
+
+  test(`store.unloadRecord on a record with default RecordData with relationship to a record with custom RecordData does not error`, async function (assert) {
+    run(() => chris.unloadRecord());
+  });
+
+  test(`store.unloadRecord on a record with custom RecordData with relationship to a record with default RecordData does not error`, async function (assert) {
+    run(() => shen.unloadRecord());
+  });
+});
+
+module('RecordData - Instantiation', function (hooks) {
   let store;
   setupTest(hooks);
 
@@ -31,288 +105,6 @@ module('RecordData Compatibility', function (hooks) {
     owner.register('model:person', Person);
     owner.register('model:pet', Pet);
     store = owner.lookup('service:store');
-  });
-
-  class V1CustomRecordData {
-    constructor(identifier, storeWrapper) {
-      this.type = identifier.type;
-      this.id = identifier.id || null;
-      this.clientId = identifier.lid;
-      this.storeWrapper = storeWrapper;
-      this.attributes = null;
-      this.relationships = null;
-    }
-
-    pushData(jsonApiResource, shouldCalculateChanges) {
-      let oldAttrs = this.attributes;
-      let changedKeys;
-
-      this.attributes = jsonApiResource.attributes || null;
-
-      if (shouldCalculateChanges) {
-        changedKeys = Object.keys(Object.assign({}, oldAttrs, this.attributes));
-      }
-
-      return changedKeys || [];
-    }
-
-    getAttr(member) {
-      return this.attributes !== null ? this.attributes[member] : undefined;
-    }
-
-    // TODO missing from RFC but required to implement
-    _initRecordCreateOptions(options) {
-      return options !== undefined ? options : {};
-    }
-    // TODO missing from RFC but required to implement
-    getResourceIdentifier() {
-      return {
-        id: this.id,
-        type: this.type,
-        clientId: this.clientId,
-      };
-    }
-    isEmpty() {
-      return false;
-    }
-    // TODO missing from RFC but required to implement
-    unloadRecord() {
-      this.attributes = null;
-      this.relationships = null;
-    }
-    // TODO missing from RFC but required to implement
-    isNew() {
-      return this.id === null;
-    }
-    isDeleted() {
-      return false;
-    }
-    isDeletionCommitted() {
-      return false;
-    }
-
-    adapterDidCommit() {}
-    didCreateLocally() {}
-    adapterWillCommit() {}
-    saveWasRejected() {}
-    adapterDidDelete() {}
-    recordUnloaded() {}
-    rollbackAttributes() {}
-    rollbackAttribute() {}
-    changedAttributes() {}
-    hasChangedAttributes() {}
-    setAttr() {}
-    getHasMany() {}
-    addToHasMany() {}
-    removeFromHasMany() {}
-    getBelongsTo() {}
-  }
-  class V2CustomRecordData {
-    version = '2';
-    constructor(identifier, storeWrapper) {
-      this.type = identifier.type;
-      this.id = identifier.id || null;
-      this.clientId = identifier.lid;
-      this.storeWrapper = storeWrapper;
-      this.attributes = null;
-      this.relationships = null;
-    }
-
-    pushData(identifier, jsonApiResource, shouldCalculateChanges) {
-      let oldAttrs = this.attributes;
-      let changedKeys;
-
-      this.attributes = jsonApiResource.attributes || null;
-
-      if (shouldCalculateChanges) {
-        changedKeys = Object.keys(Object.assign({}, oldAttrs, this.attributes));
-      }
-
-      return changedKeys || [];
-    }
-
-    getAttr(identifier, member) {
-      return this.attributes !== null ? this.attributes[member] : undefined;
-    }
-
-    clientDidCreate(options) {
-      return options !== undefined ? options : {};
-    }
-    isEmpty() {
-      return false;
-    }
-    unloadRecord() {
-      this.attributes = null;
-      this.relationships = null;
-    }
-    isNew() {
-      return this.id === null;
-    }
-    isDeleted() {
-      return false;
-    }
-    isDeletionCommitted() {
-      return false;
-    }
-
-    adapterDidCommit() {}
-    didCreateLocally() {}
-    adapterWillCommit() {}
-    saveWasRejected() {}
-    adapterDidDelete() {}
-    recordUnloaded() {}
-    rollbackAttributes() {}
-    rollbackAttribute() {}
-    changedAttributes() {}
-    hasChangedAttributes() {}
-    setAttr() {}
-    update() {}
-    getRelationship() {}
-  }
-
-  const CustomRecordData = DEPRECATE_V1_RECORD_DATA ? V1CustomRecordData : V2CustomRecordData;
-
-  test(`store.unloadRecord on a record with default RecordData with relationship to a record with custom RecordData does not error`, async function (assert) {
-    const originalCreateRecordDataFor = store.createRecordDataFor;
-    let customCalled = 0,
-      customCalledFor = [],
-      originalCalled = 0,
-      originalCalledFor = [];
-    store.createRecordDataFor = function provideCustomRecordData(identifier, storeWrapper) {
-      if (identifier.type === 'pet') {
-        customCalled++;
-        customCalledFor.push(identifier);
-        return new CustomRecordData(identifier, storeWrapper);
-      } else {
-        originalCalled++;
-        originalCalledFor.push(identifier);
-        return originalCreateRecordDataFor.call(this, identifier, storeWrapper);
-      }
-    };
-
-    let chris = store.push({
-      data: {
-        type: 'person',
-        id: '1',
-        attributes: { name: 'Chris' },
-        relationships: {
-          pets: {
-            data: [
-              { type: 'pet', id: '1' },
-              { type: 'pet', id: '2' },
-            ],
-          },
-        },
-      },
-      included: [
-        {
-          type: 'pet',
-          id: '1',
-          attributes: { name: 'Shen' },
-          relationships: {
-            owner: { data: { type: 'person', id: '1' } },
-          },
-        },
-        {
-          type: 'pet',
-          id: '2',
-          attributes: { name: 'Prince' },
-          relationships: {
-            owner: { data: { type: 'person', id: '1' } },
-          },
-        },
-      ],
-    });
-    let pets = chris.pets;
-    let shen = pets.at(0);
-
-    if (DEPRECATE_V1_RECORD_DATA) {
-      assert.expectDeprecation({ id: 'ember-data:deprecate-v1-cache', count: 2 });
-    }
-
-    assert.strictEqual(shen.name, 'Shen', 'We found Shen');
-    assert.strictEqual(customCalled, 2, 'we used the custom record-data for pet');
-    assert.deepEqual(
-      customCalledFor.map((i) => {
-        return { type: i.type, id: i.id };
-      }),
-      [
-        { id: '1', type: 'pet' },
-        { id: '2', type: 'pet' },
-      ],
-      'we used the cutom record-data for the correct pets'
-    );
-    assert.strictEqual(originalCalled, 1, 'we used the default record-data for person');
-    assert.deepEqual(
-      originalCalledFor.map((i) => {
-        return { type: i.type, id: i.id };
-      }),
-      [{ id: '1', type: 'person' }],
-      'we used the default record-data for the correct person'
-    );
-
-    try {
-      run(() => chris.unloadRecord());
-      assert.ok(true, 'expected `unloadRecord()` not to throw');
-    } catch (e) {
-      assert.ok(false, 'expected `unloadRecord()` not to throw');
-    }
-  });
-
-  test(`store.unloadRecord on a record with custom RecordData with relationship to a record with default RecordData does not error`, async function (assert) {
-    const originalCreateRecordDataFor = store.createRecordDataFor;
-    store.createRecordDataFor = function provideCustomRecordData(identifier, storeWrapper) {
-      if (identifier.type === 'pet') {
-        return new CustomRecordData(identifier, storeWrapper);
-      } else {
-        return originalCreateRecordDataFor.call(this, identifier, storeWrapper);
-      }
-    };
-
-    let chris = store.push({
-      data: {
-        type: 'person',
-        id: '1',
-        attributes: { name: 'Chris' },
-        relationships: {
-          pets: {
-            data: [
-              { type: 'pet', id: '1' },
-              { type: 'pet', id: '2' },
-            ],
-          },
-        },
-      },
-      included: [
-        {
-          type: 'pet',
-          id: '1',
-          attributes: { name: 'Shen' },
-          relationships: {
-            owner: { data: { type: 'person', id: '1' } },
-          },
-        },
-        {
-          type: 'pet',
-          id: '2',
-          attributes: { name: 'Prince' },
-          relationships: {
-            owner: { data: { type: 'person', id: '1' } },
-          },
-        },
-      ],
-    });
-    let pets = chris.pets;
-    let shen = pets.at(0);
-
-    assert.strictEqual(shen.name, 'Shen', 'We found Shen');
-
-    try {
-      run(() => shen.unloadRecord());
-      assert.ok(true, 'expected `unloadRecord()` not to throw');
-    } catch (e) {
-      assert.ok(false, 'expected `unloadRecord()` not to throw');
-    }
   });
 
   test(`store.findRecord does not eagerly instantiate record data`, async function (assert) {
@@ -324,7 +116,8 @@ module('RecordData Compatibility', function (hooks) {
       }
     }
 
-    store.createRecordDataFor = function (identifier, storeWrapper) {
+    store.createRecordDataFor = function (type, id, lid, storeWrapper) {
+      const identifier = this.identifierCache.getOrCreateRecordIdentifier({ type, id, lid });
       return new TestRecordData(identifier, storeWrapper);
     };
     this.owner.register(
@@ -369,10 +162,6 @@ module('RecordData Compatibility', function (hooks) {
     assert.strictEqual(recordDataInstances, 0, 'initially no instances');
 
     await store.findRecord('pet', '1');
-
-    if (DEPRECATE_V1_RECORD_DATA) {
-      assert.expectDeprecation({ id: 'ember-data:deprecate-v1-cache', count: 1 });
-    }
 
     assert.strictEqual(recordDataInstances, 1, 'record data created after promise fulfills');
   });
