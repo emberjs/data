0472842987c835e6e35b88c8f40e7563c01b3158
Sergey Astapov <sergeastapov@gmail.com>
2021-11-02 17:12:48 -0400

    [Chore]: Make custom transform tests real

--- packages/-ember-data/tests/unit/transform/custom-test.js
+++ packages/-ember-data/tests/unit/transform/custom-test.js
@@ -2,24 +2,53 @@ import { module, test } from 'qunit';
 
 import { setupTest } from 'ember-qunit';
 
+import Model, { attr } from '@ember-data/model';
+import JSONSerializer from '@ember-data/serializer/json';
 import Transform from '@ember-data/serializer/transform';
 
 module('unit/transform - CustomTransform', function (hooks) {
   setupTest(hooks);
 
   test('#serialize', async function (assert) {
-    class CustomTransform extends Transform {}
-    this.owner.register('transform:custom', CustomTransform);
-    const transform = this.owner.lookup('transform:custom');
+    const store = this.owner.lookup('service:store');
 
-    assert.throws(() => transform.serialize(''), null, 'throws with missing serialize method');
+    class User extends Model {
+      @attr('custom') name;
+    }
+    this.owner.register('model:user', User);
+
+    class CustomTransform extends Transform {}
+
+    this.owner.register('transform:custom', CustomTransform);
+    this.owner.register('serializer:user', JSONSerializer);
+
+    assert.throws(
+      () => store.normalize('user', { id: 1, name: 'Chris' }),
+      TypeError,
+      'throws with missing deserialize method'
+    );
   });
 
   test('#deserialize', async function (assert) {
+    class User extends Model {
+      @attr('custom') name;
+    }
+    this.owner.register('model:user', User);
+
     class CustomTransform extends Transform {}
     this.owner.register('transform:custom', CustomTransform);
-    const transform = this.owner.lookup('transform:custom');
+    this.owner.register('serializer:user', JSONSerializer);
 
-    assert.throws(() => transform.transform.deserialize(''), null, 'throws with missing serialize method');
+    const userRecord = this.owner.lookup('service:store').push({
+      data: {
+        type: 'user',
+        id: '1',
+        attributes: {
+          name: 'Chris',
+        },
+      },
+    });
+
+    assert.throws(() => userRecord.serialize(), TypeError, 'throws with missing serialize method');
   });
 });
