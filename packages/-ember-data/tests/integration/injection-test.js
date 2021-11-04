c178c64be02879c00bd3d345bfe4d728987ddd3b
Igor Terzic <terzicigor@gmail.com>
2021-11-03 17:04:50 -0700

    [cleanup]: Remove implicit store injection slated for Ember v4.0.0 (#… (#7736)
    * [cleanup]: Remove implicit store injection slated for Ember v4.0.0 (#7667)
    
    * CI timeouts testing bump (#7734)
    
    Co-authored-by: Scott Newcomer <snewcomer@users.noreply.github.com>

--- packages/-ember-data/tests/integration/injection-test.js
+++ packages/-ember-data/tests/integration/injection-test.js
@@ -1,10 +1,6 @@
-import Service from '@ember/service';
-
 import { module, test } from 'qunit';
 
 import { setupTest } from 'ember-qunit';
-
-import Model from '@ember-data/model';
 
 module('integration/injection factoryFor enabled', function (hooks) {
   setupTest(hooks);
@@ -34,38 +30,5 @@ module('integration/injection factoryFor enabled', function (hooks) {
     assert.strictEqual(modelClass, Model, 'expected the factory itself to be returned');
 
     assert.equal(modelClass.modelName, 'super-villain', 'expected the factory itself to be returned');
-  });
-});
-
-module('integration/injection eager injections', function (hooks) {
-  setupTest(hooks);
-  let store;
-  let Apple = Service.extend();
-
-  hooks.beforeEach(function () {
-    let { owner } = this;
-
-    owner.register('model:foo', Model.extend());
-    owner.register('service:apple', Apple);
-    owner.inject('model:foo', 'apple', 'service:apple');
-
-    store = this.owner.lookup('service:store');
-  });
-
-  test('did inject', async function (assert) {
-    // TODO likely this test should instead test that we can use service injections
-    // on models (e.g. that owner is properly setup for it).
-    assert.expectDeprecation(
-      () => {
-        let foo = store.createRecord('foo');
-        let apple = foo.get('apple');
-        let appleService = this.owner.lookup('service:apple');
-
-        assert.ok(apple, `'model:foo' instance should have an 'apple' property`);
-        assert.ok(apple === appleService, `'model:foo.apple' should be the apple service`);
-        assert.ok(apple instanceof Apple, `'model:foo'.apple should be an instance of 'service:apple'`);
-      },
-      { id: 'implicit-injections', count: 1, when: { ember: '>=3.27.0' } }
-    );
   });
 });
