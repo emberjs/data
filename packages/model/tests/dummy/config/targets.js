45a355a3bdd43abadb99521154fedc4f03c7dce9
Scott Newcomer <snewcomer24@gmail.com>
2021-11-04 18:46:10 -0500

    Remove TARGET_IE11 flag

--- packages/model/tests/dummy/config/targets.js
+++ packages/model/tests/dummy/config/targets.js
@@ -2,12 +2,6 @@
 
 const browsers = ['last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions'];
 
-const needsIE11 = !!process.env.TARGET_IE11;
-
-if (needsIE11) {
-  browsers.push('ie 11');
-}
-
 module.exports = {
   browsers,
   node: 'current',
--- packages/record-data/tests/dummy/config/targets.js
+++ packages/record-data/tests/dummy/config/targets.js
@@ -1,11 +1,6 @@
 'use strict';
 
 const browsers = ['last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions'];
-const targetIsIE11 = process.env.TESTEM_CI_LAUNCHER === 'IE' || !!process.env.TARGET_IE11;
-
-if (targetIsIE11) {
-  browsers.push('ie 11');
-}
 
 module.exports = {
   browsers,
