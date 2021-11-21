ae52843a995db127e899a390d276dd2421624966
Scott Newcomer <snewcomer24@gmail.com>
2021-11-21 13:14:18 -0600

    Add lts 3.28 testing scenario

--- packages/-ember-data/config/ember-try.js
+++ packages/-ember-data/config/ember-try.js
@@ -72,6 +72,14 @@ module.exports = function () {
           },
         },
         {
+          name: 'ember-lts-3.28',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.28.0',
+            },
+          },
+        },
+        {
           name: 'ember-release',
           npm: {
             devDependencies: {
