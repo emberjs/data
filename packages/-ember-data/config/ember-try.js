c48363eaa9e43eb6e0e4ca7b5a368ff46d85c30c
Igor Terzic <iterzic@linkedin.com>
2021-11-23 17:16:12 -0800

    bump ember lts

--- packages/-ember-data/config/ember-try.js
+++ packages/-ember-data/config/ember-try.js
@@ -56,18 +56,18 @@ module.exports = function () {
           },
         },
         {
-          name: 'ember-lts-3.20',
-          npm: {
-            devDependencies: {
-              'ember-source': '~3.20.0',
-            },
-          },
-        },
-        {
           name: 'ember-lts-3.24',
           npm: {
             devDependencies: {
               'ember-source': '~3.24.0',
+            },
+          },
+        },
+        {
+          name: 'ember-lts-3.28',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.28.0',
             },
           },
         },
