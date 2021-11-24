62bc8b6af61a65d3c0c76029dcb40a6dba81f955
Scott Newcomer <snewcomer24@gmail.com>
2021-11-24 07:18:43 -0600

    native symbol after rebase with ie11 hacks removal

--- packages/-ember-data/addon/setup-container.js
+++ packages/-ember-data/addon/setup-container.js
@@ -4,9 +4,8 @@ import Route from '@ember/routing/route';
 import { DEBUG } from '@glimmer/env';
 
 import Store from '@ember-data/store';
-import { symbol } from '@ember-data/store/-private';
 
-const EMBER_DATA_STORE = symbol('ember-data-store');
+const EMBER_DATA_STORE = Symbol('ember-data-store');
 
 function initializeStore(application) {
   // we can just use registerOptionsForType when we no longer
