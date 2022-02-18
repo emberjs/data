ffcf6f39399e9566303b50a2747bbbe417e0fdcb
Scott Newcomer <snewcomer24@gmail.com>
2022-02-17 22:32:14 -0600

    rm docs

--- packages/store/addon/-private/system/stateful-promise.ts
+++ packages/store/addon/-private/system/stateful-promise.ts
@@ -7,64 +7,24 @@ export class StatefulPromise {
   declare isDestroyed: boolean;
   declare isDestroying: boolean;
 
-  /**
-   * Whether the loading promise is still pending
-   *
-   * @property {boolean} isPending
-   * @public
-   */
   @tracked isPending: boolean = false;
-  /**
-   * Whether the loading promise rejected
-   *
-   * @property {boolean} isRejected
-   * @public
-   */
+
   @tracked isRejected: boolean = false;
-  /**
-   * Whether the loading promise succeeded
-   *
-   * @property {boolean} isResolved
-   * @public
-   */
+
   @tracked isResolved: boolean = false;
 
   constructor(promise: Promise<unknown>) {
     this.promise = tapPromise(this, promise);
   }
 
-  /**
-   * chain this promise
-   *
-   * @method then
-   * @public
-   * @param success
-   * @param fail
-   * @returns Promise
-   */
   then(success: (value: unknown) => unknown, failure: (reason: unknown) => PromiseLike<never>) {
     return this.promise!.then(success, failure);
   }
 
-  /**
-   * catch errors thrown by this promise
-   * @method catch
-   * @public
-   * @param callback
-   * @returns Promise
-   */
   catch(cb: (reason: unknown) => PromiseLike<never>) {
     return this.promise!.catch(cb);
   }
 
-  /**
-   * run cleanup after this promise completes
-   *
-   * @method finally
-   * @public
-   * @param callback
-   * @returns Promise
-   */
   finally(cb: () => void) {
     return this.promise!.finally(cb);
   }
