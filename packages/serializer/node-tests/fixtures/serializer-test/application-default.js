2651e1b8a643c8dd8c3db0dfa82ed3926ceedf8d
Bert De Block <bert.deblock@bagaar.be>
2022-02-21 21:11:55 +0100

    [RFC 637] Customizable test setups (#7887)

--- packages/serializer/node-tests/fixtures/serializer-test/application-default.js
+++ packages/serializer/node-tests/fixtures/serializer-test/application-default.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import { setupTest } from 'my-app/tests/helpers';
 
 module('Unit | Serializer | application', function(hooks) {
   setupTest(hooks);
--- packages/serializer/node-tests/fixtures/transform-test/default.js
+++ packages/serializer/node-tests/fixtures/transform-test/default.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import { setupTest } from 'my-app/tests/helpers';
 
 module('Unit | Transform | foo', function(hooks) {
   setupTest(hooks);
