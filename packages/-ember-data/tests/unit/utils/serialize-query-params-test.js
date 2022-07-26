4d15ba53c2edd162767c1d288c8da726acab754d
Cameron Dubas <camerondubas@gmail.com>
2022-07-26 08:36:02 +0100

    [BUGFIX] serialize null array items to string

--- packages/-ember-data/tests/unit/utils/serialize-query-params-test.js
+++ packages/-ember-data/tests/unit/utils/serialize-query-params-test.js
@@ -27,6 +27,19 @@ module('Unit | serializeQueryParams', function () {
     assert.deepEqual(result, 'filter%5Bchildren%5D%5B%5D=1&filter%5Bchildren%5D%5B%5D=2');
   });
 
+  test('it treats null values in arrays as empty strings', function (assert) {
+    assert.expect(1);
+
+    const qp = {
+      filter: {
+        children: [null, 2],
+      },
+    };
+    const result = serializeQueryParams(qp);
+
+    assert.deepEqual(result, 'filter%5Bchildren%5D%5B%5D=&filter%5Bchildren%5D%5B%5D=2');
+  });
+
   test('it works with + sign', function (assert) {
     assert.expect(1);
 
