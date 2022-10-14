182395b3fb19c39fe2fe92dbd279faf1898cee64
Chris Thoburn <runspired@users.noreply.github.com>
2022-10-14 01:15:07 -0700

    fix inspector type detection (#8213)

--- packages/debug/addon/setup.js
+++ packages/debug/addon/setup.js
@@ -1,3 +1,5 @@
+
+// dirty hack to add the known models to the typesMap
 import Store from '@ember-data/store';
 
 const StoreTypesMap = new WeakMap();
@@ -8,12 +10,24 @@ export function typesMapFor(store) {
   if (typesMap === undefined) {
     typesMap = new Map();
     StoreTypesMap.set(store, typesMap);
+    installHook(store, typesMap);
   }
 
   return typesMap;
 }
 
-// override _createRecordData to add the known models to the typesMap
+// EmberData 4.7+
+function installHook(store, typesMap) {
+  const getRecordData = store._instanceCache.getRecordData;
+  store._instanceCache.getRecordData = function(identifier) {
+    if (!typesMap.has(identifier.type)) {
+      typesMap.set(identifier.type, false);
+    }
+    return getRecordData.call(this, identifier);
+  }
+}
+
+// EmberData <= 4.6
 const __createRecordData = Store.prototype._createRecordData;
 Store.prototype._createRecordData = function (identifier) {
   const typesMap = typesMapFor(this);
