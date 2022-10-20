ec700ef037130396f377def0534c16ebf0a751b6
Chris Thoburn <runspired@users.noreply.github.com>
2022-10-19 19:29:23 -0700

    chore: fix asset-size check for pnpm (#8229)
    * fix asset-size check for pnpm
    
    * fix checks
    
    * fixes
    
    * more label gates
    
    * add label check
    
    * fix syntax
    
    * fix trigger
    
    * fix flow
    
    * fix types?
    
    * all of them
    
    * fix label
    
    * remove no-rollup scenario

--- scripts/asset-size-tracking/src/create-comment-text.js
+++ scripts/asset-size-tracking/src/create-comment-text.js
@@ -5,19 +5,11 @@ const GITHUB_SHA = process.argv[2];
 
 const ModernDiffPath = path.resolve(__dirname, '../../../tmp/asset-sizes/diff.txt');
 const ModernAnalysisPath = path.resolve(__dirname, '../../../tmp/asset-sizes/experiment-analysis.txt');
-const ModernDiffPathNoRollup = path.resolve(__dirname, '../../../tmp/asset-sizes/diff-no-rollup.txt');
-const ModernAnalysisPathNoRollup = path.resolve(
-  __dirname,
-  '../../../tmp/asset-sizes/experiment-analysis-no-rollup.txt'
-);
 
 const ModernDiffText = fs.readFileSync(ModernDiffPath);
 const ModernAnalysisText = fs.readFileSync(ModernAnalysisPath);
-const ModernDiffTextNoRollup = fs.readFileSync(ModernDiffPathNoRollup);
-const ModernAnalysisTextNoRollup = fs.readFileSync(ModernAnalysisPathNoRollup);
 
-const commentText = `Asset Size Report for ${GITHUB_SHA}\n\n**Modern Builds**\n${ModernDiffText}\n<details>\n  <summary>Full Asset Analysis (Modern)</summary>\n\n\`\`\`${ModernAnalysisText}\n\`\`\`\n</details>
-\n**Modern Builds (No Rollup)**\n${ModernDiffTextNoRollup}\n<details>\n  <summary>Full Asset Analysis (Modern)</summary>\n\n\`\`\`${ModernAnalysisTextNoRollup}\n\`\`\`\n</details>`;
+const commentText = `Asset Size Report for ${GITHUB_SHA}\n\n**Modern Builds**\n${ModernDiffText}\n<details>\n  <summary>Full Asset Analysis (Modern)</summary>\n\n\`\`\`${ModernAnalysisText}\n\`\`\`\n</details>`;
 const commentJSON = {
   body: commentText,
 };
