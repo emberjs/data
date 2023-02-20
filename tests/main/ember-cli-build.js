68a3425e35b94853496d0fdb76170cc84b899ed2
Markus Sanin <markus.sanin@zeppelin-group.com>
2023-02-20 11:04:13 +0100

    Remove comment

--- tests/main/ember-cli-build.js
+++ tests/main/ember-cli-build.js
@@ -81,13 +81,6 @@ module.exports = function (defaults) {
     },
   });
 
-  /*
-    This build file specifies the options for the dummy test app of this
-    addon, located in `/tests/dummy`
-    This build file does *not* influence how the addon or the app using it
-    behave. You most likely want to be modifying `./index.js` or app's build file
-  */
-
   const { maybeEmbroider } = require('@embroider/test-setup');
   return maybeEmbroider(app, {
     skipBabel: [
