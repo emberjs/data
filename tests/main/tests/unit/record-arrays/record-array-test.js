1adf81ca3a231dd697145fe0f876c116cc0b05ae
Ewan McDougall <ewan@mrloop.com>
2023-02-25 03:05:30 +0000

    fix: deprecated record-array rejectBy use optional value if supplied (#8399)
    If an optional value supplied as second arg to `rejectBy` it should be
    compared to the value retrieved using the `key`.

--- tests/main/tests/unit/record-arrays/record-array-test.js
+++ tests/main/tests/unit/record-arrays/record-array-test.js
@@ -142,6 +142,49 @@ module('unit/record-arrays/record-array - DS.RecordArray', function (hooks) {
   );
 
   deprecatedTest(
+    '#rejectBy',
+    { id: 'ember-data:deprecate-array-like', until: '5.0', count: 3 },
+    async function (assert) {
+      this.owner.register('model:tag', Tag);
+      let store = this.owner.lookup('service:store');
+
+      let records = store.push({
+        data: [
+          {
+            type: 'tag',
+            id: '1',
+            attributes: {
+              name: 'first',
+            },
+          },
+          {
+            type: 'tag',
+            id: '3',
+          },
+          {
+            type: 'tag',
+            id: '5',
+            attributes: {
+              name: 'fifth',
+            },
+          },
+        ],
+      });
+
+      let recordArray = new RecordArray({
+        type: 'recordType',
+        identifiers: records.map(recordIdentifierFor),
+        store,
+      });
+
+      assert.strictEqual(recordArray.length, 3);
+      assert.strictEqual(recordArray.rejectBy('id', '3').length, 2);
+      assert.strictEqual(recordArray.rejectBy('id').length, 0);
+      assert.strictEqual(recordArray.rejectBy('name').length, 1);
+    }
+  );
+
+  deprecatedTest(
     '#lastObject and #firstObject',
     { id: 'ember-data:deprecate-array-like', until: '5.0', count: 2 },
     async function (assert) {
