23fa7bab394b72f284225cc77450f857cf30c984
Chris Thoburn <runspired@gmail.com>
2023-03-10 19:40:40 -0800

    fixes

--- packages/store/src/-private/managers/notification-manager.ts
+++ packages/store/src/-private/managers/notification-manager.ts
@@ -13,7 +13,10 @@ import type Store from '../store-service';
 export type UnsubscribeToken = object;
 let tokenId = 0;
 
-const Cache = new Map<StableRecordIdentifier | 'resource' | 'document', Map<UnsubscribeToken, NotificationCallback | ResourceOperationCallback | DocumentOperationCallback>>();
+const Cache = new Map<
+  StableRecordIdentifier | 'resource' | 'document',
+  Map<UnsubscribeToken, NotificationCallback | ResourceOperationCallback | DocumentOperationCallback>
+>();
 const Tokens = new Map<UnsubscribeToken, StableRecordIdentifier>();
 
 export type NotificationType = 'attributes' | 'relationships' | 'identity' | 'errors' | 'meta' | 'state';
@@ -31,7 +34,7 @@ export interface ResourceOperationCallback {
 
 export interface DocumentOperationCallback {
   // document updates
-  (identifier: Identifier, notificationType: 'updated' |'added' | 'removed'): void;
+  (identifier: Identifier, notificationType: 'updated' | 'added' | 'removed'): void;
 }
 
 // TODO this isn't importable anyway, remove and use a map on the manager?
@@ -88,7 +91,10 @@ export default class NotificationManager {
    * @param {NotificationCallback} callback
    * @returns {UnsubscribeToken} an opaque token to be used with unsubscribe
    */
-  subscribe(identifier: StableRecordIdentifier | 'resource' | 'document', callback: NotificationCallback | ResourceOperationCallback | DocumentOperationCallback): UnsubscribeToken {
+  subscribe(
+    identifier: StableRecordIdentifier | 'resource' | 'document',
+    callback: NotificationCallback | ResourceOperationCallback | DocumentOperationCallback
+  ): UnsubscribeToken {
     assert(`Expected to receive a stable Identifier to subscribe to`, isStableIdentifier(identifier));
     let map = Cache.get(identifier);
 
@@ -117,6 +123,8 @@ export default class NotificationManager {
   }
 
   /**
+   * Custom Caches and Application Code should not call this method directly.
+   *
    * @method notify
    * @param identifier
    * @param value
@@ -129,14 +137,14 @@ export default class NotificationManager {
   notify(identifier: StableRecordIdentifier, value: 'added' | 'removed'): boolean;
   notify(identifier: StableRecordIdentifier, value: NotificationType | 'added' | 'removed', key?: string): boolean {
     assert(
-      `Notify does not accept a key argument for the namespace '${value}'. Received key '${key}'.`,
+      `Notify does not accept a key argument for the namespace '${value}'. Received key '${key || ''}'.`,
       !key || value === 'attributes' || value === 'relationships'
     );
     if (!isStableIdentifier(identifier)) {
       if (LOG_NOTIFICATIONS) {
         // eslint-disable-next-line no-console
         console.log(
-          `Notifying: Expected to receive a stable Identifier to notify '${value}' '${key}' with, but ${String(
+          `Notifying: Expected to receive a stable Identifier to notify '${value}' '${key || ''}' with, but ${String(
             identifier
           )} is not in the cache`,
           identifier
@@ -147,7 +155,7 @@ export default class NotificationManager {
 
     if (LOG_NOTIFICATIONS) {
       // eslint-disable-next-line no-console
-      console.log(`Notifying: ${String(identifier)}\t${value}\t${key}`);
+      console.log(`Notifying: ${String(identifier)}\t${value}\t${key || ''}`);
     }
     let callbackMap = Cache.get(identifier);
     if (!callbackMap || !callbackMap.size) {
