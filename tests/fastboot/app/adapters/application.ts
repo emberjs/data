7f401b45eae88f0309f421a7fd4493907fa22058
Chris Thoburn <runspired@gmail.com>
2023-03-16 16:07:17 -0700

    fix types

--- tests/fastboot/app/adapters/application.ts
+++ tests/fastboot/app/adapters/application.ts
@@ -1,10 +1,11 @@
 import RESTAdapter from '@ember-data/adapter/rest';
+import type { SnapshotRecordArray } from '@ember-data/store/-private';
 
 export default class ApplicationAdapter extends RESTAdapter {
   namespace = 'api';
 
-  urlForFindAll(...args: unknown[]) {
-    let url = super.urlForFindAll(...args);
+  urlForFindAll(type: string, snapshots: SnapshotRecordArray) {
+    let url = super.urlForFindAll(type, snapshots);
     if (url.endsWith('/')) {
       url = url.substring(0, url.length - 2);
     }
