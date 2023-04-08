411d840783ed064dea2c628470316c2bad23386d
Chris Thoburn <runspired@gmail.com>
2023-04-07 23:57:36 -0700

    fix embroider scenario

---
+++ tests/embroider-basic-compat/app/adapters/application.ts
@@ -0,0 +1,14 @@
+import RESTAdapter from '@ember-data/adapter/rest';
+import type { SnapshotRecordArray } from '@ember-data/legacy-compat/-private';
+
+export default class ApplicationAdapter extends RESTAdapter {
+  namespace = 'api';
+
+  urlForFindAll(type: string, snapshots: SnapshotRecordArray) {
+    let url = super.urlForFindAll(type, snapshots);
+    if (url.endsWith('/')) {
+      url = url.substring(0, url.length - 2);
+    }
+    return url + '.json';
+  }
+}
