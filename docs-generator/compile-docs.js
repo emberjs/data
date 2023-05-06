f755bbe54e4391900e6c4473cb11d2ec5f86c81c
Chris Thoburn <runspired@users.noreply.github.com>
2023-05-06 12:51:06 -0700

    fix: docs generation should maintain a stable relative path (#8598)

--- docs-generator/compile-docs.js
+++ docs-generator/compile-docs.js
@@ -29,16 +29,54 @@ function generateOptions() {
   return Y.Project.init(config);
 }
 
+function rewritePath(filePath) {
+  if (filePath.startsWith('../packages')) {
+    return filePath.replace('../packages/', '../');
+  }
+  if (filePath.startsWith('../')) {
+    return filePath.replace('../', '../../');
+  }
+  return filePath;
+}
+
 async function main() {
   const options = generateOptions();
 
   const yuidocCompiler = new Y.YUIDoc(options);
-  const json = yuidocCompiler.run();
-  const builder = new Y.DocBuilder(options, json);
+  const initialJson = yuidocCompiler.run();
+
+  const builder = new Y.DocBuilder(options, initialJson);
 
   await new Promise((resolve) => {
     builder.compile(resolve);
   });
+
+  const dataFile = path.join(__dirname, '../packages/-ember-data/dist/docs/data.json');
+  const json = JSON.parse(fs.readFileSync(dataFile));
+
+  const newFiles = {};
+  Object.keys(json.files).forEach((key) => {
+    const newKey = rewritePath(key);
+    newFiles[newKey] = json.files[key];
+    newFiles[newKey].name = newKey;
+  });
+  json.files = newFiles;
+
+  Object.keys(json.classes).forEach((key) => {
+    const val = json.classes[key];
+    if (val.file) {
+      val.file = rewritePath(val.file);
+    }
+  });
+
+  Object.keys(json.modules).forEach((key) => {
+    const val = json.modules[key];
+    if (val.file) {
+      val.file = rewritePath(val.file);
+    }
+  });
+
+  fs.writeFileSync(dataFile, JSON.stringify(json, null, 2));
 }
 
 main();
