ce79567a999eacd034e8b0e695dd1550aac91ee3
Jacob Beltran <jake@beltran.dev>
2023-05-19 13:09:24 -0700

    WIP Starting out request-utils package

---
+++ packages/request-utils/addon-main.js
@@ -0,0 +1,19 @@
+module.exports = {
+  name: require('./package.json').name,
+
+  treeForVendor() {
+    return;
+  },
+  treeForPublic() {
+    return;
+  },
+  treeForStyles() {
+    return;
+  },
+  treeForAddonStyles() {
+    return;
+  },
+  treeForApp() {
+    return;
+  },
+};
