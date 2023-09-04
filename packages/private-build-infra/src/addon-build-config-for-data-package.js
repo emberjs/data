885893e6e12711c491b38270130c505d0defc0b4
Chris Thoburn <runspired@users.noreply.github.com>
2023-09-04 11:21:14 -0700

    chore: eliminate dead build code (#8829)

--- packages/private-build-infra/src/addon-build-config-for-data-package.js
+++ packages/private-build-infra/src/addon-build-config-for-data-package.js
@@ -1,18 +1,7 @@
 const calculateCacheKeyForTree = require('calculate-cache-key-for-tree');
-const Funnel = require('broccoli-funnel');
-const merge = require('broccoli-merge-trees');
 const BroccoliDebug = require('broccoli-debug');
-const VersionChecker = require('ember-cli-version-checker');
 
-const rollupPrivateModule = require('./utilities/rollup-private-module');
 const detectModule = require('./utilities/detect-module');
-
-function isProductionEnv() {
-  let isProd = /production/.test(process.env.EMBER_ENV);
-  let isTest = process.env.EMBER_CLI_TEST_COMMAND;
-
-  return isProd && !isTest;
-}
 
 function addonBuildConfigForDataPackage(pkg) {
   return {
@@ -26,7 +15,6 @@ function addonBuildConfigForDataPackage(pkg) {
       //     ' for ' +
       //     (typeof this.parent.name === 'function' ? this.parent.name() : this.parent.name)
       // );
-      this._prodLikeWarning();
       this.debugTree = BroccoliDebug.buildDebugCallback(`ember-data:${pkg.name}`);
       this.options = this.options || {};
       Object.assign(this.options, {
@@ -54,15 +42,6 @@ function addonBuildConfigForDataPackage(pkg) {
       });
     },
 
-    _prodLikeWarning() {
-      let emberEnv = process.env.EMBER_ENV;
-      if (emberEnv !== 'production' && /production/.test(emberEnv)) {
-        this._warn(
-          `Production-like values for EMBER_ENV are deprecated (your EMBER_ENV is "${emberEnv}") and support will be removed in Ember Data 4.0.0. If using ember-cli-deploy, please configure your build using 'production'. Otherwise please set your EMBER_ENV to 'production' for production builds.`
-        );
-      }
-    },
-
     isDevelopingAddon() {
       if (typeof this.parent.name === 'string' && this.parent.name === 'ember-data') {
         return this.parent.isDevelopingAddon();
@@ -70,53 +49,8 @@ function addonBuildConfigForDataPackage(pkg) {
       return this._super(...arguments);
     },
 
-    _warn(message) {
-      let chalk = require('chalk');
-      let warning = chalk.yellow('WARNING: ' + message);
-
-      if (this.ui && this.ui.writeWarnLine) {
-        this.ui.writeWarnLine(message);
-      } else if (this.ui) {
-        this.ui.writeLine(warning);
-      } else {
-        // eslint-disable-next-line no-console
-        console.log(warning);
-      }
-    },
-
-    _suppressUneededRollupWarnings(message, next) {
-      if (message.code === 'CIRCULAR_DEPENDENCY') {
-        return;
-      } else if (message.code === 'NON_EXISTENT_EXPORT') {
-        // ignore type imports
-        if (message.message.indexOf(`@ember-data/types`) !== -1) {
-          return;
-        }
-      } else if (message.code === 'UNRESOLVED_IMPORT') {
-        if (!this.isDevelopingAddon()) {
-          // don't print these for consumers
-          return;
-        } else {
-          const chalk = require('chalk');
-          // make warning actionable
-          // eslint-disable-next-line no-console
-          console.log(
-            chalk.yellow(
-              `\n\n⚠️  Add ${chalk.white(
-                message.source
-              )} to the array returned by externalDependenciesForPrivateModule in index.js of ${chalk.white(
-                this.name
-              )}\n\n`
-            )
-          );
-          throw message.message;
-        }
-      }
-      next(message);
-    },
-
     shouldIncludeChildAddon(addon) {
-      if (addon.name.startsWith('@ember-data')) {
+      if (addon.name.startsWith('@ember-data/')) {
         if (this.name === 'ember-data' || addon.name === '@ember-data/tracking') {
           return true;
         }
@@ -124,16 +58,6 @@ function addonBuildConfigForDataPackage(pkg) {
         return false;
       }
       return true;
-    },
-
-    getOutputDirForVersion() {
-      let VersionChecker = require('ember-cli-version-checker');
-      let checker = new VersionChecker(this);
-      let emberCli = checker.for('ember-cli', 'npm');
-
-      let requiresModulesDir = emberCli.satisfies('< 3.0.0');
-
-      return requiresModulesDir ? 'modules' : '';
     },
 
     buildBabelOptions() {
@@ -181,69 +105,6 @@ function addonBuildConfigForDataPackage(pkg) {
 
     cacheKeyForTree(treeType) {
       return calculateCacheKeyForTree(treeType, this);
-    },
-
-    externalDependenciesForPrivateModule() {
-      return [];
-    },
-
-    treeForAddon(tree) {
-      if (process.env.EMBER_DATA_ROLLUP_PRIVATE === 'false' || this.shouldRollupPrivate !== true) {
-        return this._super.treeForAddon.call(this, tree);
-      }
-
-      tree = this.debugTree(tree, 'input');
-      this._setupBabelOptions();
-
-      let babel = this.addons.find((addon) => addon.name === 'ember-cli-babel');
-      let externalDeps = this.externalDependenciesForPrivateModule();
-
-      const host = this._findHost();
-
-      // don't print this for consumers
-      if (this.isDevelopingAddon()) {
-        // eslint-disable-next-line no-console
-        console.log(
-          `Rolling up ${this.name} private modules with the following external dependencies: ['${externalDeps.join(
-            "', '"
-          )}']`
-        );
-      }
-      let checker = new VersionChecker(this.project);
-      let emberVersion = checker.for('ember-source');
-      let analyzer = this.registry.load('js').find((plugin) => plugin.name === 'ember-auto-import-analyzer');
-
-      let privateTree = rollupPrivateModule(tree, {
-        packageName: pkg.name,
-        babelCompiler: babel,
-        babelOptions: this.options.babel,
-        emberVersion: emberVersion,
-        emberCliBabelOptions: host.options && host.options['ember-cli-babel'] ? host.options['ember-cli-babel'] : {},
-        onWarn: this._suppressUneededRollupWarnings.bind(this),
-        externalDependencies: this.externalDependenciesForPrivateModule(),
-        destDir: this.getOutputDirForVersion(),
-        analyzer,
-      });
-
-      let withoutPrivate = new Funnel(tree, {
-        exclude: ['-private', isProductionEnv() ? '-debug' : false].filter(Boolean),
-
-        destDir: pkg.name,
-      });
-
-      // use the default options
-      let publicTree = babel.transpileTree(this.debugTree(withoutPrivate, 'babel-public:input'));
-      publicTree = this.debugTree(publicTree, 'babel-public:output');
-
-      if (analyzer) {
-        publicTree = analyzer.toTree.call(analyzer, publicTree, undefined, undefined, { treeType: 'addon' });
-      }
-
-      let destDir = this.getOutputDirForVersion();
-
-      publicTree = new Funnel(publicTree, { destDir });
-
-      return this.debugTree(merge([publicTree, privateTree]), 'final');
     },
 
     _emberDataConfig: null,
