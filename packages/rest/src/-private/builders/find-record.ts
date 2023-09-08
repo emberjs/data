c7818d7e842e3b504bcb208c3dbe49a990d23162
Kirill Shaplyko <Baltazore@users.noreply.github.com>
2023-09-08 19:16:14 +0200

    Add request options documentation parts to find-record builder (#8848)

--- packages/rest/src/-private/builders/find-record.ts
+++ packages/rest/src/-private/builders/find-record.ts
@@ -48,6 +48,27 @@ type FindRecordOptions = ConstrainedRequestOptions & {
  * const data = await store.request(options);
  * ```
  *
+ * **Supplying Options to Modify the Request Behavior**
+ *
+ * The following options are supported:
+ *
+ * - `host` - The host to use for the request, defaults to the `host` configured with `setBuildURLConfig`.
+ * - `namespace` - The namespace to use for the request, defaults to the `namespace` configured with `setBuildURLConfig`.
+ * - `resourcePath` - The resource path to use for the request, defaults to pluralizing and camelCasing the supplied type
+ * - `reload` - Whether to forcibly reload the request if it is already in the store, not supplying this
+ *      option will delegate to the store's lifetimes service, defaulting to `false` if none is configured.
+ * - `backgroundReload` - Whether to reload the request if it is already in the store, but to also resolve the
+ *      promise with the cached value, not supplying this option will delegate to the store's lifetimes service,
+ *      defaulting to `false` if none is configured.
+ * - `urlParamsSetting` - an object containing options for how to serialize the query params (see `buildQueryParams`)
+ *
+ * ```ts
+ * import { findRecord } from '@ember-data/rest/request';
+ *
+ * const options = findRecord('person', '1', { include: ['pets', 'friends'] }, { namespace: 'api/v2' });
+ * const data = await store.request(options);
+ * ```
+ *
  * @method findRecord
  * @public
  * @static
