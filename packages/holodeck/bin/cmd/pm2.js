9f98f17e101ca9e5c255cca960ece09b62ecf214
Chris Thoburn <runspired@gmail.com>
2023-09-24 03:46:28 -0700

    add more logs

--- packages/holodeck/bin/cmd/pm2.js
+++ packages/holodeck/bin/cmd/pm2.js
@@ -7,7 +7,7 @@ export default async function pm2Delegate(cmd, _args) {
   return new Promise((resolve, reject) => {
     pm2.connect((err) => {
       if (err) {
-        console.log('not able to connect');
+        console.log('not able to connect to pm2');
         console.error(err);
         process.exit(2);
       }
@@ -23,11 +23,14 @@ export default async function pm2Delegate(cmd, _args) {
         (err, apps) => {
           pm2.disconnect(); // Disconnects from PM2
           if (err) {
-            console.log('not able to stop');
+            console.log(`not able to ${cmd} pm2`);
             console.error(err);
             reject(err);
+          } else {
+            console.log(`pm2 ${cmd} successful`);
+            resolve();
           }
-          resolve();
+
         }
       );
     });
