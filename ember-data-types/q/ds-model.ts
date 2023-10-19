d14bf20ebf98844856c95087351b8369c607b4fe
Chris Thoburn <runspired@gmail.com>
2023-10-19 12:52:00 -0700

    some nice model things

--- ember-data-types/q/ds-model.ts
+++ ember-data-types/q/ds-model.ts
@@ -1,12 +1,21 @@
-import type Model from '@ember-data/model';
+import { AttributeSchema, RelationshipSchema } from './record-data-schemas';
 
-export type ModelSchema = Pick<
-  typeof Model,
-  | 'modelName'
-  | 'fields'
-  | 'attributes'
-  | 'relationshipsByName'
-  | 'eachAttribute'
-  | 'eachRelationship'
-  | 'eachTransformedAttribute'
->;
+type GenericRecord = Record<string, unknown>;
+export interface ModelSchema<T extends object = GenericRecord> {
+  modelName: string;
+  fields: Map<keyof T & string, 'attribute' | 'belongsTo' | 'hasMany'>;
+  attributes: Map<keyof T & string, AttributeSchema>;
+  relationshipsByName: Map<keyof T & string, RelationshipSchema>;
+  eachAttribute<K extends keyof T & string>(
+    callback: (this: ModelSchema<T>, key: K, attribute: AttributeSchema) => void,
+    binding?: T
+  ): void;
+  eachRelationship<K extends keyof T & string>(
+    callback: (this: ModelSchema<T>, key: K, relationship: RelationshipSchema) => void,
+    binding?: T
+  ): void;
+  eachTransformedAttribute<K extends keyof T & string>(
+    callback: (this: ModelSchema<T>, key: K, type: string | null) => void,
+    binding?: T
+  ): void;
+}
