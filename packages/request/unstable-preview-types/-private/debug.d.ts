dcfa9235e7f993b0d9c11393a007baae00cccbec
Chris Thoburn <runspired@gmail.com>
2023-10-20 02:13:12 -0700

    repare new core package

--- packages/request/unstable-preview-types/-private/debug.d.ts
+++
@@ -1,5 +0,0 @@
-import { Context } from './context';
-import type { RequestInfo } from './types';
-export declare function deepFreeze<T = unknown>(value: T): T;
-export declare function assertValidRequest(request: RequestInfo | Context, isTopLevel: boolean): asserts request is RequestInfo;
-//# sourceMappingURL=debug.d.ts.map
