586a5eb289511d049fb2ce0459e90ddd02fc501b
Chris Thoburn <runspired@gmail.com>
2023-10-20 00:53:57 -0700

    more test cleanup

---
+++ tests/ember-data__serializer/app/services/store.ts
@@ -0,0 +1,54 @@
+import JSONAPICache from '@ember-data/json-api';
+import {
+  adapterFor,
+  cleanup,
+  LegacyNetworkHandler,
+  normalize,
+  pushPayload,
+  serializeRecord,
+  serializerFor,
+} from '@ember-data/legacy-compat';
+import type Model from '@ember-data/model';
+import { buildSchema, instantiateRecord, modelFor, teardownRecord } from '@ember-data/model/hooks';
+import RequestManager from '@ember-data/request';
+import Fetch from '@ember-data/request/fetch';
+import BaseStore, { CacheHandler } from '@ember-data/store';
+import type { CacheCapabilitiesManager } from '@ember-data/store/-types/q/cache-store-wrapper';
+import type { StableRecordIdentifier } from '@ember-data/store/-types/q/identifier';
+
+export default class Store extends BaseStore {
+  constructor(args) {
+    super(args);
+    this.requestManager = new RequestManager();
+    this.requestManager.use([LegacyNetworkHandler, Fetch]);
+    this.requestManager.useCache(CacheHandler);
+    this.registerSchema(buildSchema(this));
+  }
+
+  createCache(capabilities: CacheCapabilitiesManager) {
+    return new JSONAPICache(capabilities);
+  }
+
+  instantiateRecord(identifier: StableRecordIdentifier, createRecordArgs: Record<string, unknown>) {
+    return instantiateRecord.call(this, identifier, createRecordArgs);
+  }
+
+  teardownRecord(record: Model) {
+    teardownRecord.call(this, record);
+  }
+
+  modelFor(type: string) {
+    return modelFor.call(this, type) || super.modelFor(type);
+  }
+
+  serializeRecord = serializeRecord;
+  pushPayload = pushPayload;
+  adapterFor = adapterFor;
+  serializerFor = serializerFor;
+  normalize = normalize;
+
+  destroy() {
+    cleanup.call(this);
+    super.destroy();
+  }
+}
