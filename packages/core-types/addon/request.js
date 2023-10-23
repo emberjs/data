c2c4f0b444c9f5b5f9b80a1c2b22f8515dc121d0
Chris Thoburn <runspired@gmail.com>
2023-10-23 02:37:25 -0700

    remove untracked files

--- packages/core-types/addon/request.js
+++
@@ -1,5 +0,0 @@
-const SkipCache = Symbol.for('wd:skip-cache');
-const EnableHydration = Symbol.for('wd:enable-hydration');
-const IS_FUTURE = Symbol('IS_FUTURE');
-const STRUCTURED = Symbol('DOC');
-export { EnableHydration, IS_FUTURE, STRUCTURED, SkipCache };
