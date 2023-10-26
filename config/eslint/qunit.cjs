4a92f48831b523e27726d426023f94b3b0e4c5b5
Krystan HuffMenne <kmenne+github@gmail.com>
2023-10-26 13:12:45 -0700

    Extract qunit ESLint config

---
+++ config/eslint/qunit.cjs
@@ -0,0 +1,18 @@
+const isolation = require('./isolation.cjs');
+
+function defaults(config = {}) {
+  return {
+    files: config.files || ['tests/**/*-test.{js,ts}'],
+    extends: ['plugin:qunit/recommended'],
+    rules: Object.assign(
+      isolation.rules({
+        allowedImports: ['@ember/debug', '@ember/test-helpers', 'qunit'],
+      }),
+      {}
+    ),
+  };
+}
+
+module.exports = {
+  defaults,
+};
