181de012e2fae4f7e4bbc0b010b5343e14b25cee
Chris Thoburn <runspired@gmail.com>
2023-11-09 19:47:49 -0800

    all the things

--- packages/model/src/migration-support.ts
+++ packages/model/src/migration-support.ts
@@ -1,5 +1,8 @@
 import { assert } from '@ember/debug';
 
+import { importSync } from '@embroider/macros';
+
+import { upgradeStore } from '@ember-data/legacy-compat/-private';
 import { recordIdentifierFor } from '@ember-data/store';
 import { RecordStore } from '@warp-drive/core-types/symbols';
 
@@ -17,14 +20,57 @@ type Derivation<R, T> = (record: R, options: Record<string, unknown> | null, pro
 type SchemaService = {
   registerDerivation(name: string, derivation: Derivation<unknown, unknown>): void;
 };
-
-const LegacyFields = ['constructor', 'currentState', 'unloadRecord', 'errors'];
+// 'isDestroying', 'isDestroyed'
+const LegacyFields = [
+  '_createSnapshot',
+  'adapterError',
+  'belongsTo',
+  'changedAttributes',
+  'constructor',
+  'currentState',
+  'deleteRecord',
+  'destroyRecord',
+  'dirtyType',
+  'errors',
+  'hasDirtyAttributes',
+  'hasMany',
+  'isDeleted',
+  'isEmpty',
+  'isError',
+  'isLoaded',
+  'isLoading',
+  'isNew',
+  'isSaving',
+  'isValid',
+  'reload',
+  'rollbackAttributes',
+  'save',
+  'serialize',
+  'unloadRecord',
+];
 
 function unloadRecord(this: MinimalLegacyRecord) {
   if (this.currentState.isNew && (this.isDestroyed || this.isDestroying)) {
     return;
   }
   this[RecordStore].unloadRecord(this);
+}
+
+function serialize(this: MinimalLegacyRecord, options?: Record<string, unknown>) {
+  upgradeStore(this[RecordStore]);
+  return this[RecordStore].serializeRecord(this, options);
+}
+
+function createSnapshot(this: MinimalLegacyRecord) {
+  const store = this[RecordStore];
+
+  upgradeStore(store);
+  if (!store._fetchManager) {
+    const FetchManager = (importSync('@ember-data/legacy-compat/-private') as typeof import('@ember-data/legacy-compat/-private')).FetchManager;
+    store._fetchManager = new FetchManager(store);
+  }
+
+  return store._fetchManager.createSnapshot(recordIdentifierFor(this));
 }
 
 const LegacySupport = new WeakMap<MinimalLegacyRecord, Record<string, unknown>>();
@@ -37,18 +83,61 @@ function legacySupport(record: MinimalLegacyRecord, options: Record<string, unkn
   }
 
   switch (prop) {
+    case '_createSnapshot':
+      return createSnapshot;
+    case 'adapterError':
+      return record.currentState.adapterError;
+    case 'belongsTo':
+      throw new Error('not implemented');
+    case 'changedAttributes':
+      throw new Error('not implemented');
     case 'constructor':
       return (state._constructor = state._constructor || {
+        isModel: true,
         modelName: recordIdentifierFor(record).type,
       });
     case 'currentState':
       return (state.recordState = state.recordState || new RecordState(record));
-    case 'unloadRecord':
-      return unloadRecord;
+    case 'deleteRecord':
+      throw new Error('not implemented');
+    case 'destroyRecord':
+      throw new Error('not implemented');
+    case 'dirtyType':
+      return record.currentState.dirtyType;
     case 'errors':
       // @ts-expect-error
       // eslint-disable-next-line @typescript-eslint/no-unsafe-call
       return (state.errors = state.errors || Errors.create({ __record: record }));
+    case 'hasDirtyAttributes':
+      return record.currentState.isDirty;
+    case 'hasMany':
+      throw new Error('not implemented');
+    case 'isDeleted':
+      return record.currentState.isDeleted;
+    case 'isEmpty':
+      return record.currentState.isEmpty;
+    case 'isError':
+      return record.currentState.isError;
+    case 'isLoaded':
+      return record.currentState.isLoaded;
+    case 'isLoading':
+      return record.currentState.isLoading;
+    case 'isNew':
+      return record.currentState.isNew;
+    case 'isSaving':
+      return record.currentState.isSaving;
+    case 'isValid':
+      return record.currentState.isValid;
+    case 'reload':
+      throw new Error('not implemented');
+    case 'rollbackAttributes':
+      throw new Error('not implemented');
+    case 'save':
+      throw new Error('not implemented');
+    case 'serialize':
+      return serialize;
+    case 'unloadRecord':
+      return unloadRecord;
     default:
       assert(`${prop} is not a supported legacy field`, false);
   }
