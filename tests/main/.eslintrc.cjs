94f7e3a00de35c36fa8620e127b9d265f1738e38
Chris Thoburn <runspired@gmail.com>
2024-03-09 11:03:31 -0800

    lint cleanup

--- tests/main/.eslintrc.cjs
+++ tests/main/.eslintrc.cjs
@@ -32,23 +32,7 @@ module.exports = {
     // the files list here
     // see https://github.com/emberjs/data/issues/6233#issuecomment-849279594
     typescript.defaults({
-      files: [
-        './tests/helpers/accessors.ts',
-        './tests/integration/identifiers/configuration-test.ts',
-        './tests/integration/identifiers/lid-reflection-test.ts',
-        './tests/integration/identifiers/new-records-test.ts',
-        './tests/integration/identifiers/polymorphic-scenarios-test.ts',
-        './tests/integration/identifiers/record-identifier-for-test.ts',
-        './tests/integration/identifiers/scenarios-test.ts',
-        './tests/integration/model-errors-test.ts',
-        './tests/integration/record-data/record-data-errors-test.ts',
-        './tests/integration/record-data/record-data-state-test.ts',
-        './tests/integration/record-data/record-data-test.ts',
-        './tests/integration/record-data/store-wrapper-test.ts',
-        './tests/integration/relationships/rollback-test.ts',
-        './tests/integration/request-state-service-test.ts',
-        './tests/unit/custom-class-support/custom-class-model-test.ts',
-      ],
+      files: [],
       rules: {
         '@typescript-eslint/no-unsafe-assignment': 'off',
         '@typescript-eslint/no-unsafe-call': 'off',
