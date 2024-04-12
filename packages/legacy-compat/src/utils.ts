638626832fa1ff15f4c218d8f85393294c34422d
Chris Thoburn <runspired@users.noreply.github.com>
2024-04-11 19:42:48 -0700

    add label to error

--- packages/legacy-compat/src/utils.ts
+++ packages/legacy-compat/src/utils.ts
@@ -34,7 +34,7 @@ const AssertFn: AssertFunc = ((message: string, condition: unknown) => {
     _AssertFn(message, condition);
   }
   if (DEBUG) {
-    throw new Error(message);
+    throw new Error(`Assertion Failed: ${message}`);
   }
 }) as unknown as AssertFunc;
 let NormalizedType: Normalizer = (str: string) => {
