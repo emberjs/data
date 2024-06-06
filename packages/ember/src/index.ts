7d7874b2d28f8830e4418fa0973faf0e96281543
Chris Thoburn <runspired@users.noreply.github.com>
2024-06-06 01:13:52 -0700

    keep gts ext

--- packages/ember/src/index.ts
+++ packages/ember/src/index.ts
@@ -1,4 +1,4 @@
 export { getRequestState } from './-private/request-state';
 export { getPromiseState } from './-private/promise-state';
-export { Request } from './-private/request';
-export { Await, Throw } from './-private/await';
+export { Request } from './-private/request.gts';
+export { Await, Throw } from './-private/await.gts';
