94f06d69c9251fd562132a3e25a3a92ceea7547d
Krystan HuffMenne <kmenne+github@gmail.com>
2024-06-13 11:11:08 -0700

    Improve query types for legacy-compat/builders

--- packages/legacy-compat/src/builders/find-record.ts
+++ packages/legacy-compat/src/builders/find-record.ts
@@ -55,21 +55,17 @@ type FindRecordBuilderOptions = Omit<FindRecordOptions, 'preload'>;
   @public
   @static
   @for @ember-data/legacy-compat/builders
-  @param {string|object} type - either a string representing the name of the resource or a ResourceIdentifier object containing both the type (a string) and the id (a string) for the record or an lid (a string) of an existing record
+  @param {string|object} resource - either a string representing the name of the resource or a ResourceIdentifier object containing both the type (a string) and the id (a string) for the record or an lid (a string) of an existing record
   @param {string|number|object} id - optional object with options for the request only if the first param is a ResourceIdentifier, else the string id of the record to be retrieved
   @param {FindRecordBuilderOptions} [options] - if the first param is a string this will be the optional options for the request. See examples for available options.
   @return {FindRecordRequestInput} request config
 */
 export function findRecordBuilder<T extends TypedRecordInstance>(
-  resource: TypeFromInstance<T>,
+  type: TypeFromInstance<T>,
   id: string,
   options?: FindRecordBuilderOptions
 ): FindRecordRequestInput<TypeFromInstance<T>, T>;
-export function findRecordBuilder(
-  resource: string,
-  id: string,
-  options?: FindRecordBuilderOptions
-): FindRecordRequestInput;
+export function findRecordBuilder(type: string, id: string, options?: FindRecordBuilderOptions): FindRecordRequestInput;
 export function findRecordBuilder<T extends TypedRecordInstance>(
   resource: ResourceIdentifierObject<TypeFromInstance<T>>,
   options?: FindRecordBuilderOptions
