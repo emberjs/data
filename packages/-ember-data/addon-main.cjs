ec734d3a326c46cab102b96d7ccb7309822360be
Chris Thoburn <runspired@users.noreply.github.com>
2024-06-13 19:52:54 -0700

    fix: add deprecation and avoid breaking configs

--- packages/-ember-data/addon-main.cjs
+++ packages/-ember-data/addon-main.cjs
@@ -13,4 +13,19 @@ if (pkg['ember-addon'].version === 1) {
   delete addon.treeForApp;
 }
 
+const included = addon.included;
+addon.included = function includedIntercept() {
+  // we access this as a side-effect to ember-cli will give us a super call
+  const sup = this._super.included;
+  if (this.hasBeenCalled) {
+    return included?.apply(this, arguments);
+  }
+  this.hasBeenCalled = true;
+  const app = this.app;
+  const dirname = app.project.root;
+  const { setConfig } = require('@warp-drive/build-config/cjs-set-config.cjs');
+  setConfig(app, dirname, Object.assign({}, app.options?.emberData, { ___legacy_support: true }));
+  return included?.apply(this, arguments);
+};
+
 module.exports = addon;
--- packages/build-config/vite.config-cjs.mjs
+++ packages/build-config/vite.config-cjs.mjs
@@ -1,6 +1,6 @@
 import { createConfig } from '@warp-drive/internal-config/vite/config.js';
 
-export const externals = ['babel-import-util', 'fs'];
+export const externals = ['babel-import-util', 'fs', 'path'];
 
 export const entryPoints = [
   './cjs-src/transforms/babel-plugin-transform-asserts.js',
@@ -8,6 +8,7 @@ export const entryPoints = [
   './cjs-src/transforms/babel-plugin-transform-features.js',
   './cjs-src/transforms/babel-plugin-transform-logging.js',
   './cjs-src/addon-shim.js',
+  './src/cjs-set-config.ts',
 ];
 
 export default createConfig(
