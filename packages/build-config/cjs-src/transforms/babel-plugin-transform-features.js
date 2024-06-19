24161513ca931afe3e8cdd426977658f62de65ac
Chris Thoburn <runspired@users.noreply.github.com>
2024-06-19 10:39:47 -0700

    fix: inflector support

--- packages/build-config/cjs-src/transforms/babel-plugin-transform-features.js
+++ packages/build-config/cjs-src/transforms/babel-plugin-transform-features.js
@@ -64,9 +64,9 @@ export default function (babel) {
             specifier.scope.removeOwnBinding(localBindingName);
             specifier.remove();
           });
-        }
-        if (path.get('specifiers').length === 0) {
-          path.remove();
+          if (path.get('specifiers').length === 0) {
+            path.remove();
+          }
         }
       },
 
