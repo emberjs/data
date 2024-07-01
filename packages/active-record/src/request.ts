f550309acea1f2365e5f18ab478d014d2ee193f4
Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>
2024-07-02 00:42:52 +0530

    fix: code snippet in json-api/request.ts documentation

--- packages/active-record/src/request.ts
+++ packages/active-record/src/request.ts
@@ -31,7 +31,7 @@ import { findRecord } from '@ember-data/active-record/request';
 
 const options = findRecord('ember-developer', '1', { include: ['pets', 'friends'] });
 
-/\*
+/*
   => {
     url: 'https://api.example.com/v1/ember_developers/1?include=friends,pets',
     method: 'GET',
@@ -39,7 +39,7 @@ const options = findRecord('ember-developer', '1', { include: ['pets', 'friends'
     op: 'findRecord';
     records: [{ type: 'ember-developer', id: '1' }]
   }
-*\/
+*\
 ```
 
 Request builder output may be used with either `requestManager.request` or `store.request`.
