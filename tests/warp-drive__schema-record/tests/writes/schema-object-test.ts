32480818b4a30d6e77580e85cd854ad6226a37e6
Rich Glazerman <rglazerman@squareup.com>
2024-07-02 08:33:26 -0700

    Add write tests

--- tests/warp-drive__schema-record/tests/writes/schema-object-test.ts
+++ tests/warp-drive__schema-record/tests/writes/schema-object-test.ts
@@ -14,11 +14,18 @@ type address = {
   state: string;
   zip: string | number;
 };
+
+type business = {
+  name: string;
+  address?: address;
+  addresses?: address[];
+}
 interface User {
   id: string;
   $type: 'user';
   name: string;
   address: address | null;
+  business: business | null;
   [Type]: 'user';
 }
 
@@ -489,4 +496,292 @@ module('Writes | schema-object fields', function (hooks) {
       };
     }, /Field notAField does not exist on schema object address/);
   });
+
+  test('we can edit nested schema-object fields', function (assert) {
+    const store = this.owner.lookup('service:store') as Store;
+    const { schema } = store;
+    registerDerivations(schema);
+
+    schema.registerResource({
+      identity: null,
+      type: 'address',
+      fields: [
+        {
+          name: 'street',
+          kind: 'field',
+        },
+        {
+          name: 'city',
+          kind: 'field',
+        },
+        {
+          name: 'state',
+          kind: 'field',
+        },
+        {
+          name: 'zip',
+          kind: 'field',
+        },
+      ],
+    });
+    schema.registerResource({
+      identity: null,
+      type: 'business',
+      fields: [
+        {
+          name: 'name',
+          kind: 'field',
+        },
+        {
+          name: 'address',
+          type: 'address',
+          kind: 'schema-object',
+        },
+      ],
+    });
+    schema.registerResource(
+      withDefaults({
+        type: 'user',
+        fields: [
+          {
+            name: 'name',
+            kind: 'field',
+          },
+          {
+            name: 'address',
+            type: 'address',
+            kind: 'schema-object',
+          },
+          {
+            name: 'business',
+            type: 'business',
+            kind: 'schema-object',
+          },
+        ],
+      })
+    );
+
+    const sourceAddress: address = {
+      street: '123 Main St',
+      city: 'Anytown',
+      state: 'NY',
+      zip: '12345',
+    };
+    const sourceBusinessAddress: address = {
+      street: '456 Elm St',
+      city: 'Anytown',
+      state: 'NY',
+      zip: '12345',
+    };
+    const record = store.push<User>({
+      data: {
+        type: 'user',
+        id: '1',
+        attributes: {
+          name: 'Rey Skybarker',
+          address: sourceAddress,
+          business: { name: 'Acme', address: sourceBusinessAddress },
+        },
+      },
+    });
+    
+    assert.strictEqual(record.id, '1', 'id is accessible');
+    assert.strictEqual(record.$type, 'user', '$type is accessible');
+    assert.strictEqual(record.name, 'Rey Skybarker', 'name is accessible');
+    assert.propEqual(
+      record.address,
+      { street: '123 Main St', city: 'Anytown', state: 'NY', zip: '12345' },
+      'we can access address object'
+    );
+    assert.strictEqual(record.address, record.address, 'We have a stable object reference');
+    assert.notStrictEqual(record.address, sourceAddress);
+    assert.strictEqual(record.business?.name, 'Acme');
+    assert.propEqual(record.business?.address, { street: '456 Elm St', city: 'Anytown', state: 'NY', zip: '12345' });
+
+    // test that the data entered the cache properly
+    const identifier = recordIdentifierFor(record);
+    const cachedResourceData = store.cache.peek(identifier);
+
+    assert.deepEqual(
+      cachedResourceData?.attributes?.address,
+      {
+        street: '123 Main St',
+        city: 'Anytown',
+        state: 'NY',
+        zip: '12345',
+      },
+      'the cache values are correct for the object field'
+    );
+    assert.deepEqual(
+      cachedResourceData?.attributes?.business,
+      {
+        name: 'Acme',
+        address: {
+          street: '456 Elm St',
+          city: 'Anytown',
+          state: 'NY',
+          zip: '12345',
+        },
+      },
+      'the cache values are correct for a nested object field'
+    );
+    record.business!.address = { street: '789 Oak St', city: 'Sometown', state: 'NJ', zip: '23456' };
+    assert.propEqual(
+      record.business?.address,
+      { street: '789 Oak St', city: 'Sometown', state: 'NJ', zip: '23456' },
+      'we can access nested address object'
+    );
+    assert.strictEqual(record.business?.address, record.business?.address, 'We have a stable object reference');
+    // Test that the data entered teh cache properly
+    const cachedResourceData2 = store.cache.peek(identifier);
+    assert.deepEqual(
+      cachedResourceData2?.attributes?.business,
+      {
+        name: 'Acme',
+        address: {
+          street: '789 Oak St',
+          city: 'Sometown',
+          state: 'NJ',
+          zip: '23456',
+        },
+      },
+      'the cache values are correct for a nested object field'
+    );
+  });
+
+  test('we can edit nested nest schema-array fields inside a schema-object', function (assert) {
+    const store = this.owner.lookup('service:store') as Store;
+    const { schema } = store;
+    registerDerivations(schema);
+
+    schema.registerResource({
+      identity: null,
+      type: 'address',
+      fields: [
+        {
+          name: 'street',
+          kind: 'field',
+        },
+        {
+          name: 'city',
+          kind: 'field',
+        },
+        {
+          name: 'state',
+          kind: 'field',
+        },
+        {
+          name: 'zip',
+          kind: 'field',
+        },
+      ],
+    });
+    schema.registerResource({
+      identity: null,
+      type: 'business',
+      fields: [
+        {
+          name: 'name',
+          kind: 'field',
+        },
+        {
+          name: 'addresses',
+          type: 'address',
+          kind: 'schema-array',
+        },
+      ],
+    });
+    schema.registerResource(
+      withDefaults({
+        type: 'user',
+        fields: [
+          {
+            name: 'name',
+            kind: 'field',
+          },
+          {
+            name: 'address',
+            type: 'address',
+            kind: 'schema-object',
+          },
+          {
+            name: 'business',
+            type: 'business',
+            kind: 'schema-object',
+          },
+        ],
+      })
+    );
+    const sourceAddress: address = {
+      street: '123 Main St',
+      city: 'Anytown',
+      state: 'NY',
+      zip: '12345',
+    };
+    const sourceBusinessAddress1: address = {
+      street: '456 Elm St',
+      city: 'Anytown',
+      state: 'NY',
+      zip: '12345',
+    };
+    const sourceBusinessAddress2: address = {
+      street: '789 Oak St',
+      city: 'Sometown',
+      state: 'NJ',
+      zip: '23456',
+    };
+    const record = store.push<User>({
+      data: {
+        type: 'user',
+        id: '1',
+        attributes: {
+          name: 'Rey Skybarker',
+          address: sourceAddress,
+          business: { name: 'Acme', addresses: [sourceBusinessAddress1, sourceBusinessAddress2] },
+        },
+      },
+    });
+
+    assert.strictEqual(record.id, '1', 'id is accessible');
+    assert.strictEqual(record.$type, 'user', '$type is accessible');
+    assert.strictEqual(record.name, 'Rey Skybarker', 'name is accessible');
+    assert.propEqual(
+      record.address,
+      { street: '123 Main St', city: 'Anytown', state: 'NY', zip: '12345' },
+      'we can access address object'
+    );
+    assert.strictEqual(record.address, record.address, 'We have a stable object reference');
+    assert.strictEqual(record.business?.name, 'Acme');
+    assert.propEqual(record.business?.addresses, [
+      { street: '456 Elm St', city: 'Anytown', state: 'NY', zip: '12345' },
+      { street: '789 Oak St', city: 'Sometown', state: 'NJ', zip: '23456' },
+    ]);
+    assert.strictEqual(record.business?.addresses, record.business?.addresses, 'We have a stable array reference');
+    record.business!.addresses![0] = { street: '123 Main St', city: 'Anytown', state: 'NY', zip: '12345' };
+    assert.propEqual(
+      record.business?.addresses,
+      [
+        { street: '123 Main St', city: 'Anytown', state: 'NY', zip: '12345' },
+        { street: '789 Oak St', city: 'Sometown', state: 'NJ', zip: '23456' },
+      ],
+      'we can access nested address object'
+    );
+    assert.strictEqual(record.business?.addresses, record.business?.addresses, 'We have a stable array reference');
+    // Test that the data entered teh cache properly
+    const identifier = recordIdentifierFor(record);
+    const cachedResourceData = store.cache.peek(identifier);
+    assert.deepEqual(
+      cachedResourceData?.attributes?.business,
+      {
+        name: 'Acme',
+        addresses: [
+          { street: '123 Main St', city: 'Anytown', state: 'NY', zip: '12345' },
+          { street: '789 Oak St', city: 'Sometown', state: 'NJ', zip: '23456' },
+        ],
+      },
+      'the cache values are correct for a nested object field'
+    );
+
+  });
+
 });
