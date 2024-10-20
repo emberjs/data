fbcfbc7b397d5e6a0d5a972a744dc3c7c376e4d9
Chris Thoburn <runspired@users.noreply.github.com>
2024-10-19 21:02:39 -0700

    fix: use an explicit re-export file for the data-adapter

--- packages/debug/vite.config.mjs
+++ packages/debug/vite.config.mjs
@@ -1,8 +1,14 @@
 import { createConfig } from '@warp-drive/internal-config/vite/config.js';
 
-export const externals = ['@ember/debug/data-adapter', '@ember/service', '@ember/object/observers', '@ember/array'];
+export const externals = [
+  '@ember-data/debug/data-adapter',
+  '@ember/debug/data-adapter',
+  '@ember/service',
+  '@ember/object/observers',
+  '@ember/array',
+];
 
-export const entryPoints = ['./src/index.ts', './src/data-adapter.ts'];
+export const entryPoints = ['./src/index.ts', './src/data-adapter.ts', './src/_app_/data-adapter.js'];
 
 export default createConfig(
   {
