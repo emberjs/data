b156fcc8ca971ad00aa66d1e7081f19f59f1603b
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-11-04 18:40:37 -0500

    Describe more

--- packages/model/src/-private.ts
+++ packages/model/src/-private.ts
@@ -9,11 +9,14 @@ export { default as PromiseBelongsTo } from './-private/promise-belongs-to';
 export { default as PromiseManyArray } from './-private/promise-many-array';
 // We can't re-export from here, becaues it creates a module cycle
 // (which prevents vite and sometimes webpack from loading)
-// - @ember-data/model/-private
+// - RelatedCollection from @ember-data/model/-private
 //   -> @ember-data/model/has-many-abc123 (private module from rollup)
-//     -> @ember-data/store/-private
+//     -> RecordArray from @ember-data/store/-private
 //       -> @ember-data/store/index-abc123 (private module from rollup)
-//         -> importSync of @ember-data/model/-private
+//         -> _modelForMixin from importSync of @ember-data/model/-private
+//
+// So by pulling out _modelForMixin, we don't use importSync
+// to the same module that our calls stack includes
 //export { default as _modelForMixin } from './-private/model-for-mixin';
 
 // // Used by tests
