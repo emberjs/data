39e410bcfd7a0dfdf8f035e5a71b953e499936f7
Chris Thoburn <runspired@users.noreply.github.com>
2024-11-12 14:28:19 -0800

    port @types changes from main

--- @types/ember-data-qunit-asserts/index.d.ts
+++ @types/ember-data-qunit-asserts/index.d.ts
@@ -1,3 +1,7 @@
+import type { CacheOperation, NotificationType } from '@ember-data/store/-private/managers/notification-manager';
+import type { StableRecordIdentifier } from '@warp-drive/core-types';
+import type { StableDocumentIdentifier } from '@warp-drive/core-types/identifier';
+
 declare global {
   interface DeprecationConfig {
     id: string;
@@ -15,24 +19,70 @@ declare global {
   }
 
   interface Assert {
-    expectDeprecation(options: Partial<DeprecationConfig> & { id: string; count: number }): void;
-    expectDeprecation(callback: () => unknown, options: DeprecationConfig | string | RegExp): Promise<void>;
-    expectNoDeprecation(callback: () => unknown): Promise<void>;
+    expectDeprecation(options: DeprecationConfig, label?: string): void;
+    expectDeprecation(
+      callback: () => void | Promise<void>,
+      options: DeprecationConfig | string | RegExp,
+      label?: string
+    ): Promise<void>;
+    expectNoDeprecation(
+      callback: () => void | Promise<void>,
+      label?: string,
+      filter?: (deprecation: FoundDeprecation) => boolean
+    ): Promise<void>;
     expectWarning(callback: () => unknown, options: WarningConfig | string | RegExp): Promise<void>;
     expectNoWarning(callback: () => unknown): Promise<void>;
     expectAssertion(callback: () => unknown, matcher: string | RegExp): Promise<void>;
     expectNoAssertion(callback: () => unknown): Promise<void>;
+    /**
+     * Asserts that each member of actual strictly matches the corresponding member of expected.
+     * Asserts that actual is an array and has the same length as expected.
+     */
+    arrayStrictEquals<T>(actual: unknown, expected: T[], message: string): void;
+    /**
+     * Asserts that the given identifier has been notified of a change to the given bucket
+     * and optional key the given number of times during the test.
+     *
+     * Clears the notification count for the given identifier, bucket and key after the assertion
+     * is made so that it is easy to assert notification counts in between steps of a test.
+     */
+    notified(
+      identifier: StableDocumentIdentifier | StableRecordIdentifier,
+      bucket: NotificationType | CacheOperation,
+      key: string | null,
+      count: number
+    ): void;
+
+    watchNotifications(store?: unknown): void;
+    clearNotifications(): void;
   }
 
   namespace QUnit {
     export interface Assert {
       expectDeprecation(options: { id: string; count: number; until?: string }): void;
-      expectDeprecation(callback: () => unknown, options: DeprecationConfig | string | RegExp): Promise<void>;
-      expectNoDeprecation(callback: () => unknown): Promise<void>;
+      expectDeprecation(
+        callback: () => void | Promise<void>,
+        options: DeprecationConfig | string | RegExp
+      ): Promise<void>;
+      expectNoDeprecation(
+        callback: () => void | Promise<void>,
+        label?: string,
+        filter?: (deprecation: FoundDeprecation) => boolean
+      ): Promise<void>;
       expectWarning(callback: () => unknown, options: WarningConfig | string | RegExp): Promise<void>;
       expectNoWarning(callback: () => unknown): Promise<void>;
       expectAssertion(callback: () => unknown, matcher: string | RegExp): Promise<void>;
       expectNoAssertion(callback: () => unknown): Promise<void>;
+      arrayStrictEquals<T>(unknown, expected: T[], message: string): void;
+      notified(
+        identifier: StableDocumentIdentifier | StableRecordIdentifier,
+        bucket: NotificationType | CacheOperation,
+        key: string | null,
+        count: number
+      ): void;
+
+      watchNotifications(store?: unknown): void;
+      clearNotifications(): void;
     }
   }
 
--- ember-data-types/q/promise-proxies.ts
+++
@@ -1,3 +0,0 @@
-// shim type until we can properly type
-// these proxies
-export type PromiseProxy<T> = Promise<T>;
