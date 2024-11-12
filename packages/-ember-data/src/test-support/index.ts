0f7ab81ccd7930e51175520e2396004bf878f1ab
Chris Thoburn <runspired@users.noreply.github.com>
2024-11-12 15:05:05 -0800

    move -ember-data source-code from addon to src directory

--- packages/-ember-data/src/test-support/index.ts
+++ packages/-ember-data/src/test-support/index.ts
@@ -1,9 +1,11 @@
-import { assert } from '@ember/debug';
+import type Owner from '@ember/owner';
 import { render as renderTemplate, settled } from '@ember/test-helpers';
 
 import * as QUnit from 'qunit';
 
-import { PRODUCTION } from '@ember-data/env';
+import type Store from '@ember-data/store';
+import { PRODUCTION } from '@warp-drive/build-config/env';
+import { assert } from '@warp-drive/build-config/macros';
 
 /*
   Temporary replacement for the render test helper
@@ -11,17 +13,18 @@ import { PRODUCTION } from '@ember-data/env';
   an app to incrementally migrate to tests that render async
   relationships in stages with potential for tests in between.
 */
-export async function render(template) {
+export async function render(template: object) {
   await renderTemplate(template);
-  const owner = QUnit.config.current.testEnvironment.owner;
-  const pending = owner.lookup('service:store')._getAllPending();
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  const owner = QUnit.config.current.testEnvironment.owner as Owner;
+  const pending = (owner.lookup('service:store') as Store)._getAllPending();
 
   // this should only be necessary in production tests
   // where @ember/test-waiters is deactivated :()
   if (PRODUCTION) {
     assert(
       `No pending requests exist in this test, use \`import { render } from '@ember/test-helpers';\``,
-      pending.length
+      pending?.length
     );
 
     await pending;
