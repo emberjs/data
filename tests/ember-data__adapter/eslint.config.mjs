a2ba1e3c616a129c73c739116dd6516288913d92
Chris Thoburn <runspired@users.noreply.github.com>
2024-11-12 17:01:43 -0800

    rename adapter-encapsulation ember-data__adapter test suite and update configs

---
+++ tests/ember-data__adapter/eslint.config.mjs
@@ -0,0 +1,36 @@
+// @ts-check
+import { globalIgnores } from '@warp-drive/internal-config/eslint/ignore.js';
+import * as node from '@warp-drive/internal-config/eslint/node.js';
+import * as typescript from '@warp-drive/internal-config/eslint/typescript.js';
+import * as js from '@warp-drive/internal-config/eslint/browser.js';
+import * as diagnostic from '@warp-drive/internal-config/eslint/diagnostic.js';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  // all ================
+  globalIgnores(),
+
+  // browser (js/ts) ================
+  js.browser({
+    srcDirs: ['app', 'tests'],
+    allowedImports: ['@ember/application'],
+  }),
+
+  // browser (js/ts) ================
+  typescript.browser({
+    files: ['**/*.ts'],
+    srcDirs: ['app', 'tests'],
+    allowedImports: ['@ember/application'],
+  }),
+
+  // node (module) ================
+  node.esm(),
+
+  // node (script) ================
+  node.cjs(),
+
+  // Test Support ================
+  diagnostic.browser({
+    allowedImports: ['@ember/object'],
+  }),
+];
