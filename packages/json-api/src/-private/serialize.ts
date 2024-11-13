e6f127b30149267d7d4b74f99834cab952d69a9e
Chris Thoburn <runspired@users.noreply.github.com>
2024-11-12 16:55:02 -0800

    copy json-api builder source-code from main

---
+++ packages/json-api/src/-private/serialize.ts
@@ -0,0 +1,126 @@
+/**
+ * @module @ember-data/json-api/request
+ */
+import { assert } from '@warp-drive/build-config/macros';
+import type { StableRecordIdentifier } from '@warp-drive/core-types';
+import type { Cache } from '@warp-drive/core-types/cache';
+import type { Relationship } from '@warp-drive/core-types/cache/relationship';
+import type { Value } from '@warp-drive/core-types/json/raw';
+import type { ResourceObject } from '@warp-drive/core-types/spec/json-api-raw';
+
+type ChangedRelationshipData = {
+  data: Relationship['data'];
+};
+
+export type JsonApiResourcePatch = {
+  type: string;
+  id: string | null;
+  lid: string;
+  attributes?: Record<string, Value>;
+  relationships?: Record<string, ChangedRelationshipData>;
+};
+
+/**
+ * Serializes the current state of a resource or array of resources for use with POST or PUT requests.
+ *
+ * @method serializeResources
+ * @static
+ * @public
+ * @for @ember-data/json-api/request
+ * @param {Cache} cache}
+ * @param {StableRecordIdentifier} identifier
+ * @return {object} An object with a `data` property containing the serialized resource patch
+ */
+export function serializeResources(cache: Cache, identifiers: StableRecordIdentifier): { data: ResourceObject };
+export function serializeResources(cache: Cache, identifiers: StableRecordIdentifier[]): { data: ResourceObject[] };
+export function serializeResources(
+  cache: Cache,
+  identifiers: StableRecordIdentifier | StableRecordIdentifier[]
+): { data: ResourceObject | ResourceObject[] } {
+  return {
+    data: Array.isArray(identifiers)
+      ? identifiers.map((identifier) => _serializeResource(cache, identifier))
+      : _serializeResource(cache, identifiers),
+  };
+}
+
+function _serializeResource(cache: Cache, identifier: StableRecordIdentifier): ResourceObject {
+  const { id, lid, type } = identifier;
+  // yup! this method actually does nothing. It's just here for the dev assertion
+  // and to assist in providing a little sugar to the consuming app via the `serializeResources` utility
+  const record = cache.peek(identifier) as ResourceObject;
+  assert(
+    `A record with id ${String(id)} and type ${type} for lid ${lid} was not found not in the supplied Cache.`,
+    record
+  );
+
+  return record;
+}
+
+/**
+ * Serializes changes to a resource for use with PATCH requests.
+ *
+ * Only attributes which are changed are serialized.
+ * Only relationships which are changed are serialized.
+ *
+ * Collection relationships serialize the collection as a whole.
+ *
+ * If you would like to serialize updates to a collection more granularly
+ * (for instance, as operations) request the diff from the store and
+ * serialize as desired:
+ *
+ * ```ts
+ * const relationshipDiffMap = cache.changedRelationships(identifier);
+ * ```
+ *
+ * @method serializePatch
+ * @static
+ * @public
+ * @for @ember-data/json-api/request
+ * @param {Cache} cache}
+ * @param {StableRecordIdentifier} identifier
+ * @return {object} An object with a `data` property containing the serialized resource patch
+ */
+export function serializePatch(
+  cache: Cache,
+  identifier: StableRecordIdentifier
+  // options: { include?: string[] } = {}
+): { data: JsonApiResourcePatch } {
+  const { id, lid, type } = identifier;
+  const record = cache.peek(identifier) as ResourceObject;
+  assert(
+    `A record with id ${String(id)} and type ${type} for lid ${lid} was not found not in the supplied Cache.`,
+    record
+  );
+
+  const data: JsonApiResourcePatch = {
+    type,
+    lid,
+    id,
+  };
+
+  if (cache.hasChangedAttrs(identifier)) {
+    const attrsChanges = cache.changedAttrs(identifier);
+    const attributes: ResourceObject['attributes'] = {};
+
+    Object.keys(attrsChanges).forEach((key) => {
+      const change = attrsChanges[key];
+      const newVal = change[1] as Value | undefined;
+      attributes[key] = newVal === undefined ? null : newVal;
+    });
+
+    data.attributes = attributes;
+  }
+
+  const changedRelationships = cache.changedRelationships(identifier);
+  if (changedRelationships.size) {
+    const relationships: Record<string, ChangedRelationshipData> = {};
+    changedRelationships.forEach((diff, key) => {
+      relationships[key] = { data: diff.localState };
+    });
+
+    data.relationships = relationships;
+  }
+
+  return { data };
+}
