3e8a2cb6dbae2cb1aa295d38d29c0a28c911b7cf
Chris Thoburn <runspired@users.noreply.github.com>
2024-11-19 19:56:22 -0800

    add regression test

--- tests/embroider-basic-compat/tests/acceptance/visit-test.js
+++ tests/embroider-basic-compat/tests/acceptance/visit-test.js
@@ -3,6 +3,7 @@ import { visit } from '@ember/test-helpers';
 import { module, test } from 'qunit';
 
 import { setupApplicationTest } from 'ember-qunit';
+import { DEPRECATE_CATCH_ALL } from '@warp-drive/build-config/deprecations';
 
 module('it works', function (hooks) {
   setupApplicationTest(hooks);
@@ -19,5 +20,13 @@ module('it works', function (hooks) {
     const record = store.createRecord('user', { name: 'Chris' });
 
     assert.strictEqual(record.name, 'Chris', 'correct name');
+  });
+
+  test('we can strip individual deprecations', async function (assert) {
+    if (DEPRECATE_CATCH_ALL) {
+      assert.ok(false, 'we should have stripped the deprecation');
+    } else {
+      assert.ok(true, 'we stripped the deprecation');
+    }
   });
 });
