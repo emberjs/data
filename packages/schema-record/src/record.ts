de867ef56c2c9ecbdf9e67768c561bbdf0973e7e
Leo Euclides <leoeuclidss@gmail.com>
2024-12-10 17:54:46 -0300

    Adds schema record hasMany relationships in sync linksMode

--- packages/schema-record/src/record.ts
+++ packages/schema-record/src/record.ts
@@ -3,6 +3,7 @@ import { dependencySatisfies, importSync, macroCondition } from '@embroider/macr
 import type { MinimalLegacyRecord } from '@ember-data/model/-private/model-methods';
 import type Store from '@ember-data/store';
 import type { NotificationType } from '@ember-data/store';
+import type { RelatedCollection as ManyArray } from '@ember-data/store/-private';
 import { recordIdentifierFor, setRecordIdentifier } from '@ember-data/store/-private';
 import { addToTransaction, entangleSignal, getSignal, type Signal, Signals } from '@ember-data/tracking/-private';
 import { assert } from '@warp-drive/build-config/macros';
@@ -18,6 +19,7 @@ import {
   computeAttribute,
   computeDerivation,
   computeField,
+  computeHasMany,
   computeLocal,
   computeObject,
   computeResource,
@@ -330,6 +332,21 @@ export class SchemaRecord {
             entangleSignal(signals, receiver, field.name);
             return getLegacySupport(receiver as unknown as MinimalLegacyRecord).getBelongsTo(field.name);
           case 'hasMany':
+            if (field.options.linksMode) {
+              entangleSignal(signals, receiver, field.name);
+
+              return computeHasMany(
+                store,
+                schema,
+                cache,
+                target,
+                identifier,
+                field,
+                propArray,
+                Mode[Editable],
+                Mode[Legacy]
+              );
+            }
             if (!HAS_MODEL_PACKAGE) {
               assert(
                 `Cannot use hasMany fields in your schema unless @ember-data/model is installed to provide legacy model support.  ${field.name} should likely be migrated to be a collection field.`
@@ -618,6 +635,17 @@ export class SchemaRecord {
                 } else if (field.kind === 'resource') {
                   // FIXME
                 } else if (field.kind === 'hasMany') {
+                  if (field.options.linksMode) {
+                    const peeked = peekManagedArray(self, field) as ManyArray | undefined;
+                    if (peeked) {
+                      // const arrSignal = peeked[ARRAY_SIGNAL];
+                      // arrSignal.shouldReset = true;
+                      // addToTransaction(arrSignal);
+                      peeked.notify();
+                    }
+                    return;
+                  }
+
                   assert(`Expected to have a getLegacySupport function`, getLegacySupport);
                   assert(`Can only use hasMany fields when the resource is in legacy mode`, Mode[Legacy]);
 
