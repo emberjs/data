279ed0b84e4ea5e856be58ffab1ababc1a35cade
Chris Thoburn <runspired@users.noreply.github.com>
2024-12-18 17:31:02 -0800

    fixup deprecations of invalid import locations

--- packages/-ember-data/app/services/store.js
+++ packages/-ember-data/app/services/store.js
@@ -1,1 +1,18 @@
+import { deprecate } from '@ember/debug';
+
 export { default } from 'ember-data/store';
+import { DISABLE_6X_DEPRECATIONS } from '@warp-drive/build-config/deprecations';
+
+deprecate(
+  "You are relying on ember-data auto-magically installing the store service. Use `export { default } from 'ember-data/store';` in app/services/store.js instead",
+  /* inline-macro-config */ DISABLE_6X_DEPRECATIONS,
+  {
+    id: 'ember-data:deprecate-legacy-imports',
+    for: 'ember-data',
+    until: '6.0',
+    since: {
+      enabled: '5.2',
+      available: '5.2',
+    },
+  }
+);
--- packages/-ember-data/app/transforms/boolean.js
+++ packages/-ember-data/app/transforms/boolean.js
@@ -1,1 +1,19 @@
+import { deprecate } from '@ember/debug';
+
+import { DISABLE_6X_DEPRECATIONS } from '@warp-drive/build-config/deprecations';
+
 export { BooleanTransform as default } from '@ember-data/serializer/transform';
+
+deprecate(
+  "You are relying on ember-data auto-magically installing the BooleanTransform. Use `export { BooleanTransform as default } from '@ember-data/serializer/transform';` in app/transforms/boolean.js instead",
+  /* inline-macro-config */ DISABLE_6X_DEPRECATIONS,
+  {
+    id: 'ember-data:deprecate-legacy-imports',
+    for: 'ember-data',
+    until: '6.0',
+    since: {
+      enabled: '5.2',
+      available: '5.2',
+    },
+  }
+);
