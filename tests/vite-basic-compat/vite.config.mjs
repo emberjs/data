00017ae84be5bb352be37196f8e2dfe2085f0695
Chris Thoburn <runspired@users.noreply.github.com>
2024-12-18 18:34:57 -0800

    chore: fixup vite test app

--- tests/vite-basic-compat/vite.config.mjs
+++ tests/vite-basic-compat/vite.config.mjs
@@ -1,44 +1,15 @@
 import { defineConfig } from 'vite';
-import { resolver, hbs, scripts, templateTag, optimizeDeps, compatPrebuild, assets, contentFor } from '@embroider/vite';
+import { extensions, classicEmberSupport, ember } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
 
-const extensions = ['.mjs', '.gjs', '.js', '.mts', '.gts', '.ts', '.hbs', '.json'];
-
-export default defineConfig(({ mode }) => {
-  return {
-    resolve: {
+export default defineConfig({
+  plugins: [
+    classicEmberSupport(),
+    ember(),
+    // extra plugins here
+    babel({
+      babelHelpers: 'runtime',
       extensions,
-    },
-    plugins: [
-      hbs(),
-      templateTag(),
-      scripts(),
-      resolver(),
-      compatPrebuild(),
-      assets(),
-      contentFor(),
-
-      babel({
-        babelHelpers: 'runtime',
-        extensions,
-      }),
-    ],
-    optimizeDeps: optimizeDeps(),
-    server: {
-      port: 4200,
-    },
-    build: {
-      outDir: 'dist',
-      rollupOptions: {
-        input: {
-          main: 'index.html',
-          ...(shouldBuildTests(mode) ? { tests: 'tests/index.html' } : undefined),
-        },
-      },
-    },
-  };
+    }),
+  ],
 });
-
-function shouldBuildTests(mode) {
-  return mode !== 'production' || process.env.FORCE_BUILD_TESTS;
-}
