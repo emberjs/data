11f67112ccb3ac017ba03dab2e3d244bf48b3eeb
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-01-27 18:40:10 -0500

    Revert changes to vite-basic-compat

--- tests/vite-basic-compat/ember-cli-build.js
+++ tests/vite-basic-compat/ember-cli-build.js
@@ -3,18 +3,8 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 const { maybeEmbroider } = require('@embroider/test-setup');
 
-module.exports = async function (defaults) {
-  const { setConfig } = await import('@warp-drive/build-config');
-
+module.exports = function (defaults) {
   let app = new EmberApp(defaults, {});
-
-  /**
-   * TODO: find another way to set this config,
-   *       because the most modern of apps will not have a compat build
-   */
-  setConfig(app, __dirname, {
-    compatWith: process.env.EMBER_DATA_FULL_COMPAT ? '99.0' : null,
-  });
 
   return maybeEmbroider(app);
 };
