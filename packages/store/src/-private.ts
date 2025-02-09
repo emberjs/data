20af555bc8f41335bfec91545b4236c2f99238d2
Chris Thoburn <runspired@users.noreply.github.com>
2025-02-08 17:19:28 -0800

    perf: dont notify for attrs on insertions or committed changes that are referentially equal (#9654)
    * perf: dont notify for attribute changes that are referentially equal
    
    * fix commit sha
    
    * fix node version
    
    * update tracerbench
    
    * bump tracerbench again
    
    * bump tracerbench again
    
    * pass complex flags
    
    * feat: use flags
    
    * feat: use flags
    
    * try again
    
    * try again
    
    * fix name
    
    * fix hotspots
    
    * fixup test signature
    
    * feat: add nicer logs, fix emergent behavior

--- packages/store/src/-private.ts
+++ packages/store/src/-private.ts
@@ -49,3 +49,5 @@ export { setRecordIdentifier, StoreMap } from './-private/caches/instance-cache'
 export { setCacheFor } from './-private/caches/cache-utils';
 export { normalizeModelName as _deprecatingNormalize } from './-private/utils/normalize-model-name';
 export type { StoreRequestInput } from './-private/cache-handler/handler';
+
+export { log, logGroup } from './-private/debug/utils';
--- tests/main/ember-cli-build.js
+++ tests/main/ember-cli-build.js
@@ -57,6 +57,17 @@ module.exports = async function (defaults) {
 
   setConfig(app, __dirname, {
     compatWith: process.env.EMBER_DATA_FULL_COMPAT ? '99.0' : null,
+    debug: {
+      // LOG_GRAPH: true,
+      // LOG_IDENTIFIERS: true,
+      // LOG_NOTIFICATIONS: true,
+      // LOG_INSTANCE_CACHE: true,
+      // LOG_MUTATIONS: true,
+      // LOG_PAYLOADS: true,
+      // LOG_REQUESTS: true,
+      // LOG_REQUEST_STATUS: true,
+      // LOG_OPERATIONS: true,
+    },
   });
 
   return app.toTree();
