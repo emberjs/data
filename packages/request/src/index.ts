65e7e2c3976e7005c4a658b338e6720ec9ba5305
Chris Thoburn <runspired@users.noreply.github.com>
2025-02-19 14:02:49 -0800

    fix lint

--- packages/request/src/index.ts
+++ packages/request/src/index.ts
@@ -16,9 +16,7 @@ export { setPromiseResult, getPromiseResult } from './-private/promise-cache';
 export type { Awaitable } from './-private/promise-cache';
 
 // @ts-expect-error adding to globalThis
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 globalThis.setWarpDriveLogging = setWarpDriveLogging;
 
 // @ts-expect-error adding to globalThis
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 globalThis.getRuntimeConfig = getRuntimeConfig;
