3ffb4bc5ce67cec170e48862519a77423b508433
Chris Thoburn <runspired@users.noreply.github.com>
2025-02-19 14:57:17 -0800

    feat: runtime logging activation (#9683)
    * feat: runtime logging activation
    
    * fix bugs
    
    * more fixes
    
    * fix lint
    
    * fix build
    
    * fix more lint
    
    * nicer names

--- packages/request/src/index.ts
+++ packages/request/src/index.ts
@@ -1,3 +1,5 @@
+import { getRuntimeConfig, setLogging } from '@warp-drive/build-config/runtime';
+
 export { RequestManager as default } from './-private/manager';
 export { createDeferred } from './-private/future';
 export type { Future, Handler, CacheHandler, NextFn } from './-private/types';
@@ -12,3 +14,9 @@ export type {
 } from '@warp-drive/core-types/request';
 export { setPromiseResult, getPromiseResult } from './-private/promise-cache';
 export type { Awaitable } from './-private/promise-cache';
+
+// @ts-expect-error adding to globalThis
+globalThis.setWarpDriveLogging = setLogging;
+
+// @ts-expect-error adding to globalThis
+globalThis.getWarpDriveRuntimeConfig = getRuntimeConfig;
