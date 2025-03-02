00865b8c548ab186376fc41fe567ba05d9958654
Chris Thoburn <runspired@users.noreply.github.com>
2025-03-02 00:51:43 -0800

    fix types

--- tests/fastboot/app/serializers/application.ts
+++ tests/fastboot/app/serializers/application.ts
@@ -1,6 +1,4 @@
-// @ts-expect-error
 import JSONSerializer from '@ember-data/serializer/json';
-// @ts-expect-error
 import { EmbeddedRecordsMixin } from '@ember-data/serializer/rest';
 
 export default JSONSerializer.extend(EmbeddedRecordsMixin, {});
