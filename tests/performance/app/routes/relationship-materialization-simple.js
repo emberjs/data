1a6b085c5e46e43e09e1348aa6b0a3bcea766cbf
Chris Thoburn <runspired@users.noreply.github.com>
2025-03-03 17:21:33 -0800

    more updates to harness

--- tests/performance/app/routes/relationship-materialization-simple.js
+++ tests/performance/app/routes/relationship-materialization-simple.js
@@ -1,24 +1,27 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
+import { query } from '@ember-data/json-api/request';
+
 export default Route.extend({
   store: service(),
 
-  model() {
+  async model() {
     performance.mark('start-find-all');
-    return this.store.findAll('car', { reload: true }).then((cars) => {
-      performance.mark('start-materialization');
-      const flattened = cars.map((car) => {
-        // enforce materialization of our relationships
-        return {
-          name: car.id,
-          size: car.size.name,
-          make: car.make.name,
-          colors: car.colors.map((color) => color.name),
-        };
-      });
-      performance.mark('end-materialization');
-      return flattened;
+    await this.store.request(query('car', {}, { resourcePath: 'fixtures/relationship-materialization-simple.json' }));
+    const cars = this.store.peekAll('car');
+
+    performance.mark('start-materialization');
+    const flattened = cars.map((car) => {
+      // enforce materialization of our relationships
+      return {
+        name: car.id,
+        size: car.size.name,
+        make: car.make.name,
+        colors: car.colors.map((color) => color.name),
+      };
     });
+    performance.mark('end-materialization');
+    return flattened;
   },
 });
