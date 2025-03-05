1d88bfecbb83848e350f8d9a4ddbd5d3791efe3e
Chris Thoburn <runspired@users.noreply.github.com>
2025-03-04 23:12:25 -0800

    another pass through

--- tests/performance/server/index.ts
+++ tests/performance/server/index.ts
@@ -9,19 +9,64 @@ const RESP_OBJECT = {
   'Cache-Control': 'public, max-age=604800',
 };
 
-Bun.serve({
-  port: process.env.FIXTURE_API_PORT ? Number(process.env.FIXTURE_API_PORT) : 9999,
-  async fetch(request) {
-    const url = request.url.split('/fixtures')[1];
-    const filePath = join(FIXTURES_LOCATION + url + '.br');
-    const fileRef = Bun.file(filePath);
+const dist = join(__dirname, '../', process.argv[2] ?? 'dist');
+const port = process.argv[3] && process.argv[3] === '-p' ? (Number(process.argv[4]) ?? 9999) : 9999;
+const host = `http://localhost:${port}`;
 
-    if (!(await fileRef.exists())) {
+Bun.serve({
+  port,
+  async fetch(request) {
+    if (request.url.includes('/fixtures/')) {
+      const url = request.url.split('/fixtures')[1];
+      const filePath = join(FIXTURES_LOCATION + url + '.br');
+      const fileRef = Bun.file(filePath);
+
+      if (!(await fileRef.exists())) {
+        return new Response('Not Found', { status: 404 });
+      }
+
+      return new Response(fileRef, {
+        headers: new Headers(RESP_OBJECT),
+      });
+    }
+
+    const fileName = request.url.split(host)[1];
+    let filePath = join(dist, fileName === '/' ? '/index.html.br' : request.url.split(host)[1] + '.br');
+    let fileRef = Bun.file(filePath);
+    let exists = await fileRef.exists();
+    if (!exists && (fileName === '/' || fileName.endsWith('.js') || fileName.endsWith('.css'))) {
+      return new Response('Not Found', { status: 404 });
+    } else if (!exists) {
+      filePath = join(dist, '/index.html.br');
+      fileRef = Bun.file(filePath);
+      exists = await fileRef.exists();
+    }
+
+    if (!exists) {
       return new Response('Not Found', { status: 404 });
     }
 
+    // prettier-ignore
+    const mimeType = filePath.endsWith('.html.br') ? 'text/html'
+      : filePath.endsWith('.js.br') ? 'application/javascript'
+      : filePath.endsWith('.css.br') ? 'text/css'
+      : filePath.endsWith('.json.br') ? 'application/json'
+      : filePath.endsWith('.svg.br') ? 'image/svg+xml'
+      : filePath.endsWith('.png.br') ? 'image/png'
+      : filePath.endsWith('.jpg.br') ? 'image/jpeg'
+      : 'text/plain';
+
+    const headers = new Headers();
+    // We always compress and chunk the response
+    headers.set('Content-Encoding', 'br');
+    // we don't cache since tests will often reuse similar urls for different payload
+    headers.set('Cache-Control', 'max-age=31536000, public');
+    // streaming requires Content-Length
+    headers.set('Content-Length', String(fileRef.size));
+    headers.set('Content-Type', mimeType);
+
     return new Response(fileRef, {
-      headers: new Headers(RESP_OBJECT),
+      headers,
     });
   },
 });
@@ -29,6 +74,6 @@ Bun.serve({
 console.log(
   styleText(
     'grey',
-    `Fixture API running at ${styleText('yellow', 'http://') + styleText('cyan', 'localhost') + styleText('magenta', `:${process.env.FIXTURE_API_PORT || 9999}`)}`
+    `Application running at ${styleText('yellow', 'http://') + styleText('cyan', 'localhost') + styleText('yellow', `:${port}`)}`
   )
 );
--- tests/performance/vite.config.mjs
+++ tests/performance/vite.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import { extensions, classicEmberSupport, ember } from '@embroider/vite';
 import { babel } from '@rollup/plugin-babel';
+import { compression } from 'vite-plugin-compression2';
+// import { analyzer } from 'vite-bundle-analyzer';
+
+import zlib from 'zlib';
 
 export default defineConfig({
   plugins: [
@@ -11,10 +15,56 @@ export default defineConfig({
       babelHelpers: 'runtime',
       extensions,
     }),
+    // analyzer(),
+    compression({
+      algorithm: 'brotliCompress',
+      compressionOptions: {
+        params: {
+          [zlib.constants.BROTLI_PARAM_MODE]: zlib.constants.BROTLI_MODE_TEXT,
+          // brotli currently defaults to 11 but lets be explicit
+          [zlib.constants.BROTLI_PARAM_QUALITY]: zlib.constants.BROTLI_MAX_QUALITY,
+        },
+      },
+      deleteOriginalAssets: true,
+    }),
   ],
   server: {
     proxy: {
       '/fixtures': `http://localhost:${process.env.FIXTURE_API_PORT || '9999'}`,
+    },
+  },
+  mode: 'production',
+  build: {
+    minify: true,
+    reportCompressedSize: false,
+    terserOptions: {
+      compress: {
+        ecma: 2024,
+        passes: 6, // slow, but worth it
+        negate_iife: false,
+        sequences: 30,
+        defaults: true,
+        arguments: false,
+        keep_fargs: false,
+        toplevel: false,
+        unsafe: true,
+        unsafe_comps: true,
+        unsafe_math: true,
+        unsafe_symbols: true,
+        unsafe_proto: true,
+        unsafe_undefined: true,
+        inline: 5,
+        reduce_funcs: false,
+      },
+      mangle: {
+        keep_classnames: true,
+        keep_fnames: true,
+        module: true,
+      },
+      format: { beautify: true },
+      toplevel: false,
+      sourceMap: false,
+      ecma: 2024,
     },
   },
 });
