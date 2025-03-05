0f83bf3edc37550532af6e1da4de322bc7c587ad
Chris Thoburn <runspired@users.noreply.github.com>
2025-03-04 23:35:29 -0800

    make easier to debug

--- tests/performance/server/index.ts
+++ tests/performance/server/index.ts
@@ -2,13 +2,6 @@ import { styleText } from 'node:util';
 import { join } from 'path';
 const FIXTURES_LOCATION = join(__dirname, '../fixtures/generated');
 
-const RESP_OBJECT = {
-  'Content-Type': 'application/json',
-  'Transfer-Encoding': 'chunked',
-  'Content-Encoding': 'br',
-  'Cache-Control': 'public, max-age=604800',
-};
-
 const dist = join(__dirname, '../', process.argv[2] ?? 'dist');
 const port = process.argv[3] && process.argv[3] === '-p' ? (Number(process.argv[4]) ?? 9999) : 9999;
 const host = `http://localhost:${port}`;
@@ -16,22 +9,17 @@ const host = `http://localhost:${port}`;
 Bun.serve({
   port,
   async fetch(request) {
+    let filePath = '';
+    let fileName = '';
+
     if (request.url.includes('/fixtures/')) {
-      const url = request.url.split('/fixtures')[1];
-      const filePath = join(FIXTURES_LOCATION + url + '.br');
-      const fileRef = Bun.file(filePath);
-
-      if (!(await fileRef.exists())) {
-        return new Response('Not Found', { status: 404 });
-      }
-
-      return new Response(fileRef, {
-        headers: new Headers(RESP_OBJECT),
-      });
+      fileName = request.url.split('/fixtures')[1];
+      filePath = join(FIXTURES_LOCATION, fileName + '.br');
+    } else {
+      fileName = request.url.split(host)[1];
+      filePath = join(dist, fileName === '/' ? '/index.html.br' : request.url.split(host)[1] + '.br');
     }
 
-    const fileName = request.url.split(host)[1];
-    let filePath = join(dist, fileName === '/' ? '/index.html.br' : request.url.split(host)[1] + '.br');
     let fileRef = Bun.file(filePath);
     let exists = await fileRef.exists();
     if (!exists && (fileName === '/' || fileName.endsWith('.js') || fileName.endsWith('.css'))) {
@@ -43,6 +31,7 @@ Bun.serve({
     }
 
     if (!exists) {
+      console.log(styleText('red', `File not found: ${filePath}`));
       return new Response('Not Found', { status: 404 });
     }
 
@@ -59,12 +48,14 @@ Bun.serve({
     const headers = new Headers();
     // We always compress and chunk the response
     headers.set('Content-Encoding', 'br');
+    headers.set('Transfer-Encoding', 'chunked');
     // we don't cache since tests will often reuse similar urls for different payload
     headers.set('Cache-Control', 'max-age=31536000, public');
     // streaming requires Content-Length
     headers.set('Content-Length', String(fileRef.size));
     headers.set('Content-Type', mimeType);
 
+    console.log(styleText('green', `\tServing: ${filePath}`));
     return new Response(fileRef, {
       headers,
     });
