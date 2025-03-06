7fe1571faea21150f1937cec436b1df09d77e7fa
Chris Thoburn <runspired@users.noreply.github.com>
2025-03-06 01:53:45 -0800

    chore: remove mixin workaround

--- tests/performance/app/models/record-mixin-a.js
+++
@@ -1,5 +0,0 @@
-import Model from '@ember-data/model';
-
-import RecordMixinA from '../mixins/record-mixin-a';
-
-export default Model.extend(RecordMixinA);
--- tests/performance/app/models/record-mixin-c.js
+++
@@ -1,5 +0,0 @@
-import Model from '@ember-data/model';
-
-import RecordMixinC from '../mixins/record-mixin-c';
-
-export default Model.extend(RecordMixinC);
