f5d25b67e12db737ec392d89e381e1cc22a8708d
Chris Thoburn <runspired@users.noreply.github.com>
2025-03-07 22:33:56 -0800

    tooling updates

--- internal-tooling/src/sync-logos.ts
+++ internal-tooling/src/sync-logos.ts
@@ -2,40 +2,155 @@
 
 import path from 'path';
 import fs from 'fs';
+import debug from 'debug';
+import chalk from 'chalk';
 
-function getMonorepoRoot() {
-  return path.resolve(__dirname, '../..');
+const log = debug('wd:sync-logos');
+
+async function getMonorepoRoot() {
+  const MAX_DEPTH = 10;
+  // are we in the root?
+  let currentDir = process.cwd();
+  let depth = 0;
+  while (depth < MAX_DEPTH) {
+    const lockfileFile = path.join(currentDir, 'pnpm-lock.yaml');
+    if (await Bun.file(lockfileFile).exists()) {
+      return currentDir;
+    }
+    currentDir = path.join(currentDir, '../');
+    depth++;
+  }
+
+  throw new Error(`Could not find monorepo root from cwd ${process.cwd()}`);
+}
+
+function isDirectorySymlink(dirPath: string) {
+  try {
+    const stats = fs.lstatSync(dirPath);
+    console.log({ isSymlink: stats.isSymbolicLink(), isDirectory: stats.isDirectory() });
+    return stats.isSymbolicLink() && stats.isDirectory();
+  } catch (error) {
+    return false;
+  }
+}
+
+async function copyFiles({
+  packageDir,
+  packageLogosDir,
+  logosDir,
+  isLinked,
+  isCopied,
+}: {
+  packageDir: string;
+  packageLogosDir: string;
+  logosDir: string;
+  isLinked: boolean;
+  isCopied: boolean;
+}) {
+  // if we are in copy mode, remove any existing symlink and copy the files
+  if (isLinked) {
+    fs.rmdirSync(packageLogosDir, { recursive: true });
+    log(`\t\t\t🗑️ Deleted existing symlink for ${packageDir}/logos`);
+  } else if (isCopied) {
+    fs.rmSync(packageLogosDir, { recursive: true, force: true });
+    log(`\t\t\t🗑️ Deleted existing non-symlinked version of ${packageDir}/logos`);
+  }
+  fs.mkdirSync(packageLogosDir, { recursive: true });
+  log(`\t\t\t📁 Created ${packageDir}/logos`);
+
+  for (const logo of fs.readdirSync(logosDir, { recursive: true, encoding: 'utf-8' })) {
+    const logoPath = path.join(logosDir, logo);
+    const destPath = path.join(packageLogosDir, logo);
+    fs.copyFileSync(logoPath, destPath);
+    log(`\t\t\t📁 Copied ${logo} to ${packageDir}/logos`);
+  }
+}
+
+async function symlinkFiles({
+  packageDir,
+  packageLogosDir,
+  logosDir,
+  isLinked,
+  isCopied,
+}: {
+  packageDir: string;
+  packageLogosDir: string;
+  logosDir: string;
+  isLinked: boolean;
+  isCopied: boolean;
+}) {
+  // if we are in symlink mode, symlink if none is present.
+  // if one is present, do nothing
+  // if what is present is not a symlink, remove it and symlink
+  if (isLinked) {
+    log(`\t\t\t🔗 Symlink already exists for ${packageDir}/logos`);
+    return;
+  } else if (isCopied) {
+    fs.rmSync(packageLogosDir, { recursive: true, force: true });
+    log(`\t\t\t🗑️ Deleted existing non-symlinked version of ${packageDir}/logos`);
+  }
+
+  const source = packageLogosDir;
+  const target = path.relative(source, logosDir);
+  fs.symlinkSync(target, source, 'junction');
+  log(`\t\t\t🔗 Symlinked ${logosDir} to ${packageDir}/logos`);
+}
+
+async function updatePackageJson({ packageDir, packagesDir }: { packageDir: string; packagesDir: string }) {
+  // ensure "files" field in package.json includes "logos"
+  const packageJsonPath = path.join(packagesDir, packageDir, 'package.json');
+  const packageJsonFile = Bun.file(packageJsonPath);
+  const pkg = await packageJsonFile.json();
+  if (!pkg.files) {
+    pkg.files = ['logos'];
+    await packageJsonFile.write(JSON.stringify(pkg, null, 2));
+    log(`\t\t📝 Added "logos" to "files" in ${packageDir}/package.json`);
+  } else if (!pkg.files.includes('logos')) {
+    pkg.files.push('logos');
+    await packageJsonFile.write(JSON.stringify(pkg, null, 2));
+    log(`\t\t📝 Added "logos" to "files" in ${packageDir}/package.json`);
+  }
 }
 
 async function main() {
-  const monorepoRoot = getMonorepoRoot();
+  const copyLogos = process.argv.includes('--copy');
+
+  log(
+    `\n\t${chalk.gray('=').repeat(60)}\n\t\t${chalk.magentaBright('@warp-drive/')}${chalk.greenBright('internal-tooling')} Sync Logos\n\t${chalk.gray('=').repeat(60)}\n\n\t\t${chalk.gray(`Syncing logo files via ${chalk.yellow(copyLogos ? 'copy' : 'symlink')} from monorepo root to each package`)}\n\n`
+  );
+  const monorepoRoot = await getMonorepoRoot();
 
   // sync the logos from the monorepo root to each
   // package directory that has a logos directory
 
-  const logosDir = path.resolve(monorepoRoot, 'logos');
-  const packagesDir = path.resolve(monorepoRoot, 'packages');
+  const logosDir = path.join(monorepoRoot, 'logos');
+  const packagesDir = path.join(monorepoRoot, 'packages');
 
   for (const packageDir of fs.readdirSync(packagesDir)) {
-    const packageLogosDir = path.resolve(packageDir, 'logos');
-    console.log(`Syncing logos to ${packageLogosDir}`);
-    fs.rmdirSync(packageLogosDir, { recursive: true });
-    fs.mkdirSync(packageLogosDir, { recursive: true });
+    const packageLogosDir = path.join(packagesDir, packageDir, 'logos');
+    const isLinked = isDirectorySymlink(packageLogosDir);
+    const isCopied = !isLinked && fs.existsSync(packageLogosDir);
+    log(`\t\t🔁 Syncing logos to ${packageDir}`);
 
-    for (const logo of fs.readdirSync(logosDir)) {
-      const logoPath = path.resolve(logosDir, logo);
-      const destPath = path.resolve(packageLogosDir, logo);
-      fs.copyFileSync(logoPath, destPath);
+    if (!copyLogos) {
+      await symlinkFiles({
+        packageDir,
+        packageLogosDir,
+        logosDir,
+        isLinked,
+        isCopied,
+      });
+    } else {
+      await copyFiles({
+        packageDir,
+        packageLogosDir,
+        logosDir,
+        isLinked,
+        isCopied,
+      });
     }
 
-    // ensure "files" field in package.json includes "logos"
-    const packageJsonPath = path.resolve(packageDir, 'package.json');
-    const packageJsonFile = Bun.file(packageJsonPath);
-    const pkg = await packageJsonFile.json();
-    if (!pkg.files.includes('logos')) {
-      pkg.files.push('logos');
-      await packageJsonFile.write(JSON.stringify(pkg, null, 2));
-    }
+    await updatePackageJson({ packageDir, packagesDir });
   }
 }
 
