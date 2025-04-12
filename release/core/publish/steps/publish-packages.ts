c28da6167e180ad535b0935eabcc7db2c3dbdf5a
Chris Thoburn <runspired@users.noreply.github.com>
2025-04-11 17:10:56 -0700

    chore: improve error resilience of release script

--- release/core/publish/steps/publish-packages.ts
+++ release/core/publish/steps/publish-packages.ts
@@ -40,58 +40,118 @@ export async function publishPackages(
   }
 
   let publishCount = 0;
+  let error: Error | null = null;
+  const errors: Error[] = [];
   for (const [, strat] of strategy) {
     const pkg = packages.get(strat.name)!;
-    token = await publishPackage(config, strat.distTag, pkg.tarballPath, config.get('dry_run') as boolean, token);
+    [token, error] = await publishPackage(
+      config,
+      strat.distTag,
+      pkg.tarballPath,
+      config.get('dry_run') as boolean,
+      token
+    );
+    if (error) {
+      console.log(chalk.red(`\t🚫 Error publishing ${chalk.cyan(pkg.pkgData.name)} to npm: ${error.message}`));
+      errors.push(error);
+      continue;
+    }
     publishCount++;
 
     if (strat.stage === 'alpha' || strat.stage === 'beta') {
-      token = await updateDistTag(strat.name, pkg.pkgData.version, 'latest', config.get('dry_run') as boolean, token);
+      [token, error] = await updateDistTag(
+        strat.name,
+        pkg.pkgData.version,
+        'latest',
+        config.get('dry_run') as boolean,
+        token
+      );
+      if (error) {
+        console.log(
+          chalk.red(`\t🚫 Error updating dist-tag for ${chalk.cyan(pkg.pkgData.name)} to latest: ${error.message}`)
+        );
+        errors.push(error);
+      }
     }
 
     if (strat.mirrorPublish) {
-      token = await publishPackage(
+      [token, error] = await publishPackage(
         config,
         strat.distTag,
         pkg.mirrorTarballPath,
         config.get('dry_run') as boolean,
         token
       );
+      if (error) {
+        console.log(
+          chalk.red(`\t🚫 Error publishing ${chalk.cyan(pkg.pkgData.name)} <Mirror Package> to npm: ${error.message}`)
+        );
+        errors.push(error);
+        continue;
+      }
       publishCount++;
 
       if (strat.stage === 'alpha' || strat.stage === 'beta') {
-        token = await updateDistTag(
+        [token, error] = await updateDistTag(
           strat.mirrorPublishTo,
           pkg.pkgData.version,
           'latest',
           config.get('dry_run') as boolean,
           token
         );
+        if (error) {
+          console.log(
+            chalk.red(
+              `\t🚫 Error updating dist-tag for ${chalk.cyan(pkg.pkgData.name)} <Mirror Package> to latest: ${error.message}`
+            )
+          );
+          errors.push(error);
+        }
       }
     }
     if (strat.typesPublish) {
-      token = await publishPackage(
+      [token, error] = await publishPackage(
         config,
         strat.distTag,
         pkg.typesTarballPath,
         config.get('dry_run') as boolean,
         token
       );
+      if (error) {
+        console.log(
+          chalk.red(`\t🚫 Error publishing ${chalk.cyan(pkg.pkgData.name)} <Types Package> to npm: ${error.message}`)
+        );
+        errors.push(error);
+        continue;
+      }
       publishCount++;
 
       if (strat.stage === 'alpha' || strat.stage === 'beta') {
-        token = await updateDistTag(
+        [token, error] = await updateDistTag(
           strat.typesPublishTo,
           pkg.pkgData.version,
           'latest',
           config.get('dry_run') as boolean,
           token
         );
+        if (error) {
+          console.log(
+            chalk.red(`\t🚫 Error updating dist-tag for ${chalk.cyan(pkg.pkgData.name)} to latest: ${error.message}`)
+          );
+          errors.push(error);
+        }
       }
     }
   }
 
   console.log(`✅ ` + chalk.cyan(`published ${chalk.greenBright(publishCount)} 📦 packages to npm`));
+  if (errors.length > 0) {
+    console.log(chalk.red(`🚫 ${errors.length} errors occurred while publishing packages to npm`));
+    for (const error of errors) {
+      console.log(chalk.red(error.message));
+    }
+    throw new Error(`${errors.length} errors occurred while publishing packages to npm.`);
+  }
 }
 
 export async function getOTPToken(config: Map<string, string | number | boolean | null>, reprompt?: boolean) {
@@ -116,7 +176,7 @@ async function publishPackage(
   tarball: string,
   dryRun: boolean,
   otp?: string
-): Promise<string | undefined> {
+): Promise<[string | undefined, Error | null]> {
   let cmd = `npm publish ${tarball} --tag=${distTag} --access=public`;
 
   if (otp) {
@@ -129,17 +189,16 @@ async function publishPackage(
 
   try {
     await exec({ cmd, condense: true });
-  } catch (e) {
-    if (!otp || !(e instanceof Error)) {
-      throw e;
+  } catch (e: unknown) {
+    const error = !(e instanceof Error) ? new Error(e as string) : e;
+    if (otp) {
+      if (error.message.includes('E401') || error.message.includes('EOTP')) {
+        otp = await getOTPToken(config, true);
+        return publishPackage(config, distTag, tarball, dryRun, otp);
+      }
     }
-    if (e.message.includes('E401') || e.message.includes('EOTP')) {
-      otp = await getOTPToken(config, true);
-      return publishPackage(config, distTag, tarball, dryRun, otp);
-    } else {
-      throw e;
-    }
+    return [otp, error];
   }
 
-  return otp;
+  return [otp, null];
 }
