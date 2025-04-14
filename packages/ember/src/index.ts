16cd2fddfd8c7ab9ad2f45cd2e4d2cefc29e8c56
Chris Thoburn <runspired@users.noreply.github.com>
2025-04-14 12:17:38 -0700

    docs: add note about handlebars usage to @warp-drive/ember

--- packages/ember/src/index.ts
+++ packages/ember/src/index.ts
@@ -14,6 +14,33 @@
  * and components that enable you to build robust performant apps with
  * elegant control flow.
  *
+ * ## Using .hbs
+ *
+ * The components and utils this library exports are intended for use with Glimmer
+ * Flavored JavaScript (gjs). To use them in handlebars files, your app should re-
+ * export them. For instance:
+ *
+ * *app/components/await.ts*
+ * ```ts
+ * export { Await as default } from '@warp-drive/ember';
+ * ```
+ *
+ * ```hbs
+ * <Await @promise={{this.getTheData}}></Await>
+ * ```
+ *
+ * This allows renaming them to avoid conflicts just by using a different filename
+ * if desired:
+ *
+ * *app/components/warp-drive-await.ts*
+ * ```ts
+ * export { Await as default } from '@warp-drive/ember';
+ * ```
+ *
+ * ```hbs
+ * <WarpDriveAwait @promise={{this.getTheData}}></WarpDriveAwait>
+ * ```
+ *
  * @module @warp-drive/ember
  * @main @warp-drive/ember
  */
