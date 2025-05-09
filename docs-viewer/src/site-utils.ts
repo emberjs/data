a8cb571096ad042a75d1cd1f98f0f033d79e0894
Chris Thoburn <runspired@users.noreply.github.com>
2025-05-09 01:22:53 -0700

    docs: programatically build guides nav, begin work on api doc extraction (#9981)

---
+++ docs-viewer/src/site-utils.ts
@@ -0,0 +1,57 @@
+import path from 'path';
+// @ts-expect-error missing in node types
+import { globSync } from 'node:fs';
+
+function segmentToTitle(segment: string) {
+  const value = segment.split('-').map((s) => s.charAt(0).toUpperCase() + s.slice(1));
+  if (!isNaN(Number(value[0]))) {
+    value.shift();
+  }
+  const result = value.join(' ').replace('.md', '');
+
+  return result === 'Index' ? 'Introduction' : result;
+}
+
+export async function getGuidesStructure() {
+  const GuidesDirectoryPath = path.join(__dirname, '../../guides');
+  const glob = globSync('**/*.md', { cwd: GuidesDirectoryPath }) as string[];
+
+  const groups: Record<string, any> = {};
+
+  for (const filepath of glob) {
+    const segments = filepath.split(path.sep);
+    const lastSegment = segments.pop()!;
+    let group = groups;
+
+    for (let i = 0; i < segments.length; i++) {
+      const segment = segments[i];
+      if (!group[segment]) {
+        group[segment] = {
+          text: segmentToTitle(segment),
+          collapsed: true,
+          items: {},
+        };
+      }
+      group = group[segment].items;
+    }
+
+    group[lastSegment] = {
+      text: segmentToTitle(lastSegment),
+      link: `/guides/${filepath.replace(/\.md$/, '')}`,
+    };
+  }
+
+  // deep iterate converting items objects to arrays
+  return deepConvert(groups);
+}
+
+function deepConvert(obj: Record<string, any>) {
+  const groups = Array.from(Object.values(obj));
+
+  for (const group of groups) {
+    if (group.items) {
+      group.items = deepConvert(group.items);
+    }
+  }
+  return groups;
+}
