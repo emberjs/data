cec6c9622d7cd347cb06c13954cafa567537438b
Leo Euclides <leoeuclidss@gmail.com>
2025-05-29 16:34:50 -0300

    Exports getPaginationState on @warp-drive/core/-private

--- warp-drive-packages/core/src/store/-private.ts
+++ warp-drive-packages/core/src/store/-private.ts
@@ -52,6 +52,7 @@ export {
   type RequestLoadingState,
   type RequestCacheRequestState as RequestState,
 } from './-private/new-core-tmp/request-state.ts';
+export { getPaginationState } from './-private/new-core-tmp/pagination-state.ts';
 
 export { createMemo, type SignalHooks, waitFor } from './-private/new-core-tmp/reactivity/configure.ts';
 export {
