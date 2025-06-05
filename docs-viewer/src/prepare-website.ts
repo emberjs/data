ed9b227f5528f4b6397412d6aa07f2b30f6b4e76
Chris Thoburn <runspired@users.noreply.github.com>
2025-06-05 15:56:39 -0700

    docs: fix contributing guide output path

--- docs-viewer/src/prepare-website.ts
+++ docs-viewer/src/prepare-website.ts
@@ -9,7 +9,7 @@ export async function main() {
   const guidesPath = join(__dirname, '../../guides');
   const contributingPath = join(__dirname, '../../contributing');
   const copiedPath = join(__dirname, '../docs.warp-drive.io/guides');
-  const copiedContributingPath = join(__dirname, '../docs.warp-drive.io/guide/contributing');
+  const copiedContributingPath = join(__dirname, '../docs.warp-drive.io/guides/contributing');
 
   // use Bun to create the symlink if it doesn't exist
 
