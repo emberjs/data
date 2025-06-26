a611ad28776a115bdea0ec35df49979f62b8c0f9
Chris Thoburn <runspired@users.noreply.github.com>
2025-06-25 20:07:57 -0700

    Update install.ts

--- warp-drive-packages/tc39-proposal-signals/src/install.ts
+++ warp-drive-packages/tc39-proposal-signals/src/install.ts
@@ -1,47 +1,20 @@
 /**
  * Uses: https://github.com/proposal-signals/signal-polyfill
  */
-
 import { Signal } from 'signal-polyfill';
 
-import { setupSignals } from '@warp-drive/core/configure';
-import type { SignalHooks } from '@warp-drive/core/store/-private';
+import { setupSignals, type SignalHooks, type HooksOptions } from '@warp-drive/core/configure';
 
-type State = Signal['State'];
-
-export function buildSignalConfig(options: {
-  wellknown: {
-    Array: symbol | string;
-  };
-}) {
+export function buildSignalConfig(options: HooksOptions) {
   return {
-    createSignal(obj: object, key: string | symbol): State {
-      return new Signal.State(null, { equals: () => false });
-    },
-    consumeSignal(signal: State) {
-      signal.get();
-    },
-    notifySignal(signal: Signal) {
-      signal.set();
-    },
+    createSignal: (obj: object, key: string | symbol) => new Signal.State(null, { equals: () => false }),
+    consumeSignal: (signal: Signal.State) => void signal.get(),
+    notifySignal: (signal: Signal.State) => void signal.set(),
     createMemo: <F>(object: object, key: string | symbol, fn: () => F): (() => F) => {
       const memo = Signal.Computed(fn);
       return () => memo.get();
     },
-
-    /**
-     * TC39's Signals proposal example-implementation does not have run loop
-     */
-    willSyncFlushWatchers: () => {
-      //@ts-expect-error
-      return !!_backburner.currentInstance && _backburner._autorun !== true;
-    },
-    /**
-     * TC39's Signals proposal example-implementation has no test instrumentation for promises
-     */
-    waitFor: async <K>(promise: Promise<K>): Promise<K> => {
-      return promise;
-    },
+    willSyncFlushWatchers: () => false,
   } satisfies SignalHooks;
 }
 
