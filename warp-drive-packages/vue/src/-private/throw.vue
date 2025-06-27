c5c136a22222b2e475ee9e363458fb0f4a7cff49
Chris Thoburn <cthoburn@auditboard.com>
2025-06-27 03:53:15 -0700

    feat: @warp-drive/vue

---
+++ warp-drive-packages/vue/src/-private/throw.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts" generic="E = Error | string | object">
+import { onMounted } from "vue";
+const props = defineProps<{
+  /**
+   * The error to throw.
+   */
+  error: E;
+}>();
+
+onMounted(() => {
+  throw props.error;
+});
+</script>
+
+<script lang="ts">
+/**
+ * The `<Throw />` component is used to throw an error in a template.
+ *
+ * That's all it does. So don't use it unless the application should
+ * throw an error if it reaches this point in the template.
+ *
+ * ```vue
+ * <Throw :error="myerror" />
+ * ```
+ */
+export default {
+  name: "Throw",
+};
+</script>
+
+<template></template>
