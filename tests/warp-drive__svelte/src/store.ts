a53623b505bdf7088fd0ccab4bf9e4eb57632876
Jaco Joubert <jaco@hey.com>
2025-06-28 12:15:48 -0400

    Add test

---
+++ tests/warp-drive__svelte/src/store.ts
@@ -0,0 +1,28 @@
+import { Fetch, RequestManager, Store as WarpStore } from '@warp-drive/core';
+import { instantiateRecord, registerDerivations, SchemaService, teardownRecord } from '@warp-drive/core/reactive';
+import type { CacheCapabilitiesManager, ResourceKey } from '@warp-drive/core/types';
+import { JSONAPICache } from '@warp-drive/json-api';
+import { CacheHandler } from '@warp-drive/core';
+
+export default class Store extends WarpStore {
+  requestManager = new RequestManager().use([Fetch]).useCache(CacheHandler);
+
+  createSchemaService() {
+    const schema = new SchemaService();
+    registerDerivations(schema);
+
+    return schema;
+  }
+
+  createCache(capabilities: CacheCapabilitiesManager) {
+    return new JSONAPICache(capabilities);
+  }
+
+  instantiateRecord(identifier: ResourceKey, createArgs?: Record<string, unknown>) {
+    return instantiateRecord(this, identifier, createArgs);
+  }
+
+  teardownRecord(record: unknown): void {
+    return teardownRecord(record);
+  }
+}
---
+++ tests/warp-drive__svelte/tests/store.test.ts
@@ -0,0 +1,81 @@
+import { assert, expect, test } from 'vitest';
+
+import { Type } from '@warp-drive/core/types/symbols';
+import Store from '../src/store';
+import { registerDerivations } from '@warp-drive/core/reactive';
+
+interface User {
+  id: string | null;
+  $type: 'user';
+  firstName: string;
+  lastName: string;
+  readonly fullName: string;
+}
+
+test('we can derive from simple fields', () => {
+  const store = new Store();
+  const { schema } = store;
+
+  function concat(record: any, options: Record<string, unknown> | null, _prop: string): string {
+    if (!options) throw new Error(`options is required`);
+    const opts = options as { fields: string[]; separator?: string };
+    return opts.fields.map((field) => record[field]).join(opts.separator ?? '');
+  }
+  concat[Type] = 'concat';
+
+  schema.registerDerivation(concat);
+  registerDerivations(schema);
+
+  schema.registerResource({
+    type: 'user',
+    identity: { kind: '@id', name: 'id' },
+    fields: [
+      {
+        name: 'firstName',
+        kind: 'field',
+      },
+      {
+        name: 'lastName',
+        kind: 'field',
+      },
+      {
+        name: 'fullName',
+        type: 'concat',
+        options: { fields: ['firstName', 'lastName'], separator: ' ' },
+        kind: 'derived',
+      },
+    ],
+  });
+
+  const record = store.push({
+    data: {
+      id: '1',
+      type: 'user',
+      attributes: {
+        firstName: 'Rey',
+        lastName: 'Pupatine',
+      },
+    },
+  }) as User;
+
+  assert.strictEqual(record.id, '1', 'id is accessible');
+  assert.strictEqual(record.$type, 'user', '$type is accessible');
+  assert.strictEqual(record.firstName, 'Rey', 'firstName is accessible');
+  assert.strictEqual(record.lastName, 'Pupatine', 'lastName is accessible');
+  assert.strictEqual(record.fullName, 'Rey Pupatine', 'fullName is accessible');
+
+  store.push({
+    data: {
+      id: '1',
+      type: 'user',
+      attributes: {
+        firstName: 'Rey',
+        lastName: 'Skybarker',
+      },
+    },
+  });
+
+  assert.strictEqual(record.firstName, 'Rey', 'firstName is accessible');
+  assert.strictEqual(record.lastName, 'Skybarker', 'lastName is accessible');
+  assert.strictEqual(record.fullName, 'Rey Skybarker', 'fullName is accessible');
+});
