520b9735cb9653bb33b17c02419d498499d0419e
Chris Thoburn <cthoburn@auditboard.com>
2025-07-01 00:18:09 -0700

    fix tests

--- tests/ember-data__serializer/tests/test-helper.js
+++ tests/ember-data__serializer/tests/test-helper.js
@@ -6,9 +6,15 @@ import { setup } from 'qunit-dom';
 import { start } from 'ember-qunit';
 
 import configureAsserts from '@ember-data/unpublished-test-infra/test-support/asserts/index';
+import { Store } from '@warp-drive/core';
+import { Model, restoreDeprecatedModelRequestBehaviors } from '@warp-drive/legacy/model';
+import { restoreDeprecatedStoreBehaviors } from '@warp-drive/legacy/store';
 
 import Application from '../app';
 import config from '../config/environment';
+
+restoreDeprecatedStoreBehaviors(Store);
+restoreDeprecatedModelRequestBehaviors(Model);
 
 setup(QUnit.assert);
 configureAsserts(QUnit.hooks);
