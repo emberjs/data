44d2bf60e963b72ebb42e1c74925c4778be71571
Chris Thoburn <cthoburn@auditboard.com>
2025-07-01 18:10:52 -0700

    chore: fix adapter in tests

--- tests/main/app/services/store.js
+++ tests/main/app/services/store.js
@@ -1,8 +1,18 @@
 import Store from 'ember-data/store';
 
-import { adapterFor, normalize, pushPayload, serializeRecord, serializerFor } from '@ember-data/legacy-compat';
+import {
+  adapterFor,
+  LegacyNetworkHandler,
+  normalize,
+  pushPayload,
+  serializeRecord,
+  serializerFor,
+} from '@ember-data/legacy-compat';
+import RequestManager from '@ember-data/request';
+import Fetch from '@ember-data/request/fetch';
 
 export default class extends Store {
+  requestManager = new RequestManager().use([LegacyNetworkHandler, Fetch]);
   adapterFor = adapterFor;
   serializerFor = serializerFor;
   serializeRecord = serializeRecord;
