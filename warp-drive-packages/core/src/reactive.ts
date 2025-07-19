061a2d21cb336cf3793c85fcbf0badfd6cb190cd
Chris Thoburn <cthoburn@auditboard.com>
2025-07-19 14:13:49 -0700

    fix save mistake

--- warp-drive-packages/core/src/reactive.ts
+++ warp-drive-packages/core/src/reactive.ts
@@ -312,4 +312,4 @@ export {
   Checkout,
 } from './reactive/-private/symbols';
 export { type ReactiveDocument } from './reactive/-private/document';
-export { getExpensiveSubscription } from './store/-private/new-core-tmp/expensive-subscription.ts';
+export { getExpensiveRequestSubscription } from './store/-private/new-core-tmp/expensive-subscription.ts';
