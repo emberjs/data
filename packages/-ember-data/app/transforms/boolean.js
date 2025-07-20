e570dc57de810387a8a993c9d3fae7617aee4209
Chris Thoburn <cthoburn@auditboard.com>
2025-07-20 01:40:06 -0700

    fix: don't print auto-inject deprecation until used

--- packages/-ember-data/app/transforms/boolean.js
+++ packages/-ember-data/app/transforms/boolean.js
@@ -1,17 +1,22 @@
 import { deprecate } from '@ember/debug';
 
-export { BooleanTransform as default } from '@ember-data/serializer/transform';
+import { BooleanTransform } from '@ember-data/serializer/transform';
 
-deprecate(
-  "You are relying on ember-data auto-magically installing the BooleanTransform. Use `export { BooleanTransform as default } from '@ember-data/serializer/transform';` in app/transforms/boolean.js instead",
-  false,
-  {
-    id: 'ember-data:deprecate-legacy-imports',
-    for: 'ember-data',
-    until: '6.0',
-    since: {
-      enabled: '5.2',
-      available: '4.13',
-    },
+export default class DeprecatedBooleanTransform extends BooleanTransform {
+  constructor(...args) {
+    super(...args);
+    deprecate(
+      "You are relying on ember-data auto-magically installing the BooleanTransform. Use `export { BooleanTransform as default } from '@ember-data/serializer/transform';` in app/transforms/boolean.js instead",
+      false,
+      {
+        id: 'ember-data:deprecate-legacy-imports',
+        for: 'ember-data',
+        until: '6.0',
+        since: {
+          enabled: '5.2',
+          available: '4.13',
+        },
+      }
+    );
   }
-);
+}
--- packages/-ember-data/app/transforms/date.js
+++ packages/-ember-data/app/transforms/date.js
@@ -1,17 +1,22 @@
 import { deprecate } from '@ember/debug';
 
-export { DateTransform as default } from '@ember-data/serializer/transform';
+import { DateTransform } from '@ember-data/serializer/transform';
 
-deprecate(
-  "You are relying on ember-data auto-magically installing the DateTransform. Use `export { DateTransform as default } from '@ember-data/serializer/transform';` in app/transforms/date.js instead",
-  false,
-  {
-    id: 'ember-data:deprecate-legacy-imports',
-    for: 'ember-data',
-    until: '6.0',
-    since: {
-      enabled: '5.2',
-      available: '4.13',
-    },
+export default class DeprecatedDateTransform extends DateTransform {
+  constructor(...args) {
+    super(...args);
+    deprecate(
+      "You are relying on ember-data auto-magically installing the DateTransform. Use `export { DateTransform as default } from '@ember-data/serializer/transform';` in app/transforms/date.js instead",
+      false,
+      {
+        id: 'ember-data:deprecate-legacy-imports',
+        for: 'ember-data',
+        until: '6.0',
+        since: {
+          enabled: '5.2',
+          available: '4.13',
+        },
+      }
+    );
   }
-);
+}
