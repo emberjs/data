71e49bf17aca3a77556d56b472a9b22da1492796
Chris Thoburn <cthoburn@auditboard.com>
2025-08-05 23:29:04 -0700

    cleanup

--- warp-drive-packages/tc39-proposal-signals/src/install.ts
+++ warp-drive-packages/tc39-proposal-signals/src/install.ts
@@ -5,17 +5,17 @@ import { Signal } from 'signal-polyfill';
 
 import { setupSignals, type SignalHooks, type HooksOptions } from '@warp-drive/core/configure';
 
-export function buildSignalConfig(options: HooksOptions) {
+export function buildSignalConfig(options: HooksOptions): SignalHooks<Signal.State<unknown>> {
   return {
     createSignal: (obj: object, key: string | symbol) => new Signal.State(null, { equals: () => false }),
-    consumeSignal: (signal: Signal.State) => void signal.get(),
-    notifySignal: (signal: Signal.State) => void signal.set(),
+    consumeSignal: (signal: Signal.State<unknown>) => void signal.get(),
+    notifySignal: (signal: Signal.State<unknown>) => void signal.set(1),
     createMemo: <F>(object: object, key: string | symbol, fn: () => F): (() => F) => {
-      const memo = Signal.Computed(fn);
+      const memo = new Signal.Computed<F>(fn);
       return () => memo.get();
     },
     willSyncFlushWatchers: () => false,
-  } satisfies SignalHooks;
+  };
 }
 
 setupSignals(buildSignalConfig);
