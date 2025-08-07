d4fad35314021d84864f6a1f2ab2fa7302812fe6
Chris Thoburn <cthoburn@auditboard.com>
2025-08-06 17:40:35 -0700

    feat: react signals

---
+++ warp-drive-packages/react/src/-private/reactive-context.tsx
@@ -0,0 +1,58 @@
+import { Signal } from "signal-polyfill";
+import { createContext, JSX, useSyncExternalStore, type Context } from "react";
+
+export function useWatcher(): Signal.subtle.Watcher {
+  let pending = false;
+  let destroyed = false;
+  let notifyReact: (() => void) | null = null;
+
+  const clearWatcher = () => {
+    watcher.unwatch(...Signal.subtle.introspectSources(watcher));
+  };
+
+  const watcher = new Signal.subtle.Watcher(() => {
+    if (!pending && !destroyed) {
+      pending = true;
+      queueMicrotask(() => {
+        pending = false;
+        if (destroyed) return;
+
+        // any time signals have changed, we notify React that our store has updated
+        if (notifyReact) notifyReact();
+
+        // we eliminate the list of watched signals since it will repopulate during
+        // the next render cycle.
+        clearWatcher();
+
+        // tell the Watcher to start watching for changes again
+        // by signaling that notifications have been flushed.
+        watcher.watch();
+      });
+    }
+  });
+
+  return useSyncExternalStore(
+    (notifyChanged: () => void) => {
+      destroyed = false;
+      notifyReact = notifyChanged;
+      return () => {
+        clearWatcher();
+        destroyed = true;
+        notifyReact = null;
+      };
+    },
+    () => watcher
+  );
+}
+
+export const WatcherContext: Context<Signal.subtle.Watcher | null> = createContext<Signal.subtle.Watcher | null>(null);
+
+export function ReactiveContext({ children }: { children: JSX.Element }): JSX.Element {
+  const watcher = useWatcher();
+  /**
+   * Unlike other frameworks, React does not have a built-in way to provide
+   * a context value other than by rendering an extra component.
+   *
+   */
+  return <WatcherContext value={watcher}>{children}</WatcherContext>;
+}
--- warp-drive-packages/react/src/install.ts
+++ warp-drive-packages/react/src/install.ts
@@ -1,3 +1,50 @@
 /**
- * This file intentionally empty for scaffolding.
+ * Unlike reactive frameworks, React does not have the ability to support
+ * fine-grained reactivity. However, we can approximate it to "good enough"
+ * granularity by keeping track of signals used within a specific context.
+ *
+ * React also does not have a built-in way to memoize functions the way that
+ * reactive frameworks do, but by building overtop of other Signal libraries
+ * we can provide this.
+ *
+ * Due to the above limitations, @warp-drive/react/install is built
+ * overtop @warp-drive/tc39-proposal-signals/install.
+ *
+ * The TC39 Watcher especially is valuable here, as it allows us to subscribe to changes
+ * to the dependency graph of a memo and not just a signal.
  */
+
+import { useContext } from 'react';
+import { Signal } from 'signal-polyfill';
+
+import { type HooksOptions, setupSignals, type SignalHooks } from '@warp-drive/core/configure';
+import { buildSignalConfig as _buildSignalConfig } from '@warp-drive/tc39-proposal-signals/install';
+
+import { WatcherContext } from './-private/reactive-context';
+
+export function buildSignalConfig(options: HooksOptions): SignalHooks<Signal.State<unknown>> {
+  const config = _buildSignalConfig(options);
+  const newConfig = Object.assign({}, config);
+
+  newConfig.consumeSignal = (signal: Signal.State<unknown>) => {
+    // ensure signals are watched by our closest watcher
+    const watcher = useContext(WatcherContext);
+    watcher?.watch(signal);
+    config.consumeSignal(signal);
+  };
+
+  newConfig.createMemo = <F>(object: object, key: string | symbol, fn: () => F): (() => F) => {
+    const memo = new Signal.Computed<F>(fn);
+    return () => {
+      // ensure computeds are watched by our closest watcher as well
+      const watcher = useContext(WatcherContext);
+      watcher?.watch(memo);
+
+      return memo.get();
+    };
+  };
+
+  return newConfig;
+}
+
+setupSignals(buildSignalConfig);
