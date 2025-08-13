cf27fdd6322bf3c72ddd739773dda117bac1ec1b
Chris Thoburn <cthoburn@auditboard.com>
2025-08-12 23:02:10 -0700

    updates

--- packages/diagnostic/src/spec.ts
+++ packages/diagnostic/src/spec.ts
@@ -1,6 +1,11 @@
+import { setup as ignoreSetup } from 'qunit-dom';
+
 import { module, skip, test, todo } from './-define';
 import type { Diagnostic as TestAssert, Hooks, TestContext } from './-types';
 import type { TestHelpers } from './helpers/install';
+
+// just to trigger the TS consumption
+ignoreSetup({} as Assert);
 
 declare module './-types' {
   interface Diagnostic {
