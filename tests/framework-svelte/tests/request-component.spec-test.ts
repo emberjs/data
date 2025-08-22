857fbfb5b6e4a5ce07c510c39ff0d35267c003f4
Chris Thoburn <runspired@users.noreply.github.com>
2025-08-21 20:34:21 -0700

    Add svelte test app (#10275)
    * chore: add svelte test app
    
    * fix build
    
    * fix lint
    
    * fix lint

---
+++ tests/framework-svelte/tests/request-component.spec-test.ts
@@ -0,0 +1,7 @@
+import { module, test } from '@warp-drive/diagnostic';
+
+module('request-component', function () {
+  test('test app is configured', function (assert) {
+    assert.true(true, 'App is configured correctly');
+  });
+});
