f36aa0be9383be1ef3e2996fb24598a2e552f2c6
Chris Thoburn <cthoburn@auditboard.com>
2025-08-21 20:10:32 -0700

    fix lint

--- tests/framework-svelte/tests/request-component.spec-test.ts
+++ tests/framework-svelte/tests/request-component.spec-test.ts
@@ -1,7 +1,7 @@
-import { module, test } from "@warp-drive/diagnostic";
+import { module, test } from '@warp-drive/diagnostic';
 
-module("request-component", () => {
-  test("test app is configured", async (assert) => {
-    assert.true(true, "App is configured correctly");
+module('request-component', () => {
+  test('test app is configured', async (assert) => {
+    assert.true(true, 'App is configured correctly');
   });
 });
