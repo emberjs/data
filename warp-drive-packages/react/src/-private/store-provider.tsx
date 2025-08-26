bad2f582f462719fbe063aa9555db17204cb35c7
Chris Thoburn <cthoburn@auditboard.com>
2025-08-26 04:15:55 -0700

    chore: improve docs categories and store config

--- warp-drive-packages/react/src/-private/store-provider.tsx
+++ warp-drive-packages/react/src/-private/store-provider.tsx
@@ -2,8 +2,14 @@ import { Store } from "@warp-drive/core";
 import { assert } from "@warp-drive/core/build-config/macros";
 import { createContext, JSX, ReactNode, use, useMemo } from "react";
 
+/**
+ * @category Contexts
+ */
 const StoreContext = createContext<Store | null>(null);
 
+/**
+ * @category Hooks
+ */
 export function useStore(): Store {
   const store = use(StoreContext);
   assert(
@@ -13,8 +19,17 @@ export function useStore(): Store {
   return store;
 }
 
-export function StoreProvider($props: { Store: Store; children: ReactNode }): JSX.Element {
-  const store = useMemo(() => new Store(), [$props.Store]);
+type WithExistingStore = { store: Store; children: ReactNode };
+type WithNewStore = { Store: typeof Store; children: ReactNode };
+
+/**
+ * @category Components
+ */
+export function StoreProvider($props: WithExistingStore | WithNewStore): JSX.Element {
+  const store = useMemo(
+    () => ("store" in $props ? $props.store : new $props.Store()),
+    ["store" in $props ? $props.store : $props.Store]
+  );
 
   return <StoreContext value={store}>{$props.children}</StoreContext>;
 }
