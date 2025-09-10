66fa4e7e4f279579c0dd100cbff30a1d7b990c46
Chris Thoburn <cthoburn@auditboard.com>
2025-09-10 00:46:12 -0700

    drop moved files

--- packages/diagnostic/src/react/test-helpers.ts
+++
@@ -1,8 +0,0 @@
-export async function maybeRerender(): Promise<void> {
-  // signal update
-  await Promise.resolve();
-  // render
-  await Promise.resolve();
-  // running of effects
-  await Promise.resolve();
-}
