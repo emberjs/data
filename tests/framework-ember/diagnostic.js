d4ec86de1b535c3bf21ea83722b78f96781437b5
Chris Thoburn <cthoburn@auditboard.com>
2025-09-10 00:51:41 -0700

    simp

--- tests/framework-ember/diagnostic.js
+++ tests/framework-ember/diagnostic.js
@@ -4,20 +4,12 @@ import holodeck from '@warp-drive/holodeck';
 await launch({
   async setup(options) {
     const port = options.port + 1;
-    const launched = await holodeck.launchProgram({
+    await holodeck.launchProgram({
       port,
     });
-    return {
-      proxy: {
-        '/api': launched.location,
-        [launched.recordingPath]: launched.location,
-      },
-    };
   },
   async cleanup() {
     await holodeck.endProgram();
   },
   entry: './dist-test/index.html',
-  useCors: false,
-  debug: true,
 });
